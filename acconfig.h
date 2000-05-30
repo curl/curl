784c29c6c5fb53edb0c33b65d6a20902eaa7ef53
Daniel Stenberg <daniel@haxx.se>
2000-05-30 22:26:57 +0000

    Added GETHOSTBYADDR_R_NARGS and GETHOSTBYNAME_R_NARGS

--- acconfig.h
+++ acconfig.h
@@ -10,3 +10,8 @@
 /* Define cpu-machine-OS */
 #undef OS
 
+/* Number of arguments your gethostbyaddr_r() function accepts */
+#undef GETHOSTBYADDR_R_NARGS
+
+/* Number of arguments your gethostbyname_r() function accepts */
+#undef GETHOSTBYNAME_R_NARGS
