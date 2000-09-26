bac96e9f495eda8b147170a11d051b139884ad41
Daniel Stenberg <daniel@haxx.se>
2000-09-26 22:28:46 +0000

    Added strlcpy() since it turns out some krb4-implementations don't include their own, even if mine did!

--- lib/krb4.c
+++ lib/krb4.c
@@ -72,6 +72,27 @@ struct krb4_data {
     char realm[REALM_SZ];
 };
 
+#ifndef HAVE_STRLCPY
+
+size_t
+strlcpy (char *dst, const char *src, size_t dst_sz)
+{
+    size_t n;
+    char *p;
+
+    for (p = dst, n = 0;
+	 n + 1 < dst_sz && *src != '\0';
+	 ++p, ++src, ++n)
+	*p = *src;
+    *p = '\0';
+    if (*src == '\0')
+	return n;
+    else
+	return n + strlen (src);
+}
+
+#endif
+
 static int
 krb4_check_prot(void *app_data, int level)
 {
