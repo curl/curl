5a07305dc837f7c226fe18142653f012e07a3270
Daniel Stenberg <daniel@haxx.se>
2000-11-15 15:36:41 +0000

    not printf()ing %s normally for character that weren't isprint() made things go weird, had to remove this. I should use trio soon for all the *printf() stuff as this is too broken

--- lib/mprintf.c
+++ lib/mprintf.c
@@ -1035,16 +1035,9 @@ static int addbyter(int output, FILE *data)
  
   if(infop->length < infop->max) {
     /* only do this if we haven't reached max length yet */
-    if (isprint(output) || isspace(output))
-      {
-	infop->buffer[0] = (char)output; /* store */
-	infop->buffer++; /* increase pointer */
-	infop->length++; /* we are now one byte larger */
-      }
-    else
-      {
-	return StoreNonPrintable(output, infop);
-      }
+    infop->buffer[0] = (char)output; /* store */
+    infop->buffer++; /* increase pointer */
+    infop->length++; /* we are now one byte larger */
     return output; /* fputc() returns like this on success */
   }
   return -1;
