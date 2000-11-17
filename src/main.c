7f77a061dd9b40637e44cef6332c4855c2336e55
Daniel Stenberg <daniel@haxx.se>
2000-11-17 10:08:39 +0000

    allows \r \n \t \v in config file parameters within quotes

--- src/main.c
+++ src/main.c
@@ -88,6 +88,8 @@
 #include "../lib/memdebug.h"
 #endif
 
+#define DEBUG_CONFIG
+
 #ifndef __cplusplus        /* (rabe) */
 typedef char bool;
 #endif                     /* (rabe) */
@@ -1046,12 +1048,31 @@ static int parseconfig(char *filename,
         ptr=param;
         while(*line && (*line != '\"')) {
           if(*line == '\\') {
+            char out;
             line++;
-            if(!*line) {
+
+            /* default is to output the letter after the backslah */
+            switch(out = *line) {
+            case '\0':
+              continue; /* this'll break out of the loop */
+            case 't':
+              out='\t';
+              break;
+            case 'n':
+              out='\n';
+              break;
+            case 'r':
+              out='\r';
+              break;
+            case 'v':
+              out='\v';
               break;
             }
+            *ptr++=out;
+            line++;
           }
-          *ptr++=*line++;
+          else
+            *ptr++=*line++;
         }
         *ptr=0; /* always zero terminate */
 
