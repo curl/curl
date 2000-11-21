56ac132401bf514374388fc1e2b193fc58429e2f
Daniel Stenberg <daniel@haxx.se>
2000-11-21 13:18:30 +0000

    removed the storenonprintable function as it isn't used anymore

--- lib/mprintf.c
+++ lib/mprintf.c
@@ -1001,33 +1001,6 @@ static int dprintf_formatf(
   return done;
 }
 
-static int StoreNonPrintable(int output, struct nsprintf *infop)
-{
-  /* If the character isn't printable then we convert it */
-  char work[64], *w;
-  int num = output;
-
-  w = &work[sizeof(work)];
-  *(--w) = (char)0;
-  for(; num > 0; num /= 10) {
-    *(--w) = lower_digits[num % 10];
-  }
-  if (infop->length + strlen(w) + 1 < infop->max)
-    {
-      infop->buffer[0] = '\\';
-      infop->buffer++;
-      infop->length++;
-      for (; *w; w++)
-	{
-	  infop->buffer[0] = *w;
-	  infop->buffer++;
-	  infop->length++;
-	}
-      return output;
-    }
-  return -1;
-}
-
 /* fputc() look-alike */
 static int addbyter(int output, FILE *data)
 {
