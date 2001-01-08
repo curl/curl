370d7f7527ea146a053f68d148c504f0884d3211
Daniel Stenberg <daniel@haxx.se>
2001-01-08 22:30:30 +0000

    Added source header. Made the prototype not being set if HAVE_GETPASS_R is set, as those systems are likely to have it already set in a system header and this prototype has proven to cause problems on SCO systems.

--- lib/getpass.h
+++ lib/getpass.h
@@ -1,8 +1,35 @@
 #ifndef __GETPASS_H
 #define __GETPASS_H
+/*****************************************************************************
+ *                                  _   _ ____  _     
+ *  Project                     ___| | | |  _ \| |    
+ *                             / __| | | | |_) | |    
+ *                            | (__| |_| |  _ <| |___ 
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) 2000, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * In order to be useful for every potential user, curl and libcurl are
+ * dual-licensed under the MPL and the MIT/X-derivate licenses.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the MPL or the MIT/X-derivate
+ * licenses. You may pick one of these licenses.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ * $Id$
+ *****************************************************************************/
+#ifndef HAVE_GETPASS_R
+/* If there's a system-provided function named like this, we trust it is
+   also found in one of the standard headers. */
+
 /*
  * Returning NULL will abort the continued operation!
  */
 char* getpass_r(char *prompt, char* buffer, size_t buflen );
+#endif
 
 #endif
