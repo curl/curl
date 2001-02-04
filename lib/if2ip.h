9ab5d30e3bd82e8e02adf5c67c505fb1aa316373
Daniel Stenberg <daniel@haxx.se>
2001-02-04 19:00:27 +0000

    Ingo Ralf Blum made it compile with the newest cygwin

--- lib/if2ip.h
+++ lib/if2ip.h
@@ -24,7 +24,7 @@
  *****************************************************************************/
 #include "setup.h"
 
-#if ! defined(WIN32) && ! defined(__BEOS__)
+#if ! defined(WIN32) && ! defined(__BEOS__) && !defined(__CYGWIN32__)
 extern char *Curl_if2ip(char *interface, char *buf, int buf_size);
 #else
 #define Curl_if2ip(a,b,c) NULL
