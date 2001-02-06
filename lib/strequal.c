09f6fc22eda36fa0d6ec9205cfa27ba7b204996b
Daniel Stenberg <daniel@haxx.se>
2001-02-06 09:12:39 +0000

    silly me, corrected the strlcat() to compile

--- lib/strequal.c
+++ lib/strequal.c
@@ -80,7 +80,7 @@ int Curl_strnequal(const char *first, const char *second, size_t max)
  *
  * 
  */
-size_t strlcat(char *dst, const char *src, size_t size)
+size_t strlcat(char *dst, const char *src, size_t siz)
 {
   char *d = dst;
   const char *s = src;
