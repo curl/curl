7e36c4437e895db51efe92cbe0bb1fdd41981b23
Daniel Stenberg <daniel@haxx.se>
2001-02-08 13:52:38 +0000

    today's FTP response check fix

--- lib/ftp.c
+++ lib/ftp.c
@@ -282,7 +282,7 @@ int Curl_GetFTPResponse(int sockfd, char *buf,
          */
         if(CURLE_OK != Curl_read(conn, sockfd, ptr, 1, &keepon))
           keepon = FALSE;
-        else if(keepon < 0)
+        else if(keepon <= 0)
           error = SELECT_ERROR;
         else if ((*ptr == '\n') || (*ptr == '\r'))
           keepon = FALSE;
