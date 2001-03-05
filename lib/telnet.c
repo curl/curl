0cb4eba0023684cc588546f09251c96a26c1dcde
Daniel Stenberg <daniel@haxx.se>
2001-03-05 14:01:13 +0000

    free the struct on done

--- lib/telnet.c
+++ lib/telnet.c
@@ -1017,6 +1017,10 @@ CURLcode Curl_telnet_done(struct connectdata *conn)
 {
   struct TELNET *tn = (struct TELNET *)conn->proto.telnet;
   curl_slist_free_all(tn->telnet_vars);
+
+  free(conn->proto.telnet);
+  conn->proto.telnet = NULL;
+
   return CURLE_OK;
 }
 
