ea409d037423ce232d7a805cb9ee4da6fd414801
Daniel Stenberg <daniel@haxx.se>
2001-03-19 08:42:00 +0000

    7.7-beta5 commit

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -476,7 +476,7 @@ char *curl_getenv(char *variable);
 char *curl_version(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.7-beta3"
+#define LIBCURL_VERSION "7.7-beta5"
 #define LIBCURL_VERSION_NUM 0x070700
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
