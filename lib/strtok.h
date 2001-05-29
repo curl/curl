034f6378da4e548efbbb13e65e4a734198dc09e6
Daniel Stenberg <daniel@haxx.se>
2001-05-29 19:17:03 +0000

    Added strtok.[ch]

---
+++ lib/strtok.h
@@ -0,0 +1,36 @@
+/*****************************************************************************
+ *                                  _   _ ____  _     
+ *  Project                     ___| | | |  _ \| |    
+ *                             / __| | | | |_) | |    
+ *                            | (__| |_| |  _ <| |___ 
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) 2000, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * In order to be useful for every potential user, curl and libcurl are
+ * dual-licensed under the MPL and the MIT/X-derivate licenses.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the MPL or the MIT/X-derivate
+ * licenses. You may pick one of these licenses.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ * $Id$
+ *****************************************************************************/
+
+#ifndef _CURL_STRTOK_R_H
+#define _CURL_STRTOK_R_H
+
+#include <stddef.h>
+#include "setup.h"
+
+#ifndef HAVE_STRTOK_R
+char *curl_strtok_r(char *s, const char *delim, char **last);
+#define strtok_r curl_strtok_r
+#endif
+
+#endif
+
