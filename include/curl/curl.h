95c94bf658e00c15060d87b052a71b849d6666f3
Daniel Stenberg <daniel@haxx.se>
2001-05-31 06:10:25 +0000

    the CURL_GLOBAL flags are now used to set what parts to init globally

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -573,9 +573,10 @@ typedef enum {
   CURLCLOSEPOLICY_LAST /* last, never use this */
 } curl_closepolicy;
 
-#define CURL_GLOBAL_NOT_SSL (1<<0)
-#define CURL_GLOBAL_NOTHING CURL_GLOBAL_NOT_SSL
-#define CURL_GLOBAL_DEFAULT 0
+#define CURL_GLOBAL_SSL (1<<0)
+#define CURL_GLOBAL_ALL (CURL_GLOBAL_SSL)
+#define CURL_GLOBAL_NOTHING 0
+#define CURL_GLOBAL_DEFAULT CURL_GLOBAL_ALL
 
 #ifdef  __cplusplus
 }
