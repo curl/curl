97f3099ff60b5587fd2d81f0c91d41e283364565
Daniel Stenberg <daniel@haxx.se>
2001-08-15 18:33:52 +0000

    curl_getenv() now takes a const char * as argument

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -493,7 +493,7 @@ void curl_formfree(struct HttpPost *form);
 
 /* Unix and Win32 getenv function call, this returns a malloc()'ed string that
    MUST be free()ed after usage is complete. */
-char *curl_getenv(char *variable);
+char *curl_getenv(const char *variable);
 
 /* Returns a static ascii string of the libcurl version. */
 char *curl_version(void);
