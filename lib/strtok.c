56f6815d3d2e9d4b2823f64825fc0bbf4aa96379
Daniel Stenberg <daniel@haxx.se>
2001-08-24 10:25:02 +0000

    rewrite to work around BSD announcement license issues, this is also somewhat easier to understand if I may say so. It is slightly slower.

--- lib/strtok.c
+++ lib/strtok.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___ 
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 2000, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 2001, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * In order to be useful for every potential user, curl and libcurl are
  * dual-licensed under the MPL and the MIT/X-derivate licenses.
@@ -21,108 +21,46 @@
  * $Id$
  *****************************************************************************/
 
-
 #include "setup.h"
 
 #ifndef HAVE_STRTOK_R
-
-/*
- * Copyright (c) 1998 Softweyr LLC.  All rights reserved.
- *
- * strtok_r, from Berkeley strtok
- * Oct 13, 1998 by Wes Peters <wes@softweyr.com>
- *
- * Copyright (c) 1988, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notices, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notices, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *
- *	This product includes software developed by Softweyr LLC, the
- *      University of California, Berkeley, and its contributors.
- *
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY SOFTWEYR LLC, THE REGENTS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL SOFTWEYR LLC, THE
- * REGENTS, OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <stddef.h>
 
 char *
-Curl_strtok_r(char *s, const char *delim, char **last)
+Curl_strtok_r(char *ptr, const char *sep, char **end)
 {
-  char *spanp;
-  int c, sc;
-  char *tok;
+  if (!ptr)
+    /* we got NULL input so then we get our last position instead */
+    ptr = *end;
 
-  if (s == NULL && (s = *last) == NULL) {
-    return NULL;
-  }
+  /* pass all letters that are including in the separator string */
+  while (*ptr && strchr(sep, *ptr))
+    ++ptr;
 
-  /*
-   * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
-   */
- cont:
-  c = *s++;
-  for (spanp = (char *)delim; (sc = *spanp++) != 0; ) {
-    if (c == sc) {
-      goto cont;
+  if (*ptr) {
+    /* so this is where the next piece of string starts */
+    char *start = ptr;
+
+    /* set the end pointer to the first byte after the start */
+    *end = ptr + 1;
+
+    /* scan through the string to find where it ends, it ends on a
+       null byte or a character that exists in the separator string */
+    while (**end && !strchr(sep, **end))
+      ++*end;
+
+    if (**end) {
+      /* the end is not a null byte */
+      **end = '\0';, /* zero terminate it! */
+      ++*end;        /* advance the last pointer to beyond the null byte */
     }
+
+    return start; /* return the position where the string starts */
   }
 
-  if (c == 0) {		/* no non-delimiter characters */
-    *last = NULL;
-    return NULL;
-  }
-  tok = s - 1;
-  
-  /*
-   * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
-   * Note that delim must have one NUL; we stop if we see that, too.
-   */
-  for (;;) {
-    c = *s++;
-    spanp = (char *)delim;
-    do {
-      if ((sc = *spanp++) == c) {
-        if (c == 0) {
-          s = NULL;
-        }
-        else {
-          char *w = s - 1;
-          *w = '\0';
-        }
-        *last = s;
-        return tok;
-      }
-    }
-    while (sc != 0);
-  }
-  /* NOTREACHED */
+  /* we ended up on a null byte, there are no more strings to find! */
+  return NULL;
 }
 
-#endif
+#endif /* this was only compiled if strtok_r wasn't present */
 
