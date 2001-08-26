9e5dfc15ac7f7e31bf61bb172fda28da979b8137
Daniel Stenberg <daniel@haxx.se>
2001-08-26 14:28:05 +0000

    improved the test

--- lib/cookie.c
+++ lib/cookie.c
@@ -644,10 +644,8 @@ int main(int argc, char **argv)
   struct CookieInfo *c=NULL;
   if(argc>1) {
     c = Curl_cookie_init(argv[1], c);
-    c = Curl_cookie_init(argv[1], c);
-    c = Curl_cookie_init(argv[1], c);
-
     Curl_cookie_add(c, TRUE, "PERSONALIZE=none;expires=Monday, 13-Jun-1988 03:04:55 GMT; domain=.fidelity.com; path=/ftgw; secure");
+    c = Curl_cookie_init(argv[1], c);
 
     Curl_cookie_output(c);
     Curl_cookie_cleanup(c);
