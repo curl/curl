e2e3c95d3b2e34e9119f966937e90c741e2abba0
Sterling Hughes <sterling@bumblebury.com>
2001-09-07 03:30:30 +0000

    fix for emacs

--- lib/ftp.c
+++ lib/ftp.c
@@ -1830,10 +1830,9 @@ CURLcode Curl_ftp_disconnect(struct connectdata *conn)
 }
 
 /*
- * Local variables:
- * tab-width: 2
- * c-basic-offset: 2
- * End:
+ * local variables:
+ * eval: (load-file "../curl-mode.el")
+ * end:
  * vim600: et sw=2 ts=2 sts=2 tw=78 fdm=marker
  * vim<600: et sw=2 ts=2 sts=2 tw=78
  */
