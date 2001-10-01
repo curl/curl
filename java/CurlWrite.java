2a07626aa8dcf601eb575cac4c8817fa0e8e245e
Daniel Stenberg <daniel@haxx.se>
2001-10-01 07:46:45 +0000

    moved to the java module

--- java/CurlWrite.java
+++
@@ -1,9 +0,0 @@
-public interface CurlWrite
-{
-  /**
-   * handleString gets called by libcurl on each chunk of data
-   * we receive from the remote server
-   */
-  public int handleString(byte s[]);
-}
-
