f0fa858885a490c4184f82e7273656be83a5c586
Daniel Stenberg <daniel@haxx.se>
2001-10-01 22:50:20 +0000

    added comment to the tvdiff

--- lib/timeval.c
+++ lib/timeval.c
@@ -65,6 +65,10 @@ struct timeval Curl_tvnow (void)
  return now;
 }
 
+/*
+ * Make sure that the first argument is the more recent time, as otherwise
+ * we'll get a weird negative time-diff back...
+ */
 double Curl_tvdiff (struct timeval t1, struct timeval t2)
 {
  return (double)(t1.tv_sec - t2.tv_sec) + ((t1.tv_usec-t2.tv_usec)/1000000.0);
