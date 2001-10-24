c6822f5a7f4b188dd09f9c0a582d8e76c63be7c3
Daniel Stenberg <daniel@haxx.se>
2001-10-24 11:36:55 +0000

    T. Bharath found this memory leak. It occurs when we replace an internally already existing cookie with a new one.

--- lib/cookie.c
+++ lib/cookie.c
@@ -374,6 +374,9 @@ Curl_cookie_add(struct CookieInfo *c,
           free(clist->maxage);
 
         *clist = *co;  /* then store all the new data */
+
+        free(co);   /* free the newly alloced memory */
+        co = clist; /* point to the previous struct instead */
       }
 
     }
