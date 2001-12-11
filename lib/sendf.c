c67f2da283c8cb8dc3cd7c783c73ef0ff8726215
Daniel Stenberg <daniel@haxx.se>
2001-12-11 15:08:27 +0000

    solaris 2.5.1 needs the sys/types.h file before the sys/socket.h

--- lib/sendf.c
+++ lib/sendf.c
@@ -26,6 +26,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
 
 #ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>	/* required for send() & recv() prototypes */
