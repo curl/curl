fefc7ea6008c48cbc3c346da4da55169da8c3adc
Daniel Stenberg <daniel@haxx.se>
2002-01-14 23:14:24 +0000

    a memory leak when name lookup failed is now removed

--- lib/hostip.c
+++ lib/hostip.c
@@ -131,13 +131,14 @@ Curl_addrinfo *Curl_resolv(struct SessionHandle *data,
   }
 
   /* Create a new cache entry */
-  p = (struct curl_dns_cache_entry *) malloc(sizeof(struct curl_dns_cache_entry));
-  if (!p) {
+  p = (struct curl_dns_cache_entry *)
+    malloc(sizeof(struct curl_dns_cache_entry));
+  if (!p)
     return NULL;
-  }
 
   p->addr = Curl_getaddrinfo(data, hostname, port, bufp);
   if (!p->addr) {
+    free(p);
     return NULL;
   }
   p->timestamp = now;
