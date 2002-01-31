a8dd13db4c1f1b4d44c403f33bde3d292b167628
Daniel Stenberg <daniel@haxx.se>
2002-01-31 14:24:55 +0000

    struct HttpHeader died ages ago, corrected comments

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___ 
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 2001, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 2002, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * In order to be useful for every potential user, curl and libcurl are
  * dual-licensed under the MPL and the MIT/X-derivate licenses.
@@ -272,7 +272,7 @@ typedef enum {
   /* Set cookie in request: */
   CINIT(COOKIE, OBJECTPOINT, 22),
 
-  /* This points to a linked list of headers, struct HttpHeader kind */
+  /* This points to a linked list of headers, struct curl_slist kind */
   CINIT(HTTPHEADER, OBJECTPOINT, 23),
 
   /* This points to a linked list of post entries, struct HttpPost */
@@ -613,8 +613,8 @@ CURLcode curl_global_init(long flags);
 void curl_global_cleanup(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.9.3"
-#define LIBCURL_VERSION_NUM 0x070903
+#define LIBCURL_VERSION "7.9.4-pre1"
+#define LIBCURL_VERSION_NUM 0x070904
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
 struct curl_slist {
