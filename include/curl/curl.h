60b2e74fa3bb0feb684f29632f50990f18c7abbb
Daniel Stenberg <daniel@haxx.se>
2002-03-05 10:15:38 +0000

    corrected the progress callback prototype!!!

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -75,10 +75,10 @@ struct HttpPost {
 };
 
 typedef int (*curl_progress_callback)(void *clientp,
-                                      size_t dltotal,
-                                      size_t dlnow,
-                                      size_t ultotal,
-                                      size_t ulnow);
+                                      double dltotal,
+                                      double dlnow,
+                                      double ultotal,
+                                      double ulnow);
 
 typedef size_t (*curl_write_callback)(char *buffer,
                                       size_t size,
@@ -616,7 +616,7 @@ CURLcode curl_global_init(long flags);
 void curl_global_cleanup(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.9.5-pre4"
+#define LIBCURL_VERSION "7.9.5-pre6"
 #define LIBCURL_VERSION_NUM 0x070905
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
