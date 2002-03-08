fe3c8740010a9ca421606db87adcabe9a5432d55
Daniel Stenberg <daniel@haxx.se>
2002-03-08 15:31:44 +0000

    detect fclose(NULL)

--- lib/memdebug.c
+++ lib/memdebug.c
@@ -200,7 +200,15 @@ FILE *curl_fopen(const char *file, const char *mode,
 
 int curl_fclose(FILE *file, int line, const char *source)
 {
-  int res=(fclose)(file);
+  int res;
+
+  if(NULL == file) {
+    fprintf(stderr, "ILLEGAL flose() on NULL at %s:%d\n",
+            source, line);
+    exit(2);
+  }
+
+  res=(fclose)(file);
   if(logfile)
     fprintf(logfile, "FILE %s:%d fclose(%p)\n",
             source, line, file);
