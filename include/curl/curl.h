ce021b79a7d073e343e5904e0fbb6a64ba3f7bb6
Daniel Stenberg <daniel@haxx.se>
2002-03-13 12:09:52 +0000

    CURLFORM_ARRAYSTART and ARRAYEND are now history

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -576,13 +576,13 @@ typedef enum {
   CFINIT(CONTENTSLENGTH),
   CFINIT(FILECONTENT),
   CFINIT(ARRAY),
-  CFINIT(ARRAY_START), /* below are the options allowed within a array */
+  CFINIT(OBSOLETE),
   CFINIT(FILE),
   CFINIT(CONTENTTYPE),
   CFINIT(CONTENTHEADER),
   CFINIT(FILENAME),
   CFINIT(END),
-  CFINIT(ARRAY_END),   /* up are the options allowed within a array */
+  CFINIT(OBSOLETE2),
 
   CURLFORM_LASTENTRY /* the last unusued */
 } CURLformoption;
@@ -624,7 +624,7 @@ CURLcode curl_global_init(long flags);
 void curl_global_cleanup(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.9.5"
+#define LIBCURL_VERSION "7.9.5-cvs"
 #define LIBCURL_VERSION_NUM 0x070905
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
