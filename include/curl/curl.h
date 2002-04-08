2a72641a0b54dd5db079fd6f797e47a5907d6b2a
Daniel Stenberg <daniel@haxx.se>
2002-04-08 22:53:22 +0000

    7.9.6-pre4 commit

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -637,7 +637,7 @@ CURLcode curl_global_init(long flags);
 void curl_global_cleanup(void);
 
 /* This is the version number */
-#define LIBCURL_VERSION "7.9.6-pre2"
+#define LIBCURL_VERSION "7.9.6-pre3"
 #define LIBCURL_VERSION_NUM 0x070906
 
 /* linked-list structure for the CURLOPT_QUOTE option (and other) */
