086daf913cd5022d8cf5e165ae08bf565242dd84
Daniel Stenberg <daniel@haxx.se>
2002-08-13 12:12:08 +0000

    mr Hartroth correctly pointed out that poll() isn't really that portable so we need to hack around it when not there

--- src/main.c
+++ src/main.c
@@ -1878,8 +1878,26 @@ static int parseconfig(const char *filename,
 
 static void go_sleep(long ms)
 {
+#ifdef HAVE_POLL
   /* portable subsecond "sleep" */
   poll((void *)0, 0, ms);
+#else
+  /* systems without poll() need other solutions */
+
+#ifdef WIN32
+  /* Windows offers a millisecond sleep */
+  Sleep(ms);
+#else
+  /* Other systems must use select() for this */
+  struct timeval timeout;
+
+  timeout.tv_sec = 0;
+  timeout.tv_usec = ms * 1000;
+
+  select(0, NULL,  NULL, NULL, &timeout);
+#endif
+
+#endif
 }
 
 struct OutStruct {
