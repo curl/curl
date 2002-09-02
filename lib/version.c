5dd40bd0bb7aa58672be15b9d2bc75e6d4ef2ccb
Daniel Stenberg <daniel@haxx.se>
2002-09-02 21:59:31 +0000

    adjusted to more HTTP-strict version strings and to offer LIBZ info

--- lib/version.c
+++ lib/version.c
@@ -33,7 +33,7 @@ char *curl_version(void)
 {
   static char version[200];
   char *ptr;
-  strcpy(version, LIBCURL_NAME " " LIBCURL_VERSION );
+  strcpy(version, LIBCURL_NAME "/" LIBCURL_VERSION );
   ptr=strchr(version, '\0');
 
 #ifdef USE_SSLEAY
@@ -56,7 +56,7 @@ char *curl_version(void)
         sub[0]='\0';
     }
 
-    sprintf(ptr, " (OpenSSL %lx.%lx.%lx%s)",
+    sprintf(ptr, " OpenSSL/%lx.%lx.%lx%s",
             (ssleay_value>>28)&0xf,
             (ssleay_value>>20)&0xff,
             (ssleay_value>>12)&0xff,
@@ -65,7 +65,7 @@ char *curl_version(void)
 
 #else
 #if (SSLEAY_VERSION_NUMBER >= 0x900000)
-  sprintf(ptr, " (SSL %lx.%lx.%lx)",
+  sprintf(ptr, " OpenSSL/%lx.%lx.%lx",
           (SSLEAY_VERSION_NUMBER>>28)&0xff,
           (SSLEAY_VERSION_NUMBER>>20)&0xff,
           (SSLEAY_VERSION_NUMBER>>12)&0xf);
@@ -79,7 +79,7 @@ char *curl_version(void)
     else
       sub[0]='\0';
 
-    sprintf(ptr, " (SSL %x.%x.%x%s)",
+    sprintf(ptr, " SSL/%x.%x.%x%s",
             (SSLEAY_VERSION_NUMBER>>12)&0xff,
             (SSLEAY_VERSION_NUMBER>>8)&0xf,
             (SSLEAY_VERSION_NUMBER>>4)&0xf, sub);
@@ -89,23 +89,16 @@ char *curl_version(void)
   ptr=strchr(ptr, '\0');
 #endif
 
-#if defined(KRB4) || defined(ENABLE_IPV6)
-  strcat(ptr, " (");
-  ptr+=2;
 #ifdef KRB4
-  sprintf(ptr, "krb4 ");
+  sprintf(ptr, " krb4");
   ptr += strlen(ptr);
 #endif
 #ifdef ENABLE_IPV6
-  sprintf(ptr, "ipv6 ");
+  sprintf(ptr, " ipv6");
   ptr += strlen(ptr);
 #endif
-  sprintf(ptr, "enabled)");
-  ptr += strlen(ptr);
-#endif
-
-#ifdef USE_ZLIB
-  sprintf(ptr, " (zlib %s)", zlibVersion());
+#ifdef HAVE_LIBZ
+  sprintf(ptr, " zlib/%s", zlibVersion());
   ptr += strlen(ptr);
 #endif
 
