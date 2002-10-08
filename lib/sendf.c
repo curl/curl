91b84b89e4d057d7c1a022dc1b6ce44e3738dcf0
Daniel Stenberg <daniel@haxx.se>
2002-10-08 16:10:37 +0000

    failf() now sends the text to the debug function callback

--- lib/sendf.c
+++ lib/sendf.c
@@ -153,6 +153,9 @@ void Curl_failf(struct SessionHandle *data, const char *fmt, ...)
   if(data->set.errorbuffer && !data->state.errorbuf) {
     vsnprintf(data->set.errorbuffer, CURL_ERROR_SIZE, fmt, ap);
     data->state.errorbuf = TRUE; /* wrote error string */
+
+    Curl_debug(data, CURLINFO_TEXT, data->set.errorbuffer,
+               strlen(data->set.errorbuffer));
   }
   va_end(ap);
 }
