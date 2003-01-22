aa90436435fab813d181e92d2530f78183bdbf0f
Daniel Stenberg <daniel@haxx.se>
2003-01-22 06:59:52 +0000

    put the configure options in the setup file was well make -i show lib/config.h and some initial checks to prevent this running multiple times without the CVS having changed

--- testcurl.sh
+++ testcurl.sh
@@ -44,19 +44,27 @@ if [ -z "$desc" ]; then
   fixed="3"
 fi
 
+if [ -z "$desc" ]; then
+  echo "please enter your additional arguments to configure"
+  echo "examples: --with-ssl --enable-debug --enable-ipv6 --with-krb4"
+  read confopts
+  fixed="3"
+fi
+
 
 if [ "$fixed" -gt "0" ]; then
   echo "name='$name'" > setup
   echo "email='$email'" >> setup
   echo "desc='$desc'" >> setup
+  echo "confopts='$confopts'" >> setup
 fi
 
 echo "testcurl: STARTING HERE"
 echo "testcurl: NAME = $name"
 echo "testcurl: EMAIL = $email"
 echo "testcurl: DESC = $desc"
+echo "testcurl: CONFOPTS = $confopts"
 echo "testcurl: version = $version"
-echo "testcurl: confopts = $1"
 echo "testcurl: date = `date -u`"
 
 # Make $pwd to become the path without newline. We'll use that in order to cut
@@ -79,12 +87,35 @@ rm -rf $build
 # create a dir to build in
 mkdir $build
 
+if [ -d $build ]; then
+  echo "testcurl: build dir $build was created fine"
+else
+  echo "testcurl: failed to create dir $build"
+  die
+fi
+
 # get in the curl source tree root
 cd curl
 
 echo "testcurl: update from CVS"
 # update quietly to the latest CVS
 cvs -Q up -dP 2>&1
+
+# figure out the current collected CVS status
+newstat="../allcvs.log"
+oldstat="../oldcvs.log"
+find . -name Entries -exec cat {} \; > "$newstat"
+
+if [ -r "$oldstat" ]; then
+  # there is a previous cvs stat file to compare with
+  if { cmp "$oldstat" "$newstat"; } then
+    echo "testcurl: there has been a change in the CVS"
+  else
+    echo "testcurl: this is the same CVS status as before"
+    echo "testcurl: ALREADY TESTED THIS SETUP BEFORE"
+    #die
+  fi
+fi
 
 # remove possible left-overs from the past
 rm -f configure
@@ -113,8 +144,11 @@ else
   die
 fi
 
+echo "testcurl: display lib/config.h"
+grep "^ *#" lib/config.h
+
 echo "testcurl: now run make"
-make 2>&1 | sed -e "s:$pwd::g"
+make -i 2>&1 | sed -e "s:$pwd::g"
 
 if [ -f src/curl ]; then
   echo "testcurl: src/curl was created fine"
@@ -131,6 +165,9 @@ if { grep "^TESTFAIL:" build.log; } then
 else
   echo "testcurl: the tests were successful!"  
 fi
+
+# store the cvs status for the next time
+mv $newstat $oldstat
 
 # get out of dir
 cd ..
