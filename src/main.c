21c16f923c0618cf35fdc71921b3544f8ea04eca
Daniel Stenberg <daniel@haxx.se>
2003-02-03 21:36:52 +0000

    Julian Noble pointed out that capath is indeed working fine on Windows these days since the c_rehash tool is written (fixed) to do the proper action even on file systems that don't support symlinks.

--- src/main.c
+++ src/main.c
@@ -358,7 +358,7 @@ static void help(void)
   puts("    --engine <eng>  Specifies the crypto engine to use (HTTPS)\n"
        "    --cacert <file> CA certifciate to verify peer against (SSL)\n"
        "    --capath <directory> CA directory (made using c_rehash) to verify\n"
-       "                    peer against (SSL, NOT Windows)\n"
+       "                    peer against (SSL)\n"
        "    --ciphers <list> What SSL ciphers to use (SSL)\n"
        "    --compressed    Request a compressed response (using deflate).");
   puts("    --connect-timeout <seconds> Maximum time allowed for connection\n"
