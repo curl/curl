10026bb62ebb7ecdfcfcc5bf7538361212ec8df2
Daniel Stenberg <daniel@haxx.se>
2003-02-03 22:15:33 +0000

    scan through the PATH as well, to find stunnel

--- tests/stunnel.pm
+++ tests/stunnel.pm
@@ -1,6 +1,6 @@
 sub checkstunnel {
     my @paths=("/usr/sbin", "/usr/local/sbin", "/sbin", "/usr/bin",
-               "/usr/local/bin");
+               "/usr/local/bin", split(":", $ENV{'PATH'}));
     for(@paths) {
         if( -x "$_/stunnel") {
             return "$_/stunnel";
