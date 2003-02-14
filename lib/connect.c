c3dfe50aafb5f1687889da114d811f823791ebd7
Daniel Stenberg <daniel@haxx.se>
2003-02-14 09:11:51 +0000

    Fix Curl_is_connected() even more to deal with waitconnect() return codes even better (also based on input from Martin).

--- lib/connect.c
+++ lib/connect.c
@@ -144,7 +144,11 @@ int Curl_nonblock(int socket,    /* operate on this */
 }
 
 /*
- * Return 0 on fine connect, -1 on error and 1 on timeout.
+ * waitconnect() returns:
+ * 0    fine connect
+ * -1   select() error
+ * 1    select() timeout
+ * 2    select() returned with an error condition
  */
 static
 int waitconnect(int sockfd, /* socket */
@@ -409,7 +413,7 @@ CURLcode Curl_is_connected(struct connectdata *conn,
     if(err)
       return CURLE_COULDNT_CONNECT;
   }
-  else if(2 == rc)
+  else if(1 != rc)
     return CURLE_COULDNT_CONNECT;
 
   /*
