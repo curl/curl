f8d552dde584b2615143a258e624d85a911c7a65
Daniel Stenberg <daniel@haxx.se>
2003-03-12 08:54:11 +0000

    include "config.h" from the lib's private dir

--- tests/libtest/test.h
+++ tests/libtest/test.h
@@ -2,5 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifdef HAVE_CONFIG_H
+/* Now include the config.h file from libcurl's private libdir, so that we
+   get good in-depth knowledge about the system we're building this on */
+#include "config.h"
+#endif
+
 extern char *arg2; /* set by first.c to the argv[2] or NULL */
 
