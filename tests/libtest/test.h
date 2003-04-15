4b1203d4c9051442a0f5544421886df7ac67ff1d
Daniel Stenberg <daniel@haxx.se>
2003-04-15 13:32:26 +0000

    include config.h before all system headers, so that _FILE_OFFSET_BITS and similar is set properly by us first

--- tests/libtest/test.h
+++ tests/libtest/test.h
@@ -1,12 +1,12 @@
-#include <curl.h>
-#include <stdio.h>
-#include <string.h>
-
 #ifdef HAVE_CONFIG_H
 /* Now include the config.h file from libcurl's private libdir, so that we
    get good in-depth knowledge about the system we're building this on */
 #include "config.h"
 #endif
+
+#include <curl.h>
+#include <stdio.h>
+#include <string.h>
 
 extern char *arg2; /* set by first.c to the argv[2] or NULL */
 
