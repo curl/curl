ceef206c21a763095ce407982a4963cdbb8afa92
Daniel Stenberg <daniel@haxx.se>
2003-06-03 08:07:06 +0000

    include the time headers just like we used to do in the curl/curl.h header once upon the time

--- src/main.c
+++ src/main.c
@@ -50,9 +50,18 @@
 #include <winsock.h>
 #endif
 
-#ifdef HAVE_SYS_TIME_H
+#ifdef TIME_WITH_SYS_TIME
+/* We can include both fine */
 #include <sys/time.h>
+#include <time.h>
+#else
+#ifdef HAVE_SYS_TIME_H
+# include <sys/time.h>
+#else
+# include <time.h>
 #endif
+#endif
+
 
 #include "version.h"
 
