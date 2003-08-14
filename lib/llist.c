4281470fca67dd51ffba1799d303e2eedaa1efa8
Daniel Stenberg <daniel@haxx.se>
2003-08-14 15:06:08 +0000

    Curl_llist_destroy() checks the input for non-NULL

--- lib/llist.c
+++ lib/llist.c
@@ -159,10 +159,10 @@ Curl_llist_count(curl_llist *list)
 void 
 Curl_llist_destroy(curl_llist *list, void *user)
 {
-  while (list->size > 0) {
-    Curl_llist_remove(list, CURL_LLIST_TAIL(list), user);
-  }
+  if(list) {
+    while (list->size > 0)
+      Curl_llist_remove(list, CURL_LLIST_TAIL(list), user);
 
-  free(list);
-  list = NULL;
+    free(list);
+  }
 }
