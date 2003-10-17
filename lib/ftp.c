9e3f54431d8fd1873a05f5b99de5296b285a77b8
Daniel Stenberg <daniel@haxx.se>
2003-10-17 09:26:28 +0000

    made the code deal with empty name and password

--- lib/ftp.c
+++ lib/ftp.c
@@ -483,7 +483,7 @@ CURLcode Curl_ftp_connect(struct connectdata *conn)
 #endif
   
   /* send USER */
-  FTPSENDF(conn, "USER %s", ftp->user);
+  FTPSENDF(conn, "USER %s", ftp->user?ftp->user:"");
 
   /* wait for feedback */
   result = Curl_GetFTPResponse(&nread, conn, &ftpcode);
@@ -499,7 +499,7 @@ CURLcode Curl_ftp_connect(struct connectdata *conn)
   else if(ftpcode == 331) {
     /* 331 Password required for ...
        (the server requires to send the user's password too) */
-    FTPSENDF(conn, "PASS %s", ftp->passwd);
+    FTPSENDF(conn, "PASS %s", ftp->passwd?ftp->passwd:"");
     result = Curl_GetFTPResponse(&nread, conn, &ftpcode);
     if(result)
       return result;
