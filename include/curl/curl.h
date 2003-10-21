2e9d100d0355e076b16ee141b16ab5d5e21cd0bd
Daniel Stenberg <daniel@haxx.se>
2003-10-21 14:11:25 +0000

    pre5

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -29,7 +29,7 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.10.8-pre4"
+#define LIBCURL_VERSION "7.10.8-pre5"
 
 /* This is the numeric version of the libcurl version number, meant for easier
    parsing and comparions by programs. The LIBCURL_VERSION_NUM define will
--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 #define CURL_NAME "curl"
-#define CURL_VERSION "7.10.8-pre4"
+#define CURL_VERSION "7.10.8-pre5"
 #define CURL_ID CURL_NAME " " CURL_VERSION " (" OS ") "
