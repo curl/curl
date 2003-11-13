776f0bd95e92f72e16d9e836f92d83a313bbedd7
Daniel Stenberg <daniel@haxx.se>
2003-11-13 07:33:51 +0000

    don't log failed socket() calls

--- lib/memdebug.c
+++ lib/memdebug.c
@@ -204,7 +204,7 @@ void curl_dofree(void *ptr, int line, const char *source)
 int curl_socket(int domain, int type, int protocol, int line, char *source)
 {
   int sockfd=(socket)(domain, type, protocol);
-  if(logfile)
+  if(logfile && (sockfd!=-1))
     fprintf(logfile, "FD %s:%d socket() = %d\n",
             source, line, sockfd);
   return sockfd;
