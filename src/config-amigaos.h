29d3deaee84bbd4944a8318d5f3867cf9a6df140
Daniel Stenberg <daniel@haxx.se>
2003-12-10 13:54:12 +0000

    native amiga support by Diego Casorran

---
+++ src/config-amigaos.h
@@ -0,0 +1,30 @@
+
+#define OS "AmigaOS"
+
+#define HAVE_UNISTD_H		1
+#define HAVE_STRDUP		1
+#define HAVE_UTIME		1
+#define HAVE_UTIME_H		1
+#define HAVE_SYS_TYPES_H	1
+#define HAVE_SYS_SOCKET_H	1
+#define HAVE_WRITABLE_ARGV	1
+#define HAVE_SYS_TIME_H		1
+#define HAVE_TIME_H		1
+#define TIME_WITH_SYS_TIME	1
+#define HAVE_TERMIOS_H		1
+
+#define HAVE_PWD_H		1
+
+/* futher implementation?... */
+//#define HAVE_TCGETATTR	1
+//#define HAVE_TCSETATTR	1
+
+/* futher usergroup.library usage?... */
+//#define HAVE_GETPWUID		1
+//#define HAVE_GETEUID		1
+
+
+#ifndef F_OK
+# define F_OK 0
+#endif
+
