3c1bb361939e7462dd4956da6f03fe5209f2ae59
Daniel Stenberg <daniel@haxx.se>
2004-01-13 08:57:01 +0000

    use the *correct* header!

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -1,26 +1,26 @@
 #!/usr/bin/env perl
-#############################################################################
-#                                  _   _ ____  _     
-#  Project                     ___| | | |  _ \| |    
-#                             / __| | | | |_) | |    
-#                            | (__| |_| |  _ <| |___ 
+#***************************************************************************
+#                                  _   _ ____  _
+#  Project                     ___| | | |  _ \| |
+#                             / __| | | | |_) | |
+#                            | (__| |_| |  _ <| |___
 #                             \___|\___/|_| \_\_____|
 #
 # Copyright (C) 1998 - 2004, Daniel Stenberg, <daniel@haxx.se>, et al.
 #
-# In order to be useful for every potential user, curl and libcurl are
-# dual-licensed under the MPL and the MIT/X-derivate licenses.
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://curl.haxx.se/docs/copyright.html.
 #
 # You may opt to use, copy, modify, merge, publish, distribute and/or sell
 # copies of the Software, and permit persons to whom the Software is
-# furnished to do so, under the terms of the MPL or the MIT/X-derivate
-# licenses. You may pick one of these licenses.
+# furnished to do so, under the terms of the COPYING file.
 #
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
 # $Id$
-#######################################################################
+###########################################################################
 # These should be the only variables that might be needed to get edited:
 
 use strict;
