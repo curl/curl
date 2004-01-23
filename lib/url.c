334c3df09d46dcd167df0d3046b45e4b47fb1328
Daniel Stenberg <daniel@haxx.se>
2004-01-23 08:36:03 +0000

    Proxy username and password on persistant connections could easily get messed up. Vincent Bronner detected this.

--- lib/url.c
+++ lib/url.c
@@ -2853,7 +2853,7 @@ static CURLcode CreateConnection(struct SessionHandle *data,
     /* get the user+password information from the old_conn struct since it may
      * be new for this request even when we re-use an existing connection */
     conn->bits.user_passwd = old_conn->bits.user_passwd;
-    conn->bits.proxy_user_passwd = conn->bits.proxy_user_passwd;
+    conn->bits.proxy_user_passwd = old_conn->bits.proxy_user_passwd;
 
     /* If we speak over a proxy, we need to copy the host name too, as it
        might be another remote host even when re-using a connection */
