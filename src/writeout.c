37817667e2d633ef8121236da114db6c0ce30a60
Daniel Stenberg <daniel@haxx.se>
2004-02-25 15:41:36 +0000

    use %ld when printfing longs

--- src/writeout.c
+++ src/writeout.c
@@ -119,17 +119,17 @@ void ourWriteOut(CURL *curl, char *writeinfo)
               case VAR_HTTP_CODE:
                 if(CURLE_OK ==
                    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &longinfo))
-                  fprintf(stream, "%03d", longinfo);
+                  fprintf(stream, "%03ld", longinfo);
                 break;
               case VAR_HEADER_SIZE:
                 if(CURLE_OK ==
                    curl_easy_getinfo(curl, CURLINFO_HEADER_SIZE, &longinfo))
-                  fprintf(stream, "%d", longinfo);
+                  fprintf(stream, "%ld", longinfo);
                 break;
               case VAR_REQUEST_SIZE:
                 if(CURLE_OK ==
                    curl_easy_getinfo(curl, CURLINFO_REQUEST_SIZE, &longinfo))
-                  fprintf(stream, "%d", longinfo);
+                  fprintf(stream, "%ld", longinfo);
                 break;
               case VAR_TOTAL_TIME:
                 if(CURLE_OK ==
