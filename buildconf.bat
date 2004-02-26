b6a4ca9bc82c4dbf4f3abaa6ff63bb832fa1bd03
Daniel Stenberg <daniel@haxx.se>
2004-02-26 16:13:13 +0000

    this works like buildconf + configure does on unixes

---
+++ buildconf.bat
@@ -0,0 +1,17 @@
+@echo off
+REM set up a CVS tree to build when there's no autotools
+REM $Revision$
+REM $Date$
+
+REM create ca-bundle.h
+echo /* This file is generated automatically */ >lib\ca-bundle.h
+echo #define CURL_CA_BUNDLE getenv("CURL_CA_BUNDLE") >>lib\ca-bundle.h
+
+REM create getdate.c
+copy lib\getdate.c.cvs lib\getdate.c
+
+REM create hugehelp.c
+copy src\hugehelp.c.cvs src\hugehelp.c
+
+REM create Makefile
+copy Makefile.dist Makefile
