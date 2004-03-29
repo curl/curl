d85c21994f849c8ef98152023435622f42b4dc95
Daniel Stenberg <daniel@haxx.se>
2004-03-29 06:22:57 +0000

    netinet/tcp.h may require netinet/in.h to be include before

--- lib/connect.c
+++ lib/connect.c
@@ -32,10 +32,15 @@
 #ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h> /* <netinet/tcp.h> may need it */
+#endif
 #ifdef HAVE_NETINET_TCP_H
 #include <netinet/tcp.h> /* for TCP_NODELAY */
 #endif
+#ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
+#endif
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
