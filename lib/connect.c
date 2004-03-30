d5b8971ff360e596cfbefbc5964f2a3c2e6415ec
Daniel Stenberg <daniel@haxx.se>
2004-03-30 13:05:45 +0000

    typecast setsockopt()'s 4th argument to void * to make compilers complain less

--- lib/connect.c
+++ lib/connect.c
@@ -490,7 +490,8 @@ static void Curl_setNoDelay(struct connectdata *conn,
 #ifdef TCP_NODELAY
   struct SessionHandle *data= conn->data;
   socklen_t onoff = (socklen_t) data->tcp_nodelay;
-  if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &onoff, sizeof(onoff)) < 0)
+  if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (void *)&onoff,
+                sizeof(onoff)) < 0)
     infof(data, "Could not set TCP_NODELAY: %s\n",
           Curl_strerror(conn, Curl_ourerrno()));
   else
