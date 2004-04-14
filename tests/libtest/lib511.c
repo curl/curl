aba6c2b89d96f661c2b4d77fc925b47921008e3d
Daniel Stenberg <daniel@haxx.se>
2004-04-14 06:30:37 +0000

    Added test case 511 in an attempt to repeat bug report #934666 "storage leak in ftp.c", but it shows no leaking.

---
+++ tests/libtest/lib511.c
@@ -0,0 +1,14 @@
+#include "test.h"
+
+int test(char *URL)
+{
+  CURLcode res;
+  CURL *curl = curl_easy_init();
+  curl_easy_setopt(curl, CURLOPT_URL, URL);
+  curl_easy_setopt(curl, CURLOPT_FILETIME, 1);
+  curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);  
+  return (int)res;
+}
+
