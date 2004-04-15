f57efa189936946562c682d50e3e807c7a3655bf
Daniel Stenberg <daniel@haxx.se>
2004-04-15 13:37:19 +0000

    clean the log directory between each single test, so that we can better display all logs and only logs with relevant data when a test fails and -p is used.

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -1180,13 +1180,9 @@ sub singletest {
         }
     }
 
+    # the test succeeded, remove all log files
     if(!$keepoutfiles) {
-        # remove the stdout and stderr files
-        unlink($STDOUT);
-        unlink($STDERR);
-        unlink($CURLOUT); # remove the downloaded results
-
-        unlink("$LOGDIR/upload.$testnum");  # remove upload leftovers
+        cleardir($LOGDIR);        
     }
 
     unlink($FTPDCMD); # remove the instructions for this test
@@ -1550,23 +1546,10 @@ sub displaylogs {
     closedir DIR;
     my $log;
 
-    my %interest=('curl.log' => 1,
-                  'server.input' => 1,
-                  'server.response' => 1,
-                  'sws.log' => 1,
-                  'ftpd.log' => 1,
-                  );
-
-    print "== Contents of files in the log/ dir:\n";
+    print "== Contents of files in the log/ dir after test $testnum\n";
     foreach $log (sort @logs) {
-        my $num = $log;
-        $num =~ s/[^0-9]//g; # cut off all non-digits
-
-        # the log file is:
-        # generally interesting OR
-        # contains our test case number AND
-        # contains more than zero bytes
-        if(($interest{$log} || ($num  == $testnum)) && (-s "$LOGDIR/$log")) {
+        # the log file contains more than zero bytes
+        if(-s "$LOGDIR/$log") {
             print "== Start of file $log\n";
             displaylogcontent("$LOGDIR/$log");
             print "== End of file $log\n";
