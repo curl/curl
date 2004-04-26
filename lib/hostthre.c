4aac210a83f28db45686647b90e31e1385b25e48
Daniel Stenberg <daniel@haxx.se>
2004-04-26 14:18:42 +0000

    Gisle fixed a mistaken check

--- lib/hostthre.c
+++ lib/hostthre.c
@@ -498,7 +498,7 @@ Curl_addrinfo *Curl_getaddrinfo(struct connectdata *conn,
 
   /* see if we have an IPv6 stack */
   s = socket(PF_INET6, SOCK_DGRAM, 0);
-  if (s != CURL_SOCKET_BAD) {
+  if (s == CURL_SOCKET_BAD) {
     /* Some non-IPv6 stacks have been found to make very slow name resolves
      * when PF_UNSPEC is used, so thus we switch to a mere PF_INET lookup if
      * the stack seems to be a non-ipv6 one. */
