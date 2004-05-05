6bdcfecbcf587e8ac39d9f6e51b81dcdb361f977
Daniel Stenberg <daniel@haxx.se>
2004-05-05 14:08:52 +0000

    Joe Halpin fixed the warning on the typecast from data pointer to function pointer!

--- lib/ldap.c
+++ lib/ldap.c
@@ -164,7 +164,11 @@ static dynafunc DynaGetFunction(const char *name)
 
 #if defined(HAVE_DLOPEN) || defined(HAVE_LIBDL)
   if (libldap) {
-    func = (dynafunc)dlsym(libldap, name);
+    /* This typecast magic below was brought by Joe Halpin. In ISO C, you
+     * cannot typecast a data pointer to a function pointer, but that's
+     * exactly what we need to do here to avoid compiler warnings on picky
+     * compilers! */
+    *(void**) (&func) = dlsym(libldap, name);
   }
 #elif defined(WIN32)
   if (libldap) {
