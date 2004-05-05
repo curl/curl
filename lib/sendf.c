dcf5e52b62c816e4a760d5cb169ac0d36e40cc9f
Daniel Stenberg <daniel@haxx.se>
2004-05-05 07:01:33 +0000

    ERR_error_string() returns an unsigned long so we should use one of those for the return code

--- lib/sendf.c
+++ lib/sendf.c
@@ -238,7 +238,7 @@ CURLcode Curl_write(struct connectdata *conn,
     int err;
     char error_buffer[120]; /* OpenSSL documents that this must be at least
                                120 bytes long. */
-    int sslerror;
+    unsigned long sslerror;
     int rc = SSL_write(conn->ssl[num].handle, mem, len);
 
     if(rc < 0) {
@@ -394,7 +394,7 @@ int Curl_read(struct connectdata *conn, /* connection data */
         {
           char error_buffer[120]; /* OpenSSL documents that this must be at
                                      least 120 bytes long. */
-          int sslerror = ERR_get_error();
+          unsigned long sslerror = ERR_get_error();
           failf(conn->data, "SSL read: %s, errno %d",
                 ERR_error_string(sslerror, error_buffer),
                 Curl_ourerrno() );
