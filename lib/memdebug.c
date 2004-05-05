a331aa02213238d8efae9a046fea93860c62fc87
Daniel Stenberg <daniel@haxx.se>
2004-05-05 06:57:04 +0000

    make the memlimit final NULL return get written to stderr as wella

--- lib/memdebug.c
+++ lib/memdebug.c
@@ -91,6 +91,9 @@ static bool countcheck(const char *func, int line, const char *source)
       if(logfile && source)
         fprintf(logfile, "LIMIT %s:%d %s reached memlimit\n",
                 source, line, func);
+      if(source)
+        fprintf(stderr, "LIMIT %s:%d %s reached memlimit\n",
+                source, line, func);
       return TRUE; /* RETURN ERROR! */
     }
     else
