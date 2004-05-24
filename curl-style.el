37c269d0bacc3ac7d7c506998b4ddbf104c4f9d8
Daniel Stenberg <daniel@haxx.se>
2004-05-24 12:12:09 +0000

    meta-m runs delete-trailing-whitespace in curl-mode

--- curl-style.el
+++ curl-style.el
@@ -33,6 +33,7 @@
   ;; keybindings for C, C++, and Objective-C.  We can put these in
   ;; c-mode-base-map because of inheritance ...
   (define-key c-mode-base-map "\M-q" 'c-fill-paragraph)
+  (define-key c-mode-base-map "\M-m" 'delete-trailing-whitespace)
   (setq c-recognize-knr-p nil)
   ;;; (add-hook 'write-file-hooks 'delete-trailing-whitespace t)
   (setq show-trailing-whitespace t)
