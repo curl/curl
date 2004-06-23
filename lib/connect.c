818aed35e2f7c1602333c0b2ecc0444272966cc7
Daniel Stenberg <daniel@haxx.se>
2004-06-23 09:08:03 +0000

    static functions are better not Curl_ prefixed to make their static status more obvious

--- lib/connect.c
+++ lib/connect.c
@@ -514,8 +514,8 @@ CURLcode Curl_is_connected(struct connectdata *conn,
   return CURLE_OK;
 }
 
-static void Curl_setNoDelay(struct connectdata *conn,
-                            curl_socket_t sockfd)
+static void tcpnodelay(struct connectdata *conn,
+                       curl_socket_t sockfd)
 {
 #ifdef TCP_NODELAY
   struct SessionHandle *data= conn->data;
@@ -648,7 +648,7 @@ CURLcode Curl_connecthost(struct connectdata *conn,  /* context */
     infof(data, "  Trying %s... ", addr_buf);
 
     if(data->set.tcp_nodelay)
-      Curl_setNoDelay(conn, sockfd);
+      tcpnodelay(conn, sockfd);
 
     if(conn->data->set.device) {
       /* user selected to bind the outgoing socket to a specified "device"
