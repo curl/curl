0031d76f2a77e583528a817bcf7b605052b95893
Daniel Stenberg <daniel@haxx.se>
2004-06-24 10:43:22 +0000

    use snprintf() to be on the safe side

--- lib/http.c
+++ lib/http.c
@@ -144,7 +144,7 @@ static CURLcode Curl_output_basic(struct connectdata *conn, bool proxy)
     pwd = conn->passwd;
   }
 
-  sprintf(data->state.buffer, "%s:%s", user, pwd);
+  snprintf(data->state.buffer, sizeof(data->state.buffer), "%s:%s", user, pwd);
   if(Curl_base64_encode(data->state.buffer,
                         strlen(data->state.buffer),
                         &authorization) > 0) {
