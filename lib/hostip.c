f2a99d7d74da7b6be418b74a6c2d474e184b0810
Daniel Stenberg <daniel@haxx.se>
2004-06-24 15:05:39 +0000

    the _num_chars() function is not used, removing

--- lib/hostip.c
+++ lib/hostip.c
@@ -200,30 +200,6 @@ const char *Curl_printable_address(const Curl_addrinfo *ip,
 }
 
 /*
- * Count the number of characters that an integer would use in a string
- * (base 10).
- */
-static int _num_chars(int i)
-{
-  int chars = 0;
-
-  /* While the number divided by 10 is greater than one,
-   * re-divide the number by 10, and increment the number of
-   * characters by 1.
-   *
-   * this relies on the fact that for every multiple of 10,
-   * a new digit is added onto every number
-   */
-  do {
-    chars++;
-
-    i = (int) i / 10;
-  } while (i >= 1);
-
-  return chars;
-}
-
-/*
  * Return a hostcache id string for the providing host + port, to be used by
  * the DNS caching.
  */
