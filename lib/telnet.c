62f04579610e3d34955331ed8f3b16bfdaa2fae1
Daniel Stenberg <daniel@haxx.se>
2004-06-24 12:07:36 +0000

    length limit the sscanf() parsing to prevent buffer overflow

--- lib/telnet.c
+++ lib/telnet.c
@@ -878,7 +878,7 @@ static void suboption(struct connectdata *conn)
         tmplen = (strlen(v->data) + 1);
         /* Add the variable only if it fits */
         if(len + tmplen < (int)sizeof(temp)-6) {
-          sscanf(v->data, "%127[^,],%s", varname, varval);
+          sscanf(v->data, "%127[^,],%127s", varname, varval);
           snprintf((char *)&temp[len], sizeof(temp) - len,
                    "%c%s%c%s", CURL_NEW_ENV_VAR, varname,
                    CURL_NEW_ENV_VALUE, varval);
