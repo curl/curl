6d60ff6ea1047607459ce29016463e8d5313919c
Daniel Stenberg <daniel@haxx.se>
2004-07-29 07:19:27 +0000

    removed C++ comment to please picky source checkers

--- ares/windows_port.c
+++ ares/windows_port.c
@@ -45,7 +45,9 @@ ares_gettimeofday(struct timeval *tv, struct timezone *tz)
         li.LowPart  = ft.dwLowDateTime;
         li.HighPart = ft.dwHighDateTime;
         t  = li.QuadPart;       /* In 100-nanosecond intervals */
-        //t -= EPOCHFILETIME;     /* Offset to the Epoch time */
+#if 0
+        t -= EPOCHFILETIME;     /* Offset to the Epoch time */
+#endif
         t /= 10;                /* In microseconds */
         tv->tv_sec  = (long)(t / 1000000);
         tv->tv_usec = (long)(t % 1000000);
