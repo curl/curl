62f97f1817379f448ce069add864bbd9f93806a1
Daniel Stenberg <daniel@haxx.se>
2004-10-07 22:56:24 +0000

    use curl_strnequal(), not strncasecmp()

--- lib/security.c
+++ lib/security.c
@@ -80,7 +80,7 @@ name_to_level(const char *name)
 {
   int i;
   for(i = 0; i < (int)sizeof(level_names)/(int)sizeof(level_names[0]); i++)
-    if(!strncasecmp(level_names[i].name, name, strlen(name)))
+    if(curl_strnequal(level_names[i].name, name, strlen(name)))
       return level_names[i].level;
   return (enum protection_level)-1;
 }
