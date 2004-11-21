e80f566a1489401f04e3ed1a08ccb456cf778954
Daniel Stenberg <daniel@haxx.se>
2004-11-21 12:42:28 +0000

    added comment for windows people about READFUNCTION being needed

--- docs/examples/ftpupload.c
+++ docs/examples/ftpupload.c
@@ -72,6 +72,12 @@ int main(int argc, char **argv)
     /* now specify which file to upload */
     curl_easy_setopt(curl, CURLOPT_READDATA, hd_src);
 
+    /* NOTE: if you want this example to work on Windows with libcurl as a
+       DLL, you MUST also provide a read callback with
+       CURLOPT_READFUNCTION. Failing to do so will give you a crash since a
+       DLL may not use the variable's memory when passed in to it from an app
+       like this. */
+
     /* and give the size of the upload (optional) */
     curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, file_info.st_size);
 
