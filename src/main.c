9a8ba19b7318fe38e46ae6366da49b516af13fd1
Gisle Vanem <gvanem@broadpark.no>
2004-12-17 18:31:40 +0000

    s/_write/write/g

--- src/main.c
+++ src/main.c
@@ -237,7 +237,7 @@ static int ftruncate64 (int fd, curl_off_t where)
   if (_lseeki64(fd, where, SEEK_SET) < 0)
      return -1;
 
-  if (_write(fd, 0, 0) < 0)
+  if (write(fd, 0, 0) < 0)
      rc = -1;
   _lseeki64(fd, curr, SEEK_SET);
   return rc;
