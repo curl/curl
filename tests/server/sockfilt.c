366b62af2d8d0f166da75b149309228570f7d981
Daniel Stenberg <daniel@haxx.se>
2005-05-01 12:56:09 +0000

    always use the libcurl-provided *printf() functions

--- tests/server/sockfilt.c
+++ tests/server/sockfilt.c
@@ -75,6 +75,9 @@
 #include <netdb.h>
 #endif
 
+#define ENABLE_CURLX_PRINTF
+/* make the curlx header define all printf() functions to use the curlx_*
+   versions instead */
 #include "curlx.h" /* from the private lib dir */
 #include "getpart.h"
 #include "inet_pton.h"
