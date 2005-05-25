50e9522a06bc8f04d81a7d418fc7810f00ceb141
Daniel Stenberg <daniel@haxx.se>
2005-05-25 22:12:57 +0000

    silense a warning

--- tests/server/sockfilt.c
+++ tests/server/sockfilt.c
@@ -435,8 +435,10 @@ static curl_socket_t mksock(bool use_ipv6)
   curl_socket_t sock;
 #ifdef ENABLE_IPV6
   if(!use_ipv6)
+#else
+    (void)use_ipv6;
 #endif
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+  sock = socket(AF_INET, SOCK_STREAM, 0);
 #ifdef ENABLE_IPV6
   else
     sock = socket(AF_INET6, SOCK_STREAM, 0);
