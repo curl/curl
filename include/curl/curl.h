2b7a38d0324c3aa2a5579e7b5d2e5a4465bc565a
Daniel Stenberg <daniel@haxx.se>
2005-08-17 09:12:08 +0000

    removed old info about curl_getdate() just simply isn't true and hasn't been true since the getdate() parser code rewrite

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -1205,11 +1205,10 @@ CURL_EXTERN void curl_slist_free_all(struct curl_slist *);
  * DESCRIPTION
  *
  * Returns the time, in seconds since 1 Jan 1970 of the time string given in
- * the first argument. The time argument in the second parameter is for cases
- * where the specified time is relative now, like 'two weeks' or 'tomorrow'
- * etc.
+ * the first argument. The time argument in the second parameter is unused
+ * and should be set to NULL.
  */
-CURL_EXTERN time_t curl_getdate(const char *p, const time_t *now);
+CURL_EXTERN time_t curl_getdate(const char *p, const time_t *unused);
 
 #define CURLINFO_STRING   0x100000
 #define CURLINFO_LONG     0x200000
