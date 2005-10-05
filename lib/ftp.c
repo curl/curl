3beb2f6b5d3307bdc044aa91a35748c44ec7c26a
Daniel Stenberg <daniel@haxx.se>
2005-10-05 06:09:20 +0000

    CURL_TIMECOND_IFMODSINCE actually requires that the remote document has been modded since the given time, so we should compare <= and not just <.

--- lib/ftp.c
+++ lib/ftp.c
@@ -1798,7 +1798,7 @@ static CURLcode ftp_state_mdtm_resp(struct connectdata *conn,
       switch(data->set.timecondition) {
       case CURL_TIMECOND_IFMODSINCE:
       default:
-        if(data->info.filetime < data->set.timevalue) {
+        if(data->info.filetime <= data->set.timevalue) {
           infof(data, "The requested document is not new enough\n");
           ftp->no_transfer = TRUE; /* mark this to not transfer data */
           state(conn, FTP_STOP);
