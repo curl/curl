0f125d9bcf016d092788a904e2b7a12121a153f6
Daniel Stenberg <daniel@haxx.se>
2005-11-13 22:54:00 +0000

    prevent compiler warning

--- src/main.c
+++ src/main.c
@@ -1499,7 +1499,7 @@ static ParameterError getparameter(char *flag, /* f or -long-flag */
           curl_off_t value = curlx_strtoofft(nextarg, &unit, 0);
 
           if(strlen(unit) != 1)
-            unit="w"; /* unsupported */
+            unit=(char *)"w"; /* unsupported */
 
           switch(*unit) {
           case 'G':
