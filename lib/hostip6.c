d14588120f4f7634f56ada128f8f298bef21152f
Yang Tse <yangsita@gmail.com>
2005-12-13 13:50:22 +0000

    Undo last 'fix', since it was not the proper one.

--- lib/hostip6.c
+++ lib/hostip6.c
@@ -143,9 +143,10 @@ int curl_dogetnameinfo(const struct sockaddr *sa, socklen_t salen,
                        char *serv, size_t servlen, int flags,
                        int line, const char *source)
 {
-  int res = (getnameinfo)(sa, (size_t)salen, 
-                          host, hostlen, serv, servlen, 
-                          (unsigned int)flags);
+  int res = (getnameinfo)(sa, salen, 
+                          host, hostlen, 
+                          serv, servlen, 
+                          flags); 
   if(0 == res) {
     /* success */
     if(logfile)
