532a560d87ba8d4d5dbc0be6ec5ddb78a819362a
Daniel Stenberg <daniel@haxx.se>
2006-01-02 09:13:56 +0000

    we're working on 1.3.1 (or more)

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -5,11 +5,11 @@
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 3
-#define ARES_VERSION_PATCH 0
+#define ARES_VERSION_PATCH 1
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.3.0"
+#define ARES_VERSION_STR "1.3.1"
 
 const char *ares_version(int *version);
 
