77516822f643849082fbcde2b0bff75f155f1142
Dan Fandrich <dan@coneharvesters.com>
2006-08-21 22:28:19 +0000

    Workaround for Cray UNICOS 9.0 to fix ftp.

--- lib/hostip.c
+++ lib/hostip.c
@@ -609,7 +609,14 @@ Curl_addrinfo *Curl_ip2addr(in_addr_t num, const char *hostname, int port)
   h = &buf->hostentry;
   h->h_addr_list = &buf->h_addr_list[0];
   addrentry = &buf->addrentry;
+#ifdef _CRAYC
+  /* On UNICOS, s_addr is a bit field and for some reason assigning to it
+   * doesn't work.  There must be a better fix than this ugly hack.
+   */
+  memcpy(addrentry, &num, SIZEOF_in_addr);
+#else
   addrentry->s_addr = num;
+#endif
   h->h_addr_list[0] = (char*)addrentry;
   h->h_addr_list[1] = NULL;
   h->h_addrtype = AF_INET;
