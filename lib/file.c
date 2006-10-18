a8996b9e5268f8ca33deca21e1375a8887c50d40
Daniel Stenberg <daniel@haxx.se>
2006-10-18 14:47:58 +0000

    use the return code from lseek() to detect problems and bail out if so

--- lib/file.c
+++ lib/file.c
@@ -367,8 +367,11 @@ CURLcode Curl_file(struct connectdata *conn, bool *done)
   if(fstated)
     Curl_pgrsSetDownloadSize(data, expected_size);
 
-  if(data->reqdata.resume_from)
-    lseek(fd, data->reqdata.resume_from, SEEK_SET);
+  if(data->reqdata.resume_from) {
+    if(data->reqdata.resume_from !=
+       lseek(fd, data->reqdata.resume_from, SEEK_SET))
+      return CURLE_BAD_DOWNLOAD_RESUME;
+  }
 
   Curl_pgrsTime(data, TIMER_STARTTRANSFER);
 
