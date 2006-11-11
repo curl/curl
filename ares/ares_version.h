68d4b77d44960c83331bbe2b8f5a3a85cbd509f0
Daniel Stenberg <daniel@haxx.se>
2006-11-11 22:23:48 +0000

    we did 1.3.2 and are now on the 1.3.3 track!

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -5,11 +5,11 @@
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 3
-#define ARES_VERSION_PATCH 1
+#define ARES_VERSION_PATCH 3
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.3.1"
+#define ARES_VERSION_STR "1.3.3-CVS"
 
 #ifdef  __cplusplus
 extern "C" {
