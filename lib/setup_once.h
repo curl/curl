8d11767048349f60361760e57e27e11644759b3b
Daniel Stenberg <daniel@haxx.se>
2006-12-16 22:28:08 +0000

    recv() doesn't take MSG_NOSIGNAL in its forth argument so let's not pass it. Brendan Jurd pointed out.

--- lib/setup_once.h
+++ lib/setup_once.h
@@ -81,7 +81,7 @@
 #define sread(x,y,z) (ssize_t)recv((RECV_TYPE_ARG1)(x), \
                                    (RECV_TYPE_ARG2)(y), \
                                    (RECV_TYPE_ARG3)(z), \
-                                   (RECV_TYPE_ARG4)(SEND_4TH_ARG))
+                                   (RECV_TYPE_ARG4)(0))
 #endif
 #else /* HAVE_RECV */
 #ifndef sread
