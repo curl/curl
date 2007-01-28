e5adab39b165baa3f3f8d7222573f46d54e30771
Daniel Stenberg <daniel@haxx.se>
2007-01-28 12:58:13 +0000

    curl_easy_reset() now resets the CA bundle path correctly

--- lib/easy.c
+++ lib/easy.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2007, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -87,6 +87,7 @@
 #include "progress.h"
 #include "easyif.h"
 #include "sendf.h" /* for failf function prototype */
+#include <ca-bundle.h>
 
 #define _MPRINTF_REPLACE /* use our functions only */
 #include <curl/mprintf.h>
