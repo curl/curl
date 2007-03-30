6fc151577e39ccfefc63342e6cbaf752238c0478
Daniel Stenberg <daniel@haxx.se>
2007-03-30 20:50:31 +0000

    pointless check for 'out' being non-NULL, since it was badly done and is unnecessary - found by coverity.com scan

--- src/main.c
+++ src/main.c
@@ -2799,7 +2799,7 @@ static size_t my_fwrite(void *buffer, size_t sz, size_t nmemb, void *stream)
   struct OutStruct *out=(struct OutStruct *)stream;
   struct Configurable *config = out->config;
 
-  if(out && !out->stream) {
+  if(!out->stream) {
     /* open file for writing */
     out->stream=fopen(out->filename, "wb");
     if(!out->stream) {
