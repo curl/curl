3fef839f7514eeeadd5b4574a533146aabc595c5
Dan Fandrich <dan@coneharvesters.com>
2007-05-15 00:36:56 +0000

    Added call to setvbuf (disabled by default for speed) to flush the memdebug log file after every line and avoid losing the last few log entries if curl crashes.

--- lib/memdebug.c
+++ lib/memdebug.c
@@ -73,6 +73,10 @@ void curl_memdebug(const char *logname)
       logfile = fopen(logname, "w");
     else
       logfile = stderr;
+#ifdef MEMDEBUG_LOG_SYNC
+    /* Flush the log file after every line so the log isn't lost in a crash */
+    setvbuf(logfile, (char *)NULL, _IOLBF, 0);
+#endif
   }
 }
 
