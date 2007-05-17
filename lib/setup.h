0427e94465661ef1b8127c3cd483dc5ffab04f2a
Dan Fandrich <dan@coneharvesters.com>
2007-05-17 06:04:44 +0000

    Added support for compiling under Minix 3.1.3 using ACK.

--- lib/setup.h
+++ lib/setup.h
@@ -259,6 +259,12 @@
 #define sclose(x) CloseSocket(x)
 #endif
 
+#ifdef __minix
+/* Minix 3 versions up to at least 3.1.3 are missing these prototypes */
+extern char * strtok_r(char *s, const char *delim, char **last);
+extern struct tm * gmtime_r(const time_t * const timep, struct tm *tmp);
+#endif
+
 #define DIR_CHAR      "/"
 #ifndef DOT_CHAR
 #define DOT_CHAR      "."
