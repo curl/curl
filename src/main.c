70b1900dd13d16f2e83f571407a614541d5ac9ba
Daniel Stenberg <daniel@haxx.se>
2007-05-24 20:58:25 +0000

    'mytx' in bug report #1723194 (http://curl.haxx.se/bug/view.cgi?id=1723194) pointed out that the warnf() function in the curl tool didn't properly deal with the cases when excessively long words were used in the string to chop up.

--- src/main.c
+++ src/main.c
@@ -501,6 +501,10 @@ static void warnf(struct Configurable *config, const char *fmt, ...)
         while(!ISSPACE(ptr[cut]) && cut) {
           cut--;
         }
+        if(0 == cut)
+          /* not a single cutting position was found, just cut it at the
+             max text width then! */
+          cut = WARN_TEXTWIDTH-1;
 
         fwrite(ptr, cut + 1, 1, config->errors);
         fputs("\n", config->errors);
