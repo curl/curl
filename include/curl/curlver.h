3dff58b3aa14e2a6c0c3a0e35eb27da654288553
Daniel Stenberg <daniel@haxx.se>
2007-06-25 13:52:16 +0000

    fix the version string as well

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -28,7 +28,7 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.16.3-CVS"
+#define LIBCURL_VERSION "7.16.4-CVS"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
