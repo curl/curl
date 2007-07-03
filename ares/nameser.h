07bf3c86b64283765f8ba39e89a959cd4dc36ecc
Gunter Knauf <gk@gknw.de>
2007-07-03 18:18:40 +0000

    trial to catch problem with Daniels cross-mingw ares builds.

--- ares/nameser.h
+++ ares/nameser.h
@@ -32,7 +32,9 @@ struct iovec
 int ares_writev (SOCKET s, const struct iovec *vector, size_t count);
 #define writev(s,vect,count)  ares_writev(s,vect,count)
 
+#ifndef HAVE_STRUCT_TIMEVAL
 struct timezone { int dummy; };
+#endif
 
 int ares_gettimeofday(struct timeval *tv, struct timezone *tz);
 #define gettimeofday(tv,tz) ares_gettimeofday(tv,tz)
