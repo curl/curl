15c821934011bfa0b3d7f3f93d0349adc2ab40e9
Daniel Stenberg <daniel@haxx.se>
2007-07-14 22:39:22 +0000

    Added test case 540 and lib540.c, the 'proxyauth.c' test app posted by Shmulik Regev on the libcurl mailing list on 10 Jul 2007, converted to a test case.

---
+++ tests/libtest/lib540.c
@@ -0,0 +1,119 @@
+/*****************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * $Id$
+ *
+ * This is the 'proxyauth.c' test app posted by Shmulik Regev on the libcurl
+ * mailing list on 10 Jul 2007, converted to a test case.
+ *
+ * argv1 = URL
+ * argv2 = proxy
+ * argv3 = proxyuser:password
+ */
+
+#include "test.h"
+
+#define PROXY arg2
+#define PROXYUSERPWD arg3
+
+static void init(CURLM *cm, const char* url, const char* userpwd)
+{
+  CURL *eh = curl_easy_init();
+
+  curl_easy_setopt(eh, CURLOPT_URL, url);
+  curl_easy_setopt(eh, CURLOPT_PROXY, PROXY);
+  curl_easy_setopt(eh, CURLOPT_PROXYUSERPWD, userpwd);
+  curl_easy_setopt(eh, CURLOPT_PROXYAUTH, CURLAUTH_ANY);
+  curl_easy_setopt(eh, CURLOPT_VERBOSE, 1);
+  curl_easy_setopt(eh, CURLOPT_HEADER, 1);
+
+  curl_multi_add_handle(cm, eh);
+}
+
+static int loop(CURLM *cm, const char* url, const char* userpwd)
+{
+  CURLMsg *msg;
+  long L;
+  int M, Q, U = -1;
+  fd_set R, W, E;
+  struct timeval T;
+
+  init(cm, url, userpwd);
+
+  while (U) {
+    while (CURLM_CALL_MULTI_PERFORM == curl_multi_perform(cm, &U));
+
+    if (U) {
+      FD_ZERO(&R);
+      FD_ZERO(&W);
+      FD_ZERO(&E);
+
+      if (curl_multi_fdset(cm, &R, &W, &E, &M)) {
+        fprintf(stderr, "E: curl_multi_fdset\n");
+        return EXIT_FAILURE;
+      }
+
+      /* In a real-world program you OF COURSE check the return that maxfd is
+         bigger than -1 so that the call to select() below makes sense! */
+
+      if (curl_multi_timeout(cm, &L)) {
+        fprintf(stderr, "E: curl_multi_timeout\n");
+        return EXIT_FAILURE;
+      }
+
+      if(L != -1) {
+        T.tv_sec = L/1000;
+        T.tv_usec = (L%1000)*1000;
+      }
+      else {
+        T.tv_sec = 5;
+        T.tv_usec = 0;
+      }
+
+      if (0 > select(M+1, &R, &W, &E, &T)) {
+        fprintf(stderr, "E: select\n");
+        return EXIT_FAILURE;
+      }
+    }
+
+    while ((msg = curl_multi_info_read(cm, &Q))) {
+      if (msg->msg == CURLMSG_DONE) {
+        char *url;
+        CURL *e = msg->easy_handle;
+        curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, &url);
+        fprintf(stderr, "R: %d - %s <%s>\n",
+                msg->data.result, curl_easy_strerror(msg->data.result), url);
+        curl_multi_remove_handle(cm, e);
+        curl_easy_cleanup(e);
+      }
+      else {
+        fprintf(stderr, "E: CURLMsg (%d)\n", msg->msg);
+      }
+    }
+  }
+
+  return 1;
+}
+
+int test(char *URL)
+{
+  CURLM *cm;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  cm = curl_multi_init();
+  loop(cm, URL, PROXYUSERPWD);
+
+  fprintf(stderr, "lib540: now we do the request again\n");
+  loop(cm, URL, PROXYUSERPWD);
+
+  curl_multi_cleanup(cm);
+
+  curl_global_cleanup();
+
+  return EXIT_SUCCESS;
+}
