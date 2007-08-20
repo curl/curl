1a8d8aa2273bd4e29de385bee2db024cd0193b82
Dan Fandrich <dan@coneharvesters.com>
2007-08-20 17:53:38 +0000

    Increase timeout for accept for improved reliability on loaded servers.

--- tests/ftpserver.pl
+++ tests/ftpserver.pl
@@ -585,7 +585,7 @@ sub PASV_command {
         local $SIG{ALRM} = sub { die "alarm\n" };
 
 	# assume swift operations unless explicitly slow
-	alarm ($controldelay?20:5);
+	alarm ($controldelay?20:7);
 
         # Wait for 'CNCT'
 	my $input;
