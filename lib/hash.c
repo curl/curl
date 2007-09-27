ad05b22de350f87f295a09fb6e0745b91216aa21
Dan Fandrich <dan@coneharvesters.com>
2007-09-27 18:12:03 +0000

    Renamed a few variables to avoid shadowing global declarations.

--- lib/hash.c
+++ lib/hash.c
@@ -117,12 +117,12 @@ mk_hash_element(const void *key, size_t key_len, const void *p)
     (struct curl_hash_element *) malloc(sizeof(struct curl_hash_element));
 
   if(he) {
-    void *dup = malloc(key_len);
-    if(dup) {
+    void *dupkey = malloc(key_len);
+    if(dupkey) {
       /* copy the key */
-      memcpy(dup, key, key_len);
+      memcpy(dupkey, key, key_len);
 
-      he->key = dup;
+      he->key = dupkey;
       he->key_len = key_len;
       he->ptr = (void *) p;
     }
