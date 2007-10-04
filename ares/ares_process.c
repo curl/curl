91b38857ef9ab1f345f0c8914ef3feec803f8752
Steinar H. Gunderson <sesse@google.com>
2007-10-04 08:12:12 +0000

    Removed a piece of redundant code (process_answer already takes care of it).

--- ares/ares_process.c
+++ ares/ares_process.c
@@ -530,12 +530,6 @@ static void process_answer(ares_channel channel, unsigned char *abuf,
             next_server(channel, query, now);
           return;
         }
-      if (!same_questions(query->qbuf, query->qlen, abuf, alen))
-        {
-          if (query->server == whichserver)
-            next_server(channel, query, now);
-          return;
-        }
     }
 
   end_query(channel, query, ARES_SUCCESS, abuf, alen);
