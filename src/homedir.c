7f62028d668eaddd1aeaed42c8a0e3ce4947b51f
Daniel Stenberg <daniel@haxx.se>
2007-11-01 21:49:59 +0000

    Toby Peterson patched a memory problem in the command line tool that happened when a user had a home dir as an empty string. curl would then do free() on a wrong area.

--- src/homedir.c
+++ src/homedir.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2005, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2007, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -106,6 +106,8 @@ char *homedir(void)
 #endif
      if (home && home[0])
        home = strdup(home);
+     else
+       home = NULL;
    }
  }
 #endif /* PWD-stuff */
