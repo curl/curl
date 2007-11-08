c960cd41e84aa3d86cd0a5cd6c0d8b4ae6c96344
Yang Tse <yangsita@gmail.com>
2007-11-08 16:32:31 +0000

    Fix comment

--- tests/server/tftp.h
+++ tests/server/tftp.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2007, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -23,7 +23,7 @@
  * $Id$
  ***************************************************************************/
 
-/* This file is a rewrite/clone of the arpa/telnet.h file for systems without
+/* This file is a rewrite/clone of the arpa/tftp.h file for systems without
    it. */
 
 #define	SEGSIZE 512 /* data segment size */
