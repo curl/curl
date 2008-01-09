c249a8aa1b3cdd4a706820092542bb6237596240
Yang Tse <yangsita@gmail.com>
2008-01-09 01:11:59 +0000

    Fix file Id

--- tests/sshhelp.pm
+++ tests/sshhelp.pm
@@ -18,7 +18,7 @@
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
-# $Id: 
+# $Id$
 #***************************************************************************
 
 package sshhelp;
