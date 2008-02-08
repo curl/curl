4c841a1f0cf53d363d2ba3351c558ce8b06e9c25
Daniel Stenberg <daniel@haxx.se>
2008-02-08 22:02:00 +0000

    - Mike Hommey filed and fixed bug report #1889856   (http://curl.haxx.se/bug/view.cgi?id=1889856): When using the gnutls ssl   layer, cleaning-up and reinitializing curl ends up with https requests   failing with "ASN1 parser: Element was not found" errors. Obviously a   regression added in 7.16.3.

--- lib/gtls.c
+++ lib/gtls.c
@@ -110,8 +110,10 @@ static int _Curl_gtls_init(void)
 
 int Curl_gtls_cleanup(void)
 {
-  if(gtls_inited)
+  if(gtls_inited) {
     gnutls_global_deinit();
+    gtls_inited = FALSE;
+  }
   return 1;
 }
 
