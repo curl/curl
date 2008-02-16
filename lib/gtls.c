550d6f74b9223231d939ef8526090a3f387b0609
Daniel Stenberg <daniel@haxx.se>
2008-02-16 13:44:23 +0000

    oops, that was debug code not meant to be committed like this...

--- lib/gtls.c
+++ lib/gtls.c
@@ -344,10 +344,6 @@ Curl_gtls_connect(struct connectdata *conn,
   }
 
   if(data->set.ssl.verifypeer) {
-
-    gnutls_certificate_set_verify_limits(conn->ssl[sockindex].cred,
-                                         40200, 38);
-
     /* This function will try to verify the peer's certificate and return its
        status (trusted, invalid etc.). The value of status should be one or
        more of the gnutls_certificate_status_t enumerated elements bitwise
