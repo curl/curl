ab8d1464a7b5a11e1ec88c42ede77541e9314e6c
Yang Tse <yangsita@gmail.com>
2008-04-07 14:37:17 +0000

    fix compiler warning: indirection to slightly different base types

--- ares/adig.c
+++ ares/adig.c
@@ -608,32 +608,32 @@ static const unsigned char *display_rr(const unsigned char *aptr,
 
     case T_NAPTR:
 
-      printf("\t%d", DNS__16BIT(aptr)); // order
-      printf(" %d\n", DNS__16BIT(aptr + 2)); // preference
+      printf("\t%d", DNS__16BIT(aptr)); /* order */
+      printf(" %d\n", DNS__16BIT(aptr + 2)); /* preference */
 
       p = aptr + 4;
-      status = ares_expand_string(p, abuf, alen, &name, &len);
+      status = ares_expand_string(p, abuf, alen, &(unsigned char *)name, &len);
       if (status != ARES_SUCCESS)
         return NULL;
       printf("\t\t\t\t\t\t%s\n", name);
       ares_free_string(name);
       p += len;
 
-      status = ares_expand_string(p, abuf, alen, &name, &len);
+      status = ares_expand_string(p, abuf, alen, &(unsigned char *)name, &len);
       if (status != ARES_SUCCESS)
         return NULL;
       printf("\t\t\t\t\t\t%s\n", name);
       ares_free_string(name);
       p += len;
 
-      status = ares_expand_string(p, abuf, alen, &name, &len);
+      status = ares_expand_string(p, abuf, alen, &(unsigned char *)name, &len);
       if (status != ARES_SUCCESS)
         return NULL;
       printf("\t\t\t\t\t\t%s\n", name);
       ares_free_string(name);
       p += len;
 
-      status = ares_expand_string(p, abuf, alen, &name, &len);
+      status = ares_expand_string(p, abuf, alen, &(unsigned char *)name, &len);
       if (status != ARES_SUCCESS)
         return NULL;
       printf("\t\t\t\t\t\t%s", name);
