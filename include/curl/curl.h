400d9d420517a81d5f55153b0dda90aced1cbfb3
Daniel Stenberg <daniel@haxx.se>
2008-07-01 21:53:47 +0000

    - Rolland Dudemaine provided fixes to get libcurl to build for the INTEGRITY   operating system.

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -64,7 +64,7 @@
    libc5-based Linux systems. Only include it on system that are known to
    require it! */
 #if defined(_AIX) || defined(__NOVELL_LIBC__) || defined(__NetBSD__) || \
-    defined(__minix) || defined(__SYMBIAN32__)
+    defined(__minix) || defined(__SYMBIAN32__) || defined(__INTEGRITY)
 #include <sys/select.h>
 #endif
 
