da6fa34f021c5dfd3d0ecc9de6950497dc1a737a
Yang Tse <yangsita@gmail.com>
2008-07-11 18:59:00 +0000

    fix multiple header inclusion prevention definition

--- lib/parsedate.h
+++ lib/parsedate.h
@@ -1,5 +1,5 @@
-#ifndef __PARSEDATE_H
-#define __PARSEDATEL_H
+#ifndef __CURL_PARSEDATE_H
+#define __CURL_PARSEDATE_H
 /***************************************************************************
  *                                  _   _ ____  _
  *  Project                     ___| | | |  _ \| |
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2005, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2008, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -22,6 +22,7 @@
  *
  * $Id$
  ***************************************************************************/
+
 extern const char * const Curl_wkday[7];
 extern const char * const Curl_month[12];
 
