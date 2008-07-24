932b58978029f1f6445076397b0fb275ceccb110
Dan Fandrich <dan@coneharvesters.com>
2008-07-24 02:16:48 +0000

    Changed the long logfile elision code in runtests.pl to properly handle lines ending in \r.

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -2963,11 +2963,10 @@ open(CMDLOG, ">$CURLLOG") ||
 #######################################################################
 
 # Display the contents of the given file.  Line endings are canonicalized
-# and excessively long files are truncated
+# and excessively long files are elided
 sub displaylogcontent {
     my ($file)=@_;
     if(open(SINGLE, "<$file")) {
-        my $lfcount;
         my $linecount = 0;
         my $truncate;
         my @tail;
@@ -2975,29 +2974,17 @@ sub displaylogcontent {
             $string =~ s/\r\n/\n/g;
             $string =~ s/[\r\f\032]/\n/g;
             $string .= "\n" unless ($string =~ /\n$/);
-            $lfcount = $string =~ tr/\n//;
-            if($lfcount == 1) {
-                $string =~ s/\n//;
-                $string =~ s/\s*\!$//;
-                $linecount++;
-                if ($truncate) {
-                    push @tail, " $string\n";
-                } else {
-                    logmsg " $string\n";
-                }
-            }
-            else {
-                for my $line (split("\n", $string)) {
-                    $line =~ s/\s*\!$//;
-                    $linecount++;
-                    if ($truncate) {
-                        push @tail, " $line\n";
-                    } else {
-                        logmsg " $line\n";
-		    }
-                }
-            }
-            $truncate = $linecount > 1000;
+            $string =~ tr/\n//;
+	    for my $line (split("\n", $string)) {
+		$line =~ s/\s*\!$//;
+		if ($truncate) {
+		    push @tail, " $line\n";
+		} else {
+		    logmsg " $line\n";
+		}
+		$linecount++;
+		$truncate = $linecount > 1000;
+	    }
         }
         if (@tail) {
             logmsg "=== File too long: lines here were removed\n";
