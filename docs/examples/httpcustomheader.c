aab2d52b25776345f88537fd22f3d79c0fce170d
Daniel Stenberg <daniel@haxx.se>
2008-08-13 08:51:52 +0000

    httpcustomheader.c is a new tiny example showing a HTTP request with a custom header replacing an internal one

---
+++ docs/examples/httpcustomheader.c
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * $Id$
+ */
+
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  if(curl) {
+    struct curl_slist *chunk = NULL;
+
+    chunk = curl_slist_append(chunk, "Accept: moo");
+
+    /* request with the built-in Accept: */
+    curl_easy_setopt(curl, CURLOPT_URL, "localhost");
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    res = curl_easy_perform(curl);
+
+    /* redo request with our own custom Accept: */
+    res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+    res = curl_easy_perform(curl);
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
