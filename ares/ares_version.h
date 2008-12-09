21700ae515ff6862f3dc8bb919ee622d190e1dda
Daniel Stenberg <daniel@haxx.se>
2008-12-09 22:41:36 +0000

    start over on the 1.6.1 release...

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -4,12 +4,12 @@
 #define ARES__VERSION_H
 
 #define ARES_VERSION_MAJOR 1
-#define ARES_VERSION_MINOR 5
-#define ARES_VERSION_PATCH 4
+#define ARES_VERSION_MINOR 6
+#define ARES_VERSION_PATCH 1
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.5.4-CVS"
+#define ARES_VERSION_STR "1.6.1-CVS"
 
 #ifdef  __cplusplus
 extern "C" {
