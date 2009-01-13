a19e02be5e3fc41aee1ecee73a26d363e186e36e
Daniel Stenberg <daniel@haxx.se>
2009-01-13 22:21:14 +0000

    - Michael Wallner fixed a NULL pointer deref when calling   curl_easy_setup(curl, CURLOPT_COOKIELIST, "SESS") on a CURL handle with no   cookies data.

--- lib/cookie.c
+++ lib/cookie.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2008, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -878,7 +878,7 @@ void Curl_cookie_clearsess(struct CookieInfo *cookies)
 {
   struct Cookie *first, *curr, *next, *prev = NULL;
 
-  if(!cookies->cookies)
+  if(!cookies->cookies || !cookies->cookies)
     return;
 
   first = curr = prev = cookies->cookies;
