b5c599c80c18866dcb78599e99162bcf8ed4205d
Yang Tse <yangsita@gmail.com>
2009-05-02 03:16:04 +0000

    This wasn't intended to be committed.
    So, until fixed this will remain FIXME.

--- packages/vms/config-vms.h
+++ packages/vms/config-vms.h
@@ -284,7 +284,7 @@
 #define GETNAMEINFO_TYPE_ARG1 struct sockaddr *
 
 /* Define to the type of arg 2 for getnameinfo. */
-#define GETNAMEINFO_TYPE_ARG2 unsigned int
+#define GETNAMEINFO_TYPE_ARG2 FIXME
 
 /* Define to the type of args 4 and 6 for getnameinfo. */
 #define GETNAMEINFO_TYPE_ARG46 size_t
