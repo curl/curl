86f043a66c0aee8ab152b5fc5e4ada0ca64052b2
Yang Tse <yangsita@gmail.com>
2009-05-12 19:18:54 +0000

    temporary debug trace for ftp server satrtup failure on testcase #100

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -231,6 +231,7 @@ my %doesntrun;    # servers that don't work, identified by pidfile
 my $torture;
 my $tortnum;
 my $tortalloc;
+my $testnumrequiringthis;
 
 # open and close each time to allow removal at any time
 sub logmsg {
@@ -988,6 +989,7 @@ sub runftpserver {
         # it is NOT alive
         logmsg "RUN: failed to start the FTP$id$nameext server\n";
         stopserver("$pid2");
+        displaylogs($testnumrequiringthis) if($testnumrequiringthis == 100);
         $doesntrun{$pidfile} = 1;
         return (0,0);
     }
@@ -998,6 +1000,7 @@ sub runftpserver {
         logmsg "RUN: FTP$id$nameext server failed verification\n";
         # failed to talk to it properly. Kill the server and return failure
         stopserver("$ftppid $pid2");
+        displaylogs($testnumrequiringthis) if($testnumrequiringthis == 100);
         $doesntrun{$pidfile} = 1;
         return (0,0);
     }
@@ -1739,6 +1742,8 @@ sub singletest {
     my $why;
     my %feature;
     my $cmd;
+
+    $testnumrequiringthis = $testnum;
 
     # load the test case file definition
     if(loadtest("${TESTDIR}/test${testnum}")) {
