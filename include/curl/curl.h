15eaf27bc7b4ad33af275b9cd0601ef19d54ac9c
Dan Fandrich <dan@coneharvesters.com>
2009-05-28 21:32:31 +0000

    Fixed a few comment typos (from the FreeBSD ports)

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -186,7 +186,7 @@ typedef size_t (*curl_write_callback)(char *buffer,
                                       size_t nitems,
                                       void *outstream);
 
-/* this is the return codes for the seek callbacks */
+/* These are the return codes for the seek callbacks */
 #define CURL_SEEKFUNC_OK       0
 #define CURL_SEEKFUNC_FAIL     1 /* fail the entire transfer */
 #define CURL_SEEKFUNC_CANTSEEK 2 /* tell libcurl seeking can't be done, so
@@ -1516,7 +1516,7 @@ CURL_EXTERN void curl_free(void *p);
  * DESCRIPTION
  *
  * curl_global_init() should be invoked exactly once for each application that
- * uses libcurl and before any call of other libcurl function.
+ * uses libcurl and before any call of other libcurl functions.
  *
  * This function is not thread-safe!
  */
--- lib/nss.c
+++ lib/nss.c
@@ -157,7 +157,7 @@ static const cipher_s cipherlist[] = {
 #endif
 };
 
-/* following ciphers are new in NSS 3.4 and not enabled by default, therefor
+/* following ciphers are new in NSS 3.4 and not enabled by default, therefore
    they are enabled explicitly */
 static const int enable_ciphers_by_default[] = {
   TLS_DHE_DSS_WITH_AES_128_CBC_SHA,
