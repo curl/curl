efec05d5810ec238137bfa1914af614e24024417
Yang Tse <yangsita@gmail.com>
2009-06-08 14:27:36 +0000

    Ensure that buildconf.bat does nothing unless it is used with a CVS checkout.

--- buildconf.bat
+++ buildconf.bat
@@ -1,16 +1,32 @@
 @echo off
-REM set up a CVS tree to build when there's no autotools
-REM $Revision$
-REM $Date$
+REM
+REM $Id$
+REM
+REM This batch file must be used to set up a CVS tree to build on
+REM systems where there is no autotools support (i.e. Microsoft).
+REM
+REM This file is not included nor needed for curl's release
+REM archives, neither for curl's daily snapshot archives.
+
+if exist CVS-INFO goto start_doing
+ECHO ERROR: This file shall only be used with a curl CVS tree checkout.
+goto end_all
+:start_doing
 
 REM create hugehelp.c
-copy src\hugehelp.c.cvs src\hugehelp.c
+if not exist src\hugehelp.c.cvs goto end_hugehelp_c
+copy /Y src\hugehelp.c.cvs src\hugehelp.c
+:end_hugehelp_c
 
 REM create Makefile
-copy Makefile.dist Makefile
+if not exist Makefile.dist goto end_makefile
+copy /Y Makefile.dist Makefile
+:end_makefile
 
 REM create curlbuild.h
-copy include\curl\curlbuild.h.dist include\curl\curlbuild.h
+if not exist include\curl\curlbuild.h.dist goto end_curlbuild_h
+copy /Y include\curl\curlbuild.h.dist include\curl\curlbuild.h
+:end_curlbuild_h
 
 REM setup c-ares CVS tree
 if not exist ares\buildconf.bat goto end_c_ares
@@ -18,4 +34,6 @@ cd ares
 call buildconf.bat
 cd ..
 :end_c_ares
+
+:end_all
 
