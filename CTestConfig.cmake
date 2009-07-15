6dd0277c22374983e9f9b0c22ebada72625973a9
Bill Hoffman <bill.hoffman@kitware.com>
2009-07-15 19:40:46 +0000

    ENH: move dashboard location

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "CURL")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDashPublic/submit.php?project=CURL")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=CURL")
 set(CTEST_DROP_SITE_CDASH TRUE)
