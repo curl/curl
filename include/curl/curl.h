44e2832b2a06ad95a3a5241b7c1b0fece76cace5
Gunter Knauf <gk@gknw.de>
2009-08-31 13:58:47 +0000

    added curl prefix for curlbuild.h back until we find a better solution; it breaks almost all autobuilds.

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -32,7 +32,7 @@
  */
 
 #include "curlver.h"         /* libcurl version defines   */
-#include "curlbuild.h"       /* libcurl build definitions */
+#include "curl/curlbuild.h"  /* libcurl build definitions */
 #include "curlrules.h"       /* libcurl rules enforcement */
 
 /*
