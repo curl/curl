5d4a1e245b505283b1c3ec44e0a781353b335637
Gunter Knauf <gk@gknw.de>
2009-09-08 01:13:49 +0000

    added debug output for NSS certpath.

--- lib/nss.c
+++ lib/nss.c
@@ -978,6 +978,8 @@ CURLcode Curl_nss_connect(struct connectdata *conn, int sockindex)
 
     if (!NSS_IsInitialized()) {
       initialized = 1;
+      infof(conn->data, "Initializing NSS with certpath: %s\n",
+            certDir ? certDir : "none");
       if(!certDir) {
         rv = NSS_NoDB_Init(NULL);
       }
