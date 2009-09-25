af9ce990f0a418a22f171f89da9bc58f4637e9ee
Daniel Stenberg <daniel@haxx.se>
2009-09-25 20:26:44 +0000

    minor whitespace edit

--- lib/cookie.c
+++ lib/cookie.c
@@ -365,12 +365,12 @@ Curl_cookie_add(struct SessionHandle *data,
                get parsed for whatever reason. This will have the effect that
                the cookie won't match. */
             co->expires = curl_getdate(what, &now);
-            
+
             /* Session cookies have expires set to 0 so if we get that back
-               from the date parser let's add a second to make it a 
+               from the date parser let's add a second to make it a
                non-session cookie */
             if (co->expires == 0)
-                co->expires = 1;
+              co->expires = 1;
           }
           else if(!co->name) {
             co->name = strdup(name);
