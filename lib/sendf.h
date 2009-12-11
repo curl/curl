2fc1752d6e978995ae04cc36525f76fd57a4ae09
Yang Tse <yangsita@gmail.com>
2009-12-11 02:14:11 +0000

    Removed function prototype without implementation

--- lib/sendf.h
+++ lib/sendf.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2008, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -54,9 +54,6 @@ void Curl_failf(struct SessionHandle *, const char *fmt, ...);
 
 CURLcode Curl_client_write(struct connectdata *conn, int type, char *ptr,
                            size_t len);
-
-void Curl_read_rewind(struct connectdata *conn,
-                      size_t extraBytesRead);
 
 /* internal read-function, does plain socket only */
 int Curl_read_plain(curl_socket_t sockfd,
