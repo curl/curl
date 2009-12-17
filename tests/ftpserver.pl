39cc424e8135f0de57edbb9ceba3d769ec30e2f7
Yang Tse <yangsita@gmail.com>
2009-12-17 22:28:40 +0000

    Comment out hi resolution time logging to verify if this is what might be contributing 90 additional seconds to the total time some autobuilds now need to make a full test-run.

--- tests/ftpserver.pl
+++ tests/ftpserver.pl
@@ -43,11 +43,11 @@ use IPC::Open2;
 require "getpart.pm";
 require "ftp.pm";
 
-BEGIN {
-    if($] > 5.006) {
-        use Time::HiRes qw( gettimeofday );
-    }
-}
+#BEGIN {
+#    if($] > 5.006) {
+#        use Time::HiRes qw( gettimeofday );
+#    }
+#}
 
 my $ftpdnum="";
 
@@ -71,18 +71,18 @@ sub getlogfilename {
 #
 sub logmsg {
     my $now;
-    if($] > 5.006) {
-        my ($seconds, $usec) = gettimeofday();
-        my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
-            localtime($seconds);
-        $now = sprintf("%02d:%02d:%02d.%06d ", $hour, $min, $sec, $usec);
-    }
-    else {
+#   if($] > 5.006) {
+#       my ($seconds, $usec) = gettimeofday();
+#       my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+#           localtime($seconds);
+#       $now = sprintf("%02d:%02d:%02d.%06d ", $hour, $min, $sec, $usec);
+#   }
+#   else {
         my $seconds = time();
         my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
             localtime($seconds);
         $now = sprintf("%02d:%02d:%02d ", $hour, $min, $sec);
-    }
+#   }
     if(open(LOGFILEFH, ">>$logfilename")) {
         print LOGFILEFH $now;
         print LOGFILEFH @_;
