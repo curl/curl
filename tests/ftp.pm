82f5ffff8f183720aaee296b48cd67c4c003ece5
Yang Tse <yangsita@gmail.com>
2010-01-12 14:01:44 +0000

    Make sockfilter kill messages look alike server ones

--- tests/ftp.pm
+++ tests/ftp.pm
@@ -25,7 +25,7 @@ use strict;
 use warnings;
 
 use serverhelp qw(
-    servername_str
+    servername_id
     mainsockf_pidfilename
     datasockf_pidfilename
     );
@@ -178,19 +178,19 @@ sub killpid {
 #
 sub killsockfilters {
     my ($proto, $ipvnum, $idnum, $verbose) = @_;
-    my $srvrname;
+    my $server;
     my $pidfile;
     my $pid;
 
     return if($proto !~ /^(ftp|imap|pop3|smtp)$/);
 
-    $srvrname = servername_str($proto, $ipvnum, $idnum) if($verbose);
+    $server = servername_id($proto, $ipvnum, $idnum) if($verbose);
 
-    $pidfile = "./". mainsockf_pidfilename($proto, $ipvnum, $idnum);
+    $pidfile = mainsockf_pidfilename($proto, $ipvnum, $idnum);
     $pid = processexists($pidfile);
     if($pid > 0) {
-        printf("* kill pid for %s => %d\n", "${srvrname}-CTRL", $pid)
-          if($verbose);
+        printf("* kill pid for %s-%s => %d\n", $server,
+            ($proto eq 'ftp')?'ctrl':'filt', $pid) if($verbose);
         kill("KILL", $pid);
         waitpid($pid, 0);
     }
@@ -198,11 +198,11 @@ sub killsockfilters {
 
     return if($proto ne 'ftp');
 
-    $pidfile = "./". datasockf_pidfilename($proto, $ipvnum, $idnum);
+    $pidfile = datasockf_pidfilename($proto, $ipvnum, $idnum);
     $pid = processexists($pidfile);
     if($pid > 0) {
-        printf("* kill pid for %s => %d\n", "${srvrname}-DATA", $pid)
-          if($verbose);
+        printf("* kill pid for %s-data => %d\n", $server,
+            $pid) if($verbose);
         kill("KILL", $pid);
         waitpid($pid, 0);
     }
