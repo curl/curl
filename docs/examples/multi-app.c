a74e885befcded7dd958563badaaf8a5f29587aa
Daniel Stenberg <daniel@haxx.se>
2010-01-21 09:53:30 +0000

    Yun Fu pointed out a flaw in the loop that checks handles, and I indented the code more curl-style

--- docs/examples/multi-app.c
+++ docs/examples/multi-app.c
@@ -105,20 +105,23 @@ int main(int argc, char **argv)
   /* See how the transfers went */
   while ((msg = curl_multi_info_read(multi_handle, &msgs_left))) {
     if (msg->msg == CURLMSG_DONE) {
+      int idx, found = 0;
 
-       int idx, found = 0;
+      /* Find out which handle this message is about */
+      for (idx=0; idx<HANDLECOUNT; idx++) {
+        found = (msg->easy_handle == handles[idx]);
+        if(found)
+          break;
+      }
 
-       /* Find out which handle this message is about */
-       for (idx=0; (!found && (idx<HANDLECOUNT)); idx++) found = (msg->easy_handle == handles[idx]);
-
-       switch (idx) {
-         case HTTP_HANDLE:
-           printf("HTTP transfer completed with status %d\n", msg->data.result);
-           break;
-         case FTP_HANDLE:
-           printf("FTP transfer completed with status %d\n", msg->data.result);
-           break;
-       }
+      switch (idx) {
+      case HTTP_HANDLE:
+        printf("HTTP transfer completed with status %d\n", msg->data.result);
+        break;
+      case FTP_HANDLE:
+        printf("FTP transfer completed with status %d\n", msg->data.result);
+        break;
+      }
     }
   }
 
