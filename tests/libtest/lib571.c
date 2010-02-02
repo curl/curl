9f6c4daa27323603368907d31ed6a4c6e1aa9179
Yang Tse <yangsita@gmail.com>
2010-02-02 07:01:21 +0000

    include headers

--- tests/libtest/lib571.c
+++ tests/libtest/lib571.c
@@ -10,11 +10,23 @@
 
 #include "test.h"
 
+#ifdef HAVE_SYS_SOCKET_H
+#  include <sys/socket.h>
+#endif
+#ifdef HAVE_NETINET_IN_H
+#  include <netinet/in.h>
+#endif
+#ifdef HAVE_NETDB_H
+#  include <netdb.h>
+#endif
+#ifdef HAVE_ARPA_INET_H
+#  include <arpa/inet.h>
+#endif
 #ifdef HAVE_SYS_STAT_H
-#include <sys/stat.h>
+#  include <sys/stat.h>
 #endif
 #ifdef HAVE_FCNTL_H
-#include <fcntl.h>
+#  include <fcntl.h>
 #endif
 
 #include <curl/mprintf.h>
