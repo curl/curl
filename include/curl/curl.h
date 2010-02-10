99f366adf92e940ea8178da35c4189f5206bd716
Dan Fandrich <dan@coneharvesters.com>
2010-02-10 21:17:53 +0000

    Mention the minimum size of CURL_MAX_WRITE_SIZE

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -177,7 +177,9 @@ typedef int (*curl_progress_callback)(void *clientp,
   /* Tests have proven that 20K is a very bad buffer size for uploads on
      Windows, while 16K for some odd reason performed a lot better.
      We do the ifndef check to allow this value to easier be changed at build
-     time for those who feel adventurous. */
+     time for those who feel adventurous. The practical minimum is about
+     400 bytes since libcurl uses a buffer of this size as a scratch area
+     (unrelated to network send operations). */
 #define CURL_MAX_WRITE_SIZE 16384
 #endif
 
