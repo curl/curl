b7848e4a7272e5319cf5794ec59ecaa955642e1c
Daniel Stenberg <daniel@haxx.se>
2010-04-17 18:57:39 +0200

    parsedate: Value stored to 'found' is never read

--- lib/parsedate.c
+++ lib/parsedate.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2010, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -370,7 +370,6 @@ int Curl_parsedate(const char *date, time_t *output)
          (3 == sscanf(date, "%02d:%02d:%02d", &hournum, &minnum, &secnum))) {
         /* time stamp! */
         date += 8;
-        found = TRUE;
       }
       else {
         val = (int)strtol(date, &end, 10);
