d296dd4ee87426bc6982255d7a966fd8c4b7e989
Daniel Stenberg <daniel@haxx.se>
2010-07-30 22:32:57 +0200

    sethostname: avoid including unistd.h to duck for warnings

--- tests/libtest/sethostname.c
+++ tests/libtest/sethostname.c
@@ -9,14 +9,18 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #define GETHOSTNAME_ENV_VAR "CURL_GETHOSTNAME"
 
 /*
  * we force our own host name, in order to make some tests machine independent
+ *
+ * Since some systems think this prototype doesn't match the system provided
+ * function, we AVOID including unistd.h or other headers that may include the
+ * original prototype!
  */
-int gethostname(char *name, size_t namelen) {
+int gethostname(char *name, size_t namelen)
+{
   const char *force_hostname = getenv(GETHOSTNAME_ENV_VAR);
   if(force_hostname) {
     strncpy(name, force_hostname, namelen);
