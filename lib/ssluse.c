curl/curl

commit 496002ea1cd76af7fff27166e8a310ed64a434ba

- Wesley Miaw reported bug #2958179 which identified a case of looping during   OpenSSL based SSL handshaking even though the multi interface was used and   there was no good reason for it.
  http://curl.haxx.se/bug/view.cgi?id=2958179

lib/ssluse.c
@@ -2423,7 +2423,7 @@ ossl_connect_common(struct connectdata *conn,
     }
 
     retcode = ossl_connect_step2(conn, sockindex);
-    if(retcode)
+    if(retcode || (data->state.used_interface == Curl_if_multi))
       return retcode;
 
   } /* repeat step2 until all transactions are done. */

RELEASE-NOTES
@@ -21,6 +21,7 @@ This release includes the following bugfixes:
  o SMTP: use angle brackets in RCPT TO
  o curl --trace-time not using local time
  o off-by-one in the chunked encoding trailer parser
+ o superfluous blocking for OpenSSL-based SSH connects and multi interface
 
 This release includes the following known bugs:
 
@@ -30,6 +31,6 @@ This release would not have looked like this without help, code, reports and
 advice from friends like these:
 
  Steven M. Schweda, Yang Tse, Jack Zhang, Tom Donovan, Martin Hager,
- Daniel Fandrich, Patrick Monnerat, Pat Ray
+ Daniel Fandrich, Patrick Monnerat, Pat Ray, Wesley Miaw
 
         Thanks! (and sorry if I forgot to mention someone)

commit 811fcccfc97ee6545f1b9d28ac0dd9ea93a81ab8

multi: fix FTPS connecting the data connection with OpenSSL
Commit 496002ea1cd76af7f (released in 7.20.1) broke FTPS when using the
multi interface and OpenSSL was used. The condition for the non-blocking
connect was incorrect.

Reported by: Georg Lippitsch
Bug: http://curl.haxx.se/mail/lib-2010-07/0270.html

lib/ssluse.c
@@ -2430,15 +2430,14 @@ ossl_connect_common(struct connectdata *conn,
       /* socket is readable or writable */
     }
 
-    /* Run transaction, and return to the caller if it failed or if
-     * this connection is part of a multi handle and this loop would
-     * execute again. This permits the owner of a multi handle to
-     * abort a connection attempt before step2 has completed while
-     * ensuring that a client using select() or epoll() will always
-     * have a valid fdset to wait on.
+    /* Run transaction, and return to the caller if it failed or if this
+     * connection is done nonblocking and this loop would execute again. This
+     * permits the owner of a multi handle to abort a connection attempt
+     * before step2 has completed while ensuring that a client using select()
+     * or epoll() will always have a valid fdset to wait on.
      */
     retcode = ossl_connect_step2(conn, sockindex);
-    if(retcode || (data->state.used_interface == Curl_if_multi &&
+    if(retcode || (nonblocking &&
                    (ssl_connect_2 == connssl->connecting_state ||
                     ssl_connect_2_reading == connssl->connecting_state ||
                     ssl_connect_2_writing == connssl->connecting_state)))