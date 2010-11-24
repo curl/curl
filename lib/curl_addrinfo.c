1d75d308169fe1c4ffd55c04a1a947530115b8cc
Yang Tse <yangsita@gmail.com>
2010-11-24 04:36:57 +0100

    Curl_getaddrinfo_ex: sanitize function results follow-up.

--- lib/curl_addrinfo.c
+++ lib/curl_addrinfo.c
@@ -208,6 +208,18 @@ Curl_getaddrinfo_ex(const char *nodename,
     Curl_freeaddrinfo(cafirst);
     cafirst = NULL;
   }
+  else if(!cafirst) {
+#ifdef EAI_NONAME
+    /* rfc3493 conformant */
+    error = EAI_NONAME;
+#else
+    /* rfc3493 obsoleted */
+    error = EAI_NODATA;
+#endif
+#ifdef USE_WINSOCK
+    SET_SOCKERRNO(error);
+#endif
+  }
 
   *result = cafirst;
 
