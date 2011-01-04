703573c72b134d5389715882ac2057dfbab1d1b7
Daniel Stenberg <daniel@haxx.se>
2011-01-04 16:41:42 +0100

    curlcheck.h: avoid using NULL pointers

--- tests/unit/curlcheck.h
+++ tests/unit/curlcheck.h
@@ -24,7 +24,7 @@
   }
 
 #define verify_memory(dynamic, check, len)                              \
-  if(memcmp(dynamic, check, len)) {                                     \
+  if(dynamic && memcmp(dynamic, check, len)) {                          \
     fprintf(stderr, "%s:%d The dynamic string didn't match '%s'\n",     \
             __FILE__, __LINE__, check);                                 \
     unitfail++;                                                         \
