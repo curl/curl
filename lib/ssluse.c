0e9626ba39756b8f063e42ca035714ee65d160ee
Bjoern Sikora <bjoern.sikora@intra2net.com>
2011-02-02 18:25:57 +0100

    ssluse: improved error message on SSL_CTX_new failures
    "SSL: couldn't create a context" really isn't that helpful, now it'll
    also extract an explanation from OpenSSL and append to the right.

--- lib/ssluse.c
+++ lib/ssluse.c
@@ -1463,7 +1463,8 @@ ossl_connect_step1(struct connectdata *conn,
   connssl->ctx = SSL_CTX_new(req_method);
 
   if(!connssl->ctx) {
-    failf(data, "SSL: couldn't create a context!");
+    failf(data, "SSL: couldn't create a context: %s",
+          ERR_error_string(ERR_peek_last_error(), NULL));
     return CURLE_OUT_OF_MEMORY;
   }
 
