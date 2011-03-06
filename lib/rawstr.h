c9ee341bfb316b3076d372dfd5ce05b73b479c7f
Daniel Stenberg <daniel@haxx.se>
2011-03-06 23:00:28 +0100

    cpp: correct #endif placement
    The end-of-file #endif in rawstr.h was not correcly positioned after all
    prototypes.
    
    Reported by: Boris
    Bug: http://curl.haxx.se/bug/view.cgi?id=3195205

--- lib/rawstr.h
+++ lib/rawstr.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2011, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -40,5 +40,5 @@ char Curl_raw_toupper(char in);
    argument is zero-byte terminated */
 #define checkprefix(a,b)    Curl_raw_nequal(a,b,strlen(a))
 
-#endif
 void Curl_strntoupper(char *dest, const char *src, size_t n);
+#endif
