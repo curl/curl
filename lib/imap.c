60406ff7f87c950be8eee96760f9358547594bc6
Daniel Stenberg <daniel@haxx.se>
2011-03-13 14:19:16 +0100

    state: add missing state to debug table
    As a new state recently was added to the IMAP state machine it has to be
    in the array of names as well as otherwise libcurl crashes when a debug
    version runs...

--- lib/imap.c
+++ lib/imap.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2010, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2011, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -294,6 +294,7 @@ static void state(struct connectdata *conn,
     "SERVERGREET",
     "LOGIN",
     "STARTTLS",
+    "UPGRADETLS",
     "SELECT",
     "FETCH",
     "LOGOUT",
