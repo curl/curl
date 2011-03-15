e1146489913e60028e47c89f52f56457f56da54b
Daniel Stenberg <daniel@haxx.se>
2011-03-16 00:19:26 +0100

    scan-build warning
    Value stored to 'len' is never read

--- lib/socks_gssapi.c
+++ lib/socks_gssapi.c
@@ -97,10 +97,8 @@ static int check_gss_err(struct SessionHandle *data,
                                     GSS_C_NULL_OID,
                                     &msg_ctx, &status_string);
       if(maj_stat == GSS_S_COMPLETE) {
-        if(sizeof(buf) > len + status_string.length) {
+        if(sizeof(buf) > len + status_string.length)
           strcpy(buf+len, (char*) status_string.value);
-          len += status_string.length;
-        }
         gss_release_buffer(&min_stat, &status_string);
         break;
       }
