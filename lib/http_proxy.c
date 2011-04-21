17df5d8caa1009c5ca168fc9643dc9d77c219832
Dan Fandrich <dan@coneharvesters.com>
2011-04-21 14:47:07 -0700

    Include unistd.h to declare close()

--- lib/http_proxy.c
+++ lib/http_proxy.c
@@ -24,6 +24,9 @@
 
 #if !defined(CURL_DISABLE_PROXY) && !defined(CURL_DISABLE_HTTP)
 
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 #include "urldata.h"
 #include <curl/curl.h>
 #include "http_proxy.h"
