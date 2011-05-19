5db30a1d8c1250a787a65285499b949b92b0325f
Yang Tse <yangsita@gmail.com>
2011-05-19 11:48:32 +0200

    version: linkage fix
    Fix linkage on c-ares enabled Windows static builds

--- lib/version.c
+++ lib/version.c
@@ -33,7 +33,11 @@
 #include <curl/mprintf.h>
 
 #ifdef USE_ARES
-#include <ares.h>
+#  if defined(CURL_STATICLIB) && !defined(CARES_STATICLIB) && \
+     (defined(WIN32) || defined(_WIN32) || defined(__SYMBIAN32__))
+#    define CARES_STATICLIB
+#  endif
+#  include <ares.h>
 #endif
 
 #ifdef USE_LIBIDN
