a689072f33ae5a3a59586fb419b2e3d6a74f5482
Yang Tse <yangsita@gmail.com>
2011-06-01 12:13:42 +0200

    warnless: icc 9.1 workaround

--- lib/warnless.c
+++ lib/warnless.c
@@ -296,18 +296,26 @@ void curlx_FD_ZERO(fd_set *fdset)
 
 unsigned short curlx_htons(unsigned short usnum)
 {
+#if (__INTEL_COMPILER == 910) && defined(__i386__)
+  return (unsigned short)(((usnum << 8) & 0xFF00) | ((usnum >> 8) & 0x00FF));
+#else
   #pragma warning(push)
   #pragma warning(disable:810) /* conversion may lose significant bits */
   return htons(usnum);
   #pragma warning(pop)
+#endif
 }
 
 unsigned short curlx_ntohs(unsigned short usnum)
 {
+#if (__INTEL_COMPILER == 910) && defined(__i386__)
+  return (unsigned short)(((usnum << 8) & 0xFF00) | ((usnum >> 8) & 0x00FF));
+#else
   #pragma warning(push)
   #pragma warning(disable:810) /* conversion may lose significant bits */
   return ntohs(usnum);
   #pragma warning(pop)
+#endif
 }
 
 #endif /* __INTEL_COMPILER && __unix__ */
