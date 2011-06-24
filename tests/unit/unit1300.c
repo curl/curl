78480892cdbbc67f419d0acfdf1865043bc31f62
Dan Fandrich <dan@coneharvesters.com>
2011-06-24 12:32:38 -0700

    Fixed test 1300 to pass the memory torture test

--- tests/unit/unit1300.c
+++ tests/unit/unit1300.c
@@ -40,8 +40,10 @@ static CURLcode unit_setup(void)
   if(!llist)
     return CURLE_OUT_OF_MEMORY;
   llist_destination = Curl_llist_alloc(test_curl_llist_dtor);
-  if(!llist_destination)
+  if(!llist_destination) {
+      Curl_llist_destroy(llist, NULL);
       return CURLE_OUT_OF_MEMORY;
+  }
 
   return CURLE_OK;
 }
