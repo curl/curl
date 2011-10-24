134e87c53ba9a20e8b283ae984fc00e2d932b262
Yang Tse <yangsita@gmail.com>
2011-10-24 20:40:25 +0200

    ftpserver.pl: ensure integral number usage for passive mode string

--- tests/ftpserver.pl
+++ tests/ftpserver.pl
@@ -6,7 +6,7 @@
 #                            | (__| |_| |  _ <| |___
 #                             \___|\___/|_| \_\_____|
 #
-# Copyright (C) 1998 - 2010, Daniel Stenberg, <daniel@haxx.se>, et al.
+# Copyright (C) 1998 - 2011, Daniel Stenberg, <daniel@haxx.se>, et al.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution. The terms
@@ -1111,7 +1111,7 @@ sub PASV_ftp {
             $p="1,2,3,4";
         }
         sendcontrol sprintf("227 Entering Passive Mode ($p,%d,%d)\n",
-                            ($pasvport/256), ($pasvport%256));
+                            int($pasvport/256), int($pasvport%256));
     }
     else {
         # EPSV reply
