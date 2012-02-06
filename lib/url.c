ea055407fa45c4247042e7370d463364f03c3136
Daniel Stenberg <daniel@haxx.se>
2012-02-06 22:59:49 +0100

    curl_easy_reset: reset the referer string
    When CURLOPT_REFERER has been used, curl_easy_reset() did not properly
    clear it.
    
    Verified with the new test 598
    
    Bug: http://curl.haxx.se/bug/view.cgi?id=3481551
    Reported by: Michael Day

--- lib/url.c
+++ lib/url.c
@@ -272,6 +272,12 @@ void Curl_freeset(struct SessionHandle * data)
   enum dupstring i;
   for(i=(enum dupstring)0; i < STRING_LAST; i++)
     Curl_safefree(data->set.str[i]);
+
+  if(data->change.referer_alloc) {
+    Curl_safefree(data->change.referer);
+    data->change.referer_alloc = FALSE;
+  }
+  data->change.referer = NULL;
 }
 
 static CURLcode setstropt(char **charp, char * s)
