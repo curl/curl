118e73306d142a3146356ebf24e2446a65e9fe6f
Daniel Stenberg <daniel@haxx.se>
2012-04-06 14:44:36 +0200

    PolarSSL: include version number in version string
    Previously it would say PolarSSL only, now it says PolarSSL/1.1.0 in the
    same style other libs and components do.

--- lib/polarssl.c
+++ lib/polarssl.c
@@ -432,7 +432,9 @@ void Curl_polarssl_session_free(void *ptr)
 
 size_t Curl_polarssl_version(char *buffer, size_t size)
 {
-  return snprintf(buffer, size, "PolarSSL");
+  unsigned int version = version_get_number();
+  return snprintf(buffer, size, "PolarSSL/%d.%d.%d", version>>24,
+                  (version>>16)&0xff, (version>>8)&0xff);
 }
 
 static CURLcode
