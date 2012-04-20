560cd6227271752536de237e41c374d0a3e11f1d
Daniel Stenberg <daniel@haxx.se>
2012-04-20 20:40:55 +0200

    HTTP: empty chunked POST ended up in two zero size chunks
    When doing a chunked-encoded POST with -d (CURLOPT_POSTFIELDS) and the
    size of the POST was zero length, it made libcurl first send a zero
    chunk and then the terminating one. This could confuse a receiver and it
    should rather just send the terminating chunk as it does with this fix.
    
    Test case 1333 is added to verify.
    
    Bug: http://curl.haxx.se/mail/archive-2012-04/0060.html
    Reported by: Arnaud Compan

--- lib/http.c
+++ lib/http.c
@@ -1088,7 +1088,7 @@ CURLcode Curl_add_buffer_send(Curl_send_buffer *in,
     if(conn->data->set.verbose) {
       /* this data _may_ contain binary stuff */
       Curl_debug(conn->data, CURLINFO_HEADER_OUT, ptr, headlen, conn);
-      if((size_t)amount > headlen) {
+      if(bodylen) {
         /* there was body data sent beyond the initial header part, pass that
            on to the debug callback too */
         Curl_debug(conn->data, CURLINFO_DATA_OUT,
@@ -2480,11 +2480,13 @@ CURLcode Curl_http(struct connectdata *conn, bool *done)
           included_body = postsize;
         }
         else {
-          /* Append the POST data chunky-style */
-          result = Curl_add_bufferf(req_buffer, "%x\r\n", (int)postsize);
-          if(CURLE_OK == result)
-            result = Curl_add_buffer(req_buffer, data->set.postfields,
-                                     (size_t)postsize);
+          if(postsize) {
+            /* Append the POST data chunky-style */
+            result = Curl_add_bufferf(req_buffer, "%x\r\n", (int)postsize);
+            if(CURLE_OK == result)
+              result = Curl_add_buffer(req_buffer, data->set.postfields,
+                                       (size_t)postsize);
+          }
           if(CURLE_OK == result)
             result = Curl_add_buffer(req_buffer,
                                      "\x0d\x0a\x30\x0d\x0a\x0d\x0a", 7);
