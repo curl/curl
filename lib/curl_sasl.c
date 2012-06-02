cfa81b8fb00928a88ed4b76807f564d3a895a493
Steve Holme <steve_holme@hotmail.com>
2012-06-02 14:03:55 +0100

    sasl: Corrected variable names in comments and parameters

--- lib/curl_sasl.c
+++ lib/curl_sasl.c
@@ -88,7 +88,7 @@ CURLcode Curl_sasl_create_plain_message(struct SessionHandle *data,
  * Parameters:
  *
  * data    [in]     - The session handle.
- * userp   [in]     - The user name.
+ * valuep  [in]     - The user name or user's password.
  * outptr  [in/out] - The address where a pointer to newly allocated memory
  *                    holding the result will be stored upon completion.
  * outlen  [out]    - The length of the output message.
