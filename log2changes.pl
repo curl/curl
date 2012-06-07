d2fc8b4a5f53ce638cacae56375b120c0a687978
Daniel Stenberg <daniel@haxx.se>
2012-06-07 23:49:14 +0200

    log2changes.pl: fix the Version output
    Previously it could easily wrongly get repeated

--- log2changes.pl
+++ log2changes.pl
@@ -35,8 +35,6 @@ while(<STDIN>) {
         if ($ref =~ /refs\/tags\/curl-([0-9_]*)/) {
             $tag = $1;
             $tag =~ tr/_/./;
-        } else {
-            $tag = '';
         }
     }
     elsif($l =~ /^Author: *(.*) +</) {
@@ -54,6 +52,7 @@ while(<STDIN>) {
             # Version entries have a special format
             print "\nVersion " . $tag." ($date)\n";
             $oldc = "";
+            $tag = "";
         }
         if($a ne $c) {
             $extra=sprintf("\n- [%s brought this change]\n\n  ", $a);
