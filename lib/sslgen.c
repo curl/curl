68857e40d69ef792bfcc6d7395c65305a4117c51
Kamil Dudka <kdudka@redhat.com>
2012-06-08 23:02:57 +0200

    ssl: fix duplicated SSL handshake with multi interface and proxy
    Bug: https://bugzilla.redhat.com/788526
    Reported by: Enrico Scholz

--- lib/sslgen.c
+++ lib/sslgen.c
@@ -211,18 +211,18 @@ CURLcode
 Curl_ssl_connect_nonblocking(struct connectdata *conn, int sockindex,
                              bool *done)
 {
-#ifdef curlssl_connect_nonblocking
   CURLcode res;
   /* mark this is being ssl requested from here on. */
   conn->ssl[sockindex].use = TRUE;
+#ifdef curlssl_connect_nonblocking
   res = curlssl_connect_nonblocking(conn, sockindex, done);
+#else
+  *done = TRUE; /* fallback to BLOCKING */
+  res = curlssl_connect(conn, sockindex);
+#endif /* non-blocking connect support */
   if(!res && *done)
     Curl_pgrsTime(conn->data, TIMER_APPCONNECT); /* SSL is connected */
   return res;
-#else
-  *done = TRUE; /* fallback to BLOCKING */
-  return Curl_ssl_connect(conn, sockindex);
-#endif /* non-blocking connect support */
 }
 
 /*
