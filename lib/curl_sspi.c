c7cc91496176483c53cda82c5906625b46aaf213
Daniel Stenberg <daniel@haxx.se>
2012-06-11 19:06:43 +0200

    cleanup: remove trailing whitespace

--- lib/curl_sspi.c
+++ lib/curl_sspi.c
@@ -158,7 +158,7 @@ CURLcode Curl_sspi_version(int *major, int *minor, int *build, int *special)
   if(!result) {
     if(major)
       *major = (version_info->dwProductVersionMS >> 16) & 0xffff;
-  
+
     if(minor)
       *minor = (version_info->dwProductVersionMS >> 0) & 0xffff;
 
