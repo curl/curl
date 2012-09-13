d2d732f117b41d708bc1ae312a428786570ae1fb
Sergei Nikulov <sergey.nikulov@gmail.com>
2012-09-13 16:08:21 +0400

    use standard cmake findxxx modules for cmake v2.8+

--- CMake/FindOpenSSL.cmake
+++ CMake/FindOpenSSL.cmake
@@ -2,17 +2,19 @@
 # Adds OPENSSL_INCLUDE_DIRS and libeay32
 include("${CMAKE_ROOT}/Modules/FindOpenSSL.cmake")
 
-# Bill Hoffman told that libeay32 is necessary for him:
-find_library(SSL_LIBEAY NAMES libeay32)
+# starting 2.8 it is better to use standard modules
+if(CMAKE_MAJOR_VERSION EQUAL "2" AND CMAKE_MINOR_VERSION LESS "8")
+  # Bill Hoffman told that libeay32 is necessary for him:
+  find_library(SSL_LIBEAY NAMES libeay32)
 
-if(OPENSSL_FOUND)
-  if(SSL_LIBEAY)
-    list(APPEND OPENSSL_LIBRARIES ${SSL_LIBEAY})
-  else()
-    set(OPENSSL_FOUND FALSE)
+  if(OPENSSL_FOUND)
+    if(SSL_LIBEAY)
+      list(APPEND OPENSSL_LIBRARIES ${SSL_LIBEAY})
+    else()
+      set(OPENSSL_FOUND FALSE)
+    endif()
   endif()
-endif()
-
+endif() # if (CMAKE_MAJOR_VERSION EQUAL "2" AND CMAKE_MINOR_VERSION LESS "8")
 
 if(OPENSSL_FOUND)
   set(OPENSSL_INCLUDE_DIRS ${OPENSSL_INCLUDE_DIR})
