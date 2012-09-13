c1400f6f12f22d7c9a57b006f994e233b96ac627
Sergei Nikulov <sergey.nikulov@gmail.com>
2012-09-13 16:08:21 +0400

    cmake: use standard findxxx modules for cmake v2.8+

--- CMake/FindZLIB.cmake
+++ CMake/FindZLIB.cmake
@@ -1,8 +1,10 @@
 # Locate zlib
 include("${CMAKE_ROOT}/Modules/FindZLIB.cmake")
 
-find_library(ZLIB_LIBRARY_DEBUG NAMES zd zlibd zdlld zlib1d )
-
-if(ZLIB_FOUND AND ZLIB_LIBRARY_DEBUG)
-  set( ZLIB_LIBRARIES optimized "${ZLIB_LIBRARY}" debug ${ZLIB_LIBRARY_DEBUG})
+# starting 2.8 it is better to use standard modules
+if(CMAKE_MAJOR_VERSION EQUAL "2" AND CMAKE_MINOR_VERSION LESS "8")
+  find_library(ZLIB_LIBRARY_DEBUG NAMES zd zlibd zdlld zlib1d )
+  if(ZLIB_FOUND AND ZLIB_LIBRARY_DEBUG)
+    set( ZLIB_LIBRARIES optimized "${ZLIB_LIBRARY}" debug ${ZLIB_LIBRARY_DEBUG})
+  endif()
 endif()
