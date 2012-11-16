32afaaef9342c5d599db295fe06c06f0678ed75d
Fabian Keil <fk@fabiankeil.de>
2012-11-16 19:22:12 +0100

    Fix the libauthretry changes from 7c0cbcf2f61
    They broke the NTLM tests from 2023 to 2031.

--- tests/libtest/libauthretry.c
+++ tests/libtest/libauthretry.c
@@ -72,11 +72,11 @@ static long parse_auth_name(const char *arg)
 {
   if (!arg)
     return CURLAUTH_NONE;
-  if (strequal(arg, "basic") == 0)
+  if (strequal(arg, "basic"))
     return CURLAUTH_BASIC;
-  if (strequal(arg, "digest") == 0)
+  if (strequal(arg, "digest"))
     return CURLAUTH_DIGEST;
-  if (strequal(arg, "ntlm") == 0)
+  if (strequal(arg, "ntlm"))
     return CURLAUTH_NTLM;
   return CURLAUTH_NONE;
 }
