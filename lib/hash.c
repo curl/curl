14b77db1b97bb382ec97535666813dea26594bd0
Yang Tse <yangsita@gmail.com>
2012-12-25 13:30:41 +0100

    Curl_hash_clean: OOM handling fix

--- lib/hash.c
+++ lib/hash.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2011, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2012, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -249,8 +249,7 @@ Curl_hash_clean(struct curl_hash *h)
     h->table[i] = NULL;
   }
 
-  free(h->table);
-  h->table = NULL;
+  Curl_safefree(h->table);
   h->size = 0;
   h->slots = 0;
 }
