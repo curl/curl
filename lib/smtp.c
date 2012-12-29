36837c10b275829d70a5e3d6d1f3dd8b9a68f43d
Steve Holme <steve_holme@hotmail.com>
2012-12-29 23:06:25 +0000

    smtp_doing: don't call smtp_dophase_done() if already failed
    Applied the POP3 fix from commit 2897ce7dc2e1 so smtp_dophase_done()
    isn't called if smtp_multi_statemach() fails.

--- lib/smtp.c
+++ lib/smtp.c
@@ -1591,11 +1591,13 @@ static CURLcode smtp_doing(struct connectdata *conn, bool *dophase_done)
 
   if(result)
     DEBUGF(infof(conn->data, "DO phase failed\n"));
-  else
-    DEBUGF(infof(conn->data, "DO phase is complete\n"));
+  else {
+    if(*dophase_done) {
+      result = smtp_dophase_done(conn, FALSE /* not connected */);
 
-  if(*dophase_done)
-    smtp_dophase_done(conn, FALSE /* not connected */);
+      DEBUGF(infof(conn->data, "DO phase is complete\n"));
+    }
+  }
 
   return result;
 }
