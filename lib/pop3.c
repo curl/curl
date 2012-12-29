c02449ca532b67dfc0b48217538f788213b9d8c1
Steve Holme <steve_holme@hotmail.com>
2012-12-29 23:26:17 +0000

    pop3_doing: Applied debug info message when function fails
    Applied the same debug message as used in smtp_doing() and imap_doing()
    when pop3_multi_statemach() fails.

--- lib/pop3.c
+++ lib/pop3.c
@@ -1550,13 +1550,16 @@ static CURLcode pop3_dophase_done(struct connectdata *conn, bool connected)
 /* Called from multi.c while DOing */
 static CURLcode pop3_doing(struct connectdata *conn, bool *dophase_done)
 {
-  CURLcode result;
-  result = pop3_multi_statemach(conn, dophase_done);
+  CURLcode result = pop3_multi_statemach(conn, dophase_done);
 
-  if(!result && *dophase_done) {
-    result = pop3_dophase_done(conn, FALSE /* not connected */);
+  if(result)
+    DEBUGF(infof(conn->data, "DO phase failed\n"));
+  else {
+    if(*dophase_done) {
+      result = pop3_dophase_done(conn, FALSE /* not connected */);
 
-    DEBUGF(infof(conn->data, "DO phase is complete\n"));
+      DEBUGF(infof(conn->data, "DO phase is complete\n"));
+    }
   }
 
   return result;
