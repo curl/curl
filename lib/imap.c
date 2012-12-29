1576548428326f210fd067b8d4323828f979091f
Steve Holme <steve_holme@hotmail.com>
2012-12-29 23:15:05 +0000

    imap_doing: don't call imap_dophase_done() if already failed
    Applied the POP3 fix from commit 2897ce7dc2e1 so imap_dophase_done()
    isn't called if imap_multi_statemach() fails.

--- lib/imap.c
+++ lib/imap.c
@@ -1049,11 +1049,12 @@ static CURLcode imap_doing(struct connectdata *conn, bool *dophase_done)
 
   if(result)
     DEBUGF(infof(conn->data, "DO phase failed\n"));
+  else {
+    if(*dophase_done) {
+      result = imap_dophase_done(conn, FALSE /* not connected */);
 
-  if(*dophase_done) {
-    result = imap_dophase_done(conn, FALSE /* not connected */);
-
-    DEBUGF(infof(conn->data, "DO phase is complete\n"));
+      DEBUGF(infof(conn->data, "DO phase is complete\n"));
+    }
   }
 
   return result;
