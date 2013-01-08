e2bcd2ab9e5ad894a57756fa660d2a07b69171f4
Daniel Stenberg <daniel@haxx.se>
2013-01-08 23:10:26 +0100

    curl: ignore SIGPIPE
    This is a work-around for bug #1180 which is really libcurl's inability
    to ignore SIGPIPE in a few cases. With this work-around at least curl
    won't suffer from it!
    
    Bug: http://curl.haxx.se/bug/view.cgi?id=1180
    Reported by: Lluís Batlle i Rossell

--- src/tool_main.c
+++ src/tool_main.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2012, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2013, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -22,6 +22,10 @@
 #include "tool_setup.h"
 
 #include <sys/stat.h>
+
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
 
 #define ENABLE_CURLX_PRINTF
 /* use our own printf() functions */
@@ -82,6 +86,10 @@ int main(int argc, char *argv[])
   config.errors = stderr; /* default errors to stderr */
 
   main_checkfds();
+
+#ifdef HAVE_SIGNAL
+  signal(SIGPIPE, SIG_IGN);
+#endif
 
   res = operate(&config, argc, argv);
 
