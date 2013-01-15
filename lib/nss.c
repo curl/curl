26613d781725e39b0f601301a65c64e146977d8f
Kamil Dudka <kdudka@redhat.com>
2013-01-15 12:58:08 +0100

    nss: fix error messages for CURLE_SSL_{CACERT,CRL}_BADFILE
    Do not use the error messages from NSS for errors not occurring in NSS.

--- lib/nss.c
+++ lib/nss.c
@@ -1093,10 +1093,8 @@ static bool is_nss_error(CURLcode err)
   switch(err) {
   case CURLE_PEER_FAILED_VERIFICATION:
   case CURLE_SSL_CACERT:
-  case CURLE_SSL_CACERT_BADFILE:
   case CURLE_SSL_CERTPROBLEM:
   case CURLE_SSL_CONNECT_ERROR:
-  case CURLE_SSL_CRL_BADFILE:
   case CURLE_SSL_ISSUER_ERROR:
     return true;
 
