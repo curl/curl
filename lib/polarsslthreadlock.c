5e2e3cb54eff2a597bc7dc195be44d7bb7493562
Daniel Stenberg <daniel@haxx.se>
2013-02-16 13:56:08 +0100

    polarsslthreadlock: #include the proper memory and debug includes
    Pointed out by Steve Holme

--- lib/polarsslthreadlock.c
+++ lib/polarsslthreadlock.c
@@ -41,6 +41,10 @@
 
 #include "polarsslthreadlock.h"
 
+#include "curl_memory.h"
+/* The last #include file should be: */
+#include "memdebug.h"
+
 /* number of thread locks */
 #define NUMT                    2
 
