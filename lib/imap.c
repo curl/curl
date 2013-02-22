curl/curl

commit a1701eea289fe7ea80651f801cf992838a491dde

imap: Added support for the STARTTLS capability (Part One)
Introduced detection of the STARTTLS capability, in order to add support
for TLS upgrades without unconditionally sending the STARTTLS command.

lib/imap.c
@@ -371,8 +371,12 @@ static int imap_endofresp(struct pingpong *pp, int *resp)
               line[wordlen] != '\n';)
           wordlen++;
 
+        /* Does the server support the STARTTLS capability? */
+        if(len >= 8 && !memcmp(line, "STARTTLS", 8))
+          imapc->tls_supported = TRUE;
+
         /* Has the server explicitly disabled clear text authentication? */
-        if(wordlen == 13 && !memcmp(line, "LOGINDISABLED", 13))
+        else if(wordlen == 13 && !memcmp(line, "LOGINDISABLED", 13))
           imapc->login_disabled = TRUE;
 
         /* Does the server support the SASL-IR capability? */

lib/imap.h
@@ -62,6 +62,7 @@ struct imap_conn {
   int cmdid;              /* Last used command ID */
   char resptag[5];        /* Response tag to wait for */
   bool ssldone;           /* Is connect() over SSL done? */
+  bool tls_supported;     /* StartTLS capability supported by server */
   bool login_disabled;    /* LOGIN command explicitly disabled by server */
   bool ir_supported;      /* Initial response supported by server */
 };

commit 1dac29fa83a9ed1285faa7e06d42c7b4ffd98a62

imap.c: Fixed an incorrect variable reference
Fixed an incorrect variable reference which was introduced in commit
a1701eea289f as a result of a copy and paste from SMTP/POP3.

lib/imap.c
@@ -378,7 +378,7 @@ static bool imap_endofresp(struct connectdata *conn, char *line, size_t len,
           wordlen++;
 
         /* Does the server support the STARTTLS capability? */
-        if(len >= 8 && !memcmp(line, "STARTTLS", 8))
+        if(wordlen >= 8 && !memcmp(line, "STARTTLS", 8))
           imapc->tls_supported = TRUE;
 
         /* Has the server explicitly disabled clear text authentication? */