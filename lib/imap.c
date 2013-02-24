c38d69f06a175ca6a92c3792d18630529a9357c2
Jiri Hruska <jirka@fud.cz>
2013-02-24 12:23:06 +0100

    imap: Simplified command response test in imap_endofresp()

--- lib/imap.c
+++ lib/imap.c
@@ -335,12 +335,10 @@ static bool imap_endofresp(struct connectdata *conn, char *line, size_t len,
   size_t wordlen;
 
   /* Do we have a tagged command response? */
-  if(len >= id_len + 3) {
-    if(!memcmp(id, line, id_len) && line[id_len] == ' ') {
-      *resp = line[id_len + 1]; /* O, N or B */
+  if(len >= id_len + 3 && !memcmp(id, line, id_len) && line[id_len] == ' ') {
+    *resp = line[id_len + 1]; /* O, N or B */
 
-      return TRUE;
-    }
+    return TRUE;
   }
 
   /* Do we have a continuation response? */
