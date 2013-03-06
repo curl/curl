curl/curl

commit 198012ee1376707718084e57e30771824edf2847

imap: Added support for list command

lib/imap.c
@@ -1837,7 +1837,8 @@ static CURLcode imap_done(struct connectdata *conn, CURLcode status,
     conn->bits.close = TRUE; /* marked for closure */
     result = status;         /* use the already set error code */
   }
-  else if(!data->set.connect_only && !imap->custom) {
+  else if(!data->set.connect_only && !imap->custom &&
+          (imap->uid || data->set.upload)) {
     /* Handle responses after FETCH or APPEND transfer has finished */
     if(!data->set.upload)
       state(conn, IMAP_FETCH_FINAL);
@@ -1914,12 +1915,15 @@ static CURLcode imap_perform(struct connectdata *conn, bool *connected,
   else if(imap->custom && (selected || !imap->mailbox))
     /* Custom command using the same mailbox or no mailbox */
     result = imap_custom(conn);
-  else if(!imap->custom && selected)
+  else if(!imap->custom && selected && imap->uid)
     /* FETCH from the same mailbox */
     result = imap_fetch(conn);
-  else
+  else if(imap->uid)
     /* SELECT the mailbox */
     result = imap_select(conn);
+  else
+    /* LIST */
+    result = imap_list(conn);
 
   if(result)
     return result;

commit 81408d21110d17e072beb7da7b31783ce03ba9f3

imap: Fixed ftpserver.pl to allow verification even through LIST command
Commit 198012ee inadvertently broke LIST_imap().

tests/ftpserver.pl
@@ -940,20 +940,32 @@ sub STORE_imap {
 sub LIST_imap {
     my ($args) = @_;
     my ($reference, $mailbox) = split(/ /, $args, 2);
+    my @data;
 
     logmsg "LIST_imap got $args\n";
 
-    my $testno = $reference;
-    $testno =~ s/^([^0-9]*)//;
-    my $testpart = "";
-    if ($testno > 10000) {
-        $testpart = $testno % 10000;
-        $testno = int($testno / 10000);
+    if ($reference eq '"verifiedserver"') {
+         # this is the secret command that verifies that this actually is
+         # the curl test server
+         @data = ("* LIST () \"/\" \"WE ROOLZ: $$\"\r\n");
+         if($verbose) {
+             print STDERR "FTPD: We returned proof we are the test server\n";
+         }
+         logmsg "return proof we are we\n";
     }
+    else {
+        my $testno = $reference;
+        $testno =~ s/^([^0-9]*)//;
+        my $testpart = "";
+        if ($testno > 10000) {
+            $testpart = $testno % 10000;
+            $testno = int($testno / 10000);
+        }
     
-    loadtest("$srcdir/data/test$testno");
+        loadtest("$srcdir/data/test$testno");
 
-    my @data = getpart("reply", "data$testpart");
+        @data = getpart("reply", "data$testpart");
+    }
 
     for my $d (@data) {
         sendcontrol $d;