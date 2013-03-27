32bbe05f0028c063ab667c9e8149f66d41d950a5
Clemens Gruber <clemensgru@gmail.com>
2013-03-27 14:04:34 +0100

    Minor cleanup

--- docs/examples/multi-uv.c
+++ docs/examples/multi-uv.c
@@ -50,14 +50,13 @@ typedef struct curl_context_s {
 
 curl_context_t* create_curl_context(curl_socket_t sockfd)
 {
-  int r;
   curl_context_t *context;
 
   context = (curl_context_t *) malloc(sizeof *context);
 
   context->sockfd = sockfd;
 
-  r = uv_poll_init_socket(loop, &context->poll_handle, sockfd);
+  uv_poll_init_socket(loop, &context->poll_handle, sockfd);
   context->poll_handle.data = context;
 
   return context;
