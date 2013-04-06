ee7669ba3adeec052d999d9b415bb34caaa1e94b
Marc Hoersken <info@marc-hoersken.de>
2013-04-06 20:30:13 +0200

    if2ip.c: Fixed warning: unused parameter 'remote_scope'

--- lib/if2ip.c
+++ lib/if2ip.c
@@ -202,6 +202,7 @@ if2ip_result_t Curl_if2ip(int af, unsigned int remote_scope,
                           const char *interf, char *buf, int buf_size)
 {
     (void) af;
+    (void) remote_scope;
     (void) interf;
     (void) buf;
     (void) buf_size;
