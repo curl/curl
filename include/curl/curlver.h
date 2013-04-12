e0cff02061ce77ee046cb3a449c87352bc7eb6bd
Daniel Stenberg <daniel@haxx.se>
2013-04-12 11:30:32 +0200

    bump: start working towards next release

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -30,13 +30,13 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.30.0-DEV"
+#define LIBCURL_VERSION "7.30.1-DEV"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
 #define LIBCURL_VERSION_MAJOR 7
 #define LIBCURL_VERSION_MINOR 30
-#define LIBCURL_VERSION_PATCH 0
+#define LIBCURL_VERSION_PATCH 1
 
 /* This is the numeric version of the libcurl version number, meant for easier
    parsing and comparions by programs. The LIBCURL_VERSION_NUM define will
@@ -53,7 +53,7 @@
    and it is always a greater number in a more recent release. It makes
    comparisons with greater than and less than work.
 */
-#define LIBCURL_VERSION_NUM 0x071e00
+#define LIBCURL_VERSION_NUM 0x071e01
 
 /*
  * This is the date and time when the full source package was created. The
