b16b7f9d3a139cc7665208e4d19e4f90287f1e8a
Yang Tse <yangsita@gmail.com>
2013-07-11 13:29:48 +0200

    url.c: fix SIGSEGV

--- lib/url.c
+++ lib/url.c
@@ -3858,6 +3858,8 @@ static CURLcode parseurlandfillconn(struct SessionHandle *data,
   else {
     /* sanitise paths and remove ../ and ./ sequences according to RFC3986 */
     char *newp = Curl_dedotdotify(path);
+    if(!newp)
+      return CURLE_OUT_OF_MEMORY;
 
     if(strcmp(newp, path)) {
       rebuild_url = TRUE;
