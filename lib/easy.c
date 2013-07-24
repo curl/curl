50a74be12542b84a3edcebc2603cabb7ae2daa44
Yang Tse <yangsita@gmail.com>
2013-07-24 16:45:30 +0200

    easy.c: curl_easy_getinfo() fix va_start/va_end matching

--- lib/easy.c
+++ lib/easy.c
@@ -616,12 +616,16 @@ CURLcode curl_easy_getinfo(CURL *curl, CURLINFO info, ...)
 {
   va_list arg;
   void *paramp;
+  CURLcode ret;
   struct SessionHandle *data = (struct SessionHandle *)curl;
 
   va_start(arg, info);
   paramp = va_arg(arg, void *);
 
-  return Curl_getinfo(data, info, paramp);
+  ret = Curl_getinfo(data, info, paramp);
+
+  va_end(arg);
+  return ret;
 }
 
 /*
