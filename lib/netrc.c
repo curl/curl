78e6683bb0bfbe90450997c0f4c63a6a59ca1886
Daniel Stenberg <daniel@haxx.se>
2013-08-29 12:49:56 +0200

    Curl_parsenetrc: document that the arguments must be allocated

--- lib/netrc.c
+++ lib/netrc.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2012, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2013, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -50,6 +50,9 @@ enum host_lookup_state {
 
 /*
  * @unittest: 1304
+ *
+ * *loginp and *passwordp MUST be allocated if they aren't NULL when passed
+ * in.
  */
 int Curl_parsenetrc(const char *host,
                     char **loginp,
