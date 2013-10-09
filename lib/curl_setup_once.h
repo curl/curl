ca1b34b8871e8b8ce4a40589778c85118d4637c4
Gisle Vanem <gvanem@yahoo.no>
2013-10-09 14:45:42 +0200

    curl_setup_once: fix errno access for lwip on Windows
    lib/curl_setup_once.h assumed lwIP on Windows uses 'SetLastError()' to
    set network errors. It doesn't; it uses 'errno'.

--- lib/curl_setup_once.h
+++ lib/curl_setup_once.h
@@ -440,7 +440,7 @@ typedef int sig_atomic_t;
  * (or equivalent) on this platform to hide platform details to code using it.
  */
 
-#ifdef WIN32
+#if defined(WIN32) && !defined(USE_LWIPSOCK)
 #define ERRNO         ((int)GetLastError())
 #define SET_ERRNO(x)  (SetLastError((DWORD)(x)))
 #else
