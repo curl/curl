e7452415c571ae596dfdfa9233e5cc9d0a857fb4
Steve Holme <steve_holme@hotmail.com>
2013-10-16 00:57:01 +0100

    SSL: Corrected typo from commit 87861c9b0e8155

--- lib/ssluse.c
+++ lib/ssluse.c
@@ -1577,7 +1577,7 @@ ossl_connect_step1(struct connectdata *conn,
     ctx_options |= SSL_OP_NO_SSLv2;
     ctx_options |= SSL_OP_NO_SSLv3;
     ctx_options |= SSL_OP_NO_TLSv1;
-#if defined(SSL_OP_NO_TLSv1_1)
+#if defined(SSL_OP_NO_TLSv1_2)
     ctx_options |= SSL_OP_NO_TLSv1_2;
 #endif
     break;
