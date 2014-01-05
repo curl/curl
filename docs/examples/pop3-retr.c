afb65c6cf33cbd954586ae5225b7aa11858af312
Steve Holme <steve_holme@hotmail.com>
2014-01-05 12:20:26 +0000

    examples: Added POP3 RETR example

--- docs/examples/pop3-retr.c
+++ docs/examples/pop3-retr.c
@@ -23,8 +23,7 @@
 #include <curl/curl.h>
 
 /* This is a simple example showing how to retrieve mail using libcurl's POP3
- * capabilities. It builds on the pop3-retr.c example adding transport
- * security to protect the authentication details from being snooped.
+ * capabilities.
  *
  * Note that this example requires libcurl 7.20.0 or above.
  */
@@ -40,29 +39,8 @@ int main(void)
     curl_easy_setopt(curl, CURLOPT_USERNAME, "user");
     curl_easy_setopt(curl, CURLOPT_PASSWORD, "secret");
 
-    /* This will retreive message 1 from the user's mailbox. Note the use of
-     * pop3s:// rather than pop3:// to request a SSL based connection. */
-    curl_easy_setopt(curl, CURLOPT_URL, "pop3s://pop.example.com/1");
-
-    /* If you want to connect to a site who isn't using a certificate that is
-     * signed by one of the certs in the CA bundle you have, you can skip the
-     * verification of the server's certificate. This makes the connection
-     * A LOT LESS SECURE.
-     *
-     * If you have a CA cert for the server stored someplace else than in the
-     * default bundle, then the CURLOPT_CAPATH option might come handy for
-     * you. */
-#ifdef SKIP_PEER_VERIFICATION
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
-
-    /* If the site you're connecting to uses a different host name that what
-     * they have mentioned in their server certificate's commonName (or
-     * subjectAltName) fields, libcurl will refuse to connect. You can skip
-     * this check, but this will make the connection less secure. */
-#ifdef SKIP_HOSTNAME_VERFICATION
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
+    /* This will retreive message 1 from the user's mailbox */
+    curl_easy_setopt(curl, CURLOPT_URL, "pop3://pop.example.com/1");
 
     /* Perform the retr */
     res = curl_easy_perform(curl);
