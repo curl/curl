8451623b4557b03b349fa60f627ffa83951a854b
Marc Hoersken <info@marc-hoersken.de>
2014-02-14 22:17:54 +0100

    lib1515.c: Added support for Windows using the Sleep function

--- tests/libtest/lib1515.c
+++ tests/libtest/lib1515.c
@@ -35,6 +35,10 @@
 
 #define DNS_TIMEOUT 1
 
+#if defined(WIN32) || defined(_WIN32)
+#define sleep(s) Sleep(s * 1000)
+#endif
+
 static int debug_callback(CURL *curl, curl_infotype info, char *msg, size_t len, void *ptr)
 {
   (void)curl;
