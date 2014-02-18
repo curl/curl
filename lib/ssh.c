0d6225ad0d5d0d441d78604e0d95e89dfedb4062
Daniel Stenberg <daniel@haxx.se>
2014-02-18 11:21:13 +0100

    SFTP: skip reading the dir when NOBODY=1
    When asking for an SFTP directory with NOBODY set, no directory contents
    should be retrieved.
    
    Bug: http://curl.haxx.se/mail/lib-2014-02/0155.html

--- lib/ssh.c
+++ lib/ssh.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2013, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2014, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -1808,8 +1808,10 @@ static CURLcode ssh_statemach_act(struct connectdata *conn, bool *block)
 
     case SSH_SFTP_READDIR_INIT:
       Curl_pgrsSetDownloadSize(data, -1);
-      if(data->set.opt_no_body)
+      if(data->set.opt_no_body) {
         state(conn, SSH_STOP);
+        break;
+      }
 
       /*
        * This is a directory that we are trying to get, so produce a directory
