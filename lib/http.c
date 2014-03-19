47f8e99e78c3e415b32ffb692c9b150929432dc1
Gaël PORTAY <gael.portay@gmail.com>
2014-03-19 15:56:13 +0100

    polarssl: fix possible handshake timeout issue in multi.
    Because of the socket is unblocking, PolarSSL does need call to getsock to
    get the action to perform in multi environment.
    
    In some cases, it might happen we have not received yet all data to perform
    the handshake. ssh_handshake returns POLARSSL_ERR_NET_WANT_READ, the state
    is updated but because of the getsock has not the proper #define macro to,
    the library never prevents to select socket for input thus the socket will
    never be awaken when last data is available. Thus it leads to timeout.

--- lib/http.c
+++ lib/http.c
@@ -1361,8 +1361,8 @@ static CURLcode https_connecting(struct connectdata *conn, bool *done)
 #endif
 
 #if defined(USE_SSLEAY) || defined(USE_GNUTLS) || defined(USE_SCHANNEL) || \
-    defined(USE_DARWINSSL)
-/* This function is for OpenSSL, GnuTLS, darwinssl, and schannel only.
+    defined(USE_DARWINSSL) || defined(USE_POLARSSL)
+/* This function is for OpenSSL, GnuTLS, darwinssl, schannel and polarssl only.
    It should be made to query the generic SSL layer instead. */
 static int https_getsock(struct connectdata *conn,
                          curl_socket_t *socks,
