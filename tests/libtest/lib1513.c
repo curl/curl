713402982fa7cf8beb9a12a8739348f7da06c1a6
Daniel Stenberg <daniel@haxx.se>
2014-03-28 15:47:18 +0100

    lib1513: fix callback proto to silence warning

--- tests/libtest/lib1513.c
+++ tests/libtest/lib1513.c
@@ -30,9 +30,17 @@
 
 #include "memdebug.h"
 
-static int progressKiller(void *arg)
+static int progressKiller(void *arg,
+                          double dltotal,
+                          double dlnow,
+                          double ultotal,
+                          double ulnow)
 {
   (void)arg;
+  (void)dltotal;
+  (void)dlnow;
+  (void)ultotal;
+  (void)ulnow;
   return 1;
 }
 
