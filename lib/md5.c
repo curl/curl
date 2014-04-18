54df616729e8edd514b44bc71fd05314001c468e
Marc Hoersken <info@marc-hoersken.de>
2014-04-18 22:59:25 +0200

    md5.c: fix use of uninitialized variable

--- lib/md5.c
+++ lib/md5.c
@@ -147,7 +147,7 @@ static void MD5_Update(MD5_CTX *ctx,
 
 static void MD5_Final(unsigned char digest[16], MD5_CTX *ctx)
 {
-  unsigned long length;
+  unsigned long length = 0;
   CryptGetHashParam(ctx->hHash, HP_HASHVAL, NULL, &length, 0);
   if(length == 16)
     CryptGetHashParam(ctx->hHash, HP_HASHVAL, digest, &length, 0);
