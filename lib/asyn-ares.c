c56aa6f1214157e01bd7e0f9e3a7efe8089e6101
Daniel Stenberg <daniel@haxx.se>
2014-07-25 09:25:16 +0200

    c-ares: fix build without IPv6 support
    Bug: http://curl.haxx.se/mail/lib-2014-07/0337.html
    Reported-by: Spork Schivago

--- lib/asyn-ares.c
+++ lib/asyn-ares.c
@@ -669,7 +669,7 @@ CURLcode Curl_set_dns_local_ip4(struct SessionHandle *data,
 CURLcode Curl_set_dns_local_ip6(struct SessionHandle *data,
                                 const char *local_ip6)
 {
-#if (ARES_VERSION >= 0x010704)
+#if (ARES_VERSION >= 0x010704) && defined(ENABLE_IPV6)
   unsigned char a6[INET6_ADDRSTRLEN];
 
   if((!local_ip6) || (local_ip6[0] == 0)) {
