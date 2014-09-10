785395b07e01eb9085655d6ef311e34c2fca2224
Paul Howarth <paul@city-fan.org>
2014-09-10 13:08:22 +0200

    openssl: build fix for versions < 0.9.8e
    Bug: http://curl.haxx.se/mail/lib-2014-09/0064.html

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -749,6 +749,12 @@ int Curl_ossl_init(void)
      calls CONF_modules_load_file() and we use that instead and we ignore
      its return code! */
 
+  /* CONF_MFLAGS_DEFAULT_SECTION introduced some time between 0.9.8b and
+     0.9.8e */
+#ifndef CONF_MFLAGS_DEFAULT_SECTION
+#define CONF_MFLAGS_DEFAULT_SECTION 0x0
+#endif
+
   (void)CONF_modules_load_file(NULL, NULL,
                                CONF_MFLAGS_DEFAULT_SECTION|
                                CONF_MFLAGS_IGNORE_MISSING_FILE);
