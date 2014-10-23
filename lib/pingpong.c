8e34d3f9efd046ed0fe9eb0ee859959624df6b0f
Daniel Stenberg <daniel@haxx.se>
2014-10-23 22:42:15 +0200

    Curl_pp_readresp: remove superfluous assignment
    Variable already assigned a few lines up.
    
    Detected by cppcheck.

--- lib/pingpong.c
+++ lib/pingpong.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2013, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2014, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -284,8 +284,6 @@ CURLcode Curl_pp_readresp(curl_socket_t sockfd,
 
   /* number of bytes in the current line, so far */
   perline = (ssize_t)(ptr-pp->linestart_resp);
-
-  keepon=TRUE;
 
   while((pp->nread_resp<BUFSIZE) && (keepon && !result)) {
 
