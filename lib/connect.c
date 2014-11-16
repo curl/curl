43da5b20664ea04161b6e6edc9393eaa5d6b2e2d
Steve Holme <steve_holme@hotmail.com>
2014-11-16 18:19:58 +0000

    connect.c: Fixed compilation warning when no verbose string support
    warning: unused parameter 'reason'

--- lib/connect.c
+++ lib/connect.c
@@ -1333,8 +1333,13 @@ CURLcode Curl_socket(struct connectdata *conn,
 void Curl_conncontrol(struct connectdata *conn, bool closeit,
                       const char *reason)
 {
+#if defined(CURL_DISABLE_VERBOSE_STRINGS)
+  (void) reason;
+#endif
+
   infof(conn->data, "Marked for [%s]: %s\n", closeit?"closure":"keep alive",
         reason);
+
   conn->bits.close = closeit; /* the only place in the source code that should
                                  assign this bit */
 }
