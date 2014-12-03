61e71a8bc8005349f50d7b1ed3706407ba88513d
Steve Holme <steve_holme@hotmail.com>
2014-12-03 18:57:48 +0000

    sasl_gssapi: Added body to Curl_sasl_gssapi_cleanup()

--- lib/curl_sasl_gssapi.c
+++ lib/curl_sasl_gssapi.c
@@ -134,7 +134,19 @@ CURLcode Curl_sasl_create_gssapi_security_message(struct SessionHandle *data,
 
 void Curl_sasl_gssapi_cleanup(struct kerberos5data *krb5)
 {
-  (void) krb5;
+  OM_uint32 minor_status;
+
+  /* Free our security context */
+  if(krb5->context != GSS_C_NO_CONTEXT) {
+    gss_delete_sec_context(&minor_status, &krb5->context, GSS_C_NO_BUFFER);
+    krb5->context = GSS_C_NO_CONTEXT;
+  }
+
+  /* Free the SPN */
+  if(krb5->spn != GSS_C_NO_NAME) {
+    gss_release_name(&minor_status, &krb5->spn);
+    krb5->spn = GSS_C_NO_NAME;
+  }
 }
 
 #endif /* HAVE_GSSAPI && USE_KERBEROS5 */
