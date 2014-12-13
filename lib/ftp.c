5f5814ca5e94e6c11d4d3007ea1a845810e085a2
Steve Holme <steve_holme@hotmail.com>
2014-12-13 12:32:32 +0000

    ftp.c: Fixed compilation warning when no verbose string support
    ftp.c:819: warning: unused parameter 'lineno'

--- lib/ftp.c
+++ lib/ftp.c
@@ -821,12 +821,19 @@ static void _state(struct connectdata *conn,
   )
 {
   struct ftp_conn *ftpc = &conn->proto.ftpc;
-#if defined(DEBUGBUILD) && !defined(CURL_DISABLE_VERBOSE_STRINGS)
+
+#if defined(DEBUGBUILD)
+
+#if defined(CURL_DISABLE_VERBOSE_STRINGS)
+  (void) lineno;
+#else
   if(ftpc->state != newstate)
     infof(conn->data, "FTP %p (line %d) state change from %s to %s\n",
           (void *)ftpc, lineno, ftp_state_names[ftpc->state],
           ftp_state_names[newstate]);
 #endif
+#endif
+
   ftpc->state = newstate;
 }
 
