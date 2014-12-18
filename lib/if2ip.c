a390329f60f40484ba6bad55ba72628bc0b7352c
Patrick Monnerat <pm@datasphere.ch>
2014-12-18 11:05:18 +0100

    if2ip: dummy scope parameter for Curl_if2ip() call in SIOCGIFADDR-enabled code.

--- lib/if2ip.c
+++ lib/if2ip.c
@@ -191,7 +191,7 @@ bool Curl_if_is_interface_name(const char *interf)
   /* This is here just to support the old interfaces */
   char buf[256];
 
-  return (Curl_if2ip(AF_INET, 0, interf, buf, sizeof(buf)) ==
+  return (Curl_if2ip(AF_INET, 0 /* unused */, 0, interf, buf, sizeof(buf)) ==
           IF2IP_NOT_FOUND) ? FALSE : TRUE;
 }
 
