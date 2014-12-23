a4d91585094d49fe18ce9d7839cde50aa17a2bb3
Steve Holme <steve_holme@hotmail.com>
2014-12-23 00:16:07 +0000

    openssl.c Fix for compilation errors with older versions of OpenSSL
    openssl.c:1408: error: 'TLS1_1_VERSION' undeclared
    openssl.c:1411: error: 'TLS1_2_VERSION' undeclared

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -1405,12 +1405,16 @@ static void ssl_tls_trace(int direction, int ssl_ver, int content_type,
   case TLS1_VERSION:
     verstr = "TLSv1.0";
     break;
+#ifdef TLS1_1_VERSION
   case TLS1_1_VERSION:
     verstr = "TLSv1.1";
     break;
+#endif
+#ifdef TLS1_2_VERSION
   case TLS1_2_VERSION:
     verstr = "TLSv1.2";
     break;
+#endif
   default:
     snprintf(unknown, sizeof(unknown), "(%x)", ssl_ver);
     verstr = unknown;
