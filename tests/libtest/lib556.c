8f4014538415603eacb9ed43fe80201565013595
Steve Holme <steve_holme@hotmail.com>
2014-12-28 20:32:46 +0000

    test556: Fixed compilation warning
    lib556.c:90: warning: conversion to 'unsigned int' from 'size_t' may
                 alter its value

--- tests/libtest/lib556.c
+++ tests/libtest/lib556.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2011, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2014, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -21,6 +21,7 @@
  ***************************************************************************/
 #include "test.h"
 
+#include "warnless.h"
 #include "memdebug.h"
 
 /* For Windows, mainly (may be moved in a config file?) */
