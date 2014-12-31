8a3c0fbed1dc0a09f1d53579a0112b546cd3d76d
Julien Nabet <serval2412@yahoo.fr>
2014-12-31 11:40:28 +0100

    sepheaders.c: Fixed resource leak on failure

--- docs/examples/sepheaders.c
+++ docs/examples/sepheaders.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2011, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2014, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -62,6 +62,7 @@ int main(void)
   bodyfile = fopen(bodyfilename,"wb");
   if (bodyfile == NULL) {
     curl_easy_cleanup(curl_handle);
+    fclose(headerfile);
     return -1;
   }
 
