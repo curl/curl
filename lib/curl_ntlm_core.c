0ea9381b7da0abd016443f50497a521cda343554
Steve Holme <steve_holme@hotmail.com>
2015-01-02 21:09:24 +0000

    curl_ntlm_core.c: Fixed compilation warning from commit 1cb17b2a5d
    curl_ntlm_core.c:146: warning: passing 'DES_cblock' (aka 'unsigned char
                          [8]') to parameter of type 'char *' converts
                          between pointers to integer types with different
                          sign

--- lib/curl_ntlm_core.c
+++ lib/curl_ntlm_core.c
@@ -143,7 +143,7 @@ static void setup_des_key(const unsigned char *key_56,
 {
   DES_cblock key;
 
-  extend_key_56_to_64(key_56, key);
+  extend_key_56_to_64(key_56, (char *) key);
   DES_set_odd_parity(&key);
   DES_set_key(&key, ks);
 }
