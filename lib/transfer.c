cc28bc472ec421cec2ba26d653e53892998a248d
Daniel Stenberg <daniel@haxx.se>
2015-01-14 23:31:57 +0100

    Curl_pretransfer: reset expected transfer sizes
    Reported-by: Mohammad AlSaleh
    Bug: http://curl.haxx.se/mail/lib-2015-01/0065.html

--- lib/transfer.c
+++ lib/transfer.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2014, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -1342,6 +1342,7 @@ CURLcode Curl_pretransfer(struct SessionHandle *data)
 #endif
 
     Curl_initinfo(data); /* reset session-specific information "variables" */
+    Curl_pgrsResetTimesSizes(data);
     Curl_pgrsStartNow(data);
 
     if(data->set.timeout)
