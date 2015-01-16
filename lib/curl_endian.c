bb12d44471346ccc458a9edebba3c2238c113955
Steve Holme <steve_holme@hotmail.com>
2015-01-16 12:31:24 +0000

    curl_endian: Fixed build when 64-bit integers are not supported
    Bug: http://curl.haxx.se/mail/lib-2015-01/0094.html
    Reported-by: John E. Malmberg

--- lib/curl_endian.c
+++ lib/curl_endian.c
@@ -62,6 +62,7 @@ unsigned int Curl_read32_le(unsigned char *buf)
          ((unsigned int)buf[2] << 16) | ((unsigned int)buf[3] << 24);
 }
 
+#if (CURL_SIZEOF_CURL_OFF_T > 4)
 /*
  * Curl_read64_le()
  *
@@ -96,6 +97,8 @@ unsigned __int64 Curl_read64_le(unsigned char *buf)
          ((unsigned __int64)buf[6] << 48) | ((unsigned __int64)buf[7] << 56);
 }
 #endif
+
+#endif /* CURL_SIZEOF_CURL_OFF_T > 4 */
 
 /*
  * Curl_read16_be()
@@ -227,4 +230,4 @@ void Curl_write64_le(const __int64 value, unsigned char *buffer)
   Curl_write32_le((int)value, buffer);
   Curl_write32_le((int)(value >> 32), buffer + 4);
 }
-#endif
+#endif /* CURL_SIZEOF_CURL_OFF_T > 4 */
