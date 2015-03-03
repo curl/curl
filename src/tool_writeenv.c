af838b7d7b0d1aa5329de99922e78591811e5eac
Daniel Stenberg <daniel@haxx.se>
2015-03-03 23:00:54 +0100

    tool_writeenv: remove _MPRINTF_REPLACE define, it wasn't used

--- src/tool_writeenv.c
+++ src/tool_writeenv.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2012, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -27,11 +27,8 @@
 #  include <kernel.h>
 #endif
 
-#define _MPRINTF_REPLACE /* use our functions only */
 #include <curl/mprintf.h>
-
 #include "tool_writeenv.h"
-
 #include "memdebug.h" /* keep this as LAST include */
 
 static const struct
