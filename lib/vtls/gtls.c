a6a264ef2cdda1537e8a2a6c50ca9a81153d11d0
Alessandro Ghedini <alessandro@ghedini.me>
2015-03-10 00:31:05 +0100

    gtls: don't print double newline after certificate dates

--- lib/vtls/gtls.c
+++ lib/vtls/gtls.c
@@ -211,7 +211,7 @@ static void showtime(struct SessionHandle *data,
 
   snprintf(data->state.buffer,
            BUFSIZE,
-           "\t %s: %s, %02d %s %4d %02d:%02d:%02d GMT\n",
+           "\t %s: %s, %02d %s %4d %02d:%02d:%02d GMT",
            text,
            Curl_wkday[tm->tm_wday?tm->tm_wday-1:6],
            tm->tm_mday,
