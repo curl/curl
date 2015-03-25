ea152990caea2dc793f016792164d18a64711072
Jay Satiro <raysatiro@yahoo.com>
2015-03-25 02:37:20 -0400

    cyassl: Check for invalid length parameter in Curl_cyassl_random

--- lib/vtls/cyassl.c
+++ lib/vtls/cyassl.c
@@ -640,7 +640,9 @@ int Curl_cyassl_random(struct SessionHandle *data,
   (void)data;
   if(InitRng(&rng))
     return 1;
-  if(RNG_GenerateBlock(&rng, entropy, length))
+  if(length > UINT_MAX)
+    return 1;
+  if(RNG_GenerateBlock(&rng, entropy, (unsigned)length))
     return 1;
   return 0;
 }
