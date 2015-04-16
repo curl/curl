691a07dac6df0a827aa3f0b3e21000cdb362a7fb
Daniel Stenberg <daniel@haxx.se>
2015-04-17 00:38:50 +0200

    parsecfg: do not continue past a zero termination
    When a config file line ends without newline, the parsing function could
    continue reading beyond that point in memory.
    
    Reported-by: Hanno Böck

--- src/tool_parsecfg.c
+++ src/tool_parsecfg.c
@@ -187,24 +187,27 @@ int parseconfig(const char *filename, struct GlobalConfig *global)
         param = line; /* parameter starts here */
         while(*line && !ISSPACE(*line))
           line++;
-        *line = '\0'; /* zero terminate */
 
-        /* to detect mistakes better, see if there's data following */
-        line++;
-        /* pass all spaces */
-        while(*line && ISSPACE(*line))
+        if(*line) {
+          *line = '\0'; /* zero terminate */
+
+          /* to detect mistakes better, see if there's data following */
           line++;
+          /* pass all spaces */
+          while(*line && ISSPACE(*line))
+            line++;
 
-        switch(*line) {
-        case '\0':
-        case '\r':
-        case '\n':
-        case '#': /* comment */
-          break;
-        default:
-          warnf(operation->global, "%s:%d: warning: '%s' uses unquoted white "
-                "space in the line that may cause side-effects!\n",
-                filename, lineno, option);
+          switch(*line) {
+          case '\0':
+          case '\r':
+          case '\n':
+          case '#': /* comment */
+            break;
+          default:
+            warnf(operation->global, "%s:%d: warning: '%s' uses unquoted "
+                  "white space in the line that may cause side-effects!\n",
+                  filename, lineno, option);
+          }
         }
       }
 
