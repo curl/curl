86bc654532e24bdc3a76324fc976fe3be399985b
Daniel Stenberg <daniel@haxx.se>
2015-05-04 13:29:34 +0200

    OpenSSL: conditional check for SSL3_RT_HEADER
    The symbol is fairly new.
    
    Reported-by: Kamil Dudka

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -1515,8 +1515,10 @@ static const char *ssl_msg_type(int ssl_ver, int msg)
 static const char *tls_rt_type(int type)
 {
   switch(type) {
+#ifdef SSL3_RT_HEADER
   case SSL3_RT_HEADER:
     return "TLS header";
+#endif
   case SSL3_RT_CHANGE_CIPHER_SPEC:
     return "TLS change cipher";
   case SSL3_RT_ALERT:
