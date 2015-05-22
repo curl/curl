03e2a9b023f60c2190c2b8590f16b2a4f83f21a9
Daniel Stenberg <daniel@haxx.se>
2015-05-22 16:26:14 +0200

    share_init: fix OOM crash
    A failed calloc() would lead to NULL pointer use.
    
    Coverity CID 1299427.

--- lib/share.c
+++ lib/share.c
@@ -35,12 +35,13 @@ CURLSH *
 curl_share_init(void)
 {
   struct Curl_share *share = calloc(1, sizeof(struct Curl_share));
-  if(share)
+  if(share) {
     share->specifier |= (1<<CURL_LOCK_DATA_SHARE);
 
-  if(Curl_mk_dnscache(&share->hostcache)) {
-    free(share);
-    return NULL;
+    if(Curl_mk_dnscache(&share->hostcache)) {
+      free(share);
+      return NULL;
+    }
   }
 
   return share;
