8558d33d22807a0323838e9643eb33a13221fc0f
Daniel Melani <daniel.melani@gmail.com>
2015-05-27 11:27:03 +0200

    openssl: typo in comment

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -733,7 +733,7 @@ static char *SSL_strerror(unsigned long error, char *buf, size_t size)
 {
 #ifdef HAVE_ERR_ERROR_STRING_N
   /* OpenSSL 0.9.6 and later has a function named
-     ERRO_error_string_n() that takes the size of the buffer as a
+     ERR_error_string_n() that takes the size of the buffer as a
      third argument */
   ERR_error_string_n(error, buf, size);
 #else
