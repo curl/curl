9f5dcab83d77b2331ed2716f901e5cb4a0c17529
Daniel Stenberg <daniel@haxx.se>
2015-06-01 08:46:18 +0200

    http2-upload.c: use PIPEWAIT for playing HTTP/2 better

--- docs/examples/http2-upload.c
+++ docs/examples/http2-upload.c
@@ -221,6 +221,11 @@ static void setup(CURL *hnd, int num, const char *upload)
   curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0L);
   curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0L);
 
+#if (CURLPIPE_MULTIPLEX > 0)
+  /* wait for pipe connection to confirm */
+  curl_easy_setopt(hnd, CURLOPT_PIPEWAIT, 1L);
+#endif
+
   curl_hnd[num] = hnd;
 }
 
