e9f0dd43bcbeb47e72e4f9d18c86d8e4b0b19739
Daniel Stenberg <daniel@haxx.se>
2015-06-02 10:34:27 +0200

    http2: init the pushed transfer properly

--- lib/multi.c
+++ lib/multi.c
@@ -958,9 +958,16 @@ CURLMcode Curl_multi_add_perform(struct Curl_multi *multi,
 
   rc = curl_multi_add_handle(multi, data);
   if(!rc) {
+    struct SingleRequest *k = &data->req;
+
+    /* pass in NULL for 'conn' here since we don't want to init the
+       connection, only this transfer */
+    Curl_init_do(data, NULL);
+
     /* take this handle to the perform state right away */
     multistate(data, CURLM_STATE_PERFORM);
     data->easy_conn = conn;
+    k->keepon |= KEEP_RECV; /* setup to receive! */
   }
   return rc;
 }
