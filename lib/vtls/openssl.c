6b0ba3879c615c0a6e84777698a760aecd252c80
Daniel Stenberg <daniel@haxx.se>
2015-06-05 00:12:56 +0200

    openssl: removed error string #ifdef
    ERR_error_string_n() was introduced in 0.9.6, no need to #ifdef anymore

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -102,10 +102,6 @@
 #undef HAVE_PKCS12_SUPPORT
 #endif
 
-#if OPENSSL_VERSION_NUMBER >= 0x00906001L
-#define HAVE_ERR_ERROR_STRING_N 1
-#endif
-
 #if OPENSSL_VERSION_NUMBER >= 0x00909000L
 #define SSL_METHOD_QUAL const
 #else
@@ -680,15 +676,10 @@ static int x509_name_oneline(X509_NAME *a, char *buf, size_t size)
  */
 static char *SSL_strerror(unsigned long error, char *buf, size_t size)
 {
-#ifdef HAVE_ERR_ERROR_STRING_N
   /* OpenSSL 0.9.6 and later has a function named
      ERR_error_string_n() that takes the size of the buffer as a
      third argument */
   ERR_error_string_n(error, buf, size);
-#else
-  (void) size;
-  ERR_error_string(error, buf);
-#endif
   return buf;
 }
 
