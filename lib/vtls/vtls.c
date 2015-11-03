5d1fe0dec0da558ba810f67cec0056168a67d121
Alex Rousskov <rousskov@measurement-factory.com>
2015-11-03 10:37:52 -0700

    Full NSS backend support for HTTPS proxies.
    Also adjusts SSL_IS_PROXY() to work correctly without requiring
    sockindex.

--- lib/vtls/vtls.c
+++ lib/vtls/vtls.c
@@ -308,7 +308,7 @@ ssl_connect_init_proxy(struct connectdata *conn, int sockindex) {
   DEBUGASSERT(conn->bits.proxy_ssl_connected[sockindex]);
   if(ssl_connection_complete == conn->ssl[sockindex].state &&
      !conn->proxy_ssl[sockindex].use) {
-#ifdef USE_OPENSSL
+#if defined(USE_OPENSSL) || defined(USE_NSS)
     conn->proxy_ssl[sockindex] = conn->ssl[sockindex];
     memset(&conn->ssl[sockindex], 0, sizeof(conn->ssl[sockindex]));
 #else
