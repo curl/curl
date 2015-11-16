39904d6f7d8427f46aa5a514983911493b86ebe8
Daniel Stenberg <daniel@haxx.se>
2015-11-16 08:22:08 +0100

    http2: minor comment typo

--- lib/http2.c
+++ lib/http2.c
@@ -280,7 +280,7 @@ char *curl_pushheader_byname(struct curl_pushheaders *h, const char *header)
     size_t i;
     for(i=0; i<stream->push_headers_used; i++) {
       if(!strncmp(header, stream->push_headers[i], len)) {
-        /* sub-match, make sure that it us followed by a colon */
+        /* sub-match, make sure that it is followed by a colon */
         if(stream->push_headers[i][len] != ':')
           continue;
         return &stream->push_headers[i][len+1];
