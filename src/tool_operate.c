f6064354195bac99f072783cd5b8ea8ac44f34df
Daniel Stenberg <daniel@haxx.se>
2015-12-03 09:53:53 +0100

    curl: remove keepalive #ifdef checks done on libcurl's behalf
    They didn't match the ifdef logic used within libcurl anyway so they
    could indeed warn for the wrong case - plus the tool cannot know how the
    lib actually performs at that level.

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -1262,10 +1262,6 @@ static CURLcode operate_do(struct GlobalConfig *global,
         if(!config->nokeepalive) {
           my_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
           if(config->alivetime != 0) {
-#if !defined(TCP_KEEPIDLE) || !defined(TCP_KEEPINTVL)
-            warnf(config->global, "Keep-alive functionality somewhat crippled "
-                "due to missing support in your operating system!\n");
-#endif
             my_setopt(curl, CURLOPT_TCP_KEEPIDLE, config->alivetime);
             my_setopt(curl, CURLOPT_TCP_KEEPINTVL, config->alivetime);
           }
