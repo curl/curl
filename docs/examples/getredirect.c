e400a89b184d31da3ac0c5a47dce795e222c33eb
Daniel Stenberg <daniel@haxx.se>
2016-01-27 09:35:55 +0100

    getredirect.c: fix variable name
    Reported-by: Bernard Spil

--- docs/examples/getredirect.c
+++ docs/examples/getredirect.c
@@ -48,7 +48,7 @@ int main(void)
     else {
       res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
       if((res == CURLE_OK) &&
-         ((code / 100) != 3)) {
+         ((response_code / 100) != 3)) {
         /* a redirect implies a 3xx response code */
         fprintf(stderr, "Not a redirect.\n");
       }
