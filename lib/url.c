369a632a8146be58f768bc2de6dea5f7202e04f4
Anders Bakken <agbakken@gmail.com>
2016-02-10 14:55:50 -0800

    Fix potential overflow problem when comparing a null timeval with an current timeval.
    Without this I get this warning from -fsanitize=undefined on startup of
    our app:
    
    .../curl/lib/timeval.c:120:37: runtime error: signed integer overflow: 5873565 * 1000 cannot be represented in type 'long int'

--- lib/url.c
+++ lib/url.c
@@ -3089,9 +3089,10 @@ static int call_disconnect_if_dead(struct connectdata *conn,
 static void prune_dead_connections(struct SessionHandle *data)
 {
   struct timeval now = Curl_tvnow();
-  long elapsed = Curl_tvdiff(now, data->state.conn_cache->last_cleanup);
 
-  if(elapsed >= 1000L) {
+  if((!data->state.conn_cache->last_cleanup.tv_sec
+      && !data->state.conn_cache->last_cleanup.tv_usec)
+     || Curl_tvdiff(now, data->state.conn_cache->last_cleanup) >= 1000L) {
     Curl_conncache_foreach(data->state.conn_cache, data,
                            call_disconnect_if_dead);
     data->state.conn_cache->last_cleanup = now;
