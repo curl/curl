6dd6d6e6e99341fd4abf5f9f8588a74ce8128cbe
Anders Bakken <agbakken@gmail.com>
2016-03-07 11:38:26 -0800

    Sanity check pipeline pointer before accessing it.
    I got a crash with this stack:
    
    curl/lib/url.c:2873 (Curl_removeHandleFromPipeline)
    curl/lib/url.c:2919 (Curl_getoff_all_pipelines)
    curl/lib/multi.c:561 (curl_multi_remove_handle)
    curl/lib/url.c:415 (Curl_close)
    curl/lib/easy.c:859 (curl_easy_cleanup)

--- lib/url.c
+++ lib/url.c
@@ -2868,15 +2868,17 @@ static bool IsPipeliningPossible(const struct SessionHandle *handle,
 int Curl_removeHandleFromPipeline(struct SessionHandle *handle,
                                   struct curl_llist *pipeline)
 {
-  struct curl_llist_element *curr;
+  if(pipeline) {
+    struct curl_llist_element *curr;
 
-  curr = pipeline->head;
-  while(curr) {
-    if(curr->ptr == handle) {
-      Curl_llist_remove(pipeline, curr, NULL);
-      return 1; /* we removed a handle */
+    curr = pipeline->head;
+    while(curr) {
+      if(curr->ptr == handle) {
+        Curl_llist_remove(pipeline, curr, NULL);
+        return 1; /* we removed a handle */
+      }
+      curr = curr->next;
     }
-    curr = curr->next;
   }
 
   return 0;
