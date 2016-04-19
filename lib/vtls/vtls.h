cefe9f4f90bc2f3a9c837f4504e962f2a519780d
Daniel Stenberg <daniel@haxx.se>
2016-04-19 08:41:15 +0200

    vtls.h: remove a space before semicolon
    ... that the new checksrc detected

--- lib/vtls/vtls.h
+++ lib/vtls/vtls.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2016, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -99,7 +99,7 @@ CURLcode Curl_ssl_push_certinfo(struct SessionHandle * data, int certnum,
 /* extract a session ID */
 bool Curl_ssl_getsessionid(struct connectdata *conn,
                            void **ssl_sessionid,
-                           size_t *idsize) /* set 0 if unknown */;
+                           size_t *idsize); /* set 0 if unknown */
 /* add a new session ID */
 CURLcode Curl_ssl_addsessionid(struct connectdata *conn,
                                void *ssl_sessionid,
