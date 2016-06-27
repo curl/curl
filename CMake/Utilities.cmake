6e17cf9c74dccec37778327818e3e40fcfbc0a15
Michele Adduci <michele.adduci@openlimit.com>
2016-06-27 14:04:02 +0200

    Added static compilation code for Windows and Unix

--- CMake/Utilities.cmake
+++ CMake/Utilities.cmake
@@ -1,5 +1,9 @@
 # File containing various utilities
 
+#Load check for compiler flags
+include(CheckCCompilerFlag)
+include(CheckCXXCompilerFlag)
+
 # Converts a CMake list to a string containing elements separated by spaces
 function(TO_LIST_SPACES _LIST_NAME OUTPUT_VAR)
   set(NEW_LIST_SPACE)
@@ -27,5 +31,43 @@ function(IN_STR_LIST LIST_NAME ITEM_SEARCHED RETVAL)
     set(${RETVAL} FALSE PARENT_SCOPE)
   else()
     set(${RETVAL} TRUE PARENT_SCOPE)
+  endif()
+endfunction()
+
+# Set Compiler flags
+function(SET_BUILD_TYPE_FLAGS BUILD_TYPE)
+  if(WIN32)
+    set(CompilerFlags
+        CMAKE_C_FLAGS
+        CMAKE_C_FLAGS_DEBUG
+        CMAKE_C_FLAGS_MINSIZEREL
+        CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_RELMITDEBINFO
+        CMAKE_CXX_FLAGS
+        CMAKE_CXX_FLAGS_DEBUG
+        CMAKE_CXX_FLAGS_MINSIZEREL
+        CMAKE_CXX_FLAGS_RELEASE
+        CMAKE_CXX_FLAGS_RELMITDEBINFO)
+
+    #Set /MT flag for Static compiling, /MD for Shared
+    foreach(Flag ${CompilerFlags})
+      if("${BUILD_TYPE}" MATCHES "STATIC")
+        string(REPLACE "/MD" "/MT" ${Flag} "${${CompilerFlags}}")
+      else()
+        string(REPLACE "/MT" "/MD" ${Flag} "${${CompilerFlags}}")
+      endif()
+    endforeach()
+  else()
+    if("${BUILD_TYPE}" MATCHES "STATIC")
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+      set(CMAKE_EXE_LINKER_FLAGS "")
+    else()
+      set(CMAKE_EXE_LINKER_FLAGS "-static -static-libgcc -static-libstdc++")
+      set(CMAKE_EXE_LINK_DYNAMIC_C_FLAGS "")
+      set(CMAKE_EXE_LINK_DYNAMIC_CXX_FLAGS "")
+      set(CMAKE_SHARED_LIBRARY_C_FLAGS "")
+      set(CMAKE_SHARED_LIBRARY_CXX_FLAGS "")
+    endif()
   endif()
 endfunction()
