c50980807c55f91c2fb2d09f3b6dc0ae69f4cf45
Brian Prodoehl <bprodoehl@connectify.me>
2016-07-15 11:53:13 -0400

    curl_global_init: Check if IPv6 works
    - Curl_ipv6works() is not thread-safe until after the first call, so
    call it once during global init to avoid a possible race condition.
    
    Bug: https://github.com/curl/curl/issues/915
    PR: https://github.com/curl/curl/pull/918

--- lib/easy.c
+++ lib/easy.c
@@ -271,6 +271,8 @@ static CURLcode global_init(long flags, bool memoryfuncs)
     return CURLE_FAILED_INIT;
   }
 
+  (void)Curl_ipv6works();
+
 #if defined(USE_LIBSSH2) && defined(HAVE_LIBSSH2_INIT)
   if(libssh2_init(0)) {
     DEBUGF(fprintf(stderr, "Error: libssh2_init failed\n"));
