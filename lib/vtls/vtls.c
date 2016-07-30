11ec5ad4352bba384404c56e77c7fab9382fd22d
Daniel Stenberg <daniel@haxx.se>
2016-07-31 00:51:48 +0200

    TLS: only reuse connections with the same client cert
    CVE-2016-5420
    Bug: https://curl.haxx.se/docs/adv_20160803B.html

--- lib/vtls/vtls.c
+++ lib/vtls/vtls.c
@@ -99,6 +99,7 @@ Curl_ssl_config_matches(struct ssl_config_data* data,
      (data->verifyhost == needle->verifyhost) &&
      safe_strequal(data->CApath, needle->CApath) &&
      safe_strequal(data->CAfile, needle->CAfile) &&
+     safe_strequal(data->clientcert, needle->clientcert) &&
      safe_strequal(data->random_file, needle->random_file) &&
      safe_strequal(data->egdsocket, needle->egdsocket) &&
      safe_strequal(data->cipher_list, needle->cipher_list))
