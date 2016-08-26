39ba8dd09b5f2883108d3ddf15ef61bb4cff437b
Daniel Stenberg <daniel@haxx.se>
2016-08-26 10:01:19 +0200

    TLS: random file/egd doesn't have to match for conn reuse

--- lib/vtls/vtls.c
+++ lib/vtls/vtls.c
@@ -100,8 +100,6 @@ Curl_ssl_config_matches(struct ssl_config_data* data,
      safe_strequal(data->CApath, needle->CApath) &&
      safe_strequal(data->CAfile, needle->CAfile) &&
      safe_strequal(data->clientcert, needle->clientcert) &&
-     safe_strequal(data->random_file, needle->random_file) &&
-     safe_strequal(data->egdsocket, needle->egdsocket) &&
      safe_strequal(data->cipher_list, needle->cipher_list))
     return TRUE;
 
