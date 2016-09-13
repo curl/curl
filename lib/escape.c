01cf1308ee2e792c77bb1d2c9218c56a30fd40ae
Daniel Stenberg <daniel@haxx.se>
2016-09-13 23:00:50 +0200

    curl_easy_unescape: deny negative string lengths as input
    CVE-2016-7167
    
    Bug: https://curl.haxx.se/docs/adv_20160914.html

--- lib/escape.c
+++ lib/escape.c
@@ -217,14 +217,16 @@ char *curl_easy_unescape(struct Curl_easy *data, const char *string,
                          int length, int *olen)
 {
   char *str = NULL;
-  size_t inputlen = length;
-  size_t outputlen;
-  CURLcode res = Curl_urldecode(data, string, inputlen, &str, &outputlen,
-                                FALSE);
-  if(res)
-    return NULL;
-  if(olen)
-    *olen = curlx_uztosi(outputlen);
+  if(length >= 0) {
+    size_t inputlen = length;
+    size_t outputlen;
+    CURLcode res = Curl_urldecode(data, string, inputlen, &str, &outputlen,
+                                  FALSE);
+    if(res)
+      return NULL;
+    if(olen)
+      *olen = curlx_uztosi(outputlen);
+  }
   return str;
 }
 
