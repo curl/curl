75912202709e0f74a5bab91ef57254d7038f5f42
Kamil Dudka <kdudka@redhat.com>
2016-09-19 16:37:05 +0200

    nss: fix typo in ecdhe_rsa_null cipher suite string
    As it seems to be a rarely used cipher suite (for securely established
    but _unencrypted_ connections), I believe it is fine not to provide an
    alias for the misspelled variant.

--- lib/vtls/nss.c
+++ lib/vtls/nss.c
@@ -149,7 +149,7 @@ static const cipher_s cipherlist[] = {
   {"ecdh_rsa_3des_sha",          TLS_ECDH_RSA_WITH_3DES_EDE_CBC_SHA},
   {"ecdh_rsa_aes_128_sha",       TLS_ECDH_RSA_WITH_AES_128_CBC_SHA},
   {"ecdh_rsa_aes_256_sha",       TLS_ECDH_RSA_WITH_AES_256_CBC_SHA},
-  {"echde_rsa_null",             TLS_ECDHE_RSA_WITH_NULL_SHA},
+  {"ecdhe_rsa_null",             TLS_ECDHE_RSA_WITH_NULL_SHA},
   {"ecdhe_rsa_rc4_128_sha",      TLS_ECDHE_RSA_WITH_RC4_128_SHA},
   {"ecdhe_rsa_3des_sha",         TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA},
   {"ecdhe_rsa_aes_128_sha",      TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA},
