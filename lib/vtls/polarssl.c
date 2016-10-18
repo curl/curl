1e3161a20d5759409fec9aa339f79c5f71cabe65
Daniel Stenberg <daniel@haxx.se>
2016-10-18 13:11:55 +0200

    polarssl: fix unaligned SSL session-id lock

--- lib/vtls/polarssl.c
+++ lib/vtls/polarssl.c
@@ -344,13 +344,14 @@ polarssl_connect_step1(struct connectdata *conn,
     Curl_ssl_sessionid_lock(conn);
     if(!Curl_ssl_getsessionid(conn, &old_session, NULL)) {
       ret = ssl_set_session(&connssl->ssl, old_session);
-      Curl_ssl_sessionid_unlock(conn);
       if(ret) {
+        Curl_ssl_sessionid_unlock(conn);
         failf(data, "ssl_set_session returned -0x%x", -ret);
         return CURLE_SSL_CONNECT_ERROR;
       }
       infof(data, "PolarSSL re-using session\n");
     }
+    Curl_ssl_sessionid_unlock(conn);
   }
 
   ssl_set_ca_chain(&connssl->ssl,
