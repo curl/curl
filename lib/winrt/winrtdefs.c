445d9a3654a0b8d64149a17f552869a910a609e6
Aurimas Cernius <aurimasc@unity3d.com>
2016-11-29 16:05:19 +0200

    Revert "Add WinRT replacements for getenv and ExpandEnvironmentStringsA"
    This reverts commit 9a76d0b837b3142390bb77af5564468afcc7c815.

--- lib/winrt/winrtdefs.c
+++ lib/winrt/winrtdefs.c
@@ -5,22 +5,8 @@
 
 int GetTickCount(void)
 {
-  LARGE_INTEGER t;
-  return(int) (QueryPerformanceCounter(&t) ? t.QuadPart : 0);
-}
-
-char* getenv (const char* name)
-{
-  return NULL;
-}
-
-DWORD WINAPI ExpandEnvironmentStringsA(
-  _In_       LPCTSTR lpSrc,
-  _Out_opt_  LPTSTR lpDst,
-  _In_       DWORD nSize
-)
-{
-  return strlen((const char*)lpSrc);
+    LARGE_INTEGER t;
+    return(int) (QueryPerformanceCounter(&t) ? t.QuadPart : 0);
 }
 
 #endif
