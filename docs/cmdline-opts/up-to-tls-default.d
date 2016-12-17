007fc8caa101ae7b7d7a25a9723aed37b73dc8dc
Jozef Kralik <jozef.kralik@eset.sk>
2016-12-17 15:35:11 +0100

    fix documentation and added curl.1

--- docs/cmdline-opts/up-to-tls-default.d
+++ docs/cmdline-opts/up-to-tls-default.d
@@ -2,11 +2,12 @@ Long: up-to-tls-default
 Tags: Versions
 Protocols: SSL
 Added: 7.53.0
-Mutexed: up-to-tlsv1.1 up-to-tlsv1.2 up-to-tlsv1.3 ssl2 ssl3 tlsv1 tlsv1.2 tlsv1.3
+Mutexed: up-to-tlsv1.1 up-to-tlsv1.2 up-to-tlsv1.3
 Requires: TLS
-See-also: http1.1 http2
+See-also: tlsv1.0 tlsv1.1
 Help: Use TLSv1.0 or greater
 ---
-It is works with arguments --tlsv1.0, --tlsv1.1 which defines minimal version
-of TLS. And this option defines up to recommended version(TLS1.2) of TLS that
-you want to support.
+Use TLS up to recommended TLS version.
+
+It defines a range of supported TLS versions. The minimum must be defined by
+tlsv1.0 or tlsv1.1 and the maximum is defined by this argument.
--- docs/cmdline-opts/up-to-tlsv1.1.d
+++ docs/cmdline-opts/up-to-tlsv1.1.d
@@ -2,11 +2,12 @@ Long: up-to-tlsv1.1
 Tags: Versions
 Protocols: SSL
 Added: 7.53.0
-Mutexed: up-to-tlsv1.2 up-to-tlsv1.3 ssl2 ssl3 tlsv1 tlsv1.1 tlsv1.2 tlsv1.3
+Mutexed: up-to-tls-default up-to-tlsv1.2 up-to-tlsv1.3
 Requires: TLS
-See-also: http1.1 http2
+See-also: tlsv1.0
 Help: Use TLSv1.0 or greater
 ---
-It is  works with arguments --tlsv1.0, which defines minimal version of
-TLS1.0. And this option defines up to version of TLS1.1 that you want to
-support.
+Use TLS up to TLSv1.1.
+
+It defines a range of supported TLS versions. The minimum must be defined by
+tlsv1.0 and the maximum is defined by this argument.
