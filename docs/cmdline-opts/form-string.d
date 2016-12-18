111f3c1fde1ee3cbd8b7ca37ebcc805df184bf9d
Daniel Stenberg <daniel@haxx.se>
2016-12-18 16:29:59 +0100

    form-string.d: fix format mistake
    and regenerated curl.1
    
    Reported-by: Gisle Vanem

--- docs/cmdline-opts/form-string.d
+++ docs/cmdline-opts/form-string.d
@@ -6,6 +6,6 @@ See-also: form
 ---
 Similar to --form except that the value string for the named parameter is used
 literally. Leading \&'@' and \&'<' characters, and the \&';type=' string in
-the value have no special meaning. Use this in preference to --form\fP if
+the value have no special meaning. Use this in preference to --form if
 there's any possibility that the string value may accidentally trigger the
 \&'@' or \&'<' features of --form.
