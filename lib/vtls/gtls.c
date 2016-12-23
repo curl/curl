c7834ecd45194dbca9762ad8fc06072b77481a29
Daniel Stenberg <daniel@haxx.se>
2016-12-23 15:01:53 +0100

    gnutls-random: check return code for failed random

--- lib/vtls/gtls.c
+++ lib/vtls/gtls.c
@@ -1630,8 +1630,10 @@ int Curl_gtls_random(struct Curl_easy *data,
                      size_t length)
 {
 #if defined(USE_GNUTLS_NETTLE)
+  int rc;
   (void)data;
-  gnutls_rnd(GNUTLS_RND_RANDOM, entropy, length);
+  rc = gnutls_rnd(GNUTLS_RND_RANDOM, entropy, length);
+  return rc;
 #elif defined(USE_GNUTLS)
   if(data)
     Curl_gtls_seed(data); /* Initiate the seed if not already done */
