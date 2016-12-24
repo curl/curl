cdbdef6f9f05139f613000f6818f101bb5d6e4ac
Jay Satiro <raysatiro@yahoo.com>
2016-12-24 13:25:03 -0500

    cyassl: use time_t instead of long for timeout

--- lib/vtls/cyassl.c
+++ lib/vtls/cyassl.c
@@ -772,7 +772,7 @@ cyassl_connect_common(struct connectdata *conn,
   struct Curl_easy *data = conn->data;
   struct ssl_connect_data *connssl = &conn->ssl[sockindex];
   curl_socket_t sockfd = conn->sock[sockindex];
-  long timeout_ms;
+  time_t timeout_ms;
   int what;
 
   /* check if the connection has already been established */
