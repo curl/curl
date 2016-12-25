209b2302272b86c2bbe4d3d2b62e1695655f8670
Daniel Stenberg <daniel@haxx.se>
2016-12-25 11:01:17 +0100

    docs/ciphers: link to our own new page about ciphers
    ... as the former ones always go stale!

--- docs/cmdline-opts/ciphers.d
+++ docs/cmdline-opts/ciphers.d
@@ -6,11 +6,6 @@ Protocols: TLS
 Specifies which ciphers to use in the connection. The list of ciphers must
 specify valid ciphers. Read up on SSL cipher list details on this URL:
 
- https://www.openssl.org/docs/apps/ciphers.html
-
-NSS ciphers are done differently than OpenSSL and GnuTLS. The full list of NSS
-ciphers is in the NSSCipherSuite entry at this URL:
-
- https://git.fedorahosted.org/cgit/mod_nss.git/plain/docs/mod_nss.html#Directives
+ https://curl.haxx.se/docs/ssl-ciphers.html
 
 If this option is used several times, the last one will be used.
