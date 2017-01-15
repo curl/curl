25ed9ea51257c0561237d1b725c4ff3d59b3f32c
Kamil Dudka <kdudka@redhat.com>
2017-01-15 13:10:43 +0100

    nss: use the correct lock in nss_find_slot_by_name()

--- lib/vtls/nss.c
+++ lib/vtls/nss.c
@@ -365,9 +365,9 @@ static char *dup_nickname(struct Curl_easy *data, const char *str)
 static PK11SlotInfo* nss_find_slot_by_name(const char *slot_name)
 {
   PK11SlotInfo *slot;
-  PR_Lock(nss_initlock);
+  PR_Lock(nss_findslot_lock);
   slot = PK11_FindSlotByName(slot_name);
-  PR_Unlock(nss_initlock);
+  PR_Unlock(nss_findslot_lock);
   return slot;
 }
 
