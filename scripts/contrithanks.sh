40e3d6c8c21ef2f8e64c1eddb22740c9f1a6cd99
Daniel Stenberg <daniel@haxx.se>
2017-01-20 17:10:08 +0100

    contri*.sh: cut off parentheses from names too

--- scripts/contrithanks.sh
+++ scripts/contrithanks.sh
@@ -6,7 +6,7 @@
 #                            | (__| |_| |  _ <| |___
 #                             \___|\___/|_| \_\_____|
 #
-# Copyright (C) 2013-2016, Daniel Stenberg, <daniel@haxx.se>, et al.
+# Copyright (C) 2013-2017, Daniel Stenberg, <daniel@haxx.se>, et al.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution. The terms
@@ -38,6 +38,7 @@ cat ./docs/THANKS
 git log $start..HEAD | \
 egrep -ai '(^Author|^Commit|by):' | \
 cut -d: -f2- | \
+cut '-d(' -f1 | \
 cut '-d<' -f1 | \
 tr , '\012' | \
 sed 's/ and /\n/' | \
