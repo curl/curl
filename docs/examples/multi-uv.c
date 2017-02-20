889ca45ab896cc88b28bb9cc651f0bc1a8e54bc3
Jay Satiro <raysatiro@yahoo.com>
2017-02-20 00:52:16 -0500

    examples/multi-uv: checksrc compliance

--- docs/examples/multi-uv.c
+++ docs/examples/multi-uv.c
@@ -163,8 +163,8 @@ static int start_timeout(CURLM *multi, long timeout_ms, void *userp)
   }
   else {
     if(timeout_ms == 0)
-      timeout_ms = 1; /* 0 means directly call socket_action, but we'll do it in
-                         a bit */
+      timeout_ms = 1; /* 0 means directly call socket_action, but we'll do it
+                         in a bit */
     uv_timer_start(&timeout, on_timeout, timeout_ms, 0);
   }
   return 0;
