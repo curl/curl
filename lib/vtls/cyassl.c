a21cae5f97727ab357c8527b0a7dc8820e335012
Jay Satiro <raysatiro@yahoo.com>
2017-02-26 18:49:39 -0500

    cyassl: get library version string at runtime
    wolfSSL >= 3.6.0 supports getting its library version string at runtime.

--- lib/vtls/cyassl.c
+++ lib/vtls/cyassl.c
@@ -736,7 +736,9 @@ void Curl_cyassl_session_free(void *ptr)
 
 size_t Curl_cyassl_version(char *buffer, size_t size)
 {
-#ifdef WOLFSSL_VERSION
+#if LIBCYASSL_VERSION_HEX >= 0x03006000
+  return snprintf(buffer, size, "wolfSSL/%s", wolfSSL_lib_version());
+#elif defined(WOLFSSL_VERSION)
   return snprintf(buffer, size, "wolfSSL/%s", WOLFSSL_VERSION);
 #elif defined(CYASSL_VERSION)
   return snprintf(buffer, size, "CyaSSL/%s", CYASSL_VERSION);
