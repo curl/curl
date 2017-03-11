03c5223ee7fa8dcdb5c54ea58c1fb2cca67f35e6
Dan Fandrich <dan@coneharvesters.com>
2017-03-11 11:44:06 +0100

    cmdline-opts: fixed a few typos

--- docs/cmdline-opts/referer.d
+++ docs/cmdline-opts/referer.d
@@ -2,7 +2,7 @@ Long: referer
 Short: e
 Arg: <URL>
 Protocols: HTTP
-Help: Referer URL
+Help: Referrer URL
 See-also: user-agent header
 ---
 Sends the "Referrer Page" information to the HTTP server. This can also be set
