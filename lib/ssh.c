9ca613fc77db39d43615fba435c85e3943a7aec2
JDepooter <joel.depooter@safe.com>
2017-05-09 16:52:23 -0700

    SSH: Only check for timeouts with valid progress values
    In a disconnect situation, the easy handle associated with a connection may have been replaced with a 'closure handle'. This will reset all progress values associated with the connection to zero values. As a result, calculations using these values will return incorrect values.
    
    In particular, if a timeout is set, the 'time left' value will always be negative, because the start time has been reset to zero. This means that if a timeout is set, disconnections always fail with a timeout error, rather than finish successfully.

--- lib/ssh.c
+++ lib/ssh.c
@@ -2830,35 +2830,38 @@ static CURLcode ssh_block_statemach(struct connectdata *conn,
     if(result)
       break;
 
-    if(Curl_pgrsUpdate(conn))
-      return CURLE_ABORTED_BY_CALLBACK;
-
-    result = Curl_speedcheck(data, now);
-    if(result)
-      break;
-
-    left = Curl_timeleft(data, NULL, duringconnect);
-    if(left < 0) {
-      failf(data, "Operation timed out");
-      return CURLE_OPERATION_TIMEDOUT;
-    }
+    /* In some disconnect situations, progress values will have been replaced
+       with zero values, so no calculations should be performed with them. */
+    if(data->progress.start.tv_sec) {
+      if(Curl_pgrsUpdate(conn))
+        return CURLE_ABORTED_BY_CALLBACK;
+  
+      result = Curl_speedcheck(data, now);
+      if(result)
+        break;
+  
+      left = Curl_timeleft(data, NULL, duringconnect);
+      if(left < 0) {
+        failf(data, "Operation timed out");
+        return CURLE_OPERATION_TIMEDOUT;
+      }
 
 #ifdef HAVE_LIBSSH2_SESSION_BLOCK_DIRECTION
-    if(!result && block) {
-      int dir = libssh2_session_block_directions(sshc->ssh_session);
-      curl_socket_t sock = conn->sock[FIRSTSOCKET];
-      curl_socket_t fd_read = CURL_SOCKET_BAD;
-      curl_socket_t fd_write = CURL_SOCKET_BAD;
-      if(LIBSSH2_SESSION_BLOCK_INBOUND & dir)
-        fd_read = sock;
-      if(LIBSSH2_SESSION_BLOCK_OUTBOUND & dir)
-        fd_write = sock;
-      /* wait for the socket to become ready */
-      (void)Curl_socket_check(fd_read, CURL_SOCKET_BAD, fd_write,
-                              left>1000?1000:left); /* ignore result */
-    }
+      if(!result && block) {
+        int dir = libssh2_session_block_directions(sshc->ssh_session);
+        curl_socket_t sock = conn->sock[FIRSTSOCKET];
+        curl_socket_t fd_read = CURL_SOCKET_BAD;
+        curl_socket_t fd_write = CURL_SOCKET_BAD;
+        if(LIBSSH2_SESSION_BLOCK_INBOUND & dir)
+          fd_read = sock;
+        if(LIBSSH2_SESSION_BLOCK_OUTBOUND & dir)
+          fd_write = sock;
+        /* wait for the socket to become ready */
+        (void)Curl_socket_check(fd_read, CURL_SOCKET_BAD, fd_write,
+                                left>1000?1000:left); /* ignore result */
+      }
 #endif
-
+    }
   }
 
   return result;
