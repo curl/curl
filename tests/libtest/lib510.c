b1fa80b84facb94a667a6bfaa99476a161285b0d
Dan Fandrich <dan@coneharvesters.com>
2017-05-13 22:57:49 +0200

    lib510: don't write past the end of the buffer if it's too small

--- tests/libtest/lib510.c
+++ tests/libtest/lib510.c
@@ -48,6 +48,10 @@ static size_t read_callback(void *ptr, size_t size, size_t nmemb, void *userp)
 
   if(data) {
     size_t len = strlen(data);
+    if(size*nmemb < len) {
+      fprintf(stderr, "read buffer is too small to run test\n");
+      return 0;
+    }
     memcpy(ptr, data, len);
     pooh->counter++; /* advance pointer */
     return len;
