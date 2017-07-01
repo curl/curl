8d2b1de284dd1e84e9c68a1a2a75aefaf7bf7826
Daniel Stenberg <daniel@haxx.se>
2017-07-01 16:40:51 +0200

    unit1399: add logging to time comparison
    ... to enable tracking down why autobuilds fail on this
    
    Bug: #1616

--- tests/unit/unit1399.c
+++ tests/unit/unit1399.c
@@ -39,7 +39,11 @@ static void unit_stop(void)
 static bool usec_matches_seconds(time_t time_usec, int expected_seconds)
 {
   int time_sec = (int)(time_usec / usec_magnitude);
-  return time_sec == expected_seconds;
+  bool same = (time_sec == expected_seconds);
+  fprintf(stderr, "is %d us same as %d seconds? %s\n",
+          (int)time_usec, expected_seconds,
+          same?"Yes":"No");
+  return same;
 }
 
 UNITTEST_START
