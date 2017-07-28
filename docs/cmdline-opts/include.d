171f8ded26d2845ef358c62f6e95ec34468349ae
Daniel Stenberg <daniel@haxx.se>
2017-07-28 18:22:50 +0200

    include.d: clarify that it concerns the response headers
    Reported-by: olesteban at github
    Fixes #1704

--- docs/cmdline-opts/include.d
+++ docs/cmdline-opts/include.d
@@ -3,5 +3,8 @@ Short: i
 Help: Include protocol headers in the output
 See-also: verbose
 ---
-Include the HTTP-header in the output. The HTTP-header includes things like
-server-name, date of the document, HTTP-version and more...
+Include the HTTP response headers in the output. The HTTP response headers can
+include things like server name, cookies, date of the document, HTTP version
+and more...
+
+To view the request headers, consider the --verbose option.
