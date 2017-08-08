13ef623a81736e95da1cc1d13a02dd78e228adee
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-08 19:22:34 +0000

    docs/comments: Update to secure URL versions
    Closes #1741

--- lib/strtoofft.c
+++ lib/strtoofft.c
@@ -29,7 +29,7 @@
  *
  * In the ISO C standard (IEEE Std 1003.1), there is a strtoimax() function we
  * could use in case strtoll() doesn't exist...  See
- * http://www.opengroup.org/onlinepubs/009695399/functions/strtoimax.html
+ * https://www.opengroup.org/onlinepubs/009695399/functions/strtoimax.html
  */
 
 #ifdef NEED_CURL_STRTOLL
