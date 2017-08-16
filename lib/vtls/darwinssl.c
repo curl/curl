75536e529abf799995b75610a75e85b891fdf8e7
Daniel Stenberg <daniel@haxx.se>
2017-08-16 07:58:44 +0200

    darwinssi: fix error: variable length array used

--- lib/vtls/darwinssl.c
+++ lib/vtls/darwinssl.c
@@ -1797,7 +1797,7 @@ static int read_cert(const char *file, unsigned char **out, size_t *outlen)
 {
   int fd;
   ssize_t n, len = 0, cap = 512;
-  unsigned char buf[cap], *data;
+  unsigned char buf[512], *data;
 
   fd = open(file, 0);
   if(fd < 0)
