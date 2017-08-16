fd8020106c4f27190039534e54060555c19ed162
Maksim Stsepanenka <good.man@tut.by>
2017-08-16 18:33:33 +0300

    http_proxy: a compilation error was fixed

--- lib/http_proxy.c
+++ lib/http_proxy.c
@@ -410,7 +410,7 @@ static CURLcode CONNECT(struct connectdata *conn,
         }
 
         /* convert from the network encoding */
-        result = Curl_convert_from_network(data, line_start, perline);
+        result = Curl_convert_from_network(data, s->line_start, s->perline);
         /* Curl_convert_from_network calls failf if unsuccessful */
         if(result)
           return result;
