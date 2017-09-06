281b30cf79e84f510a1a7078468944c55d674e03
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-09-07 00:55:38 +0200

    OpenSSL: fix yet another mistake while encapsulating SSL backend data
    Another mistake in my manual fixups of the largely mechanical
    search-and-replace ("connssl->" -> "BACKEND->"), just like the previous
    commit concerning HTTPS proxies (and hence not caught during my
    earlier testing).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -3366,10 +3366,12 @@ static bool Curl_ossl_data_pending(const struct connectdata *conn,
                                    int connindex)
 {
   const struct ssl_connect_data *connssl = &conn->ssl[connindex];
+  const struct ssl_connect_data *proxyssl = &conn->proxy_ssl[connindex];
   if(BACKEND->handle)
     /* SSL is in use */
     return (0 != SSL_pending(BACKEND->handle) ||
-           (BACKEND->handle && 0 != SSL_pending(BACKEND->handle))) ?
+           (proxyssl->backend->handle &&
+            0 != SSL_pending(proxyssl->backend->handle))) ?
            TRUE : FALSE;
   return FALSE;
 }
