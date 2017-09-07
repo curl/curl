d77ee04bcef8abe1ee508c31850ca12b24fb3efc
Daniel Stenberg <daniel@haxx.se>
2017-09-07 15:56:54 +0200

    fixup dardinwssl too

--- lib/vtls/darwinssl.c
+++ lib/vtls/darwinssl.c
@@ -2778,7 +2778,7 @@ static void Curl_darwinssl_sha256sum(const unsigned char *tmp, /* input */
                                      unsigned char *sha256sum, /* output */
                                      size_t sha256len)
 {
-  assert(sha256len >= SHA256_DIGEST_LENGTH);
+  assert(sha256len >= CURL_SHA256_DIGEST_LENGTH);
   (void)CC_SHA256(tmp, (CC_LONG)tmplen, sha256sum);
 }
 
