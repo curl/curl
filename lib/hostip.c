8a5ba3c70cfa20160df0050abc270459bf8d31ae
Rick Deist <dreckard@users.noreply.github.com>
2017-09-30 22:30:11 +0300

    lib: fixed whitespace issues

--- lib/hostip.c
+++ lib/hostip.c
@@ -376,35 +376,35 @@ void Curl_shuffle_addr(struct Curl_easy *data,
 {
   const int num_addrs = Curl_num_addresses(*addr);
 
-  if(num_addrs > 1)  {
+  if(num_addrs > 1) {
     Curl_addrinfo **nodes;
     infof(data, "Shuffling %i addresses", num_addrs);
 
     nodes = malloc(num_addrs*sizeof(*nodes));
-    if(nodes)  {
+    if(nodes) {
       int i;
       unsigned int *rnd;
       const size_t rnd_size = num_addrs * sizeof(*rnd);
 
       /* build a plain array of Curl_addrinfo pointers */
       nodes[0] = *addr;
-      for(i = 1; i < num_addrs; i++)  {
+      for(i = 1; i < num_addrs; i++) {
         nodes[i] = nodes[i-1]->ai_next;
       }
 
       rnd = malloc(rnd_size);
-      if(rnd)  {
+      if(rnd) {
         /* Fisher-Yates shuffle */
-        if(Curl_rand(data, (unsigned char *)rnd, rnd_size) == CURLE_OK)  {
+        if(Curl_rand(data, (unsigned char *)rnd, rnd_size) == CURLE_OK) {
           Curl_addrinfo *swap_tmp;
-          for(i = num_addrs - 1; i > 0; i--)  {
+          for(i = num_addrs - 1; i > 0; i--) {
             swap_tmp = nodes[rnd[i] % (i + 1)];
             nodes[rnd[i] % (i + 1)] = nodes[i];
             nodes[i] = swap_tmp;
           }
 
           /* relink list in the new order */
-          for(i = 1; i < num_addrs; i++)  {
+          for(i = 1; i < num_addrs; i++) {
             nodes[i-1]->ai_next = nodes[i];
           }
 
@@ -439,7 +439,7 @@ Curl_cache_addr(struct Curl_easy *data,
   struct Curl_dns_entry *dns2;
 
   /* shuffle addresses if requested */
-  if(data->set.dns_shuffle_addresses)  {
+  if(data->set.dns_shuffle_addresses) {
     Curl_shuffle_addr(data, &addr);
   }
 
