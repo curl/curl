06cb8adde2ef85c3ac610f93c0fc8a34a3e299b6
Patrick Monnerat <patrick@monnerat.net>
2017-10-09 01:26:27 +0100

    mime: properly unbind mime structure in curl_mime_free().
    This allows freeing a mime structure bound to the easy handle before
    curl_easy_cleanup().
    
    Fixes #1970.

--- lib/mime.c
+++ lib/mime.c
@@ -1130,6 +1130,7 @@ void curl_mime_free(curl_mime *mime)
   curl_mimepart *part;
 
   if(mime) {
+    mime_subparts_unbind(mime);  /* Be sure it's not referenced anymore. */
     while(mime->firstpart) {
       part = mime->firstpart;
       mime->firstpart = part->nextpart;
