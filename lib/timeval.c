ebcb130ff6508ddc3b0af3902fd6fbbfa4d3dd19
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2017-10-25 16:25:39 -0400

    timeval: use mach time on MacOS if necessary
    If clock_gettime() is not supported, one can use mach_absolute_time().

--- lib/timeval.c
+++ lib/timeval.c
@@ -84,6 +84,37 @@ struct curltime curlx_tvnow(void)
   return cnow;
 }
 
+#elif defined(__APPLE__)
+
+#include <stdint.h>
+#include <mach/mach_time.h>
+
+struct curltime curlx_tvnow(void)
+{
+  /*
+  ** Monotonic timer on Mac OS is provided by mach_absolute_time(), which
+  ** returns time in Mach "absolute time units," which are platform-dependent.
+  ** To convert to nanoseconds, one must use conversion factors specified by
+  ** mach_timebase_info().
+  */
+  static mach_timebase_info_data_t timebase;
+  struct curltime cnow;
+  uint64_t usecs;
+
+  if(0 == timebase.denom)
+    (void) mach_timebase_info(&timebase);
+
+  usecs = mach_absolute_time();
+  usecs *= timebase.numer;
+  usecs /= timebase.denom;
+  usecs /= 1000;
+
+  cnow.tv_sec = usecs / 1000000;
+  cnow.tv_usec = usecs % 1000000;
+
+  return cnow;
+}
+
 #elif defined(HAVE_GETTIMEOFDAY)
 
 struct curltime curlx_tvnow(void)
