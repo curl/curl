4e9e8fa374538a4f151452ad0837bda2667d8232
Daniel Stenberg <daniel@haxx.se>
2017-10-31 15:01:23 +0100

    CURLOPT_MAXREDIRS: allow -1 as a value
    ... which is valid according to documentation. Regression since
    f121575c0b5f.
    
    Verified now in test 501.
    
    Reported-by: cbartl on github
    Fixes #2038

--- tests/libtest/lib501.c
+++ tests/libtest/lib501.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2016, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -43,6 +43,7 @@ int test(char *URL)
   }
 
   test_setopt(curl, CURLOPT_HEADER, 1L);
+  test_setopt(curl, CURLOPT_MAXREDIRS, -1L);
 
   res = curl_easy_perform(curl);
 
