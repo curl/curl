72f74d70c548123f195c3c6b49fd778ecca682fc
Daniel Stenberg <daniel@haxx.se>
2017-11-17 11:27:59 +0100

    resolve: mention the [brackets] support in the associated docs

--- docs/cmdline-opts/resolve.d
+++ docs/cmdline-opts/resolve.d
@@ -14,4 +14,6 @@ different ports.
 The provided address set by this option will be used even if --ipv4 or --ipv6
 is set to make curl use another IP version.
 
+Support for providing the IP address within [brackets] was added in 7.57.0.
+
 This option can be used many times to add many host names to resolve.
