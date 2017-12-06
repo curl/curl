2d932e4959d38fda41755a35e7c392f8d69bbdc4
Eli Young <elyscape@gmail.com>
2017-12-06 12:39:22 -0800

    lib: add missing error checks to HAProxy protocol

--- lib/http.c
+++ lib/http.c
@@ -92,7 +92,8 @@ static int http_getsock_do(struct connectdata *conn,
                            int numsocks);
 static int http_should_fail(struct connectdata *conn);
 
-static void add_haproxy_protocol_header(struct connectdata *conn, bool *done);
+static CURLcode add_haproxy_protocol_header(struct connectdata *conn,
+                                            bool *done);
 
 #ifdef USE_SSL
 static CURLcode https_connecting(struct connectdata *conn, bool *done);
@@ -1361,7 +1362,9 @@ CURLcode Curl_http_connect(struct connectdata *conn, bool *done)
 
   if(conn->data->set.haproxyprotocol) {
     /* add HAProxy PROXY protocol header */
-    add_haproxy_protocol_header(conn, done);
+    result = add_haproxy_protocol_header(conn, done);
+    if(result)
+      return result;
   }
 
   if(conn->given->protocol & CURLPROTO_HTTPS) {
@@ -1413,8 +1416,15 @@ static CURLcode add_haproxy_protocol_header(struct connectdata *conn,
            conn->data->info.conn_primary_ip,
            conn->data->info.conn_local_port,
            conn->data->info.conn_primary_port);
+
   req_buffer = Curl_add_buffer_init();
-  Curl_add_bufferf(req_buffer, proxy_header);
+  if(!req_buffer)
+    return CURLE_OUT_OF_MEMORY;
+
+  result = Curl_add_bufferf(req_buffer, proxy_header);
+  if(result)
+    return result;
+
   result = Curl_add_buffer_send(req_buffer,
                                 conn,
                                 &conn->data->info.request_size,
