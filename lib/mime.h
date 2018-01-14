a80b2c69bc9c1cdec796ed93946c7ab9fb6da26d
Patrick Monnerat <patrick@monnerat.net>
2018-01-14 03:27:22 +0100

    mime: duplicate mime tree upon easy handle duplication.
    A mime tree attached to an easy handle using CURLOPT_MIMEPOST is
    strongly bound to the handle: there is a pointer to the easy handle in
    each item of the mime tree and following the parent pointer list
    of mime items ends in a dummy part stored within the handle.
    
    Because of this binding, a mime tree cannot be shared between different
    easy handles, thus it needs to be cloned upon easy handle duplication.
    
    There is no way for the caller to get the duplicated mime tree
    handle: it is then set to be automatically destroyed upon freeing the
    new easy handle.
    
    New test 654 checks proper mime structure duplication/release.
    
    Add a warning note in curl_mime_data_cb() documentation about sharing
    user data between duplicated handles.

--- lib/mime.h
+++ lib/mime.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -122,6 +122,7 @@ struct curl_mimepart_s {
 /* Prototypes. */
 void Curl_mime_initpart(curl_mimepart *part, struct Curl_easy *easy);
 void Curl_mime_cleanpart(curl_mimepart *part);
+CURLcode Curl_mime_duppart(curl_mimepart *dst, const curl_mimepart *src);
 CURLcode Curl_mime_set_subparts(curl_mimepart *part,
                                 curl_mime *subparts, int take_ownership);
 CURLcode Curl_mime_prepare_headers(curl_mimepart *part,
