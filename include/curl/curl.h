6f984b7f4004bf906ea535a61688cd8026f6bbae
Anders Bakken <agbakken@gmail.com>
2018-01-30 16:33:51 -0800

    url: Add option CURLOPT_HAPPY_EYEBALLS_TIMEOUT
    - Add new option CURLOPT_HAPPY_EYEBALLS_TIMEOUT to set libcurl's happy
      eyeball timeout value.
    
    - Add new optval macro CURL_HET_DEFAULT to represent the default happy
      eyeballs timeout value (currently 200 ms).
    
    - Add new tool option --happy-eyeballs-timeout-ms to expose
      CURLOPT_HAPPY_EYEBALLS_TIMEOUT. The -ms suffix is used because the
      other -timeout options in the tool expect seconds not milliseconds.
    
    Closes https://github.com/curl/curl/pull/2260

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -1838,7 +1838,7 @@ typedef enum {
   /* Function that will be called before a resolver request is made */
   CINIT(RESOLVER_START_FUNCTION, FUNCTIONPOINT, 272),
 
-  /* User data to pass to the resolver start callback */
+  /* User data to pass to the resolver start callback. */
   CINIT(RESOLVER_START_DATA, OBJECTPOINT, 273),
 
   CURLOPT_LASTENTRY /* the last unused */
