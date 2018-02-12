62cf2d180ed77bb977acf5d8b348d4431256638f
Patrick Monnerat <patrick@monnerat.net>
2018-02-12 16:43:15 +0100

    smtp: fix processing of initial dot in data
    RFC 5321 4.1.1.4 specifies the CRLF terminating the DATA command
    should be taken into account when chasing the <CRLF>.<CRLF> end marker.
    Thus a leading dot character in data is also subject to escaping.
    
    Tests 911 and test server are adapted to this situation.
    New tests 951 and 952 check proper handling of initial dot in data.
    
    Closes #2304

--- tests/ftpserver.pl
+++ tests/ftpserver.pl
@@ -6,7 +6,7 @@
 #                            | (__| |_| |  _ <| |___
 #                             \___|\___/|_| \_\_____|
 #
-# Copyright (C) 1998 - 2014, 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
+# Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution. The terms
@@ -920,7 +920,7 @@ sub DATA_smtp {
                 print FILE $line if(!$nosave);
 
                 $raw .= $line;
-                if($raw =~ /\x0d\x0a\x2e\x0d\x0a/) {
+                if($raw =~ /(?:^|\x0d\x0a)\x2e\x0d\x0a/) {
                     # end of data marker!
                     $eob = 1;
                 }
