f8e8028b0fc22e29a6feebef591e97b2d8c0b26a
Daniel Stenberg <daniel@haxx.se>
2018-02-18 23:09:24 +0100

    curl --version: show PSL if the run-time lib has it enabled
    ... not of the #define was set at build-time!

--- src/tool_help.c
+++ src/tool_help.c
@@ -507,7 +507,8 @@ static const struct feat feats[] = {
   {"HTTP2",          CURL_VERSION_HTTP2},
   {"UnixSockets",    CURL_VERSION_UNIX_SOCKETS},
   {"HTTPS-proxy",    CURL_VERSION_HTTPS_PROXY},
-  {"MultiSSL",       CURL_VERSION_MULTI_SSL}
+  {"MultiSSL",       CURL_VERSION_MULTI_SSL},
+  {"PSL",            CURL_VERSION_PSL},
 };
 
 void tool_help(void)
@@ -550,9 +551,6 @@ void tool_version_info(void)
     }
 #ifdef USE_METALINK
     printf("Metalink ");
-#endif
-#ifdef USE_LIBPSL
-    printf("PSL ");
 #endif
     puts(""); /* newline */
   }
