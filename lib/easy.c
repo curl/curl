6823d6b013189a2619b3f7a64ee0b024cbf1188b
Daniel Stenberg <daniel@haxx.se>
2018-04-08 23:47:23 +0200

    easy: make easy_transfer use curl_multi_poll()
    ... simplifies the code a lot!

--- lib/easy.c
+++ lib/easy.c
@@ -661,44 +661,17 @@ static CURLcode easy_transfer(struct Curl_multi *multi)
   bool done = FALSE;
   CURLMcode mcode = CURLM_OK;
   CURLcode result = CURLE_OK;
-  struct curltime before;
-  int without_fds = 0;  /* count number of consecutive returns from
-                           curl_multi_wait() without any filedescriptors */
 
   while(!done && !mcode) {
     int still_running = 0;
-    int rc;
+    mcode = curl_multi_poll(multi, NULL, 0, 1000, NULL);
 
-    before = Curl_now();
-    mcode = curl_multi_wait(multi, NULL, 0, 1000, &rc);
-
-    if(!mcode) {
-      if(!rc) {
-        struct curltime after = Curl_now();
-
-        /* If it returns without any filedescriptor instantly, we need to
-           avoid busy-looping during periods where it has nothing particular
-           to wait for */
-        if(Curl_timediff(after, before) <= 10) {
-          without_fds++;
-          if(without_fds > 2) {
-            int sleep_ms = without_fds < 10 ? (1 << (without_fds - 1)) : 1000;
-            Curl_wait_ms(sleep_ms);
-          }
-        }
-        else
-          /* it wasn't "instant", restart counter */
-          without_fds = 0;
-      }
-      else
-        /* got file descriptor, restart counter */
-        without_fds = 0;
-
+    if(!mcode)
       mcode = curl_multi_perform(multi, &still_running);
-    }
 
     /* only read 'still_running' if curl_multi_perform() return OK */
     if(!mcode && !still_running) {
+      int rc;
       CURLMsg *msg = curl_multi_info_read(multi, &rc);
       if(msg) {
         result = msg->data.result;
