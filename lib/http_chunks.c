290fa295e71d743b1a955d912e12a552eadf24ff
Stephan Muehlstrasser <stm@pdflib.com>
2018-04-17 09:10:16 +0200

    Fix "make checksrc" failure

--- lib/http_chunks.c
+++ lib/http_chunks.c
@@ -80,7 +80,7 @@ static bool Curl_isxdigit_ascii(char digit)
 {
   return (digit >= 0x30 && digit <= 0x39) /* 0-9 */
         || (digit >= 0x41 && digit <= 0x46) /* A-F */
-        || (digit >= 0x61 && digit <= 0x66) /* a-f */;
+        || (digit >= 0x61 && digit <= 0x66); /* a-f */
 }
 
 void Curl_httpchunk_init(struct connectdata *conn)
