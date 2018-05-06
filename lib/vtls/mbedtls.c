eb49683e5591900b0c79fd9649a5aaf5549ea61f
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2018-05-06 20:32:24 +0200

    lib: Fix format specifiers

--- lib/vtls/mbedtls.c
+++ lib/vtls/mbedtls.c
@@ -815,7 +815,7 @@ static void Curl_mbedtls_session_free(void *ptr)
 static size_t Curl_mbedtls_version(char *buffer, size_t size)
 {
   unsigned int version = mbedtls_version_get_number();
-  return snprintf(buffer, size, "mbedTLS/%d.%d.%d", version>>24,
+  return snprintf(buffer, size, "mbedTLS/%u.%u.%u", version>>24,
                   (version>>16)&0xff, (version>>8)&0xff);
 }
 
