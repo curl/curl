e6994fa374ea4724b614669253d8b4520835904c
Daniel Stenberg <daniel@haxx.se>
2018-05-18 00:06:59 +0200

    stub_gssapi: fix numerous 'unused parameter' warnings
    follow-up to d9e92fd9fd1d

--- tests/libtest/stub_gssapi.c
+++ tests/libtest/stub_gssapi.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 2017-2018, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -81,6 +81,12 @@ OM_uint32 gss_init_sec_context(OM_uint32 *min,
   char *token = NULL;
   const char *creds = NULL;
   gss_ctx_id_t ctx = NULL;
+
+  (void)initiator_cred_handle;
+  (void)mech_type;
+  (void)time_req;
+  (void)input_chan_bindings;
+  (void)actual_mech_type;
 
   if(!min)
     return GSS_S_FAILURE;
@@ -221,6 +227,8 @@ OM_uint32 gss_delete_sec_context(OM_uint32 *min,
                                  gss_ctx_id_t *context_handle,
                                  gss_buffer_t output_token)
 {
+  (void)output_token;
+
   if(!min)
     return GSS_S_FAILURE;
 
@@ -256,6 +264,7 @@ OM_uint32 gss_import_name(OM_uint32 *min,
                           gss_name_t *output_name)
 {
   char *name = NULL;
+  (void)input_name_type;
 
   if(!min)
     return GSS_S_FAILURE;
@@ -297,6 +306,7 @@ OM_uint32 gss_display_status(OM_uint32 *min,
                              gss_buffer_t status_string)
 {
   const char maj_str[] = "Stub GSS error";
+  (void)mech_type;
   if(min)
     *min = 0;
 
@@ -337,6 +347,10 @@ OM_uint32 gss_display_name(OM_uint32 *min,
                            gss_buffer_t output_name_buffer,
                            gss_OID *output_name_type)
 {
+  (void)min;
+  (void)input_name;
+  (void)output_name_buffer;
+  (void)output_name_type;
   return GSS_S_FAILURE;
 }
 
@@ -350,6 +364,15 @@ OM_uint32 gss_inquire_context(OM_uint32 *min,
                               int *locally_initiated,
                               int *open_context)
 {
+  (void)min;
+  (void)context_handle;
+  (void)src_name;
+  (void)targ_name;
+  (void)lifetime_rec;
+  (void)mech_type;
+  (void)ctx_flags;
+  (void)locally_initiated;
+  (void)open_context;
   return GSS_S_FAILURE;
 }
 
@@ -361,6 +384,13 @@ OM_uint32 gss_wrap(OM_uint32 *min,
                    int *conf_state,
                    gss_buffer_t output_message_buffer)
 {
+  (void)min;
+  (void)context_handle;
+  (void)conf_req_flag;
+  (void)qop_req;
+  (void)input_message_buffer;
+  (void)conf_state;
+  (void)output_message_buffer;
   return GSS_S_FAILURE;
 }
 
@@ -371,6 +401,12 @@ OM_uint32 gss_unwrap(OM_uint32 *min,
                      int *conf_state,
                      gss_qop_t *qop_state)
 {
+  (void)min;
+  (void)context_handle;
+  (void)input_message_buffer;
+  (void)output_message_buffer;
+  (void)conf_state;
+  (void)qop_state;
   return GSS_S_FAILURE;
 }
 
@@ -382,6 +418,13 @@ OM_uint32 gss_seal(OM_uint32 *min,
                    int *conf_state,
                    gss_buffer_t output_message_buffer)
 {
+  (void)min;
+  (void)context_handle;
+  (void)conf_req_flag;
+  (void)qop_req;
+  (void)input_message_buffer;
+  (void)conf_state;
+  (void)output_message_buffer;
   return GSS_S_FAILURE;
 }
 
@@ -392,6 +435,11 @@ OM_uint32 gss_unseal(OM_uint32 *min,
                      int *conf_state,
                      int *qop_state)
 {
+  (void)min;
+  (void)context_handle;
+  (void)input_message_buffer;
+  (void)output_message_buffer;
+  (void)conf_state;
+  (void)qop_state;
   return GSS_S_FAILURE;
 }
-
