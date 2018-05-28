7c189c66080664f48758a938d852e5d0471c3621
Frank Gevaerts <frank@gevaerts.be>
2018-05-28 14:17:51 +0200

    curl.1: Fix cmdline-opts reference errors.
    --data, --form, and --ntlm were declared to be mutually exclusive with
    non-existing options. --data and --form referred to --upload (which is
    short for --upload-file and therefore did work, so this one was merely
    a bit confusing), --ntlm referred to --negotiated instead of --negotiate.
    
    Closes #2612

--- docs/cmdline-opts/form.d
+++ docs/cmdline-opts/form.d
@@ -3,7 +3,7 @@ Short: F
 Arg: <name=content>
 Help: Specify multipart MIME data
 Protocols: HTTP SMTP IMAP
-Mutexed: data head upload
+Mutexed: data head upload-file
 ---
 For HTTP protocol family, this lets curl emulate a filled-in form in which a
 user has pressed the submit button. This causes curl to POST data using the
