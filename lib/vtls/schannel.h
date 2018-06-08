274940d7438af7ef92ce3e11d75620db94675932
Gisle Vanem <gisle.vanem@gmail.com>
2018-06-08 15:10:05 +0200

    boringssl + schannel: undef X509_NAME in lib/schannel.h
    Fixes the build problem when both boringssl and schannel are enabled.
    
    Fixes #2634
    Closes #2643

--- lib/vtls/schannel.h
+++ lib/vtls/schannel.h
@@ -32,6 +32,25 @@
 
 #include "urldata.h"
 
+/* <wincrypt.h> has been included via the above <schnlsp.h>.
+ * Or in case of ldap.c, it was included via <winldap.h>.
+ * And since <wincrypt.h> has this:
+ *   #define X509_NAME  ((LPCSTR) 7)
+ *
+ * And in BoringSSL's <openssl/base.h> there is:
+ *  typedef struct X509_name_st X509_NAME;
+ *  etc.
+ *
+ * this wil cause all kinds of C-preprocessing paste errors in
+ * BoringSSL's <openssl/x509.h>: So just undefine those defines here
+ * (and only here).
+ */
+#if defined(HAVE_BORINGSSL) || defined(OPENSSL_IS_BORINGSSL)
+# undef X509_NAME
+# undef X509_CERT_PAIR
+# undef X509_EXTENSIONS
+#endif
+
 extern const struct Curl_ssl Curl_ssl_schannel;
 
 CURLcode verify_certificate(struct connectdata *conn, int sockindex);
