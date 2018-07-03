f3bc02dd718d3a8b728b243659a3cdce4e2d8286
Jeroen <jeroenooms@gmail.com>
2018-07-03 19:16:02 +0200

    fix style

--- docs/examples/crawler.c
+++ docs/examples/crawler.c
@@ -1,16 +1,30 @@
-/* Simple web crawler (License: MIT)
- * Jeroen Ooms <jeroen@berkeley.edu>
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
  *
- * Compile using:
+ * Web crawler based on curl and libxml2.
+ * Copyright (C) 2018 Jeroen Ooms <jeroenooms@gmail.com>
+ * License: MIT
+ *
+ * To compile:
  *   gcc crawler.c $(pkg-config --cflags --libs libxml-2.0 libcurl)
+ *
+ */
+/* <DESC>
+ * Web crawler based on curl and libxml2 to stress-test curl with
+ * hundreds of concurrent connections to random servers.
+ * </DESC>
  */
 
 /* Parameters */
+int max_con = 200;
 int max_total = 10000;
-int max_concurrent = 500;
-int max_connections = 200;
+int max_requests = 500;
 int max_link_per_page = 10;
-char * start_page = "https://news.ycombinator.com";
+char *start_page = "https://news.ycombinator.com";
 
 #include <libxml/HTMLparser.h>
 #include <libxml/xpath.h>
@@ -22,7 +36,8 @@ char * start_page = "https://news.ycombinator.com";
 #include <signal.h>
 
 int pending_interrupt = 0;
-void sighandler(int dummy) {
+void sighandler(int dummy)
+{
   pending_interrupt = 1;
 }
 
@@ -32,7 +47,8 @@ typedef struct {
   size_t size;
 } memory;
 
-size_t grow_buffer(void *contents, size_t sz, size_t nmemb, void *ctx) {
+size_t grow_buffer(void *contents, size_t sz, size_t nmemb, void *ctx)
+{
   size_t realsize = sz * nmemb;
   memory *mem = (memory*) ctx;
   mem->buf = realloc(mem->buf, mem->size + realsize);
@@ -41,15 +57,16 @@ size_t grow_buffer(void *contents, size_t sz, size_t nmemb, void *ctx) {
   return realsize;
 }
 
-CURL * make_handle(char * url){
-  CURL * handle = curl_easy_init();
+CURL *make_handle(char *url)
+{
+  CURL *handle = curl_easy_init();
 
   /* Important: use HTTP2 over HTTPS */
   curl_easy_setopt(handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
   curl_easy_setopt(handle, CURLOPT_URL, url);
 
   /* buffer body */
-  memory * mem = malloc(sizeof(memory));
+  memory *mem = malloc(sizeof(memory));
   mem->size = 0;
   mem->buf = malloc(1);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, grow_buffer);
@@ -73,32 +90,34 @@ CURL * make_handle(char * url){
 }
 
 /* HREF finder implemented in libxml2 but could be any HTML parser */
-size_t follow_links(CURLM *multi_handle, memory * mem, char * url){
-  int opts = HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET;
+size_t follow_links(CURLM *multi_handle, memory *mem, char *url)
+{
+  int opts = HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | \
+             HTML_PARSE_NOWARNING | HTML_PARSE_NONET;
   htmlDocPtr doc = htmlReadMemory(mem->buf, mem->size, url, NULL, opts);
   if(!doc)
     return 0;
-  xmlChar * xpath = (xmlChar*) "//a/@href";
+  xmlChar *xpath = (xmlChar*) "//a/@href";
   xmlXPathContextPtr context = xmlXPathNewContext(doc);
   xmlXPathObjectPtr result = xmlXPathEvalExpression(xpath, context);
   xmlXPathFreeContext(context);
   if(!result)
     return 0;
   xmlNodeSetPtr nodeset = result->nodesetval;
-  if(xmlXPathNodeSetIsEmpty(nodeset)){
+  if(xmlXPathNodeSetIsEmpty(nodeset)) {
     xmlXPathFreeObject(result);
     return 0;
   }
   size_t count = 0;
-  for (int i = 0; i < nodeset->nodeNr; i++) {
+  for(int i = 0; i < nodeset->nodeNr; i++) {
     double r = rand();
     int x = r * nodeset->nodeNr / RAND_MAX;
-    xmlChar * href = xmlNodeListGetString(doc, nodeset->nodeTab[x]->xmlChildrenNode, 1);
-    char * absolute = (char *) xmlBuildURI(href, (xmlChar *) url);
+    const xmlNode *node = nodeset->nodeTab[x]->xmlChildrenNode;
+    xmlChar *href = xmlNodeListGetString(doc, node, 1);
+    char *absolute = (char *) xmlBuildURI(href, (xmlChar *) url);
     if(!absolute || strlen(absolute) < 20)
       continue;
-    if(!strncmp(absolute, "http://", 7) || !strncmp(absolute, "https://", 8)){
-      //printf("Following link (%d/%d): %s\n", x, nodeset->nodeNr, absolute);
+    if(!strncmp(absolute, "http://", 7) || !strncmp(absolute, "https://", 8)) {
       curl_multi_add_handle(multi_handle, make_handle(absolute));
       if(count++ == max_link_per_page)
         break;
@@ -106,27 +125,32 @@ size_t follow_links(CURLM *multi_handle, memory * mem, char * url){
     xmlFree(href);
     xmlFree(absolute);
   }
-  xmlXPathFreeObject (result);
+  xmlXPathFreeObject(result);
   return count;
 }
 
-int main(void){
+int is_html(char *ctype)
+{
+  return ctype != NULL && strlen(ctype) > 10 && strstr(ctype, "text/html");
+}
+
+int main(void)
+{
   signal(SIGINT, sighandler);
   LIBXML_TEST_VERSION;
   curl_global_init(CURL_GLOBAL_DEFAULT);
   CURLM *multi_handle = curl_multi_init();
-  curl_multi_setopt(multi_handle, CURLMOPT_MAX_TOTAL_CONNECTIONS, max_connections);
-  //curl_multi_setopt(multi_handle, CURLMOPT_MAX_HOST_CONNECTIONS, 6);
+  curl_multi_setopt(multi_handle, CURLMOPT_MAX_TOTAL_CONNECTIONS, max_con);
+  curl_multi_setopt(multi_handle, CURLMOPT_MAX_HOST_CONNECTIONS, 6L);
 
-  //enable http/2 if available
+  /* enables http/2 if available */
   #ifdef CURLPIPE_MULTIPLEX
     curl_multi_setopt(multi_handle, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX);
   #endif
 
-  //set a html start page
+  /* sets html start page */
   curl_multi_add_handle(multi_handle, make_handle(start_page));
 
-  /* we start some action by calling perform right away */
   int msgs_left;
   int pending = 0;
   int complete = 0;
@@ -142,26 +166,28 @@ int main(void){
       if(m->msg == CURLMSG_DONE) {
         CURL *handle = m->easy_handle;
         char *url;
-        memory * mem;
+        memory *mem;
         curl_easy_getinfo(handle, CURLINFO_PRIVATE, &mem);
         curl_easy_getinfo(handle, CURLINFO_EFFECTIVE_URL, &url);
-        if(m->data.result == CURLE_OK){
+        if(m->data.result == CURLE_OK) {
           long res_status;
           curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &res_status);
-          if(res_status == 200){
-            char *ct;
-            curl_easy_getinfo(handle, CURLINFO_CONTENT_TYPE, &ct);
-            printf("[%d] HTTP 200 (%s): %s\n", complete, ct, url);
-            if(ct && strlen(ct) > 10 && strstr(ct, "text/html") && mem->size > 100){
-              if(pending < max_concurrent && (complete + pending) < max_total){
+          if(res_status == 200) {
+            char *ctype;
+            curl_easy_getinfo(handle, CURLINFO_CONTENT_TYPE, &ctype);
+            printf("[%d] HTTP 200 (%s): %s\n", complete, ctype, url);
+            if(is_html(ctype) && mem->size > 100) {
+              if(pending < max_requests && (complete + pending) < max_total) {
                 pending += follow_links(multi_handle, mem, url);
                 still_running = 1;
               }
             }
-          } else {
+          }
+          else {
             printf("[%d] HTTP %d: %s\n", complete, (int) res_status, url);
           }
-        } else {
+        }
+        else {
           printf("[%d] Connection failure: %s\n", complete, url);
         }
         curl_multi_remove_handle(multi_handle, handle);
