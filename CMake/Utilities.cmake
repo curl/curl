f32257256595841a32bd6fef1d12c1c65e134b05
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-07-05 13:21:57 +0300

    CMake: Remove unused 'output_var' from 'collect_true'
    Variable 'output_var' is not used and can be removed.
    Function 'collect_true' renamed to 'count_true'.

--- CMake/Utilities.cmake
+++ CMake/Utilities.cmake
@@ -1,14 +1,13 @@
 # File containing various utilities
 
 # Returns a list of arguments that evaluate to true
-function(collect_true output_var output_count_var)
-  set(${output_var})
+function(count_true output_count_var)
+  set(lst)
   foreach(option_var IN LISTS ARGN)
     if(${option_var})
-      list(APPEND ${output_var} ${option_var})
+      list(APPEND lst ${option_var})
     endif()
   endforeach()
-  set(${output_var} ${${output_var}} PARENT_SCOPE)
-  list(LENGTH ${output_var} ${output_count_var})
-  set(${output_count_var} ${${output_count_var}} PARENT_SCOPE)
+  list(LENGTH lst lst_len)
+  set(${output_count_var} ${lst_len} PARENT_SCOPE)
 endfunction()
