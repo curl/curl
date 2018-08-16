497d992aa41345658fb7025ccc7ab2b208c6d6ab
Han Han <hhan@thousandeyes.com>
2018-08-16 14:47:53 -0700

    darwinssl: map additional server cert validation error codes
    SSLHandle may return a wide variety of Secure Transport error codes. We are grouping those related to server certificate validation under CURLE_SSL_CACERT. Note that most of these additions should have no effect as they occur way less often than errSSLXCertChainInvalid. In addition, OCSP response error is also mapped to the appropriate code that's consistent with openssl.

--- lib/vtls/darwinssl.c
+++ lib/vtls/darwinssl.c
@@ -2412,13 +2412,31 @@ darwinssl_connect_step2(struct connectdata *conn, int sockindex)
         failf(data, "SSL certificate problem: No root certificate");
         return CURLE_SSL_CACERT;
       case errSSLCertExpired:
+      case errSSLPeerCertExpired:
         failf(data, "SSL certificate problem: Certificate chain had an "
               "expired certificate");
         return CURLE_SSL_CACERT;
       case errSSLBadCert:
+      case errSSLPeerBadCert:
         failf(data, "SSL certificate problem: Couldn't understand the server "
               "certificate format");
-        return CURLE_SSL_CONNECT_ERROR;
+        return CURLE_SSL_CACERT;
+      case errSSLCertNotYetValid:
+        fail(data, "The certificate chain had a certificate that is not yet valid.");
+        return CURLE_SSL_CACERT;
+      case errSSLPeerUnsupportedCert:
+        fail(data, "An unsupported certificate format was encountered.");
+        return CURLE_SSL_CACERT;
+      case errSSLPeerCertRevoked:
+        fail(data, "The certificate was revoked.");
+        return CURLE_SSL_CACERT;
+      case errSSLPeerCertUnknown:
+        fail(data, "The certificate is unknown.");
+        return CURLE_SSL_CACERT;
+
+      case errSSLBadCertificateStatusResponse:
+        fail(data, "Bad OCSP response.");
+        return CURLE_SSL_INVALIDCERTSTATUS;
 
       /* These are all certificate problems with the client: */
       case errSecAuthFailed:
