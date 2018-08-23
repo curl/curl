a5a4129f64afdf6f77f7ce31e1b41f280ae44e57
Leonardo Taccari <iamleot@gmail.com>
2018-08-23 23:27:34 +0200

    gopher: Do not translate `?' to `%09'
    Since GOPHER support was added in curl `?' character was automatically
    translated to `%09' (`\t').
    However, this behaviour does not seems documented in RFC 4266 and for
    search selectors it is documented to directly use `%09' in the URL.
    Apart that several gopher servers in the current gopherspace have
    CGI support where `?' is used as part of the selector and translating
    it to `%09' often leads to surprising results.

--- lib/gopher.c
+++ lib/gopher.c
@@ -93,17 +93,10 @@ static CURLcode gopher_do(struct connectdata *conn, bool *done)
   }
   else {
     char *newp;
-    size_t j, i;
 
     /* Otherwise, drop / and the first character (i.e., item type) ... */
     newp = path;
     newp += 2;
-
-    /* ... then turn ? into TAB for search servers, Veronica, etc. ... */
-    j = strlen(newp);
-    for(i = 0; i<j; i++)
-      if(newp[i] == '?')
-        newp[i] = '\x09';
 
     /* ... and finally unescape */
     result = Curl_urldecode(data, newp, 0, &sel, &len, FALSE);
