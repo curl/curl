f16bed0c45dc63864fe2097b7df939276d96d62b
Daniel Stenberg <daniel@haxx.se>
2018-08-24 10:01:42 +0200

    curl: fix time-of-check, time-of-use race in dir creation
    Patch-by: Jay Satiro
    Detected by Coverity
    Fixes #2739
    Closes #2912

--- src/tool_dirhie.c
+++ src/tool_dirhie.c
@@ -139,12 +139,10 @@ CURLcode create_dir_hierarchy(const char *outfile, FILE *errors)
         else
           snprintf(dirbuildup, outlen, "%s%s", DIR_CHAR, tempdir);
       }
-      if(access(dirbuildup, F_OK) == -1) {
-        if(-1 == mkdir(dirbuildup, (mode_t)0000750)) {
-          show_dir_errno(errors, dirbuildup);
-          result = CURLE_WRITE_ERROR;
-          break; /* get out of loop */
-        }
+      if((-1 == mkdir(dirbuildup, (mode_t)0000750)) && (errno != EEXIST)) {
+        show_dir_errno(errors, dirbuildup);
+        result = CURLE_WRITE_ERROR;
+        break; /* get out of loop */
       }
     }
     tempdir = tempdir2;
