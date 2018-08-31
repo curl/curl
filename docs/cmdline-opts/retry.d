351c0f3a551952261b2678b27952ba97eddb1754
Jay Satiro <raysatiro@yahoo.com>
2018-08-31 15:27:54 -0400

    tool_operate: Add http code 408 to transient list for --retry
    - Treat 408 request timeout as transient so that curl will retry the
      request if --retry was used.
    
    Closes #2925

--- docs/cmdline-opts/retry.d
+++ docs/cmdline-opts/retry.d
@@ -6,7 +6,7 @@ Help: Retry request if transient problems occur
 If a transient error is returned when curl tries to perform a transfer, it
 will retry this number of times before giving up. Setting the number to 0
 makes curl do no retries (which is the default). Transient error means either:
-a timeout, an FTP 4xx response code or an HTTP 5xx response code.
+a timeout, an FTP 4xx response code or an HTTP 408 or 5xx response code.
 
 When curl is about to retry a transfer, it will first wait one second and then
 for all forthcoming retries it will double the waiting time until it reaches
