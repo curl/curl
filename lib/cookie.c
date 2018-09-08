6e054623b462494c26bbaf46da7ba4b5f40ff285
Daniel Gustafsson <daniel@yesql.se>
2018-09-08 22:23:33 +0200

    cookies: fix leak when writing cookies to file
    If the formatting fails, we error out on a fatal error and
    clean up on the way out. The array was however freed within
    the wrong scope and was thus never freed in case the cookies
    were written to a file instead of STDOUT.
    
    Closes #2957

--- lib/cookie.c
+++ lib/cookie.c
@@ -1504,10 +1504,9 @@ static int cookie_output(struct CookieInfo *c, const char *dumphere)
     format_ptr = get_netscape_format(array[i]);
     if(format_ptr == NULL) {
       fprintf(out, "#\n# Fatal libcurl error\n");
-      if(!use_stdout) {
-        free(array);
+      free(array);
+      if(!use_stdout)
         fclose(out);
-      }
       return 1;
     }
     fprintf(out, "%s\n", format_ptr);
