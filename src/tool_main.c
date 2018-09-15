5c55d5f03a245a558518d851c6ef5ab6126193a0
Rich Turner <rich@bitcrazed.com>
2018-09-15 09:43:58 -0700

    Corrected ENABL_VIRTUAL_TERMINAL_INPUT declararion

--- src/tool_main.c
+++ src/tool_main.c
@@ -260,7 +260,7 @@ void configure_terminal(TerminalSettings* ts)
 
   // Define the VT flags in case we're building with an older SDK
   #ifndef ENABLE_VIRTUAL_TERMINAL_INPUT
-    #define ENABLE_VIRTUAL_TERMINAL_INPUT 0x0002
+    #define ENABLE_VIRTUAL_TERMINAL_INPUT 0x0200
   #endif
 
   #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
