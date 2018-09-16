fa2d6ba84d7f87148738f3cbccf29016dd324f87
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2018-09-16 22:04:49 +0200

    printf: fix format specifiers
    Closes #3426

--- lib/transfer.c
+++ lib/transfer.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -318,7 +318,7 @@ CURLcode Curl_fillreadbuffer(struct connectdata *conn, size_t bytes,
     /* if we're not handling trailing data, proceed as usual */
     if(data->state.trailers_state != TRAILERS_SENDING) {
       hexlen = msnprintf(hexbuffer, sizeof(hexbuffer),
-                         "%x%s", nread, endofline_native);
+                         "%zx%s", nread, endofline_native);
 
       /* move buffer pointer */
       data->req.upload_fromhere -= hexlen;
