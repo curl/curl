faec182fd0a550b34ac10bdeb967cc24759cc10b
Daniel Stenberg <daniel@haxx.se>
2018-10-31 13:36:30 +0100

    schannel: use Curl_ prefix for global private symbols
    Curl_verify_certificate() must use the Curl_ prefix since it is globally
    available in the lib and otherwise steps outside of our namespace!
    
    Closes #3201

--- lib/vtls/schannel.c
+++ lib/vtls/schannel.c
@@ -1114,7 +1114,7 @@ schannel_connect_step2(struct connectdata *conn, int sockindex)
 
 #ifdef HAS_MANUAL_VERIFY_API
   if(conn->ssl_config.verifypeer && BACKEND->use_manual_cred_validation) {
-    return verify_certificate(conn, sockindex);
+    return Curl_verify_certificate(conn, sockindex);
   }
 #endif
 
