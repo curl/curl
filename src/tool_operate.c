6a78dc4684359d84ec88a63f8dc0371c32110a58
TechnoLord <rahul.golam19@gmail.com>
2018-11-04 19:44:29 +0400

    Fixed identation

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -1473,7 +1473,7 @@ static CURLcode operate_do(struct GlobalConfig *global,
         /* new in 7.25.0 and 7.44.0 */
         {
           long mask = (config->ssl_allow_beast ? CURLSSLOPT_ALLOW_BEAST : 0) |
-		      (config->ssl_disable_tls13_middlebox ? CURLSSLOPT_TLS13_MIDDLEBOX: 0) |
+                      (config->ssl_disable_tls13_middlebox ? CURLSSLOPT_TLS13_MIDDLEBOX : 0) |
                       (config->ssl_no_revoke ? CURLSSLOPT_NO_REVOKE : 0);
           if(mask)
             my_setopt_bitmask(curl, CURLOPT_SSL_OPTIONS, mask);
