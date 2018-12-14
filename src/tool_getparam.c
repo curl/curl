GerHobbelt/thirdparty-curl

curl: Support CURLOPT_LOCALADDR to bind to local address.
This allows a user to bind to both an interface (with CURLOPT_INTERFACE)
and a local IP address.  In doing so, it allows the user to work-around
some serious performance issues on machines with lots of virtual interfaces
because curl no longer has to scan all interfaces each time it makes
a connection.

Signed-off-by: Ben Greear <greearb@candelatech.com>

src/tool_getparam.c
@@ -106,6 +106,7 @@ static const struct LongShort aliases[]= {
   {"*u", "crlf",                     FALSE},
   {"*v", "stderr",                   TRUE},
   {"*w", "interface",                TRUE},
+  {"*W", "localaddr",                TRUE},
   {"*x", "krb",                      TRUE},
   {"*x", "krb4",                     TRUE},
          /* 'krb4' is the previous name */
@@ -699,6 +700,10 @@ ParameterError getparameter(char *flag,    /* f or -long-flag */
       case 'w': /* --interface */
         /* interface */
         GetStr(&config->iface, nextarg);
+        break;
+      case 'W': /* --localaddr */
+        /* addr in dot notation */
+        GetStr(&config->localaddr, nextarg);
         break;
       case 'x': /* --krb */
         /* kerberos level string */

curl/curl

src/tool_getparam.c
@@ -112,6 +112,7 @@ static const struct LongShort aliases[]= {
   {"*u", "crlf",                     ARG_BOOL},
   {"*v", "stderr",                   ARG_FILENAME},
   {"*w", "interface",                ARG_STRING},
+  {"*W", "localaddr",                ARG_STRING},
   {"*x", "krb",                      ARG_STRING},
   {"*x", "krb4",                     ARG_STRING},
          /* 'krb4' is the previous name */
@@ -791,6 +792,10 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
       case 'w': /* --interface */
         /* interface */
         GetStr(&config->iface, nextarg);
+        break;
+      case 'W': /* --localaddr */
+        /* addr in dot notation */
+        GetStr(&config->localaddr, nextarg);
         break;
       case 'x': /* --krb */
         /* kerberos level string */