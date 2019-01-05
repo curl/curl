b0a43aade1c786f5c15d6a9fc478c7ae305d8db6
Michael Kujawa <kujo@insightfulengine.com>
2019-01-04 20:18:25 -0500

    timeval: Disable MSVC Analyzer GetTickCount warning
    Compiling with msvc /analyze and a recent Windows SDK warns against
    using GetTickCount (Suggests to use GetTickCount64 instead.)
    
    Since GetTickCount is only being used when GetTickCount64 isn't
    available, I am disabling that warning.
    
    Fixes https://github.com/curl/curl/issues/3437
    Closes https://github.com/curl/curl/pull/3440

--- lib/timeval.c
+++ lib/timeval.c
@@ -47,7 +47,16 @@ struct curltime Curl_now(void)
       (int)((count.QuadPart % freq.QuadPart) * 1000000 / freq.QuadPart);
   }
   else {
+    /* Disable /analyze warning that GetTickCount64 is preferred  */
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:28159)
+#endif
     DWORD milliseconds = GetTickCount();
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
     now.tv_sec = milliseconds / 1000;
     now.tv_usec = (milliseconds % 1000) * 1000;
   }
