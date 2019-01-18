39df4073e5413fcdbb5a38da0c1ce6f1c0ceb484
Daniel Gustafsson <daniel@yesql.se>
2019-01-19 00:42:47 +0100

    smtp: avoid risk of buffer overflow in strtol
    If the incoming len 5, but the buffer does not have a termination
    after 5 bytes, the strtol() call may keep reading through the line
    buffer until is exceeds its boundary. Fix by ensuring that we are
    using a bounded read with a temporary buffer on the stack.
    
    Bug: https://curl.haxx.se/docs/CVE-2019-3823.html
    Reported-by: Brian Carpenter (Geeknik Labs)
    CVE-2019-3823

--- lib/smtp.c
+++ lib/smtp.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -207,8 +207,12 @@ static bool smtp_endofresp(struct connectdata *conn, char *line, size_t len,
      Section 4. Examples of RFC-4954 but some e-mail servers ignore this and
      only send the response code instead as per Section 4.2. */
   if(line[3] == ' ' || len == 5) {
+    char tmpline[6];
+
     result = TRUE;
-    *resp = curlx_sltosi(strtol(line, NULL, 10));
+    memset(tmpline, '\0', sizeof(tmpline));
+    memcpy(tmpline, line, (len == 5 ? 5 : 3));
+    *resp = curlx_sltosi(strtol(tmpline, NULL, 10));
 
     /* Make sure real server never sends internal value */
     if(*resp == 1)
