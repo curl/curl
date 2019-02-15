c52620c249c47a7a916435647155abf4bbfd8018
Patrick Monnerat <patrick@monnerat.net>
2019-02-15 20:19:00 +0100

    version.c: silent scan-build even when librtmp is not enabled

--- lib/version.c
+++ lib/version.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -211,6 +211,10 @@ char *curl_version(void)
 */
   }
 #endif
+
+  /* Silent scan-build even if librtmp is not enabled. */
+  (void) left;
+  (void) ptr;
 
   initialized = true;
   return version;
