50afa478e1c4c2a640b37d8d9da477de07714d2b
Daniel Stenberg <daniel@haxx.se>
2019-03-08 16:45:34 +0100

    cmdline-opts/proxytunnel.d: the option tunnnels all protocols
    Clarify the language and simplify.
    
    Reported-by: Daniel Lublin
    Closes #3658

--- docs/cmdline-opts/proxytunnel.d
+++ docs/cmdline-opts/proxytunnel.d
@@ -3,11 +3,10 @@ Short: p
 Help: Operate through an HTTP proxy tunnel (using CONNECT)
 See-also: proxy
 ---
-When an HTTP proxy is used --proxy, this option will cause non-HTTP protocols
-to attempt to tunnel through the proxy instead of merely using it to do
-HTTP-like operations. The tunnel approach is made with the HTTP proxy CONNECT
-request and requires that the proxy allows direct connect to the remote port
-number curl wants to tunnel through to.
+When an HTTP proxy is used --proxy, this option will make curl tunnel through
+the proxy. The tunnel approach is made with the HTTP proxy CONNECT request and
+requires that the proxy allows direct connect to the remote port number curl
+wants to tunnel through to.
 
 To suppress proxy CONNECT response headers when curl is set to output headers
 use --suppress-connect-headers.
