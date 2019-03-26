93213b2421429af74a6da7f61566292aa978fb22
Christian Schmitz <support@monkeybreadsoftware.de>
2019-03-26 09:53:03 +0100

    ntlm: USE_WIN32_CRYPTO check removed to get USE_NTLM2SESSION set
    Closes #3704

--- lib/curl_ntlm_core.h
+++ lib/curl_ntlm_core.h
@@ -48,7 +48,9 @@
 /* Define USE_NTLM2SESSION in order to make the type-3 message include the
    NTLM2Session response message, requires USE_NTRESPONSES defined to 1 and a
    Crypto engine that we have curl_ssl_md5sum() for. */
-#if defined(USE_NTRESPONSES) && !defined(USE_WIN32_CRYPTO)
+#if defined(USE_NTRESPONSES) && \
+  (!defined(USE_WIN32_CRYPTO) || \
+  (defined(USE_SSL) && !defined(CURL_DISABLE_CRYPTO_AUTH)))
 #define USE_NTLM2SESSION
 #endif
 
