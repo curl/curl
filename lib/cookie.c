73891dd063e87293d144c23c4b42a0148be47bae
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2019-03-30 19:11:57 +0000

    Fix build with ALTSVC enabled and COOKIES disabled
    ALTSVC requires Curl_get_line which is defined in lib/cookie.c inside a #if
    check of HTTP and COOKIES. That makes Curl_get_line undefined if COOKIES is
    disabled. This is a workaround to define Curl_get_line unconditionally.

--- lib/cookie.c
+++ lib/cookie.c
@@ -1085,6 +1085,8 @@ Curl_cookie_add(struct Curl_easy *data,
   return co;
 }
 
+#endif /* CURL_DISABLE_HTTP || CURL_DISABLE_COOKIES */
+
 /*
  * get_line() makes sure to only return complete whole lines that fit in 'len'
  * bytes and end with a newline.
@@ -1112,6 +1114,7 @@ char *Curl_get_line(char *buf, int len, FILE *input)
   return NULL;
 }
 
+#if !defined(CURL_DISABLE_HTTP) && !defined(CURL_DISABLE_COOKIES)
 
 /*****************************************************************************
  *
