dc0712eea3f4354e29330477bcf15b96f85e205c
Marcel Raad <Marcel.Raad@teamviewer.com>
2019-04-10 19:26:35 +0200

    tests/server/util: fix Windows Unicode build
    Always use the ANSI version of FormatMessage as we don't have the
    curl_multibyte gear available here.
    
    Closes https://github.com/curl/curl/pull/3758

--- tests/server/util.c
+++ tests/server/util.c
@@ -150,8 +150,8 @@ void win32_perror(const char *msg)
   char buf[512];
   DWORD err = SOCKERRNO;
 
-  if(!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, err,
-                    LANG_NEUTRAL, buf, sizeof(buf), NULL))
+  if(!FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, err,
+                     LANG_NEUTRAL, buf, sizeof(buf), NULL))
     msnprintf(buf, sizeof(buf), "Unknown error %lu (%#lx)", err, err);
   if(msg)
     fprintf(stderr, "%s: ", msg);
