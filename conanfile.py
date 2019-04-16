68ca49c8847de321cea4bf35b49d1ba0041db4d1
deekshith <venkatadeekshith@elear.solutions>
2019-04-16 14:19:05 +0530

    Provides conan recipefile

---
+++ conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile, CMake, tools
+
+
+class CurllibConan(ConanFile):
+    name = "curl"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Elearcommonlib here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    requires="openssl/0.1@demo/test"
+    options = {"shared": [True, False]}
+    default_options = "shared=False","openssl:no_asm=True"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        if (self.settings.os == "Android"):
+            cmake.definitions["Platform"] = "android"
+        cmake.configure(source_folder=".")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.a", dst="lib", src="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["curl"]
+
