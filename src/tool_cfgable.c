1e08a7925830d7822f911165d9789e5e4f765534
Steve Holme <steve_holme@hotmail.com>
2019-04-19 14:26:47 +0100

    curl: --sasl-authzid added to support CURLOPT_SASL_AUTHZID from the tool
    Ref: https://github.com/curl/curl/issues/3653
    Ref: https://github.com/curl/curl/pull/3790
    
    NOTE: This commit was cherry-picked and is part of a series of commits
    that added the authzid feature for upcoming 7.66.0. The series was
    temporarily reverted in db8ec1f so that it would not ship in a 7.65.x
    patch release.
    
    Closes https://github.com/curl/curl/pull/4186

--- src/tool_cfgable.c
+++ src/tool_cfgable.c
@@ -133,6 +133,7 @@ static void free_config_fields(struct OperationConfig *config)
   Curl_safefree(config->krblevel);
 
   Curl_safefree(config->oauth_bearer);
+  Curl_safefree(config->sasl_authzid);
 
   Curl_safefree(config->unix_socket_path);
   Curl_safefree(config->writeout);
