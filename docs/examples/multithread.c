2f53bc61286fa94695532a2cd1c4495e9ed91928
Marcel Raad <Marcel.Raad@teamviewer.com>
2019-05-20 11:50:23 +0200

    examples: remove unsued variables
    Fixes Codacy/CppCheck warnings.
    
    Closes

--- docs/examples/multithread.c
+++ docs/examples/multithread.c
@@ -69,16 +69,15 @@ int main(int argc, char **argv)
 {
   pthread_t tid[NUMT];
   int i;
-  int error;
 
   /* Must initialize libcurl before any threads are started */
   curl_global_init(CURL_GLOBAL_ALL);
 
   for(i = 0; i< NUMT; i++) {
-    error = pthread_create(&tid[i],
-                           NULL, /* default attributes please */
-                           pull_one_url,
-                           (void *)urls[i]);
+    int error = pthread_create(&tid[i],
+                               NULL, /* default attributes please */
+                               pull_one_url,
+                               (void *)urls[i]);
     if(0 != error)
       fprintf(stderr, "Couldn't run thread number %d, errno %d\n", i, error);
     else
@@ -87,7 +86,7 @@ int main(int argc, char **argv)
 
   /* now wait for all threads to terminate */
   for(i = 0; i< NUMT; i++) {
-    error = pthread_join(tid[i], NULL);
+    pthread_join(tid[i], NULL);
     fprintf(stderr, "Thread %d terminated\n", i);
   }
 
