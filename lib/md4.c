3557871e0701b608cff2922e99df5577f1715b63
Daniel Stenberg <daniel@haxx.se>
2019-05-22 10:40:02 +0200

    md4: build correctly with openssl without MD4
    Reported-by: elsamuko at github
    Fixes #3921

--- lib/md4.c
+++ lib/md4.c
@@ -38,6 +38,10 @@
 
 #include "curl_setup.h"
 
+#ifdef USE_OPENSSL
+#include <openssl/opensslconf.h>
+#endif
+
 /* The NSS, OS/400, and when not included, OpenSSL and mbed TLS crypto
  * libraries do not provide the MD4 hash algorithm, so we use this
  * implementation of it */
