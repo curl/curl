4ae6b5bb855bd63464c78fe37598f4655bf7d801
Daniel Stenberg <daniel@haxx.se>
2019-06-08 22:39:36 +0200

    bump: start working on 7.65.2

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -30,13 +30,13 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.65.1-DEV"
+#define LIBCURL_VERSION "7.65.2-DEV"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
 #define LIBCURL_VERSION_MAJOR 7
 #define LIBCURL_VERSION_MINOR 65
-#define LIBCURL_VERSION_PATCH 1
+#define LIBCURL_VERSION_PATCH 2
 
 /* This is the numeric version of the libcurl version number, meant for easier
    parsing and comparions by programs. The LIBCURL_VERSION_NUM define will
@@ -57,7 +57,7 @@
    CURL_VERSION_BITS() macro since curl's own configure script greps for it
    and needs it to contain the full number.
 */
-#define LIBCURL_VERSION_NUM 0x074101
+#define LIBCURL_VERSION_NUM 0x074102
 
 /*
  * This is the date and time when the full source package was created. The
