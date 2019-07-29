db0a0dfb0eb41d39273b0590b992df58f38b9a4d
Daniel Stenberg <daniel@haxx.se>
2019-07-29 22:10:13 +0200

    curl: cap the maximum allowed values for retry time arguments
    ... to avoid integer overflows later when multiplying with 1000 to
    convert seconds to milliseconds.
    
    Added test 1269 to verify.
    
    Reported-by: Jason Lee
    Closes #4166

--- src/tool_paramhlp.h
+++ src/tool_paramhlp.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -33,6 +33,7 @@ void cleanarg(char *str);
 
 ParameterError str2num(long *val, const char *str);
 ParameterError str2unum(long *val, const char *str);
+ParameterError str2unummax(long *val, const char *str, long max);
 ParameterError str2udouble(double *val, const char *str, long max);
 
 long proto2num(struct OperationConfig *config, long *val, const char *str);
