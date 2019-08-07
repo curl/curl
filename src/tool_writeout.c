eb63b27b6b76ccdb712d0bd4d00d779f658a6390
Daniel Stenberg <daniel@haxx.se>
2019-08-07 09:52:14 +0200

    curl: have -w's 'http_version' show '3' for HTTP/3
    Closes #4196

--- src/tool_writeout.c
+++ src/tool_writeout.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -313,6 +313,9 @@ void ourWriteOut(CURL *curl, struct OutStruct *outs, const char *writeinfo)
                     break;
                   case CURL_HTTP_VERSION_2_0:
                     version = "2";
+                    break;
+                  case CURL_HTTP_VERSION_3:
+                    version = "3";
                     break;
                   }
 
