30e6483763a05268dd8c3902b01491d2f18571be
Daniel Stenberg <daniel@haxx.se>
2019-08-13 16:48:03 +0200

    fixup http3-present.c problems

--- docs/examples/http3-present.c
+++ docs/examples/http3-present.c
@@ -30,16 +30,16 @@ int main(void)
 {
   curl_version_info_data *ver;
 
-  curl_global_init();
+  curl_global_init(CURL_GLOBAL_ALL);
 
   ver = curl_version_info(CURLVERSION_NOW);
-  if(ver->flags & CURL_VERSION_HTTP2)
+  if(ver->features & CURL_VERSION_HTTP2)
     printf("HTTP/2 support is present\n");
 
-  if(ver->flags & CURL_VERSION_HTTP3)
+  if(ver->features & CURL_VERSION_HTTP3)
     printf("HTTP/3 support is present\n");
 
-  if(ver->flags & CURL_VERSION_ALTSVC)
+  if(ver->features & CURL_VERSION_ALTSVC)
     printf("Alt-svc support is present\n");
 
   curl_global_cleanup();
