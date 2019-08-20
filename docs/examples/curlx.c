e7cbf2a0a279dfafd2ec32e024eea2f625f4dc07
Gisle Vanem <gisle.vanem@gmail.com>
2019-08-20 15:17:16 +0200

    Initialise 'mimetype'
    With MSVC's option `/RTCu` (Uninitialized local usage checks), a simple `curlx.exe` fails with a dialogue box.
    A simple fix is just to initialise to NULL.

--- docs/examples/curlx.c
+++ docs/examples/curlx.c
@@ -277,7 +277,7 @@ int main(int argc, char **argv)
 
   int tabLength = 100;
   char *binaryptr;
-  char *mimetype;
+  char *mimetype = NULL;
   char *mimetypeaccept = NULL;
   char *contenttype;
   const char **pp;
