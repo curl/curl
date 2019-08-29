dea1d0a0d15dc732ad62f6378432f6a27bcca541
Daniel Stenberg <daniel@haxx.se>
2019-08-29 18:55:29 +0200

    quiche: decrease available buffer size, don't assign it!
    Found-by: Jeremy Lainé

--- lib/vquic/quiche.c
+++ lib/vquic/quiche.c
@@ -442,7 +442,7 @@ static ssize_t h3_stream_recv(struct connectdata *conn,
         buf[0] = '\r';
         buf[1] = '\n';
         buf += 2;
-        buffersize = 2;
+        buffersize -= 2;
         stream->firstbody = TRUE;
         recvd = 2; /* two bytes already */
       }
