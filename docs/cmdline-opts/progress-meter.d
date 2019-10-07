981c3bbe0489789f4b529d2d9f6c5158628a109b
Daniel Stenberg <daniel@haxx.se>
2019-10-07 14:54:35 +0200

    curl: --no-progress-meter
    New option that allows a user to ONLY switch off curl's progress meter
    and leave everything else in "talkative" mode.
    
    Fixes #4422

---
+++ docs/cmdline-opts/progress-meter.d
@@ -0,0 +1,8 @@
+Long: progress-meter
+Help: Show the progress meter
+See-also: verbose silent
+Added: 7.67.0
+---
+Option to only switch the progress meter output on or off (with
+--no-progress-meter) without muting or otherwise affecting warning and
+informational messages like --silent does.
