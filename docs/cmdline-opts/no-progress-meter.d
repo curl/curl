ad17b4861ce47c62178a929c6987d0ae708b9155
Daniel Stenberg <daniel@haxx.se>
2019-10-08 23:05:35 +0200

    docs: document it as --no-progress-meter instead of the reverse
    Follow-up to 93373a960c3bb4
    
    Reported-by: infinnovation-dev on github
    Fixes #4474

---
+++ docs/cmdline-opts/no-progress-meter.d
@@ -0,0 +1,10 @@
+Long: no-progress-meter
+Help: Do not show the progress meter
+See-also: verbose silent
+Added: 7.67.0
+---
+Option to switch off the progress meter output without muting or otherwise
+affecting warning and informational messages like --silent does.
+
+Note that this is the negated option name documented. You can thus use
+--progress-meter to enable the progress meter again.
