cc4cf93e52802e6b450a3dceae86c4f289a0bb65
Daniel Stenberg <daniel@haxx.se>
2019-11-28 22:27:45 +0100

    sha256: bump the copyright year range
    Follow-up from 66e21520f

--- lib/sha256.c
+++ lib/sha256.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2018, Florin Petriuc, <petriuc.florin@gmail.com>
+ * Copyright (C) 1998 - 2019, Florin Petriuc, <petriuc.florin@gmail.com>
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
