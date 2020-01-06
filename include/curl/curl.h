ade419c5b6736b04f0e6d1b623075f768fdc07da
Daniel Stenberg <daniel@haxx.se>
2020-01-06 23:31:42 +0100

    curl.h: remove WIN32 define
    It isn't our job to define this in a public header - and it defines a
    name outside of our naming scope.

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -36,15 +36,6 @@
 
 #include "curlver.h"         /* libcurl version defines   */
 #include "system.h"          /* determine things run-time */
-
-/*
- * Define WIN32 when build target is Win32 API
- */
-
-#if (defined(_WIN32) || defined(__WIN32__)) && \
-     !defined(WIN32) && !defined(__SYMBIAN32__)
-#define WIN32
-#endif
 
 #include <stdio.h>
 #include <limits.h>
