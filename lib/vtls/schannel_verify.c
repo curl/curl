29babeafec2cbe4a598a4d90d82bb80b7534f959
Daniel Stenberg <daniel@haxx.se>
2020-01-12 16:55:34 +0100

    misc: Copyright year out of date, should be 2020
    Follow-up to recent commits
    
    [skip ci]

--- lib/vtls/schannel_verify.c
+++ lib/vtls/schannel_verify.c
@@ -7,7 +7,7 @@
  *
  * Copyright (C) 2012 - 2016, Marc Hoersken, <info@marc-hoersken.de>
  * Copyright (C) 2012, Mark Salisbury, <mark.salisbury@hp.com>
- * Copyright (C) 2012 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 2012 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
