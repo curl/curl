85f625da3b3e60a222f06518245cda0d01671371
PyvesB <pierre-yves.bigourdan@bbc.co.uk>
2020-02-06 15:43:06 +0000

    digest: Do not quote algorithm in HTTP authorisation
    RFC 7616 section 3.4 (The Authorization Header Field) states that "For
    historical reasons, a sender MUST NOT generate the quoted string syntax
    for the following parameters: algorithm, qop, and nc". This removes the
    quoting for the algorithm parameter.

--- lib/vauth/digest.c
+++ lib/vauth/digest.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -899,7 +899,7 @@ static CURLcode _Curl_auth_create_digest_http_message(
 
   if(digest->algorithm) {
     /* Append the algorithm */
-    tmp = aprintf("%s, algorithm=\"%s\"", response, digest->algorithm);
+    tmp = aprintf("%s, algorithm=%s", response, digest->algorithm);
     free(response);
     if(!tmp)
       return CURLE_OUT_OF_MEMORY;
