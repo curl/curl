e21ab4ff1aecc3fccf056a658e676c23981e5672
Daniel Stenberg <daniel@haxx.se>
2020-02-09 17:13:53 +0100

    fixup lib1908 to deal with torture better

--- tests/libtest/lib1908.c
+++ tests/libtest/lib1908.c
@@ -27,20 +27,21 @@
 
 int test(char *URL)
 {
-  CURLcode ret;
+  CURLcode ret = CURLE_OK;
   CURL *hnd;
   start_test_timing();
 
   curl_global_init(CURL_GLOBAL_ALL);
 
   hnd = curl_easy_init();
-  curl_easy_setopt(hnd, CURLOPT_URL, URL);
-  curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
-  curl_easy_setopt(hnd, CURLOPT_ALTSVC, "log/altsvc-1908");
-  ret = curl_easy_perform(hnd);
-  curl_easy_reset(hnd);
-  curl_easy_cleanup(hnd);
-
+  if(hnd) {
+    curl_easy_setopt(hnd, CURLOPT_URL, URL);
+    curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(hnd, CURLOPT_ALTSVC, "log/altsvc-1908");
+    ret = curl_easy_perform(hnd);
+    curl_easy_reset(hnd);
+    curl_easy_cleanup(hnd);
+  }
   curl_global_cleanup();
   return (int)ret;
 }
