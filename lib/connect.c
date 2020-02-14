16e48a16ba972ba998eddb0b3ca2ae84ed98496c
Daniel Stenberg <daniel@haxx.se>
2020-02-15 00:00:45 +0100

    ftp: convert 'sock_accepted' to a plain boolean
    This was an array indexed with sockindex but it was only ever used for
    the secondary socket.

--- lib/connect.c
+++ lib/connect.c
@@ -1428,12 +1428,11 @@ int Curl_closesocket(struct connectdata *conn,
                       curl_socket_t sock)
 {
   if(conn && conn->fclosesocket) {
-    if((sock == conn->sock[SECONDARYSOCKET]) &&
-       conn->sock_accepted[SECONDARYSOCKET])
+    if((sock == conn->sock[SECONDARYSOCKET]) && conn->sock_accepted)
       /* if this socket matches the second socket, and that was created with
          accept, then we MUST NOT call the callback but clear the accepted
          status */
-      conn->sock_accepted[SECONDARYSOCKET] = FALSE;
+      conn->sock_accepted = FALSE;
     else {
       int rc;
       Curl_multi_closed(conn->data, sock);
