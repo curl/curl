baf0b680f955833048eaeaf12c4bed759c337904
Steve Holme <steve_holme@hotmail.com>
2020-02-23 18:46:24 +0000

    md4: Use const for the length input parameter
    This keeps the interface the same as md5 and sha256.

--- lib/md4.c
+++ lib/md4.c
@@ -508,9 +508,11 @@ static void MD4_Final(unsigned char *result, MD4_CTX *ctx)
 
 #endif /* CRYPTO LIBS */
 
-void Curl_md4it(unsigned char *output, const unsigned char *input, size_t len)
+void Curl_md4it(unsigned char *output, const unsigned char *input,
+                const size_t len)
 {
   MD4_CTX ctx;
+
   MD4_Init(&ctx);
   MD4_Update(&ctx, input, curlx_uztoui(len));
   MD4_Final(output, &ctx);
