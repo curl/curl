69334d88e8dfeb958f90ad76692f0cd679cacabb
Rolf Eike Beer <eb@emlix.com>
2020-02-24 13:34:54 +0100

    CMake: clean up and improve build instructions
    -remove check for unsupported old CMake versions
    
    -not link to c-ares library twice
    
    -modernize custom Find modules
    
     FindLibSSH2:
       -pass version to FPHSA to show it in the output
       -use LIBSSH2_VERSION define to extract the version number in one shot. This
        variable exists in the header for 10 years.
       -remove unneeded code
    
     FindNGHTTP2.cmake
       -drop needless FPHSA argument
       -mark found variables as advanced
    
     FindNSS.cmake:
       -show version number
    
     FindCARES.cmake:
       -drop default paths
       -use FPHSA instead of checking things by hand
    
    -remove needless explict variable dereference
    
    -simplify count_true()
    
    -allow all policies up to version 3.16 to be set to NEW
    
    -do not rerun check for -Wstrict-aliasing=3 every time
    
     In contrast to every other compiler flag this has a = in it, which CMake can't
     have in a variable name.
    
    -only read the interesting strings from curlver.h

--- CMake/Utilities.cmake
+++ CMake/Utilities.cmake
@@ -2,12 +2,11 @@
 
 # Returns a list of arguments that evaluate to true
 function(count_true output_count_var)
-  set(lst)
+  set(lst_len 0)
   foreach(option_var IN LISTS ARGN)
     if(${option_var})
-      list(APPEND lst ${option_var})
+      math(EXPR lst_len "${lst_len} + 1")
     endif()
   endforeach()
-  list(LENGTH lst lst_len)
   set(${output_count_var} ${lst_len} PARENT_SCOPE)
 endfunction()
