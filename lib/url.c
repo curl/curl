e58fff476f624ca50f9d5eccdc3d1d0991ebfa76
Daniel Stenberg <daniel@haxx.se>
2020-02-27 11:06:14 +0100

    Curl_is_ASCII_name: handle a NULL argument
    Make the function tolerate a NULL pointer input to avoid dereferencing
    that pointer.
    
    Follow-up to efce3ea5a85126d
    Detected by OSS-Fuzz
    
    Bug: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=20907
    Fixes #4985

--- lib/url.c
+++ lib/url.c
@@ -1438,7 +1438,11 @@ void Curl_verboseconnect(struct connectdata *conn)
  */
 bool Curl_is_ASCII_name(const char *hostname)
 {
+  /* get an UNSIGNED local version of the pointer */
   const unsigned char *ch = (const unsigned char *)hostname;
+
+  if(!hostname) /* bad input, consider it ASCII! */
+    return TRUE;
 
   while(*ch) {
     if(*ch++ & 0x80)
