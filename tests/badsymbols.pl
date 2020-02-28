77cc989acc6f0267399cecbb67959f5f2fb3f7de
Fabian Keil <fk@fabiankeil.de>
2020-02-28 10:46:22 +0100

    tests/badsymbols.pl: when opening '$incdir' fails include it in the error message

--- tests/badsymbols.pl
+++ tests/badsymbols.pl
@@ -132,7 +132,7 @@ sub scanheader {
 }
 
 
-opendir(my $dh, $incdir) || die "Can't opendir: $!";
+opendir(my $dh, $incdir) || die "Can't opendir $incdir: $!";
 my @hfiles = grep { /\.h$/ } readdir($dh);
 closedir $dh;
 
