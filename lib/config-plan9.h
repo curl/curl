d6e3273bb004013ce56bc97fae2d576696ca7579
Daniel Stenberg <daniel@haxx.se>
2020-02-29 23:39:42 +0100

    build: remove all HAVE_OPENSSL_ENGINE_H defines
    ... as there's nothing in the code that actually uses the define! The
    last reference was removed in 38203f158.
    
    Closes #5007

--- lib/config-plan9.h
+++ lib/config-plan9.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -138,7 +138,6 @@
 
 #define USE_OPENSSL 1
 #define HAVE_OPENSSL_CRYPTO_H 1
-#define HAVE_OPENSSL_ENGINE_H 1
 #define HAVE_OPENSSL_ERR_H 1
 #define HAVE_OPENSSL_PEM_H 1
 #define HAVE_OPENSSL_PKCS12_H 1
