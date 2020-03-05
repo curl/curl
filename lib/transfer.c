cf65e1d75dc153cf20ea202fa7da3671dfa680a3
Daniel Stenberg <daniel@haxx.se>
2020-03-05 23:52:41 +0100

    urldata: remove the 'stream_was_rewound' connectdata struct member
    ... as it is never set anywhere.
    
    Follow-up to 2f44e94ef

--- lib/transfer.c
+++ lib/transfer.c
@@ -890,7 +890,7 @@ static CURLcode readwrite_data(struct Curl_easy *data,
 
     } /* if(!header and data to read) */
 
-    if(conn->handler->readwrite && excess && !conn->bits.stream_was_rewound) {
+    if(conn->handler->readwrite && excess) {
       /* Parse the excess data */
       k->str += nread;
 
@@ -1235,9 +1235,7 @@ CURLcode Curl_readwrite(struct connectdata *conn,
   /* We go ahead and do a read if we have a readable socket or if
      the stream was rewound (in which case we have data in a
      buffer) */
-  if((k->keepon & KEEP_RECV) &&
-     ((select_res & CURL_CSELECT_IN) || conn->bits.stream_was_rewound)) {
-
+  if((k->keepon & KEEP_RECV) && (select_res & CURL_CSELECT_IN)) {
     result = readwrite_data(data, conn, k, &didwhat, done, comeback);
     if(result || *done)
       return result;
