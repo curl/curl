c79e428940fddc9aa1a4a5b197d3ec0f5f4fd1dd
Daniel Stenberg <daniel@haxx.se>
2020-03-29 23:51:52 +0200

    output.d: quote the URL when globbing
    Some shells do globbing of their own unless the URL is quoted, so maybe
    encourage this.
    
    Co-authored-by: Jay Satiro
    Closes #5160

--- docs/cmdline-opts/output.d
+++ docs/cmdline-opts/output.d
@@ -5,15 +5,15 @@ Help: Write to file instead of stdout
 See-also: remote-name remote-name-all remote-header-name
 ---
 Write output to <file> instead of stdout. If you are using {} or [] to fetch
-multiple documents, you can use '#' followed by a number in the <file>
-specifier. That variable will be replaced with the current string for the URL
-being fetched. Like in:
+multiple documents, you should quote the URL and you can use '#' followed by a
+number in the <file> specifier. That variable will be replaced with the current
+string for the URL being fetched. Like in:
 
- curl http://{one,two}.example.com -o "file_#1.txt"
+ curl "http://{one,two}.example.com" -o "file_#1.txt"
 
 or use several variables like:
 
- curl http://{site,host}.host[1-5].com -o "#1_#2"
+ curl "http://{site,host}.host[1-5].com" -o "#1_#2"
 
 You may use this option as many times as the number of URLs you have. For
 example, if you specify two URLs on the same command line, you can use it like
