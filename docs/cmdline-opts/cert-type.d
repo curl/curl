e9b6736bdaa5e06132199c4ad9f6d5a6614a51e8
Daniel Stenberg <daniel@haxx.se>
2020-05-13 00:17:29 +0200

    cert-type.d: mention the available types in the desc

--- docs/cmdline-opts/cert-type.d
+++ docs/cmdline-opts/cert-type.d
@@ -1,7 +1,7 @@
 Long: cert-type
 Protocols: TLS
 Arg: <type>
-Help: Certificate file type
+Help: Certificate type (DER/PEM/ENG)
 See-also: cert key key-type
 ---
 Tells curl what type the provided client certificate is using. PEM, DER, ENG
