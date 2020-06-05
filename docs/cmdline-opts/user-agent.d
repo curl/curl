695444d68b7bd1748f538e0086cf390d7c20616a
Daniel Stenberg <daniel@haxx.se>
2020-06-05 14:10:52 +0200

    user-agent.d: spell out what happens given a blank argument

--- docs/cmdline-opts/user-agent.d
+++ docs/cmdline-opts/user-agent.d
@@ -9,4 +9,8 @@ Specify the User-Agent string to send to the HTTP server. To encode blanks in
 the string, surround the string with single quote marks. This header can also
 be set with the --header or the --proxy-header options.
 
+If you give an empty argument to --user-agent (""), it will remove the header
+completely from the request. If you prefer a blank header, you can set it to a
+single space (" ").
+
 If this option is used several times, the last one will be used.
