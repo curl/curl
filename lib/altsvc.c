5c5e277de14658916fdba18bbd26e4f75e4d3c1d
Daniel Stenberg <daniel@haxx.se>
2020-06-19 16:32:43 +0200

    altsvc: bump to h3-29

--- lib/altsvc.c
+++ lib/altsvc.c
@@ -51,9 +51,9 @@
 #define MAX_ALTSVC_ALPNLEN 10
 
 #if defined(USE_QUICHE) && !defined(UNITTESTS)
-#define H3VERSION "h3-28"
+#define H3VERSION "h3-29"
 #elif defined(USE_NGTCP2) && !defined(UNITTESTS)
-#define H3VERSION "h3-28"
+#define H3VERSION "h3-29"
 #else
 #define H3VERSION "h3"
 #endif
