e2b2afbeeaccad2a5e8ab89dfb22a81242a6ea1f
Matthias Gatto <matthias.gatto@outscale.com>
2020-07-03 14:50:06 +0200

    docs: add AWS HTTP v4 Signature

---
+++ docs/cmdline-opts/aws-sigv4.d
@@ -0,0 +1,10 @@
+Long: aws-sigv4
+Arg: <provider1[:provider2]>
+Help: provides AWS V4 signature authentication
+Category: auth http
+Added: 7.75.0
+---
+provides AWS V4 signature authentication on HTTPS header:
+
+The provider argument is a string that is used by the algorithm when creating
+outgoing authentication headers.
