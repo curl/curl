de195a17189098c053d248a9ac9612ac36e2a308
Daniel Stenberg <daniel@haxx.se>
2020-08-14 10:20:05 +0200

    scripts/copyright.pl: ignore .muse files

--- scripts/copyright.pl
+++ scripts/copyright.pl
@@ -48,6 +48,7 @@ my @skiplist=(
     '\/readme',
     '^.github/', # github instruction files
     '^.dcignore', # deepcode.ai instruction file
+    '^.muse/', # muse-CI control files
 
     # docs/ files we're okay with without copyright
     'INSTALL.cmake',
