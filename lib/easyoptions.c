8643bfd4fd79b610b34380b6713ce1ab5de718d2
Daniel Stenberg <daniel@haxx.se>
2020-08-28 15:35:23 +0200

    easyoptions: provide debug function when DEBUGBUILD
    ... not CURLDEBUG as they're not always set in conjunction.
    
    Fixes #5877

--- lib/easyoptions.c
+++ lib/easyoptions.c
@@ -334,7 +334,7 @@ struct curl_easyoption Curl_easyopts[] = {
   {NULL, 0, 0, 0} /* end of table */
 };
 
-#ifdef CURLDEBUG
+#ifdef DEBUGBUILD
 /*
  * Curl_easyopts_check() is a debug-only function that returns non-zero
  * if this source file is not in sync with the options listed in curl/curl.h
