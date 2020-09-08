e36fffdba58a3e8ce0c3ab912eb5e74661926045
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-09-08 11:14:58 +0300

    Update timeval.c

--- lib/timeval.c
+++ lib/timeval.c
@@ -186,7 +186,7 @@ timediff_t Curl_timediff(struct curltime newer, struct curltime older)
   timediff_t diff = (timediff_t)newer.tv_sec-older.tv_sec;
   if(diff >= (TIMEDIFF_T_MAX/1000))
     return TIMEDIFF_T_MAX;
-  else if(diff <= (TIMEDIFF_T_MIN/1000))
+  if(diff <= (TIMEDIFF_T_MIN/1000))
     return TIMEDIFF_T_MIN;
   return diff * 1000 + (newer.tv_usec-older.tv_usec)/1000;
 }
@@ -200,7 +200,7 @@ timediff_t Curl_timediff_us(struct curltime newer, struct curltime older)
   timediff_t diff = (timediff_t)newer.tv_sec-older.tv_sec;
   if(diff >= (TIMEDIFF_T_MAX/1000000))
     return TIMEDIFF_T_MAX;
-  else if(diff <= (TIMEDIFF_T_MIN/1000000))
+  if(diff <= (TIMEDIFF_T_MIN/1000000))
     return TIMEDIFF_T_MIN;
   return diff * 1000000 + newer.tv_usec-older.tv_usec;
 }
