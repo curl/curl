4282d5f9d06a1e4c6f9b48f0075cbe959b8a000d
Daniel Stenberg <daniel@haxx.se>
2020-10-19 11:50:08 +0200

    runtests: revert the mistaken edit of $CURL
    Regression from c4693adc62

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -167,7 +167,7 @@ my $VERSION;             # curl's reported version number
 
 my $srcdir = $ENV{'srcdir'} || '.';
 my $CURL="../src/curl".exe_ext('TOOL'); # what curl executable to run on the tests
-my $VCURL="curl";   # what curl binary to use to verify the servers with
+my $VCURL=$CURL;   # what curl binary to use to verify the servers with
                    # VCURL is handy to set to the system one when the one you
                    # just built hangs or crashes and thus prevent verification
 my $DBGCURL=$CURL; #"../src/.libs/curl";  # alternative for debugging
