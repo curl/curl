9386e2a37a8ebbaaf4b9880850e9d29931d02aa3
Daniel Stenberg <daniel@haxx.se>
2020-11-03 08:14:46 +0100

    tool_debug_cb: do not assume zero-terminated data
    Follow-up to d70a5b5a0f5e3

--- src/tool_cb_dbg.c
+++ src/tool_cb_dbg.c
@@ -187,7 +187,7 @@ int tool_debug_cb(CURL *handle, curl_infotype type,
 
   switch(type) {
   case CURLINFO_TEXT:
-    fprintf(output, "%s== Info: %s", timebuf, data);
+    fprintf(output, "%s== Info: %.*s", timebuf, (int)size, data);
     /* FALLTHROUGH */
   default: /* in case a new one is introduced to shock us */
     return 0;
