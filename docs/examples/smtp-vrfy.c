curl/curl

commit ac0a88fd2552524955233198de96cc66f6b15a07

copyright: fix year ranges
Follow-up from 4d2f8006777

docs/examples/smtp-vrfy.c
@@ -1,11 +1,11 @@
-/***************************************************************************
+'/***************************************************************************
  *                                  _   _ ____  _
  *  Project                     ___| | | |  _ \| |
  *                             / __| | | | |_) | |
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms

commit afddaa6decddb6e4dd2c02b34cfbdc93659c63d2

examples: fix comment syntax
Commit ac0a88fd2 accidentally added a stray character outside of the
comment which broke compilation. Fix by removing.

Reported-by:  autobuild https://curl.se/dev/log.cgi?id=20201105084306-12742

docs/examples/smtp-vrfy.c
@@ -1,4 +1,4 @@
-'/***************************************************************************
+/***************************************************************************
  *                                  _   _ ____  _
  *  Project                     ___| | | |  _ \| |
  *                             / __| | | | |_) | |