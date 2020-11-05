ac0a88fd2552524955233198de96cc66f6b15a07
Daniel Stenberg <daniel@haxx.se>
2020-11-05 08:22:10 +0100

    copyright: fix year ranges
    Follow-up from 4d2f8006777

--- lib/vtls/mesalink.h
+++ lib/vtls/mesalink.h
@@ -7,8 +7,8 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 2017-2018, Yiming Jing, <jingyiming@baidu.com>
- * Copyright (C) 1998 - 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 2017 - 2018, Yiming Jing, <jingyiming@baidu.com>
+ * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
