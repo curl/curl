553f286a8f17e84b6957101ab37b7d817a6a2929
Daniel Stenberg <daniel@haxx.se>
2021-01-27 16:39:24 +0100

    tool_writeout: make %{errormsg} blank for no errors
    Closes #6539

--- src/tool_writeout.c
+++ src/tool_writeout.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2021, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -137,8 +137,9 @@ void ourWriteOut(CURL *curl, struct per_transfer *per, const char *writeinfo,
                 fprintf(stream, "%d", (int)result);
                 break;
               case VAR_ERRORMSG:
-                fputs(per->errorbuffer[0] ? per->errorbuffer :
-                      curl_easy_strerror(result), stream);
+                if(result)
+                  fputs(per->errorbuffer[0] ? per->errorbuffer :
+                        curl_easy_strerror(result), stream);
                 break;
               case VAR_INPUT_URL:
                 if(per->this_url)
