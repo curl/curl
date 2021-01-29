f6d13090d4b108af75426f1ba225e31c9d4f61cf
Daniel Stenberg <daniel@haxx.se>
2021-01-29 14:35:13 +0100

    copyright: fix missing year (range) updates

--- tests/unit/unit1660.c
+++ tests/unit/unit1660.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 2020 - 2021, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
