98577e4029bc794bc4b55988c2db6f900733fb3f
Jay Satiro <raysatiro@yahoo.com>
2021-02-04 14:49:56 -0500

    build: fix --disable-http-auth
    Broken since 215db08 (precedes 7.75.0).
    
    Reported-by: Benbuck Nason
    
    Fixes https://github.com/curl/curl/issues/6567

--- lib/http.c
+++ lib/http.c
@@ -871,13 +871,17 @@ Curl_http_output_auth(struct Curl_easy *data,
 #else
 /* when disabled */
 CURLcode
-Curl_http_output_auth(struct connectdata *conn,
+Curl_http_output_auth(struct Curl_easy *data,
+                      struct connectdata *conn,
                       const char *request,
+                      Curl_HttpReq httpreq,
                       const char *path,
                       bool proxytunnel)
 {
+  (void)data;
   (void)conn;
   (void)request;
+  (void)httpreq;
   (void)path;
   (void)proxytunnel;
   return CURLE_OK;
