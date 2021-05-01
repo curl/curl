fb03a4a4237ea89d8c3cc260cedbbe76ba382780
Steinwender Wolfgang <wsteinwender@pcs.com>
2021-05-01 18:35:16 +0200

    make constant more readable

--- lib/multi.c
+++ lib/multi.c
@@ -3085,7 +3085,7 @@ CURLMcode curl_multi_setopt(struct Curl_multi *multi,
     {
       long stream_window_size = va_arg(param, long);
       if((stream_window_size > 0) &&
-         ((unsigned long)stream_window_size <= ((1UL << 31) - 1)))
+         ((unsigned long)stream_window_size <= 0x7FFFFFFF))
         multi->stream_window_size = curlx_sltoui(stream_window_size);
       else
         return CURLM_BAD_FUNCTION_ARGUMENT;
