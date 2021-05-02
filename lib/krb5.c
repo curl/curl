cf9def80b626c860bdef48add848f484db92d195
Harry Sintonen <sintonen@iki.fi>
2021-05-03 00:13:41 +0300

    name_to_level: replaced bogus checkprefix with curl_strequal

--- lib/krb5.c
+++ lib/krb5.c
@@ -402,7 +402,7 @@ name_to_level(const char *name)
 {
   int i;
   for(i = 0; i < (int)sizeof(level_names)/(int)sizeof(level_names[0]); i++)
-    if(checkprefix(name, level_names[i].name))
+    if(curl_strequal(name, level_names[i].name))
       return level_names[i].level;
   return PROT_NONE;
 }
