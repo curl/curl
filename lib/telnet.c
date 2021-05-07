2ada89dc85df7f7860a416991a4875decb864412
Harry Sintonen <sintonen@iki.fi>
2021-05-07 13:09:57 +0200

    telnet: check sscanf() for correct number of matches
    CVE-2021-22898
    
    Bug: https://curl.se/docs/CVE-2021-22898.html

--- lib/telnet.c
+++ lib/telnet.c
@@ -922,7 +922,7 @@ static void suboption(struct Curl_easy *data)
         size_t tmplen = (strlen(v->data) + 1);
         /* Add the variable only if it fits */
         if(len + tmplen < (int)sizeof(temp)-6) {
-          if(sscanf(v->data, "%127[^,],%127s", varname, varval)) {
+          if(sscanf(v->data, "%127[^,],%127s", varname, varval) == 2) {
             msnprintf((char *)&temp[len], sizeof(temp) - len,
                       "%c%s%c%s", CURL_NEW_ENV_VAR, varname,
                       CURL_NEW_ENV_VALUE, varval);
