82d334001f1069b08f72797350fffd6c9f81526a
Daniel Stenberg <daniel@haxx.se>
2021-05-20 23:24:38 +0200

    copyright: update copyright year ranges to 2021

--- docs/examples/curlx.c
+++ docs/examples/curlx.c
@@ -15,7 +15,7 @@
  */
 
 /*
- * Copyright (c) 2003 - 2020 The OpenEvidence Project.  All rights reserved.
+ * Copyright (c) 2003 - 2021 The OpenEvidence Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
