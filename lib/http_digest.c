78fc6522c2bb0cf45b5daa62af745fbfaa62a412
Daniel Stenberg <daniel@haxx.se>
2021-06-30 13:13:31 +0200

    lib: fix type of len passed to *printf's %*s
    ... it needs to be 'int'. Detected by Coverity CID 1486611 (etc)
    
    Closes #7326

--- lib/http_digest.c
+++ lib/http_digest.c
@@ -146,7 +146,8 @@ CURLcode Curl_output_digest(struct Curl_easy *data,
     tmp = strchr((char *)uripath, '?');
     if(tmp) {
       size_t urilen = tmp - (char *)uripath;
-      path = (unsigned char *) aprintf("%.*s", urilen, uripath);
+      /* typecast is fine here since the value is always less than 32 bits */
+      path = (unsigned char *) aprintf("%.*s", (int)urilen, uripath);
     }
   }
   if(!tmp)
--- lib/x509asn1.c
+++ lib/x509asn1.c
@@ -518,7 +518,7 @@ static const char *GTime2str(const char *beg, const char *end)
                        beg, beg + 4, beg + 6,
                        beg + 8, beg + 10, sec1, sec2,
                        fracl? ".": "", fracl, fracp,
-                       sep, tzl, tzp);
+                       sep, (int)tzl, tzp);
 }
 
 /*
@@ -558,7 +558,7 @@ static const char *UTime2str(const char *beg, const char *end)
   return curl_maprintf("%u%.2s-%.2s-%.2s %.2s:%.2s:%.2s %.*s",
                        20 - (*beg >= '5'), beg, beg + 2, beg + 4,
                        beg + 6, beg + 8, sec,
-                       tzl, tzp);
+                       (int)tzl, tzp);
 }
 
 /*
