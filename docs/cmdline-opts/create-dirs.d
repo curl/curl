f538bc5a4062c0b6afa16bc30c76f79020c183aa
Josh Soref <2119212+jsoref@users.noreply.github.com>
2021-07-16 02:47:34 -0400

    docs: spell out directories instead of dirs in create-dirs
    Write out directories rather than using the dirs abbrevation. Also
    use plural form consistently, even if the code in the end might just
    create a single directory.
    
    Closes #7406
    Reviewed-by: Daniel Stenberg <daniel@haxx.se>
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>

--- docs/cmdline-opts/create-dirs.d
+++ docs/cmdline-opts/create-dirs.d
@@ -3,9 +3,10 @@ Help: Create necessary local directory hierarchy
 Category: curl
 ---
 When used in conjunction with the --output option, curl will create the
-necessary local directory hierarchy as needed. This option creates the dirs
-mentioned with the --output option, nothing else. If the --output file name
-uses no dir or if the dirs it mentions already exist, no dir will be created.
+necessary local directory hierarchy as needed. This option creates the
+directories mentioned with the --output option, nothing else. If the --output
+file name uses no directory, or if the directories it mentions already exist,
+no directories will be created.
 
 Created dirs are made with mode 0750 on unix style file systems.
 
