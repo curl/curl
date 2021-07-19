a088a997eb4da265f32da0693b94769cef84a19c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2021-07-18 21:51:43 -0400

    docs: fix globoff grammar

--- docs/cmdline-opts/globoff.d
+++ docs/cmdline-opts/globoff.d
@@ -4,6 +4,6 @@ Help: Disable URL sequences and ranges using {} and []
 Category: curl
 ---
 This option switches off the "URL globbing parser". When you set this option,
-you can specify URLs that contain the letters {}[] without having them being
-interpreted by curl itself. Note that these letters are not normal legal URL
-contents but they should be encoded according to the URI standard.
+you can specify URLs that contain the letters {}[] without having curl itself
+interpret them. Note that these letters are not normal legal URL contents but
+they should be encoded according to the URI standard.
