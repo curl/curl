f6401b26c88433c9e25c886f91ebdee8aa3b8aa6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2021-07-20 01:06:56 -0400

    doc/cmdline: fix grammar and typos
    Closes https://github.com/curl/curl/pull/7454
    Closes https://github.com/curl/curl/pull/7455
    Closes https://github.com/curl/curl/pull/7456
    Closes https://github.com/curl/curl/pull/7459
    Closes https://github.com/curl/curl/pull/7460
    Closes https://github.com/curl/curl/pull/7461
    Closes https://github.com/curl/curl/pull/7462
    Closes https://github.com/curl/curl/pull/7463

--- docs/cmdline-opts/metalink.d
+++ docs/cmdline-opts/metalink.d
@@ -4,5 +4,4 @@ Added: 7.27.0
 Category: misc
 ---
 This option was previously used to specify a metalink resource. Metalink
-support has unfortunately been disabled in curl since 7.78.0 due to security
-reasons.
+support has been disabled in curl since 7.78.0 for security reasons.
--- docs/cmdline-opts/pass.d
+++ docs/cmdline-opts/pass.d
@@ -4,6 +4,6 @@ Help: Pass phrase for the private key
 Protocols: SSH TLS
 Category: ssh tls auth
 ---
-Passphrase for the private key
+Passphrase for the private key.
 
 If this option is used several times, the last one will be used.
