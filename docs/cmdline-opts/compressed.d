94bd01310b4a55b8d9cf0821fed4eb70a7e7aca4
Daniel Stenberg <daniel@haxx.se>
2021-07-30 12:33:33 +0200

    compressed.d: it's a request, not an order
    Clarified
    
    Reported-by: Dan Jacobson
    Reviewed-by: Daniel Gustafsson
    Fixes #7516
    Closes #7517

--- docs/cmdline-opts/compressed.d
+++ docs/cmdline-opts/compressed.d
@@ -7,4 +7,5 @@ Request a compressed response using one of the algorithms curl supports, and
 automatically decompress the content. Headers are not modified.
 
 If this option is used and the server sends an unsupported encoding, curl will
-report an error.
+report an error. This is a request, not an order; the server may or may not
+deliver data compressed.
