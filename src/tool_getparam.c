7a3e981781d6c18ab62f51342b6aeed3e816b696
Cao ZhenXiang <caozhenxiang@protonmail.com>
2021-09-05 13:04:38 +0800

    curl: add warning for incompatible parameters usage
    --continue-at - and --remote-header-name are known incompatible parameters
    
    Closes #7674

--- src/tool_getparam.c
+++ src/tool_getparam.c
@@ -1890,11 +1890,6 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
       }
       break;
     case 'i':
-      if(config->content_disposition) {
-        warnf(global,
-              "--include and --remote-header-name cannot be combined.\n");
-        return PARAM_BAD_USE;
-      }
       config->show_headers = toggle; /* show the headers as well in the
                                         general output stream */
       break;
@@ -1910,11 +1905,6 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
         return PARAM_BAD_USE;
       break;
     case 'J': /* --remote-header-name */
-      if(config->show_headers) {
-        warnf(global,
-              "--include and --remote-header-name cannot be combined.\n");
-        return PARAM_BAD_USE;
-      }
       config->content_disposition = toggle;
       break;
     case 'k': /* allow insecure SSL connects */
@@ -2392,6 +2382,19 @@ ParameterError parse_args(struct GlobalConfig *global, int argc,
 
     if(!result)
       curlx_unicodefree(orig_opt);
+  }
+
+  if(config->content_disposition) {
+    if(config->show_headers) {
+      helpf(global->errors, "--include and --remote-header-name "
+            "cannot be combined.\n");
+      return PARAM_BAD_USE;
+    }
+    if(config->resume_from_current) {
+      helpf(global->errors, "--continue-at - and --remote-header-name "
+            "cannot be combined.\n");
+      return PARAM_BAD_USE;
+    }
   }
 
   if(result && result != PARAM_HELP_REQUESTED &&
