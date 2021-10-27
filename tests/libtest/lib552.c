df84a797116a603a4247b7dcbe92a7f9a42a2afa
Daniel Stenberg <daniel@haxx.se>
2021-10-27 09:35:28 +0200

    test552: updated to work with hyper
    Closes #7911

--- tests/libtest/lib552.c
+++ tests/libtest/lib552.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2021, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -154,8 +154,8 @@ static curlioerr ioctl_callback(CURL *handle, int cmd, void *clientp)
 {
   (void)clientp;
   if(cmd == CURLIOCMD_RESTARTREAD) {
-    printf("APPLICATION: received a CURLIOCMD_RESTARTREAD request\n");
-    printf("APPLICATION: ** REWINDING! **\n");
+    printf("APPLICATION received a CURLIOCMD_RESTARTREAD request\n");
+    printf("APPLICATION ** REWINDING! **\n");
     current_offset = 0;
     return CURLIOE_OK;
   }
