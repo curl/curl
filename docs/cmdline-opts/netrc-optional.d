ce568bbf3efa4c775078de32a25b5f10a5c60366
Daniel Stenberg <daniel@haxx.se>
2021-11-01 15:30:36 +0100

    fixup more

--- docs/cmdline-opts/netrc-optional.d
+++ docs/cmdline-opts/netrc-optional.d
@@ -6,5 +6,5 @@ Category: curl
 Example: --netrc-optional $URL
 Added: 7.9.8
 ---
-Very similar to --netrc, but this option makes the .netrc usage **optional**
+Similar to --netrc, but this option makes the .netrc usage **optional**
 and not mandatory as the --netrc option does.
