6848449433f2bbeb775870b91b9d9012d02d70b7
Daniel Stenberg <daniel@haxx.se>
2021-11-15 17:13:17 +0100

    docs/cmdline-opts: don't say "protocols: all"
    Removed the lines saying "protocols: all". It makes the output in the
    manpage look funny, and the expectation is probably by default that if
    not anything is mentioned about protocols the option apply to them all.

--- docs/cmdline-opts/doh-cert-status.d
+++ docs/cmdline-opts/doh-cert-status.d
@@ -1,6 +1,5 @@
 Long: doh-cert-status
 Help: Verify the status of the DoH server cert via OCSP-staple
-Protocols: all
 Added: 7.76.0
 Category: dns tls
 Example: --doh-cert-status --doh-url https://doh.example $URL
--- docs/cmdline-opts/doh-insecure.d
+++ docs/cmdline-opts/doh-insecure.d
@@ -1,6 +1,5 @@
 Long: doh-insecure
 Help: Allow insecure DoH server connections
-Protocols: all
 Added: 7.76.0
 Category: dns tls
 Example: --doh-insecure --doh-url https://doh.example $URL
