a2d8eac04a4eb1d5a98cf24b4e5cec5cec565d27
Daniel Stenberg <daniel@haxx.se>
2021-11-15 11:36:58 +0100

    form-escape.d: double the back-slashes for proper man page output

--- docs/cmdline-opts/form-escape.d
+++ docs/cmdline-opts/form-escape.d
@@ -4,7 +4,7 @@ Protocols: HTTP
 See-also: form
 Added: 7.81.0
 Category: http post
-Example: --form-escape --form 'field\\name=curl' 'file=@load"this' $URL
+Example: --form-escape --form 'field\\\\name=curl' 'file=@load"this' $URL
 ---
 Tells curl to pass on names of multipart form fields and files using
 backslash-escaping instead of percent-encoding.
