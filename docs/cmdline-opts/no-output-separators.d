53d8f8a26727dbf2b28e1450311ad16dbd55bb7c
Jonny <jonny@wolpzone.de>
2021-11-23 00:34:02 +0100

    fixed docs

--- docs/cmdline-opts/no-output-separators.d
+++ docs/cmdline-opts/no-output-separators.d
@@ -3,7 +3,7 @@ Help: Do not insert separators between files on stdout
 Category: curl
 See-also: silent write-out
 Example: --no-output-separators $URL/split-file.part[001-099] > original-file
-Added: 7.80.0
+Added: 7.82.0
 ---
 Disables output separators on stdout. By default, when downloading multiple
 files to stdout, curl will insert separators of the form --_curl_--<URL>
