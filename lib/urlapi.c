94d3e40a19cd813cc990e3a24125ec75742ab30a
Stefan Eissing <stefan.eissing@greenbytes.de>
2021-11-22 12:47:56 +0100

    * urlapi.c: proposal for a Curl_is_absolute_url() that always    leaves a defined 'buf' and avoids copying on urls that do not    start with a scheme.

--- lib/urlapi.c
+++ lib/urlapi.c
@@ -235,26 +235,31 @@ static void strcpy_url(char *output, const char *url, bool relative)
 bool Curl_is_absolute_url(const char *url, char *buf, size_t buflen)
 {
   size_t i;
+  char s;
+
+  if (buf && buflen)
+    buf[0] = 0; /* always leave a defined value in buf */
 #ifdef WIN32
   if(STARTS_WITH_DRIVE_PREFIX(url))
     return FALSE;
 #endif
-  for(i = 0; i < buflen && url[i]; ++i) {
-    char s = url[i];
-    if((s == ':') && (url[i + 1] == '/')) {
-      if(buf)
+  for(i = 0; i < buflen; ++i) {
+    if((s = url[i]) &&
+      (ISALNUM(s) || (s == '+') || (s == '-') || (s == '.') )) {
+      /* RFC 3986 3.1 explains:
+        scheme      = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
+      */
+    }
+    else {
+        break;
+    }
+  }
+  if(i && (url[i] == ':') && (url[i + 1] == '/')) {
+    if (buf) {
+        memcpy(buf, url, i);
         buf[i] = 0;
-      return TRUE;
     }
-    /* RFC 3986 3.1 explains:
-      scheme      = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
-    */
-    else if(ISALNUM(s) || (s == '+') || (s == '-') || (s == '.') ) {
-      if(buf)
-        buf[i] = (char)TOLOWER(s);
-    }
-    else
-      break;
+    return TRUE;
   }
   return FALSE;
 }
