45c6448e68da77d9d553bc221c449dcb3b4ad49f
Daniel Stenberg <daniel@haxx.se>
2021-12-06 17:51:40 +0100

    test1556: adjust for hyper
    Closes #8105

--- tests/libtest/lib1556.c
+++ tests/libtest/lib1556.c
@@ -5,7 +5,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2021, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -67,7 +67,7 @@ int test(char *URL)
     goto test_cleanup;
   }
 
-  printf("Max: %ld\n", (long)info.largest);
+  printf("Max = %ld\n", (long)info.largest);
 
 test_cleanup:
 
