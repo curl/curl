996221fc6c7871e8ebfa4206f65e6fe2afd22f4d
Daniel Stenberg <daniel@haxx.se>
2022-01-05 16:35:34 +0100

    gen.pl: terminate "example" sections better
    If the example (section that is prefixed with spaces) ends the
    description gen.pl would previously miss to output the terminating .fi
    
    Closes #8228

--- docs/cmdline-opts/gen.pl
+++ docs/cmdline-opts/gen.pl
@@ -6,7 +6,7 @@
 #                            | (__| |_| |  _ <| |___
 #                             \___|\___/|_| \_\_____|
 #
-# Copyright (C) 1998 - 2021, Daniel Stenberg, <daniel@haxx.se>, et al.
+# Copyright (C) 1998 - 2022, Daniel Stenberg, <daniel@haxx.se>, et al.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution. The terms
@@ -113,6 +113,9 @@ sub printdesc {
         # handle single quotes first on the line
         $d =~ s/(\s*)\'/$1\\(aq/;
         print $d;
+    }
+    if($exam) {
+        print ".fi\n"; # fill-in
     }
 }
 
