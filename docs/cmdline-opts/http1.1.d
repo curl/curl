2a19ad473a9d92cef1afeb2ee3d828017ca1a13d
Daniel Stenberg <daniel@haxx.se>
2022-01-09 21:34:19 +0100

    docs/cmdline-opts: add "mutexed" options for more http versions
    Update four http version man page sections.
    
    Closes #8254

--- docs/cmdline-opts/http1.1.d
+++ docs/cmdline-opts/http1.1.d
@@ -2,10 +2,10 @@ Long: http1.1
 Tags: Versions
 Protocols: HTTP
 Added: 7.33.0
-Mutexed: http1.0 http2
+Mutexed: http1.0 http2 http2-prior-knowledge http3
 Help: Use HTTP 1.1
 Category: http
 Example: --http1.1 $URL
-See-also: http1.1 http0.9
+See-also: http1.0 http0.9
 ---
 Tells curl to use HTTP version 1.1.
