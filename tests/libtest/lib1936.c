34238ec8faff97a012583dfd7d7ce99b6ab4f55b
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2022-01-13 15:53:52 +0100

    tests: use connect to in test
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- tests/libtest/lib1936.c
+++ tests/libtest/lib1936.c
@@ -29,6 +29,7 @@ int test(char *URL)
 {
   CURL *curl;
   CURLcode res = TEST_ERR_MAJOR_BAD;
+  struct curl_slist *connect_to = NULL;
   struct curl_slist *list = NULL;
 
   if(curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
@@ -48,6 +49,10 @@ int test(char *URL)
   test_setopt(curl, CURLOPT_USERPWD, "xxx:yyy");
   test_setopt(curl, CURLOPT_HEADER, 0L);
   test_setopt(curl, CURLOPT_URL, URL);
+  if(libtest_arg2) {
+    connect_to = curl_slist_append(connect_to, libtest_arg2);
+  }
+  test_setopt(curl, CURLOPT_CONNECT_TO, connect_to);
   list = curl_slist_append(list, "Content-Type: application/json");
   test_setopt(curl, CURLOPT_HTTPHEADER, list);
 
@@ -55,6 +60,7 @@ int test(char *URL)
 
 test_cleanup:
 
+  curl_slist_free_all(connect_to);
   curl_slist_free_all(list);
   curl_easy_cleanup(curl);
   curl_global_cleanup();
