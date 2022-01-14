6ce0189bc8324a932c3339bcbc44d2d0572a586f
Daniel Stenberg <daniel@haxx.se>
2022-01-14 08:30:26 +0100

    curl: remove "separators" (when using globbed URLs)
    Unless muted (with -s) When doing globbing, curl would output mime-like
    separators between the separate transfers. This is not documented
    anywhere, surprises users and clobbers the output.
    
    Reported-by: jonny112 on github
    Bug: https://github.com/curl/curl/discussions/8257

--- src/tool_operate.h
+++ src/tool_operate.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2021, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2022, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -69,8 +69,6 @@ struct per_transfer {
   bool ultotal_added;
 
   /* NULL or malloced */
-  char *separator_err;
-  char *separator;
   char *uploadfile;
 };
 
