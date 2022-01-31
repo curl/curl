50e74ca18ae926c342b9be98b40f96debdafee45
Jay Satiro <raysatiro@yahoo.com>
2022-01-31 01:04:19 -0500

    write-out.d: Fix num_headers formatting

--- docs/cmdline-opts/write-out.d
+++ docs/cmdline-opts/write-out.d
@@ -77,7 +77,7 @@ Number of new connects made in the recent transfer. (Added in 7.12.3)
 .TP
 .B num_headers
 The number of response headers in the most recent request (restarted at each
- redirect). Note that the status line IS NOT a header. (Added in 7.73.0)
+redirect). Note that the status line IS NOT a header. (Added in 7.73.0)
 .TP
 .B num_redirects
 Number of redirects that were followed in the request. (Added in 7.12.3)
