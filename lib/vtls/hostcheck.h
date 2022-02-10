3c1417db66ba83d93652a7320a9f9609fae5408b
Henrik Holst <henrik.holst@millistream.com>
2022-02-10 21:05:39 +0100

    Reduce strlen calls on chained certificates

--- lib/vtls/hostcheck.h
+++ lib/vtls/hostcheck.h
@@ -26,6 +26,6 @@
 
 /* returns TRUE if there's a match */
 bool Curl_cert_hostcheck(const char *match_pattern, size_t matchlen,
-                         const char *hostname);
+                         const char *hostname, size_t hostlen);
 
 #endif /* HEADER_CURL_HOSTCHECK_H */
