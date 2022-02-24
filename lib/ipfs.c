55c32f7e55bb23263d07e9d1fa4c51c1ee228c66
Chris Anderson <jchris@gmail.com>
2022-02-24 21:42:16 +0000

    test and debug log

--- lib/ipfs.c
+++ lib/ipfs.c
@@ -28,15 +28,18 @@
 #include "urldata.h"
 #include <curl/curl.h>
 #include "http.h"
+#include "sendf.h"
 
 /* The last #include file should be: */
+#include "curl_printf.h"
 #include "memdebug.h"
 
 /*
  * Forward declarations.
  */
 
-/* static CURLcode ipfs_do(struct Curl_easy *data, bool *done); */
+static CURLcode ipfs_setup(struct Curl_easy *data,
+                                struct connectdata *conn);
 
 /*
  * IPFS protocol handler.
@@ -44,7 +47,7 @@
 
 const struct Curl_handler Curl_handler_ipfs = {
   "IPFS",                             /* scheme */
-  Curl_http_setup_conn,                    /* setup_connection */
+  ipfs_setup,                    /* setup_connection */
   Curl_http,                            /* do_it */
   Curl_http_done,                          /* done */
   ZERO_NULL,                          /* do_more */
@@ -67,12 +70,51 @@ const struct Curl_handler Curl_handler_ipfs = {
 };
 /*
 see url.c line 1957 for url changes
-
-static CURLcode ipfs_do(struct Curl_easy *data, bool *done)
-{
-  CURLcode result = CURLE_OK;
-  return result;
-}
+or maybe tool_operate.c line 1185
 */
+
+// this is getting called after the connection is established, 
+// so we need to figure out how to call a host rewrite before anything else happens
+// otherwise it just tries to connect to a host based on the CID
+
+/*
+
+I've been reading the codebase and I am starting to see why this is hard. It seems 
+that by the time any of the protocol handlers are called, the DNS lookup has already been performed. 
+Or at least that changing the hostname in the setup_connection handler doesn't not have an
+effect on the behavior.
+
+Does it make sense to add a new callback to the Curl_handler that can be called for each data URL, 
+maybe from something like parseurlandfillconn?
+
+Are there any more resources to help me understand the control flow?
+
+*/
+
+
+static CURLcode ipfs_setup(struct Curl_easy *data,
+                                struct connectdata *conn)
+{
+    char *env_gate;
+    char *gate;
+    char *url;
+    env_gate = (char *)"CURL_IPFS_GATEWAY";
+    gate = curl_getenv(env_gate);
+
+    url = aprintf("http://%s/%s", gate,
+                        data->state.url);
+
+    if(!url)
+      return CURLE_OUT_OF_MEMORY;
+
+    failf(data, "Uses IPFS gateway URL: '%s'", url);
+
+    if(data->state.url_alloc)
+      free(data->state.url);
+    data->state.url = url;
+    data->state.url_alloc = TRUE;
+    return Curl_http_setup_conn(data, conn);
+}
+
 
 #endif /* CURL_DISABLE_IPFS */
