7c44b5195676203ab839d1a11d7cf0886366d589
Don <don.j.olmstead@gmail.com>
2022-03-08 10:44:34 -0800

    nonblock: restore setsockopt method to curlx_nonblock
    The implementation using setsockopt was removed when BeOS support was
    purged. However this functionality wasn't BeOS specific, it is still
    used by for example Orbis OS (Playstation 4/5 OS).
    
    Closes #8562

--- lib/nonblock.c
+++ lib/nonblock.c
@@ -73,6 +73,12 @@ int curlx_nonblock(curl_socket_t sockfd,    /* operate on this */
   long flags = nonblock ? 1L : 0L;
   return IoctlSocket(sockfd, FIONBIO, (char *)&flags);
 
+#elif defined(HAVE_SETSOCKOPT_SO_NONBLOCK)
+
+  /* Orbis OS */
+  long b = nonblock ? 1L : 0L;
+  return setsockopt(sockfd, SOL_SOCKET, SO_NONBLOCK, &b, sizeof(b));
+
 #else
 #  error "no non-blocking method was found/used/set"
 #endif
