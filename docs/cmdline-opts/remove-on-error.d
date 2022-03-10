0a142663a138951df6eedb834afb87edf15d5c05
Daniel Stenberg <daniel@haxx.se>
2022-03-10 22:56:11 +0100

    remove-on-error.d: typo
    Reported-by: Colin Leroy
    Bug: https://github.com/curl/curl/pull/8503#pullrequestreview-906520081

--- docs/cmdline-opts/remove-on-error.d
+++ docs/cmdline-opts/remove-on-error.d
@@ -6,7 +6,7 @@ Example: --remove-on-error -o output $URL
 Added: 7.83.0
 ---
 When curl returns an error when told to save output in a local file, this
-option removes that saved file before exiting. This prevevents curl from
+option removes that saved file before exiting. This prevents curl from
 leaving a partial file in the case of an error during transfer.
 
 If the output is not a file, this option has no effect.
