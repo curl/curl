68b356a1b47e201881c61bd4abfacf9a53a4534f
Dan Fandrich <dan@coneharvesters.com>
2022-03-09 22:11:13 -0800

    keepalive-time.d: It takes many probes to detect brokenness

--- docs/cmdline-opts/keepalive-time.d
+++ docs/cmdline-opts/keepalive-time.d
@@ -9,8 +9,11 @@ See-also: no-keepalive max-time
 This option sets the time a connection needs to remain idle before sending
 keepalive probes and the time between individual keepalive probes. It is
 currently effective on operating systems offering the TCP_KEEPIDLE and
-TCP_KEEPINTVL socket options (meaning Linux, recent AIX, HP-UX and more). This
-option has no effect if --no-keepalive is used.
+TCP_KEEPINTVL socket options (meaning Linux, recent AIX, HP-UX and more).
+Keepalives are used by the TCP stack to detect broken networks on idle
+connections. The number of missed keepalive probes before declaring the
+connection down is OS dependent and is commonly 9 or 10. This option has no
+effect if --no-keepalive is used.
 
 If this option is used several times, the last one will be used. If
 unspecified, the option defaults to 60 seconds.
