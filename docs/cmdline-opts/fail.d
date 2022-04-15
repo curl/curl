a94b28932b591fcc453fba57aca015c222bc2862
Daniel Stenberg <daniel@haxx.se>
2022-04-15 22:49:48 +0200

    fixup polish the description

--- docs/cmdline-opts/fail.d
+++ docs/cmdline-opts/fail.d
@@ -7,11 +7,11 @@ Category: important http
 Example: --fail $URL
 Added: 4.0
 ---
-Fail fast with no output at all on server errors. This is mostly done to
-enable scripts etc to better deal with failed attempts. In normal cases when
-an HTTP server fails to deliver a document, it returns an HTML document
-stating so (which often also describes why and more). This flag will prevent
-curl from outputting that and return error 22.
+Fail fast with no output at all on server errors. This is useful to enable
+scripts and usersto better deal with failed attempts. In normal cases when an
+HTTP server fails to deliver a document, it returns an HTML document stating
+so (which often also describes why and more). This flag will prevent curl from
+outputting that and return error 22.
 
 This method is not fail-safe and there are occasions where non-successful
 response codes will slip through, especially when authentication is involved
