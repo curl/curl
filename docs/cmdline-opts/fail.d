f7f25a742c425f9f36acfb465a7ba67137f23421
Daniel Stenberg <daniel@haxx.se>
2022-04-15 22:50:34 +0200

    fixup grrr space

--- docs/cmdline-opts/fail.d
+++ docs/cmdline-opts/fail.d
@@ -8,7 +8,7 @@ Example: --fail $URL
 Added: 4.0
 ---
 Fail fast with no output at all on server errors. This is useful to enable
-scripts and usersto better deal with failed attempts. In normal cases when an
+scripts and users to better deal with failed attempts. In normal cases when an
 HTTP server fails to deliver a document, it returns an HTML document stating
 so (which often also describes why and more). This flag will prevent curl from
 outputting that and return error 22.
