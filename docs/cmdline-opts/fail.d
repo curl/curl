36e09dc302d0bc70b1a314181cd6b5756d74217d
Daniel Stenberg <daniel@haxx.se>
2022-04-15 23:50:34 +0200

    fail.d: tweak the description
    Reviewed-by: Daniel Gustafsson
    Suggested-by: Robert Charles Muir
    Ref: https://twitter.com/rcmuir/status/1514915401574010887
    
    Closes #8714

--- docs/cmdline-opts/fail.d
+++ docs/cmdline-opts/fail.d
@@ -1,17 +1,17 @@
 Long: fail
 Short: f
 Protocols: HTTP
-Help: Fail silently (no output at all) on HTTP errors
+Help: Fail fast with no output on HTTP errors
 See-also: fail-with-body
 Category: important http
 Example: --fail $URL
 Added: 4.0
 ---
-Fail silently (no output at all) on server errors. This is mostly done to
-enable scripts etc to better deal with failed attempts. In normal cases
-when an HTTP server fails to deliver a document, it returns an HTML document
-stating so (which often also describes why and more). This flag will prevent
-curl from outputting that and return error 22.
+Fail fast with no output at all on server errors. This is useful to enable
+scripts and users to better deal with failed attempts. In normal cases when an
+HTTP server fails to deliver a document, it returns an HTML document stating
+so (which often also describes why and more). This flag will prevent curl from
+outputting that and return error 22.
 
 This method is not fail-safe and there are occasions where non-successful
 response codes will slip through, especially when authentication is involved
