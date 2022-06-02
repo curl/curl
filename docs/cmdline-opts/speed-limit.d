403efd1778add399a9222dda87a96a888ed1b91b
Daniel Stenberg <daniel@haxx.se>
2022-06-02 14:14:42 +0200

    speed-limit/time.d: mention these affect transfers in either direction
    Reported-by: Ladar Levison
    Fixes #8948

--- docs/cmdline-opts/speed-limit.d
+++ docs/cmdline-opts/speed-limit.d
@@ -7,7 +7,7 @@ Example: --speed-limit 300 --speed-time 10 $URL
 Added: 4.7
 See-also: speed-time limit-rate max-time
 ---
-If a download is slower than this given speed (in bytes per second) for
+If a transfer is slower than this given speed (in bytes per second) for
 speed-time seconds it gets aborted. speed-time is set with --speed-time and is
 30 if not set.
 
--- docs/cmdline-opts/speed-time.d
+++ docs/cmdline-opts/speed-time.d
@@ -7,11 +7,11 @@ Example: --speed-limit 300 --speed-time 10 $URL
 Added: 4.7
 See-also: speed-limit limit-rate
 ---
-If a download is slower than speed-limit bytes per second during a speed-time
-period, the download gets aborted. If speed-time is used, the default
+If a transfer runs slower than speed-limit bytes per second during a speed-time
+period, the transfer is aborted. If speed-time is used, the default
 speed-limit will be 1 unless set with --speed-limit.
 
-This option controls transfers and thus will not affect slow connects etc. If
-this is a concern for you, try the --connect-timeout option.
+This option controls transfers (in both directions) but will not affect slow
+connects etc. If this is a concern for you, try the --connect-timeout option.
 
 If this option is used several times, the last one will be used.
