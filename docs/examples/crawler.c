e90f601f307aefa49bb7ef52d1bfc05fd91a984d
Daniel Stenberg <daniel@haxx.se>
2022-06-02 14:52:17 +0200

    fixup the SPDX-License-Identifier

--- docs/examples/crawler.c
+++ docs/examples/crawler.c
@@ -18,6 +18,8 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
+ * SPDX-License-Identifier: curl
+ *
  * To compile:
  *   gcc crawler.c $(pkg-config --cflags --libs libxml-2.0 libcurl)
  *
