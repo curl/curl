3f6beb669e4edc170e167aa6cca3b4a6ec4501e9
Daniel Stenberg <daniel@haxx.se>
2022-06-27 08:46:21 +0200

    easy_lock.h: include sched.h if available to fix build
    Patched-by: Harry Sintonen

--- lib/easy_lock.h
+++ lib/easy_lock.h
@@ -36,6 +36,9 @@
 
 #elif defined (HAVE_ATOMIC)
 #include <stdatomic.h>
+#if defined(HAVE_SCHED_YIELD)
+#include <sched.h>
+#endif
 
 #define curl_simple_lock atomic_bool
 #define CURL_SIMPLE_LOCK_INIT false
