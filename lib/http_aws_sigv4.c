f9a081168e6c2322c5c39e7a6e2c7f1db9f388e6
Daniel Stenberg <daniel@haxx.se>
2022-07-01 16:43:02 +0200

    http_aws_sigv4.c: remove two unusued includes

--- lib/http_aws_sigv4.c
+++ lib/http_aws_sigv4.c
@@ -29,8 +29,6 @@
 #include "urldata.h"
 #include "strcase.h"
 #include "strdup.h"
-#include "vauth/vauth.h"
-#include "vauth/digest.h"
 #include "http_aws_sigv4.h"
 #include "curl_sha256.h"
 #include "transfer.h"
