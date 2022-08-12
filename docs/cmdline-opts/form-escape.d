e3ab0daf35738072e7ecfb83d4a3a8f1c0e4d496
Emanuele Torre <torreemanuele6@gmail.com>
2022-08-12 05:18:10 +0200

    docs/cmdline-opts: fix example and categories for --form-escape
    The example was missing a "--form" argument
    I also replaced "--form" with "-F" to shorten the line a bit since it
    was already very long.
    
    And I also moved --form-escape from the "post" category to the "upload"
    category (this is what I originally wanted to fix, before also noticing
    the mistake in the example).

--- docs/cmdline-opts/form-escape.d
+++ docs/cmdline-opts/form-escape.d
@@ -5,8 +5,8 @@ Help: Escape multipart form field/file names using backslash
 Protocols: HTTP
 See-also: form
 Added: 7.81.0
-Category: http post
-Example: --form-escape --form 'field\\name=curl' 'file=@load"this' $URL
+Category: http upload
+Example: --form-escape -F 'field\\name=curl' -F 'file=@load"this' $URL
 ---
 Tells curl to pass on names of multipart form fields and files using
 backslash-escaping instead of percent-encoding.
