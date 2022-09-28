73a7be7370035a72a95c2367afb709725b4a0ab7
Jay Satiro <raysatiro@yahoo.com>
2022-09-28 03:23:58 -0400

    squashme: always include wincrypt.h
    wincrypt defines symbols we test for such as CryptStringToBinary,
    therefore it must always be included beforehand. prior to this
    change it was not included for mingw.

--- lib/vtls/schannel.h
+++ lib/vtls/schannel.h
@@ -83,12 +83,13 @@ CURLcode Curl_verify_certificate(struct Curl_easy *data,
 /* structs to expose only in schannel.c and schannel_verify.c */
 #ifdef EXPOSE_SCHANNEL_INTERNAL_STRUCTS
 
+#include <wincrypt.h>
+
 #ifdef __MINGW32__
 #ifdef __MINGW64_VERSION_MAJOR
 #define HAS_MANUAL_VERIFY_API
 #endif
 #else
-#include <wincrypt.h>
 #ifdef CERT_CHAIN_REVOCATION_CHECK_CHAIN
 #define HAS_MANUAL_VERIFY_API
 #endif
