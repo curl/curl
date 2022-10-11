curl/curl

commit 1c52e8a3795ccdf8ec9c308f4f8f19cf10ea1f1a

fix Cygwin/MSYS compilation
_getpid is Windows API. On Cygwin variants it should remain getpid.

Fixes #8220
Closes #9255

lib/smb.c
@@ -34,7 +34,7 @@
 #include <process.h>
 #ifdef CURL_WINDOWS_APP
 #define getpid GetCurrentProcessId
-#elif !defined(MSDOS)
+#elif defined(CURL_WIN32)
 #define getpid _getpid
 #endif
 #endif

commit f937cb37f89e043a18bc9afe41fba285d87de3f2

smb: replace CURL_WIN32 with WIN32
PR #9244 aimed to fix a Cygwin/MSYS issue. It used the `CURL_WIN32`
macro, but that one is never defined while compiling curl itself. This
patch changes this to `WIN32`, assuming this was the original intent.

Regression from 1c52e8a3795ccdf8ec9c308f4f8f19cf10ea1f1a.

Ref: #8220
Ref: #9255

Closes #xxxx

lib/smb.c
@@ -34,7 +34,7 @@
 #include <process.h>
 #ifdef CURL_WINDOWS_APP
 #define getpid GetCurrentProcessId
-#elif defined(CURL_WIN32)
+#elif defined(WIN32)
 #define getpid _getpid
 #endif
 #endif