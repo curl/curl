curl/curl

commit 2b309560c1e5d6ed5c0e542e6fdffa968b0521c9

connect: change verbose IPv6 address:port to [address]:port
- Use brackets for the IPv6 address shown in verbose message when the
  format is address:port so that it is less confusing.

Before: Trying 2606:4700:4700::1111:443...
After: Trying [2606:4700:4700::1111]:443...

Bug: https://curl.se/mail/archive-2022-02/0041.html
Reported-by: David Hu

Closes #9635

lib/connect.c
@@ -1222,7 +1222,11 @@ static CURLcode singleipconnect(struct Curl_easy *data,
     Curl_closesocket(data, conn, sockfd);
     return CURLE_OK;
   }
-  infof(data, "  Trying %s:%d...", ipaddress, port);
+  infof(data, "  Trying %s%s%s:%d...",
+        (addr.family == AF_INET6 ? "[" : ""),
+        ipaddress,
+        (addr.family == AF_INET6 ? "]" : ""),
+        port);
 
 #ifdef ENABLE_IPV6
   is_tcp = (addr.family == AF_INET || addr.family == AF_INET6) &&

commit 94a2b7ae405dac433cd54b4623dbb44830a7ed0b

connect: fix build without AF_INET6
Regression from 2b309560c1e5d6ed5c0e542e6fdffa968b0521c9

Closes #xxxx

lib/connect.c
@@ -1197,6 +1197,7 @@ static CURLcode singleipconnect(struct Curl_easy *data,
 #ifdef TCP_FASTOPEN_CONNECT
   int optval = 1;
 #endif
+  const char * ipmsg;
   char buffer[STRERROR_LEN];
   curl_socket_t *sockp = &conn->tempsock[tempindex];
   *sockp = CURL_SOCKET_BAD;
@@ -1214,11 +1215,14 @@ static CURLcode singleipconnect(struct Curl_easy *data,
     Curl_closesocket(data, conn, sockfd);
     return CURLE_OK;
   }
-  infof(data, "  Trying %s%s%s:%d...",
-        (addr.family == AF_INET6 ? "[" : ""),
-        ipaddress,
-        (addr.family == AF_INET6 ? "]" : ""),
-        port);
+
+#ifdef ENABLE_IPV6
+  if(addr.family == AF_INET6)
+    ipmsg = "  Trying [%s]:%d...";
+  else
+#endif
+    ipmsg = "  Trying %s:%d...";
+  infof(data, ipmsg, ipaddress, port);
 
 #ifdef ENABLE_IPV6
   is_tcp = (addr.family == AF_INET || addr.family == AF_INET6) &&