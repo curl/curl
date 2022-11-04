c4e957bbd9f188e5af1c2c5ceda1a1653e0cfdc0
Daniel Stenberg <daniel@haxx.se>
2022-11-05 00:11:30 +0100

    fixup url-query.d

--- docs/cmdline-opts/url-query.d
+++ docs/cmdline-opts/url-query.d
@@ -12,6 +12,7 @@ Example: --url-query =encodethis http://example.net/foo
 Example: --url-query name@file $URL
 Example: --url-query @fileonly $URL
 Example: --url-query "+name=%20foo" $URL
+Multi: append
 ---
 This option adds a piece of data, usually a name + value pair, to the end of
 the URL query part. The syntax is identical to that used for --data-urlencode
