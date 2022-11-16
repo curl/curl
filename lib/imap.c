eb559c8056916f1e71570ac058d599347bebc2b4
Patrick Monnerat <patrick@monnerat.net>
2022-11-16 17:40:11 +0100

    mime: relax easy/mime structures binding
    Deprecation and removal of codeset conversion support from the library
    have released the strict need for an early binding of mime structures to
    an easy handle (https://github.com/curl/curl/commit/2610142).
    
    This constraint currently forces to create the handle before the mime
    structure and the latter cannot be attached to another handle once
    created (see https://curl.se/mail/lib-2022-08/0027.html).
    
    This commit removes the handle pointers from the mime structures
    allowing more flexibility on their use.
    
    When an easy handle is duplicated, bound mime structures must however
    still be duplicated too as their components hold send-time dynamic
    information.
    
    Closes #9927

--- lib/imap.c
+++ lib/imap.c
@@ -777,7 +777,7 @@ static CURLcode imap_perform_append(struct Curl_easy *data)
 
     /* Add external headers and mime version. */
     curl_mime_headers(&data->set.mimepost, data->set.headers, 0);
-    result = Curl_mime_prepare_headers(&data->set.mimepost, NULL,
+    result = Curl_mime_prepare_headers(data, &data->set.mimepost, NULL,
                                        NULL, MIMESTRATEGY_MAIL);
 
     if(!result)
