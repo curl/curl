a6111d2d495704a66599c14d8f10a78692f493e8
Daniel Stenberg <daniel@haxx.se>
2022-12-05 10:43:02 +0100

    fixup the looup table to also include '+' !

--- lib/base64.c
+++ lib/base64.c
@@ -53,11 +53,11 @@ static const char base64url[]=
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 static const unsigned char decodetable[] =
-{ 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 255, 255, 255, 64, 255, 255, 255,
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23, 24, 25, 255, 255, 255, 255, 255, 255, 26, 27, 28, 29, 30, 31, 32, 33,
-  34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51};
-
+{ 62, 255, 255, 255, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 255, 255, 255,
+  64, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+  17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255, 255, 26, 27, 28,
+  29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+  48, 49, 50, 51 };
 /*
  * Curl_base64_decode()
  *
@@ -117,10 +117,14 @@ CURLcode Curl_base64_decode(const char *src,
   pos = newstr;
 
   memset(lookup, 0xff, sizeof(lookup));
-  memcpy(&lookup['/'], decodetable, sizeof(decodetable));
+  memcpy(&lookup['+'], decodetable, sizeof(decodetable));
   /* replaces
-     for(c = 0; *p; c++, p++)
-       lookup[*p] = c;
+  {
+    unsigned char c;
+    const unsigned char *p = (const unsigned char *)base64;
+    for(c = 0; *p; c++, p++)
+      lookup[*p] = c;
+  }
   */
 
   /* Decode the complete quantums first */
