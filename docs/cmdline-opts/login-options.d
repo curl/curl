cd823c00a2d274b00021f4485e73b7fedd4a77d9
Chris Talbot <chris@talbothome.com>
2022-12-05 18:05:01 -0500

    imap: Provide method to disable SASL if it is advertised
    - Implement AUTH=+LOGIN for CURLOPT_LOGIN_OPTIONS to prefer plaintext
      LOGIN over SASL auth.
    
    Prior to this change there was no method to be able to fall back to
    LOGIN if an IMAP server advertises SASL capabilities. However, this may
    be desirable for e.g. a misconfigured server.
    
    Per: https://www.ietf.org/rfc/rfc5092.html#section-3.2
    
    ";AUTH=<enc-auth-type>" looks to be the correct way to specify what
    authenication method to use, regardless of SASL or not.
    
    Closes https://github.com/curl/curl/pull/10041

--- docs/cmdline-opts/login-options.d
+++ docs/cmdline-opts/login-options.d
@@ -16,3 +16,9 @@ You can use login options to specify protocol specific options that may be
 used during authentication. At present only IMAP, POP3 and SMTP support
 login options. For more information about login options please see RFC
 2384, RFC 5092 and IETF draft draft-earhart-url-smtp-00.txt
+
+Since 8.2.0, IMAP supports the login option "AUTH=+LOGIN". With this option,
+curl uses the plain (not SASL) LOGIN IMAP command even if the server advertises
+SASL authentication. Care should be taken in using this option, as it will send
+out your password in plain text. This will not work if the IMAP server disables
+the plain LOGIN (e.g. to prevent password snooping).
