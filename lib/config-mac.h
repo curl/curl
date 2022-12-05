0ed8ff2d37e7d329c2a25b88f6b1d6803df6894c
Ryan Schmidt <git@ryandesign.com>
2022-12-05 13:09:49 -0600

    config-mac: remove HAVE_SYS_SELECT_H
    When compiling for classic Mac OS with GUSI, there is no sys/select.h.
    GUSI provides the "select" function prototype in sys/time.h.
    
    Closes #10039

--- lib/config-mac.h
+++ lib/config-mac.h
@@ -40,7 +40,6 @@
 #define HAVE_ERRNO_H            1
 #define HAVE_NETINET_IN_H       1
 #define HAVE_SYS_SOCKET_H       1
-#define HAVE_SYS_SELECT_H       1
 #define HAVE_NETDB_H            1
 #define HAVE_ARPA_INET_H        1
 #define HAVE_UNISTD_H           1
