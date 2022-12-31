61eaa0af167e67f8891f7ba07d0621c120d3fdee
Cameron Blomquist <cameron@blomqu.ist>
2022-12-31 09:40:51 -0500

    http: add additional condition for including stdint.h
    stdint.h was only included in http.h when ENABLE_QUIC was defined, but
    symbols from stdint.h are also used when USE_NGHTTP2 is defined. This
    causes build errors when USE_NGHTTP2 is defined but ENABLE_QUIC is not.
    
    Closes #10185

--- lib/http.h
+++ lib/http.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2022, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2023, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -42,7 +42,7 @@ typedef enum {
 
 #ifndef CURL_DISABLE_HTTP
 
-#if defined(_WIN32) && defined(ENABLE_QUIC)
+#if defined(_WIN32) && (defined(ENABLE_QUIC) || defined(USE_NGHTTP2))
 #include <stdint.h>
 #endif
 
