3649857c3cb7c6f0f24b69d82ce89b15326a0c7f
Stefan Eissing <stefan@eissing.org>
2023-01-16 13:58:55 +0100

    undef'ing H2 ALPN const when building without HTTP2 support.

--- lib/vtls/vtls.c
+++ lib/vtls/vtls.c
@@ -1875,9 +1875,11 @@ static const struct alpn_spec ALPN_SPEC_H10 = {
 static const struct alpn_spec ALPN_SPEC_H11 = {
   { ALPN_HTTP_1_1 }, 1
 };
+#ifdef USE_HTTP2
 static const struct alpn_spec ALPN_SPEC_H2_H11 = {
   { ALPN_H2, ALPN_HTTP_1_1 }, 2
 };
+#endif
 
 const struct alpn_spec *
 Curl_alpn_get_spec(struct Curl_easy *data, struct connectdata *conn)
