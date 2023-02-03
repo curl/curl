27966184661efa591d4b7a363359ec9e234f67b9
Pronyushkin Petr <pyetr.pronyushkin@fsight.ru>
2023-02-03 11:26:30 +0500

    url: fix part of conditional expression is always true
    Closes #10407

--- lib/url.c
+++ lib/url.c
@@ -2744,7 +2744,7 @@ static CURLcode override_login(struct Curl_easy *data,
         return CURLE_OUT_OF_MEMORY;
     }
     /* no user was set but a password, set a blank user */
-    if(userp && !*userp && *passwdp) {
+    if(!*userp && *passwdp) {
       *userp = strdup("");
       if(!*userp)
         return CURLE_OUT_OF_MEMORY;
