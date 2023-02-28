c3867e93273876c5932b90b62f1ec718a8035408
Harry Sintonen <sintonen@iki.fi>
2023-02-28 13:22:53 +0200

    http: ignore content-range for 416 responses
    416 Requested Range Not Satisfiable returned by some HTTP daemons
    (nginx at least) appear to include Content-Range header. This
    confuses curl and makes it think that the whole file needs to be
    downloaded again, resulting in download failure when in fact the
    while file has already been downloaded.
    
    This change makes curl disregard Content-Range header on a 416
    response, allowing other existing logic consider the file fully
    downloaded.
    
    Fixes #10521

--- lib/http.c
+++ lib/http.c
@@ -3477,7 +3477,7 @@ CURLcode Curl_http_header(struct Curl_easy *data, struct connectdata *conn,
           k->content_range = TRUE;
       }
     }
-    else
+    else if(416 != k->httpcode)
       data->state.resume_from = 0; /* get everything */
   }
 #if !defined(CURL_DISABLE_COOKIES)
