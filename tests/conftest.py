539c3543bbce1a9e2226bd7da24084a78957c48e
Stefan Eissing <stefan@eissing.org>
2023-03-01 16:55:31 +0100

    tests: rename tests/tests-httpd to tests/http
     - httpd is only one server we test with
     - the suite coveres the HTTP protocol in general where
       the default test cases need a more beefy environment
    
    Closes #10654

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,13 +29,9 @@ from typing import Optional
 
 import pytest
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
-
-from testenv import Env, Nghttpx, Httpd
-
 
 def pytest_report_header(config, startdir):
-    return f"curl tests-httpd tests"
+    return f"curl tests"
 
 
 def pytest_addoption(parser):
@@ -49,33 +45,4 @@ def pytest_generate_tests(metafunc):
         metafunc.fixturenames.append('tmp_ct')
         metafunc.parametrize('repeat', range(count))
 
-
-@pytest.fixture(scope="package")
-def env(pytestconfig) -> Env:
-    env = Env(pytestconfig=pytestconfig)
-    level = logging.DEBUG if env.verbose > 0 else logging.INFO
-    logging.getLogger('').setLevel(level=level)
-    env.setup()
-    return env
-
-
-@pytest.fixture(scope='package')
-def httpd(env) -> Httpd:
-    httpd = Httpd(env=env)
-    assert httpd.exists(), f'httpd not found: {env.httpd}'
-    httpd.clear_logs()
-    assert httpd.start()
-    yield httpd
-    httpd.stop()
-
-
-@pytest.fixture(scope='package')
-def nghttpx(env, httpd) -> Optional[Nghttpx]:
-    if env.have_h3_server():
-        nghttpx = Nghttpx(env=env)
-        nghttpx.clear_logs()
-        assert nghttpx.start()
-        yield nghttpx
-        nghttpx.stop()
-    return None
 
