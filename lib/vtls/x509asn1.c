6b364324598a6f14c557d41c44cc5e26adc74388
Daniel Stenberg <daniel@haxx.se>
2023-03-06 11:32:55 +0100

    x509asn1: use plain %x, not %lx, when the arg is an int
    Pointed out by Coverity.
    
    Closes #10689

--- lib/vtls/x509asn1.c
+++ lib/vtls/x509asn1.c
@@ -1118,7 +1118,7 @@ CURLcode Curl_extract_certinfo(struct Curl_easy *data,
   for(ccp = cert.version.beg; ccp < cert.version.end; ccp++)
     version = (version << 8) | *(const unsigned char *) ccp;
   if(data->set.ssl.certinfo) {
-    ccp = curl_maprintf("%lx", version);
+    ccp = curl_maprintf("%x", version);
     if(!ccp)
       return CURLE_OUT_OF_MEMORY;
     result = Curl_ssl_push_certinfo(data, certnum, "Version", ccp);
