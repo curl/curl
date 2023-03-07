0ae0abbe72514a75c10bfc4108d9f254f594c086
Matt Jolly <Matt.Jolly@footclan.ninja>
2023-03-08 02:16:45 +1100

    hostip: refuse to resolve the .onion TLD
    RFC 7686 states that:
    
    > Applications that do not implement the Tor
    > protocol SHOULD generate an error upon the use of .onion and
    > SHOULD NOT perform a DNS lookup.
    
    Let's do that.
    
    https://www.rfc-editor.org/rfc/rfc7686#section-2
    
    Add test 1471 and 1472 to verify
    
    Fixes #543
    Closes #10705

--- lib/hostip.c
+++ lib/hostip.c
@@ -652,6 +652,14 @@ enum resolve_t Curl_resolv(struct Curl_easy *data,
   CURLcode result;
   enum resolve_t rc = CURLRESOLV_ERROR; /* default to failure */
   struct connectdata *conn = data->conn;
+  /* We should intentionally error and not resolve .onion TLDs */
+  size_t hostname_len = strlen(hostname);
+  if(hostname_len >= 7 &&
+     (curl_strequal(&hostname[hostname_len - 6], ".onion") ||
+      curl_strequal(&hostname[hostname_len - 7], ".onion."))) {
+    failf(data, "Not resolving .onion address (RFC 7686)");
+    return CURLRESOLV_ERROR;
+  }
   *entry = NULL;
 #ifndef CURL_DISABLE_DOH
   conn->bits.doh = FALSE; /* default is not */
