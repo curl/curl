8333d16a94b8d0c37781466ed7dded150980deb6
Viktor Szakats <commit@vsz.me>
2023-03-12 01:55:58 +0000

    config-w32.h: enable inttypes.h for mingw

--- lib/config-win32.h
+++ lib/config-win32.h
@@ -39,7 +39,8 @@
 #define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#if defined(_MSC_VER) && (_MSC_VER >= 1800)
+#if defined(__MINGW32__) || \
+    (defined(_MSC_VER) && (_MSC_VER >= 1800))
 #define HAVE_INTTYPES_H 1
 #endif
 
