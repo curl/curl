5ec15ea48fe87453b53a8df9b9f9e2277a874b73
Daniel Stenberg <daniel@haxx.se>
2023-03-14 17:24:02 +0100

    lib643: LIB644 is never defined, this is dead code
    Closes #10765

--- tests/libtest/lib643.c
+++ tests/libtest/lib643.c
@@ -35,23 +35,6 @@ struct WriteThis {
 
 static size_t read_callback(char *ptr, size_t size, size_t nmemb, void *userp)
 {
-#ifdef LIB644
-  static int count = 0;
-  (void)ptr;
-  (void)size;
-  (void)nmemb;
-  (void)userp;
-  switch(count++) {
-  case 0: /* Return a single byte. */
-    *ptr = '\n';
-    return 1;
-  case 1: /* Request abort. */
-    return CURL_READFUNC_ABORT;
-  }
-  printf("Wrongly called >2 times\n");
-  exit(1); /* trigger major failure */
-#else
-
   struct WriteThis *pooh = (struct WriteThis *)userp;
   int eof = !*pooh->readptr;
 
@@ -71,7 +54,6 @@ static size_t read_callback(char *ptr, size_t size, size_t nmemb, void *userp)
   }
 
   return 0;                         /* no more data left to deliver */
-#endif
 }
 
 static int once(char *URL, bool oldstyle)
