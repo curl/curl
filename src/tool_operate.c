ceb0312950972f73dedc4830bb3be6c92a0b2f80
Daniel Stenberg <daniel@haxx.se>
2023-03-20 23:22:08 +0100

    tool_operate: pass a long as CURLOPT_HEADEROPT argument
    Closes #10798

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -1446,7 +1446,7 @@ static CURLcode single_transfer(struct GlobalConfig *global,
           /* new in libcurl 7.36.0 */
           if(config->proxyheaders) {
             my_setopt_slist(curl, CURLOPT_PROXYHEADER, config->proxyheaders);
-            my_setopt(curl, CURLOPT_HEADEROPT, CURLHEADER_SEPARATE);
+            my_setopt(curl, CURLOPT_HEADEROPT, (long)CURLHEADER_SEPARATE);
           }
 
           /* new in libcurl 7.5 */
