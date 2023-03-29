58e85d6d1d87382b6c5fbb6053e555323780796f
Dan Fandrich <dan@coneharvesters.com>
2023-03-29 11:27:53 -0700

    tests: fix remaining servers to run with a dynamic log directory
    This final commit in the series is sufficient to allow the tests succeed
    if $LOGDIR is changed in runtests.pl.
    
    Ref: #10818
    Closes #10866

--- tests/rtspserver.pl
+++ tests/rtspserver.pl
@@ -73,6 +73,12 @@ while(@ARGV) {
             shift @ARGV;
         }
     }
+    elsif($ARGV[0] eq '--logdir') {
+        if($ARGV[1]) {
+            $logdir = $ARGV[1];
+            shift @ARGV;
+        }
+    }
     elsif($ARGV[0] eq '--srcdir') {
         if($ARGV[1]) {
             $srcdir = $ARGV[1];
@@ -118,7 +124,8 @@ if(!$logfile) {
 
 $flags .= "--pidfile \"$pidfile\" ".
     "--portfile \"$portfile\" ".
-    "--logfile \"$logfile\" ";
+    "--logfile \"$logfile\" ".
+    "--logdir \"$logdir\" ";
 $flags .= "--ipv$ipvnum --port $port --srcdir \"$srcdir\"";
 
 $| = 1;
