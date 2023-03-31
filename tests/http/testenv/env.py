3797f1a4ca8fbd1c73a93ea973a3a6931b6e689b
Stefan Eissing <stefan@eissing.org>
2023-03-31 11:07:18 +0200

    rustls: fix error in recv handling
    - when rustls is told to recieve more TLS data and its internal
      plaintext buffers are full, it returns an IOERROR
    - avoid receiving TLS data while plaintext is not read empty
    
    pytest:
    - increase curl run timeout when invoking pytest with higher verbosity
    
    Closes #10876

--- tests/http/testenv/env.py
+++ tests/http/testenv/env.py
@@ -288,7 +288,7 @@ class Env:
         self._verbose = pytestconfig.option.verbose \
             if pytestconfig is not None else 0
         self._ca = None
-        self._test_timeout = 60.0  # seconds
+        self._test_timeout = 300.0 if self._verbose > 1 else 60.0  # seconds
 
     def issue_certs(self):
         if self._ca is None:
