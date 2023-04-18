91f18a3c7064692e8f99eecfe144745c8857f1da
Stefan Eissing <stefan@eissing.org>
2023-04-18 15:35:13 +0200

    removing no longer used local var

--- lib/sendf.c
+++ lib/sendf.c
@@ -271,7 +271,6 @@ static CURLcode chop_write(struct Curl_easy *data,
   if(type & CLIENTWRITE_BODY) {
 #ifdef USE_WEBSOCKETS
     if(conn->handler->protocol & (CURLPROTO_WS|CURLPROTO_WSS)) {
-      struct HTTP *ws = data->req.p.http;
       writebody = Curl_ws_writecb;
       writebody_ptr = data;
     }
