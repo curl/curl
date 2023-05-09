6708498588729d7e446878297f9ff6f341970a38
Daniel Stenberg <daniel@haxx.se>
2023-05-09 08:28:47 +0200

    compressed.d: clarify the words on "not notifying headers"
    Reported-by: Dylan Anthony
    Fixes #11091
    Closes #11092

--- docs/cmdline-opts/compressed.d
+++ docs/cmdline-opts/compressed.d
@@ -10,7 +10,11 @@ Added: 7.10
 Multi: boolean
 ---
 Request a compressed response using one of the algorithms curl supports, and
-automatically decompress the content. Headers are not modified.
+automatically decompress the content.
+
+Response headers are not modified when saved, so if they are "interpreted"
+separately again at a later point they might appear to be saying that the
+content is (still) compressed; while in fact it has already been decompressed.
 
 If this option is used and the server sends an unsupported encoding, curl will
 report an error. This is a request, not an order; the server may or may not
