e18a044612678779682ec4b49a08d0a8d962acda
Daniel Stenberg <daniel@haxx.se>
2023-05-11 09:56:51 +0200

    headers: clear (possibly) lingering pointer in init
    The "prevhead" pointer is used for the headers storage but was not
    cleared correctly in init, which made it possible to act up when a
    handle is reused.
    
    Reported-by: Steve Herrell
    Fixes #11101
    Closes #11103

--- lib/headers.c
+++ lib/headers.c
@@ -336,6 +336,7 @@ CURLcode Curl_headers_push(struct Curl_easy *data, const char *header,
 static void headers_init(struct Curl_easy *data)
 {
   Curl_llist_init(&data->state.httphdrs, NULL);
+  data->state.prevhead = NULL;
 }
 
 /*
