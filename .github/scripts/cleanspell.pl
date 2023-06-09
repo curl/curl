56b7feb1dae1252de9d82f6fd9b2602a31b50476
Daniel Stenberg <daniel@haxx.se>
2023-06-09 12:47:55 +0200

    fixup remove these function names before spell check

--- .github/scripts/cleanspell.pl
+++ .github/scripts/cleanspell.pl
@@ -69,6 +69,7 @@ while(<F>) {
         $_ =~ s/curl_mime_(subparts|addpart|filedata|data_cb)//g;
         $_ =~ s/curl_ws_(send|recv|meta)//g;
         $_ =~ s/curl_url_(dup)//g;
+        $_ =~ s/curl_pushheader_by(name|num)//g;
         $_ =~ s/libcurl-env//g;
         $_ =~ s/(^|\W)((tftp|https|http|ftp):\/\/[a-z0-9\-._~%:\/?\#\[\]\@!\$&'()*+,;=]+)//gi;
         print O $_;
