c5c48f5d2ba9dc4b3108462c2a5510230fb6ca3a
Dan Fandrich <dan@coneharvesters.com>
2023-06-21 19:30:50 -0700

    runtests: work around a perl without SIGUSR1
    At least msys2 perl v5.32.1 doesn't seem to define this signal. Since
    this signal is only used for debugging, just ignore if setting it fails.
    
    Reported-by: Marcel Raad
    Fixes #11350
    Closes #11366

--- tests/runner.pm
+++ tests/runner.pm
@@ -189,7 +189,10 @@ sub runner_init {
             # TODO: set up better signal handlers
             $SIG{INT} = 'IGNORE';
             $SIG{TERM} = 'IGNORE';
-            $SIG{USR1} = 'IGNORE';
+            eval {
+                # some msys2 perl versions don't define SIGUSR1
+                $SIG{USR1} = 'IGNORE';
+            };
 
             $thisrunnerid = $$;
             print "Runner $thisrunnerid starting\n" if($verbose);
