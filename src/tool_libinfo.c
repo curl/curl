a67dacfd5eeab6b0bdba2941837659d6e48e831a
Daniel Stenberg <daniel@haxx.se>
2023-07-15 14:00:09 +0200

    curl: return error when asked to use an unsupported HTTP version
    When one of the following options are used but the libcurl in use does
    not support it:
    
    --http2
    --http2-prior-knowledge
    --proxy-http2
    
    Closes #11440

--- src/tool_libinfo.c
+++ src/tool_libinfo.c
@@ -73,6 +73,7 @@ bool feature_brotli = FALSE;
 bool feature_hsts = FALSE;
 bool feature_http2 = FALSE;
 bool feature_http3 = FALSE;
+bool feature_httpsproxy = FALSE;
 bool feature_libz = FALSE;
 bool feature_ntlm = FALSE;
 bool feature_ntlm_wb = FALSE;
@@ -97,7 +98,7 @@ static struct feature_name_presentp {
   {"HSTS",           &feature_hsts,       CURL_VERSION_HSTS},
   {"HTTP2",          &feature_http2,      CURL_VERSION_HTTP2},
   {"HTTP3",          &feature_http3,      CURL_VERSION_HTTP3},
-  {"HTTPS-proxy",    NULL,                CURL_VERSION_HTTPS_PROXY},
+  {"HTTPS-proxy",    &feature_httpsproxy, CURL_VERSION_HTTPS_PROXY},
   {"IDN",            NULL,                CURL_VERSION_IDN},
   {"IPv6",           NULL,                CURL_VERSION_IPV6},
   {"Kerberos",       NULL,                CURL_VERSION_KERBEROS5},
