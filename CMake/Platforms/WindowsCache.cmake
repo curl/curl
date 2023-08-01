569fd0622fb42dcb6f3ed02a13226689fa3f524f
Viktor Szakats <commit@vsz.me>
2023-08-01 10:12:24 +0000

    add always available Windows headers

--- CMake/Platforms/WindowsCache.cmake
+++ CMake/Platforms/WindowsCache.cmake
@@ -24,6 +24,11 @@
 if(NOT UNIX)
   if(WIN32)
 
+    set(HAVE_WINDOWS_H 1)
+    set(HAVE_WS2TCPIP_H 1)
+    set(HAVE_WINSOCK2_H 1)
+    set(HAVE_WINCRYPT_H 1)
+
     if(MINGW)
       set(HAVE_SNPRINTF 1)
       set(HAVE_UNISTD_H 1)
