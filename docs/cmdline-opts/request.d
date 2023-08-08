1bd6d4dee1187fde2e696286e4b38bd0b53f9689
Daniel Stenberg <daniel@haxx.se>
2023-08-08 15:44:14 +0200

    request.d: use .TP for protocol "labels"
    To render the section nicer in man page.
    
    Closes #11630

--- docs/cmdline-opts/request.d
+++ docs/cmdline-opts/request.d
@@ -11,7 +11,11 @@ Added: 6.0
 See-also: request-target
 Multi: single
 ---
-(HTTP) Specifies a custom request method to use when communicating with the
+Change the method to use when starting the transfer.
+.RS
+.TP 15
+**HTTP**
+Specifies a custom request method to use when communicating with the
 HTTP server. The specified request method will be used instead of the method
 otherwise used (which defaults to *GET*). Read the HTTP 1.1 specification for
 details and explanations. Common additional HTTP requests include *PUT* and
@@ -29,17 +33,19 @@ The method string you set with --request will be used for all requests, which
 if you for example use --location may cause unintended side-effects when curl
 does not change request method according to the HTTP 30x response codes - and
 similar.
-
-(FTP)
+.TP
+**FTP**
 Specifies a custom FTP command to use instead of *LIST* when doing file lists
 with FTP.
-
-(POP3)
+.TP
+**POP3**
 Specifies a custom POP3 command to use instead of *LIST* or *RETR*.
 (Added in 7.26.0)
-
-(IMAP)
+.TP
+**IMAP**
 Specifies a custom IMAP command to use instead of *LIST*. (Added in 7.30.0)
-
-(SMTP)
+.TP
+**SMTP**
 Specifies a custom SMTP command to use instead of *HELP* or *VRFY*. (Added in 7.34.0)
+.RE
+.IP
