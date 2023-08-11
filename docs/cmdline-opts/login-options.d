1f2beda07824e61b465a6fee984304d93d0ad515
Daniel Stenberg <daniel@haxx.se>
2023-08-12 00:19:40 +0200

    login-options.d: use link to the internet draft

--- docs/cmdline-opts/login-options.d
+++ docs/cmdline-opts/login-options.d
@@ -13,9 +13,10 @@ Multi: single
 Specify the login options to use during server authentication.
 
 You can use login options to specify protocol specific options that may be
-used during authentication. At present only IMAP, POP3 and SMTP support
-login options. For more information about login options please see RFC
-2384, RFC 5092 and the IETF draft **draft-earhart-url-smtp-00.txt**.
+used during authentication. At present only IMAP, POP3 and SMTP support login
+options. For more information about login options please see RFC 2384,
+RFC 5092 and the IETF draft
+https://datatracker.ietf.org/doc/html/draft-earhart-url-smtp-00.
 
 Since 8.2.0, IMAP supports the login option "AUTH=+LOGIN". With this option,
 curl uses the plain (not SASL) LOGIN IMAP command even if the server advertises
