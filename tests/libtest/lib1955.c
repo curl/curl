00e6d9bdf3435080fc992e87d2b35b3808d22b15
Jay Satiro <raysatiro@yahoo.com>
2023-08-12 15:06:08 -0400

    http_aws_sigv4: handle no-value user header entries
    - Handle user headers in format 'name:' and 'name;' with no value.
    
    The former is used when the user wants to remove an internal libcurl
    header and the latter is used when the user actually wants to send a
    no-value header in the format 'name:' (note the semi-colon is converted
    by libcurl to a colon).
    
    Prior to this change the AWS header import code did not special case
    either of those and the generated AWS SignedHeaders would be incorrect.
    
    Reported-by: apparentorder@users.noreply.github.com
    
    Fixes #11664
    Closes #xxxx

--- tests/libtest/lib1955.c
+++ tests/libtest/lib1955.c
@@ -49,7 +49,7 @@ int test(char *URL)
   test_setopt(curl, CURLOPT_USERPWD, "xxx");
   test_setopt(curl, CURLOPT_HEADER, 0L);
   test_setopt(curl, CURLOPT_URL, URL);
-  list = curl_slist_append(list, "test2: 1234");
+  list = curl_slist_append(list, "test3: 1234");
   if(!list)
     goto test_cleanup;
   if(libtest_arg2) {
@@ -57,8 +57,22 @@ int test(char *URL)
   }
   test_setopt(curl, CURLOPT_CONNECT_TO, connect_to);
   curl_slist_append(list, "Content-Type: application/json");
+
+  /* 'name;' user headers with no value are used to send an empty header in the
+     format 'name:' (note the semi-colon becomes a colon). this entry should
+     show in SignedHeaders without an additional semi-colon, as any other
+     header would. eg 'foo;test2;test3' and not 'foo;test2;;test3'. */
+  curl_slist_append(list, "test2;");
+
+  /* 'name:' user headers with no value are used to signal an internal header
+     of that name should be removed and are not sent as a header. this entry
+     should not show in SignedHeaders. */
   curl_slist_append(list, "test1:");
+
+  /* 'name' user headers with no separator or value are invalid and ignored.
+     this entry should not show in SignedHeaders. */
   curl_slist_append(list, "test0");
+
   curl_slist_append(list, "test_space: t\ts  m\t   end    ");
   curl_slist_append(list, "tesMixCase: MixCase");
   test_setopt(curl, CURLOPT_HTTPHEADER, list);
