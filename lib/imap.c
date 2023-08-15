3fd36001743273c09ffda9e3edc81f1acbb14729
MonkeybreadSoftware <support@monkeybreadsoftware.de>
2023-08-15 14:27:20 +0200

    Update imap.c
    space fixed

--- lib/imap.c
+++ lib/imap.c
@@ -693,7 +693,7 @@ static CURLcode imap_perform_list(struct Curl_easy *data)
           SUBJECT TO SENDER REPLY-TO CC BCC)])
           to get the list of emails
         */
-        const char *positionColon   = strchr(imap->custom_params, ':');
+        const char *positionColon = strchr(imap->custom_params, ':');
         if(positionColon) {
           const char *paren = strchr(imap->custom_params, '(');
           if(!paren || paren > positionColon)
