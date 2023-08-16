acce789a1f53d929ac81181b134c9389e88a53c1
Viktor Szakats <commit@vsz.me>
2023-08-16 16:16:02 +0000

    limit manifest setting to non-Unicode builds (tentative)
    `./configure` lacks an enable-unicode option, so for autotools builds,
    this needs to be arranged manually via:
    
    ```shell
    export RCFLAGS='-DUNICODE -D_UNICODE'
    ./configure [...]
    ```

--- lib/Makefile.mk
+++ lib/Makefile.mk
@@ -118,6 +118,7 @@ endif
 ifdef WIN32
   ifneq ($(findstring -unicode,$(CFG)),)
     CPPFLAGS += -DUNICODE -D_UNICODE
+    RCFLAGS += -DUNICODE -D_UNICODE
     CURL_LDFLAGS_BIN += -municode
   endif
 endif
