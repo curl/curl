d80d6d5359211d2c2039b7c3d700c31e0dc3aee3
Daniel Stenberg <daniel@haxx.se>
2023-08-19 20:06:14 +0200

    cmdline-opts/docs: mentioned the negative option part
    ... for --no-alpn and --no-buffer in the same style done for other --no-
    options:
    
    "Note that this is the negated option name documented."

--- docs/cmdline-opts/no-buffer.d
+++ docs/cmdline-opts/no-buffer.d
@@ -13,3 +13,6 @@ Disables the buffering of the output stream. In normal work situations, curl
 will use a standard buffered output stream that will have the effect that it
 will output the data in chunks, not necessarily exactly when the data arrives.
 Using this option will disable that buffering.
+
+Note that this is the negated option name documented. You can use --buffer to
+enable buffering again.
