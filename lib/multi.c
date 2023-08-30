7f597ca12cebe91889246b91ac2b710eac19652b
Jay Satiro <raysatiro@yahoo.com>
2023-08-30 02:01:09 -0400

    multi: remove 'processing: <url>' debug message
    - Remove debug message added by e024d566.
    
    Closes https://github.com/curl/curl/pull/11759

--- lib/multi.c
+++ lib/multi.c
@@ -638,7 +638,6 @@ CURLMcode curl_multi_add_handle(struct Curl_multi *multi,
   CONNCACHE_UNLOCK(data);
 
   multi_warn_debug(multi, data);
-  infof(data, "processing: %s", data->state.url);
 
   return CURLM_OK;
 }
