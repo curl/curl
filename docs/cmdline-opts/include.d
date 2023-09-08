fc57b5f0620f6418024a7f4180ffb94e9a457659
pszlazak <11884243+pszlazak@users.noreply.github.com>
2023-09-08 17:55:11 +0200

    include.d: explain headers not printed prior 7.75.0
    Prior to 7.75.0 response headers were not printed
    if -f/--fail was used and an error was reported by
    server.
    This was fixed in ab525c0 (precedes 7.75.0).
    
    Closes #11822

--- docs/cmdline-opts/include.d
+++ docs/cmdline-opts/include.d
@@ -14,3 +14,6 @@ include things like server name, cookies, date of the document, HTTP version
 and more...
 
 To view the request headers, consider the --verbose option.
+
+Prior to 7.75.0 curl was not able to print headers if *fail* option was used and
+there was error reported by server.
