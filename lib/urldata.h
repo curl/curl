2cb0d346aaacbe8aae7ba19f42190d802a038972
Daniel Stenberg <daniel@haxx.se>
2023-09-16 23:54:44 +0200

    http: use per-request counter to check too large headers
    Not the counter that accumulates all headers over all redirects.
    
    Follow-up to 3ee79c1674fd6
    
    Do a second check for 20 times the limit for the accumulated size for
    all headers.
    
    Fixes #11871
    Reported-by: Joshix-1 on github
    Closes #11872

--- lib/urldata.h
+++ lib/urldata.h
@@ -640,7 +640,9 @@ struct SingleRequest {
   curl_off_t pendingheader;      /* this many bytes left to send is actually
                                     header and not body */
   struct curltime start;         /* transfer started at this time */
-  unsigned int headerbytecount;  /* only count received headers */
+  unsigned int headerbytecount;  /* received server headers (not CONNECT
+                                    headers) */
+  unsigned int allheadercount;   /* all received headers (server + CONNECT) */
   unsigned int deductheadercount; /* this amount of bytes doesn't count when
                                      we check if anything has been transferred
                                      at the end of a connection. We use this
