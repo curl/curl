6bc71914383ffa849d4369013ab5f494c750a033
Daniel Stenberg <daniel@haxx.se>
2023-09-21 17:46:12 +0200

    upload-file.d: describe the file name slash/backslash handling
    Closes #11911

--- docs/cmdline-opts/upload-file.d
+++ docs/cmdline-opts/upload-file.d
@@ -9,7 +9,7 @@ Example: -T file $URL
 Example: -T "img[1-1000].png" ftp://ftp.example.com/
 Example: --upload-file "{file1,file2}" $URL
 Added: 4.0
-See-also: get head
+See-also: get head request data
 Multi: append
 ---
 This transfers the specified local file to the remote URL.
@@ -18,12 +18,18 @@ If there is no file part in the specified URL, curl appends the local file
 name to the end of the URL before the operation starts. You must use a
 trailing slash (/) on the last directory to prove to curl that there is no
 file name or curl thinks that your last directory name is the remote file name
-to use. If this is used on an HTTP(S) server, the PUT command is used.
+to use.
+
+When putting the local file name at the end of the URL, curl ignores what is
+on the left side of any slash (/) or backslash (\\) used in the file name and
+only appends what is on the right side of the rightmost such character.
 
 Use the file name "-" (a single dash) to use stdin instead of a given file.
 Alternately, the file name "." (a single period) may be specified instead of
 "-" to use stdin in non-blocking mode to allow reading server output while
 stdin is being uploaded.
+
+If this option is used with a HTTP(S) URL, the PUT method is used.
 
 You can specify one --upload-file for each URL on the command line. Each
 --upload-file + URL pair specifies what to upload and to where. curl also
