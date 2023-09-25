c76c077f4954d81eb34682786d291050552bfcb3
MonkeybreadSoftware <support@monkeybreadsoftware.de>
2023-09-25 21:33:54 +0200

    Update http.c
    extended text

--- lib/http.c
+++ lib/http.c
@@ -1159,7 +1159,7 @@ CURLcode Curl_http_input_auth(struct Curl_easy *data, bool proxy,
  * http_should_fail() determines whether an HTTP response has gotten us
  * into an error state or not.
  *
- * @param data all information about the current connection
+ * @param data all information about the current connection and the current transfer
  *
  * @retval FALSE communications should continue
  *
