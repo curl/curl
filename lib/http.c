f528d7d59a74ae713cac91347f3a83bbca9fcc83
MonkeybreadSoftware <support@monkeybreadsoftware.de>
2023-09-25 21:20:10 +0200

    Update http.c
    Fixed @param to have new name.

--- lib/http.c
+++ lib/http.c
@@ -1159,7 +1159,7 @@ CURLcode Curl_http_input_auth(struct Curl_easy *data, bool proxy,
  * http_should_fail() determines whether an HTTP response has gotten us
  * into an error state or not.
  *
- * @param conn all information about the current connection
+ * @param data all information about the current connection
  *
  * @retval FALSE communications should continue
  *
