55f6d6bd902e35c5dff07d342b519930eb77ffeb
Daniel Stenberg <daniel@haxx.se>
2023-09-28 13:44:37 +0200

    Update CMake/Platforms/WindowsCache.cmake
    Co-authored-by: Viktor Szakats <vszakats@users.noreply.github.com>

--- CMake/Platforms/WindowsCache.cmake
+++ CMake/Platforms/WindowsCache.cmake
@@ -53,6 +53,8 @@ if(NOT UNIX)
     set(HAVE_LIBZ 0)
 
     set(HAVE_ARC4RANDOM 0)
+    set(HAVE_FNMATCH 0)
+    set(HAVE_SCHED_YIELD 0)
     set(HAVE_ARPA_INET_H 0)
     set(HAVE_FCNTL_H 1)
     set(HAVE_IFADDRS_H 0)
