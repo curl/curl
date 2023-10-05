2dc867ecc3d0359f38b4825c338f0068502c1781
Alex Bozarth <ajbozart@us.ibm.com>
2023-10-05 14:30:56 -0500

    Added more version checks and cleaned up code

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -3986,21 +3986,23 @@ static CURLcode ossl_connect_step2(struct Curl_cfilter *cf,
     }
   }
   else {
-    int psigtype_nid;
+    int psigtype_nid = NID_undef;
     const char *negotiated_group_name = NULL;
 
     /* we connected fine, we're not waiting for anything else. */
     connssl->connecting_state = ssl_connect_3;
 
-    /* Informational message */
-    psigtype_nid = NID_undef;
+#if (OPENSSL_VERSION_NUMBER >= 0x30000000L)
     SSL_get_peer_signature_type_nid(backend->handle, &psigtype_nid);
 #if (OPENSSL_VERSION_NUMBER >= 0x30200000L)
-      negotiated_group_name = SSL_get0_group_name(backend->handle);
-#elif (OPENSSL_VERSION_NUMBER >= 0x30000000L)
+    negotiated_group_name = SSL_get0_group_name(backend->handle);
+#else
     negotiated_group_name =
       OBJ_nid2sn(SSL_get_negotiated_group(backend->handle) & 0x0000FFFF);
 #endif
+#endif
+
+    /* Informational message */
     infof(data, "SSL connection using %s / %s / %s / %s",
           SSL_get_version(backend->handle),
           SSL_get_cipher(backend->handle),
@@ -4109,10 +4111,6 @@ static CURLcode servercert(struct Curl_cfilter *cf,
   BIO *mem = BIO_new(BIO_s_mem());
   struct ossl_ssl_backend_data *backend =
     (struct ossl_ssl_backend_data *)connssl->backend;
-  STACK_OF(X509) *certstack;
-  long verify_result;
-  int num_cert_levels;
-  int cert_level;
 
   DEBUGASSERT(backend);
 
@@ -4276,46 +4274,53 @@ static CURLcode servercert(struct Curl_cfilter *cf,
   }
 
 #if (OPENSSL_VERSION_NUMBER >= 0x30000000L)
-  verify_result = SSL_get_verify_result(backend->handle);
-  if(verify_result != X509_V_OK)
-    certstack = SSL_get_peer_cert_chain(backend->handle);
-  else
-    certstack = SSL_get0_verified_chain(backend->handle);
-  num_cert_levels = sk_X509_num(certstack);
-  OpenSSL_add_all_algorithms();
-  OpenSSL_add_all_digests();
+  {
+    STACK_OF(X509) *certstack;
+    long verify_result;
+    int num_cert_levels;
+    int cert_level;
 
-  for(cert_level = 0; cert_level < num_cert_levels; cert_level++) {
-    char cert_algorithm[80] = "";
-    char group_name[80] = "";
-    char group_name_final[80] = "";
-    const X509_ALGOR *palg_cert = NULL;
-    const ASN1_OBJECT *paobj_cert = NULL;
-    X509 *current_cert;
-    EVP_PKEY *current_pkey;
-    int key_bits;
-    int key_sec_bits;
-    int get_group_name;
+    verify_result = SSL_get_verify_result(backend->handle);
+    if(verify_result != X509_V_OK)
+      certstack = SSL_get_peer_cert_chain(backend->handle);
+    else
+      certstack = SSL_get0_verified_chain(backend->handle);
+    num_cert_levels = sk_X509_num(certstack);
+    OpenSSL_add_all_algorithms();
+    OpenSSL_add_all_digests();
 
-    current_cert = sk_X509_value(certstack, cert_level);
+    for(cert_level = 0; cert_level < num_cert_levels; cert_level++) {
+      char cert_algorithm[80] = "";
+      char group_name[80] = "";
+      char group_name_final[80] = "";
+      const X509_ALGOR *palg_cert = NULL;
+      const ASN1_OBJECT *paobj_cert = NULL;
+      X509 *current_cert;
+      EVP_PKEY *current_pkey;
+      int key_bits;
+      int key_sec_bits;
+      int get_group_name;
 
-    X509_get0_signature(NULL, &palg_cert, current_cert);
-    X509_ALGOR_get0(&paobj_cert, NULL, NULL, palg_cert);
-    OBJ_obj2txt(cert_algorithm, sizeof(cert_algorithm), paobj_cert, 0);
+      current_cert = sk_X509_value(certstack, cert_level);
 
-    current_pkey = X509_get0_pubkey(current_cert);
-    key_bits = EVP_PKEY_bits(current_pkey);
-    key_sec_bits = EVP_PKEY_get_security_bits(current_pkey);
-    get_group_name = EVP_PKEY_get_group_name(current_pkey, group_name,
-                                             sizeof(group_name), NULL);
-    msnprintf(group_name_final, sizeof(group_name_final), "/%s", group_name);
+      X509_get0_signature(NULL, &palg_cert, current_cert);
+      X509_ALGOR_get0(&paobj_cert, NULL, NULL, palg_cert);
+      OBJ_obj2txt(cert_algorithm, sizeof(cert_algorithm), paobj_cert, 0);
 
-    infof(data,
-          "  Certificate level %d: "
-          "Public key type %s%s (%d/%d Bits/secBits), signed using %s",
-          cert_level, EVP_PKEY_get0_type_name(current_pkey),
-          get_group_name == 0 ? "" : group_name_final,
-          key_bits, key_sec_bits, cert_algorithm);
+      current_pkey = X509_get0_pubkey(current_cert);
+      key_bits = EVP_PKEY_bits(current_pkey);
+      key_sec_bits = EVP_PKEY_get_security_bits(current_pkey);
+      get_group_name = EVP_PKEY_get_group_name(current_pkey, group_name,
+                                               sizeof(group_name), NULL);
+      msnprintf(group_name_final, sizeof(group_name_final), "/%s", group_name);
+
+      infof(data,
+            "  Certificate level %d: "
+            "Public key type %s%s (%d/%d Bits/secBits), signed using %s",
+            cert_level, EVP_PKEY_get0_type_name(current_pkey),
+            get_group_name == 0 ? "" : group_name_final,
+            key_bits, key_sec_bits, cert_algorithm);
+    }
   }
 #endif
 
