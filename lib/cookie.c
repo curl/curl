84e491c01415f0f4c725a3a8d5341c840e3daf5a
Kuan-Wei Chiu <visitorckw@gmail.com>
2023-10-17 12:37:38 +0800

    cookie: branchless comparison for sorting by creation time
    In the cookie module, when sorting cookies by creation time, the
    comparison logic was previously implemented using a ternary operator,
    which had room for optimization. This change replaces the ternary
    operator with a direct subtraction, making the comparison branchless
    and reducing the number of generated assembly instructions.
    
    Signed-off-by: Kuan-Wei Chiu <visitorckw@gmail.com>

--- lib/cookie.c
+++ lib/cookie.c
@@ -1320,7 +1320,7 @@ static int cookie_sort(const void *p1, const void *p2)
     return (l2 > l1) ? 1 : -1;
 
   /* 4 - compare cookie creation time */
-  return (c2->creationtime > c1->creationtime) ? 1 : -1;
+  return c2->creationtime - c1->creationtime;
 }
 
 /*
@@ -1333,7 +1333,7 @@ static int cookie_sort_ct(const void *p1, const void *p2)
   struct Cookie *c1 = *(struct Cookie **)p1;
   struct Cookie *c2 = *(struct Cookie **)p2;
 
-  return (c2->creationtime > c1->creationtime) ? 1 : -1;
+  return c2->creationtime - c1->creationtime;
 }
 
 #define CLONE(field)                     \
