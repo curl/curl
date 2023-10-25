40211662a21c44b93a25dbb51a2c754f175cc110
Viktor Szakats <commit@vsz.me>
2023-10-25 23:37:48 +0000

    cmake: speed up zstd detection
    Before this patch we detected the presence of a specific zstd API to
    see if we can use the library. zstd introduced that API in its first
    stable release: v1.0.0.
    
    Replace that method by detecting the zstd library version instead and
    accepting if it's v1.0.0 or newer. Also display this detected version.
    
    We use the same version detection method as zstd itself, using its
    public C header.
    
    This deviates from autotools which keeps using the slow method of
    looking for the API by building a test program. The outcome is the same
    as long as zstd keeps offering this API.
    
    Closes #xxxxx

--- CMake/FindZstd.cmake
+++ CMake/FindZstd.cmake
@@ -56,11 +56,18 @@ find_library(Zstd_LIBRARY NAMES zstd
     ${PC_Zstd_LIBRARY_DIRS}
 )
 
+if(Zstd_INCLUDE_DIR)
+  file(READ "${Zstd_INCLUDE_DIR}/zstd.h" _zstd_header)
+  string(REGEX MATCH ".*define ZSTD_VERSION_MAJOR *([0-9]+).*define ZSTD_VERSION_MINOR *([0-9]+).*define ZSTD_VERSION_RELEASE *([0-9]+)" _zstd_ver "${_zstd_header}")
+  set(Zstd_VERSION "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Zstd
   REQUIRED_VARS
     Zstd_LIBRARY
     Zstd_INCLUDE_DIR
+  VERSION_VAR Zstd_VERSION
 )
 
 if(Zstd_FOUND)
