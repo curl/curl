c80ab46eb0c6c48b8394b1aca767390dffb970fe
Viktor Szakats <commit@vsz.me>
2023-11-17 21:42:54 +0000

    tests: check CMake `DISABLE` options

--- tests/disable-scan.pl
+++ tests/disable-scan.pl
@@ -29,6 +29,8 @@ use warnings;
 
 # the DISABLE options that can be set by configure
 my %disable;
+# the DISABLE options that can be set by CMakeLists.txt
+my %disable_cmake;
 # the DISABLE options that are used in C files
 my %file;
 # the DISABLE options that are documented
@@ -59,6 +61,22 @@ sub scan_configure {
     for my $e (@m4) {
         scanconf("$root/m4/$e");
     }
+}
+
+sub scanconf_cmake {
+    my ($f)=@_;
+    open S, "<$f";
+    while(<S>) {
+        if(/(CURL_DISABLE_[A-Z_]+)/g) {
+            my ($sym)=($1);
+            $disable_cmake{$sym} = 1;
+        }
+    }
+    close S;
+}
+
+sub scan_cmake {
+    scanconf_cmake("$root/CMakeLists.txt");
 }
 
 sub scan_file {
@@ -104,6 +122,7 @@ sub scan_docs {
 }
 
 scan_configure();
+scan_cmake();
 scan_sources();
 scan_docs();
 
@@ -121,10 +140,26 @@ for my $s (sort keys %disable) {
     }
 }
 
+# Check the CMakeLists.txt symbols for use in code
+for my $s (sort keys %disable_cmake) {
+    if(!$file{$s}) {
+        printf "Present in CMakeLists.txt, not used by code: %s\n", $s;
+        $error++;
+    }
+    if(!$docs{$s}) {
+        printf "Present in CMakeLists.txt, not documented in $DOCS: %s\n", $s;
+        $error++;
+    }
+}
+
 # Check the code symbols for use in configure
 for my $s (sort keys %file) {
     if(!$disable{$s}) {
         printf "Not set by configure: %s (%s)\n", $s, $file{$s};
+        $error++;
+    }
+    if(!$disable_cmake{$s}) {
+        printf "Not set by CMakeLists.txt: %s (%s)\n", $s, $file{$s};
         $error++;
     }
     if(!$docs{$s}) {
@@ -137,6 +172,10 @@ for my $s (sort keys %file) {
 for my $s (sort keys %docs) {
     if(!$disable{$s}) {
         printf "Documented but not in configure: %s\n", $s;
+        $error++;
+    }
+    if(!$disable_cmake{$s}) {
+        printf "Documented but not in CMakeLists.txt: %s\n", $s;
         $error++;
     }
     if(!$file{$s}) {
