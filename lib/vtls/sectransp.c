2d53cd5fff4f0ea25958b3a2e88c7d9a9e022c8d
Viktor Szakats <commit@vsz.me>
2023-11-17 20:41:43 +0000

    vtls/sectransp: fix `-Wunreachable-code-break`
    ```
    vtls/sectransp.c:3448:9: error: 'break' will never be executed [-Werror,-Wunreachable-code-break]
            break;
            ^~~~~
    vtls/sectransp.c:3428:9: error: 'break' will never be executed [-Werror,-Wunreachable-code-break]
            break;
            ^~~~~
    vtls/sectransp.c:3418:9: error: 'break' will never be executed [-Werror,-Wunreachable-code-break]
            break;
            ^~~~~
    ```
    Ref: https://github.com/curl/curl/actions/runs/6891484996/job/18746659406?pr=12331#step:9:1193

--- lib/vtls/sectransp.c
+++ lib/vtls/sectransp.c
@@ -3403,7 +3403,6 @@ again:
         }
         *curlcode = CURLE_AGAIN;
         return -1L;
-        break;
 
       /* errSSLClosedGraceful - server gracefully shut down the SSL session
          errSSLClosedNoNotify - server hung up on us instead of sending a
@@ -3413,7 +3412,6 @@ again:
       case errSSLClosedNoNotify:
         *curlcode = CURLE_OK;
         return 0;
-        break;
 
         /* The below is errSSLPeerAuthCompleted; it's not defined in
            Leopard's headers */
@@ -3433,7 +3431,6 @@ again:
         failf(data, "SSLRead() return error %d", err);
         *curlcode = CURLE_RECV_ERROR;
         return -1L;
-        break;
     }
   }
   return (ssize_t)processed;
