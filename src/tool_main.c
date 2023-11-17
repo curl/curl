a9fd0d0083a534ef1efa82e7546846776cbd7373
Viktor Szakats <commit@vsz.me>
2023-11-17 19:13:13 +0000

    build: always revert `#pragma GCC diagnostic` after use
    Before this patch some source files were overriding gcc warning options,
    but without restoring them at the end of the file. In CMake UNITY builds
    these options spilled over to the remainder of the source code,
    effecitvely disabling them for a larger portion of the codebase than
    intended.
    
    `#pragma clang diagnostic` didn't have such issue in the codebase.
    
    Reviewed-by: Marcel Raad
    Closes #12352

--- src/tool_main.c
+++ src/tool_main.c
@@ -220,6 +220,7 @@ static void main_free(struct GlobalConfig *config)
 #ifdef _UNICODE
 #if defined(__GNUC__)
 /* GCC doesn't know about wmain() */
+#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #endif
@@ -286,5 +287,11 @@ int main(int argc, char *argv[])
   return (int)result;
 #endif
 }
+
+#ifdef _UNICODE
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+#endif
 
 #endif /* ndef UNITTESTS */
