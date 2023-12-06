78b8e1800980a3c34347117343fa444eefcb1192
Stefan Eissing <stefan@eissing.org>
2023-12-06 12:08:20 +0100

    fix paramters to test_02_27
    - it is a special client that only ever uses http/2

--- tests/http/test_02_download.py
+++ tests/http/test_02_download.py
@@ -386,13 +386,10 @@ class TestDownload:
         r.check_exit_code(0)
 
     # test on paused transfers, based on issue #11982
-    @pytest.mark.parametrize("proto", ['h2', 'h3'])
-    def test_02_27_paused_no_cl(self, env: Env, httpd, nghttpx, proto, repeat):
-        if proto == 'h3' and not env.have_h3():
-            pytest.skip("h3 not supported")
+    def test_02_27_paused_no_cl(self, env: Env, httpd, nghttpx, repeat):
+        proto = 'h2'
         url = f'https://{env.authority_for(env.domain1, proto)}' \
-                       f'/tweak?'\
-                       '&chunks=2&chunk_size=16000'
+            '/tweak?&chunks=2&chunk_size=16000'
         client = LocalClient(env=env, name='h2-pausing')
         r = client.run(args=[url])
         r.check_exit_code(0)
