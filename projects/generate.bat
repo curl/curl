acc9750f618242cecd4b6bc789958e691a547edc
Viktor Szakats <commit@vsz.me>
2023-12-07 19:06:02 +0000

    tidy-up: whitespace

--- projects/generate.bat
+++ projects/generate.bat
@@ -58,7 +58,7 @@ rem ***************************************************************************
     set VERSION=VC14.10
   ) else if /i "%~1" == "vc14.20" (
     set VERSION=VC14.20
-  )else if /i "%~1" == "vc14.30" (
+  ) else if /i "%~1" == "vc14.30" (
     set VERSION=VC14.30
   ) else if /i "%~1" == "-clean" (
     set MODE=CLEAN
