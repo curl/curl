1e4cd519355b63b26a0a9ee1f125153bfcc4995e
Dmitry Karpov <dkarpov@roku.com>
2023-12-13 14:32:53 -0800

    cookie: avoid fopen with empty file name
    Closes #12514

--- lib/cookie.c
+++ lib/cookie.c
@@ -1227,7 +1227,7 @@ struct CookieInfo *Curl_cookie_init(struct Curl_easy *data,
 
   if(data) {
     FILE *fp = NULL;
-    if(file) {
+    if(file && *file) {
       if(!strcmp(file, "-"))
         fp = stdin;
       else {
