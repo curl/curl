1d1826a444e149f88f0c9d04e835de41c5cc10b4
Viktor Szakats <commit@vsz.me>
2023-12-16 18:40:15 +0000

    fix type to avoid warning

--- lib/mprintf.c
+++ lib/mprintf.c
@@ -1010,7 +1010,7 @@ number:
 static int addbyter(int output, FILE *data)
 {
   struct nsprintf *infop = (struct nsprintf *)data;
-  unsigned char outc = (unsigned char)output;
+  char outc = (char)output;
 
   if(infop->length < infop->max) {
     /* only do this if we haven't reached max length yet */
