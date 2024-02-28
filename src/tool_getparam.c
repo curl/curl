b6def3f85a68608bc3734c014d24fe0f3230e2fa
Daniel Stenberg <daniel@haxx.se>
2024-02-28 09:22:18 +0100

    fixup make the warning spell out the used option name

--- src/tool_getparam.c
+++ src/tool_getparam.c
@@ -1601,7 +1601,8 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
         config->ftp_ssl = toggle;
         if(config->ftp_ssl)
           warnf(global,
-                "--ssl is an insecure option, consider --ssl-reqd instead");
+                "--%s is an insecure option, consider --ssl-reqd instead",
+                a->lname);
       }
       break;
     case C_FTP_PASV: /* --ftp-pasv */
