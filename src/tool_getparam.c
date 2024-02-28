01440e4dc080828f2c38f30659ae6527a1f0b2d0
Daniel Stenberg <daniel@haxx.se>
2024-02-28 08:02:49 +0100

    getparam: make --ftp-ssl work again
    Follow-up to 9e4e527 which accidentally broke it
    
    Reported-by: Jordan Brown
    Fixes #13006

--- src/tool_getparam.c
+++ src/tool_getparam.c
@@ -1593,6 +1593,7 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
         url->flags |= GETOUT_URL;
       }
       break;
+    case C_FTP_SSL: /* --ftp-ssl */
     case C_SSL: /* --ssl */
       if(toggle && !feature_ssl)
         err = PARAM_LIBCURL_DOESNT_SUPPORT;
