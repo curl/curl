e4fff4f7708eb3a91c4656bc2b3517d1c67e63c4
Daniel Stenberg <daniel@haxx.se>
2024-03-06 09:18:42 +0100

    Revert "fixup appveyor: disable parallelism to see if we can spot a problem better"
    This reverts commit debf8b15d0b407f4a6570946e4f0979c1cd00b2e.

--- appveyor.sh
+++ appveyor.sh
@@ -60,7 +60,7 @@ if [ "${BUILD_SYSTEM}" = 'CMake' ]; then
     '-DCMAKE_INSTALL_PREFIX=C:/CURL' \
     "-DCMAKE_BUILD_TYPE=${PRJ_CFG}"
   # shellcheck disable=SC2086
-  cmake --build _bld --config "${PRJ_CFG}" -- ${BUILD_OPT:-}
+  cmake --build _bld --config "${PRJ_CFG}" --parallel 2 --clean-first -- ${BUILD_OPT:-}
   if [ "${SHARED}" = 'ON' ]; then
     cp -f -p _bld/lib/*.dll _bld/src/
   fi
