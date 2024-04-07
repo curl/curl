da5cd70ae2de56f820e9594516bb4a8003e4d348
Viktor Szakats <commit@vsz.me>
2024-04-07 12:40:38 +0000

    OS400: enable shellcheck tests

--- .github/scripts/shellcheck.sh
+++ .github/scripts/shellcheck.sh
@@ -3,10 +3,8 @@
 #
 # SPDX-License-Identifier: curl
 
-# FIXME: packages/OS400/* scripts
-
 shellcheck --version
 # shellcheck disable=SC2046
 shellcheck --exclude=1091 \
   --enable=avoid-nullary-conditions,deprecate-which \
-  $(grep -l -E '^#!(/usr/bin/env bash|/bin/sh|/bin/bash)' $(git ls-files | grep -v -F 'packages/OS400/'))
+  $(grep -l -E '^#!(/usr/bin/env bash|/bin/sh|/bin/bash)' $(git ls-files))
