16f5cfdd5983c3a4a6479f443501680d5fcd3e0b
Viktor Szakats <commit@vsz.me>
2024-04-07 11:18:26 +0000

    fix warnings curl-config.in, consistent indentation

--- .github/scripts/shellcheck.sh
+++ .github/scripts/shellcheck.sh
@@ -3,8 +3,10 @@
 #
 # SPDX-License-Identifier: curl
 
+# FIXME: packages/OS400/* scripts
+
 shellcheck --version
 # shellcheck disable=SC2046
 shellcheck --exclude=1091 \
   --enable=avoid-nullary-conditions,deprecate-which \
-  $(grep -l -E '^#!(/usr/bin/env bash|/bin/sh|/bin/bash)$' $(git ls-files))
+  $(grep -l -E '^#!(/usr/bin/env bash|/bin/sh|/bin/bash)$' $(git ls-files | grep -v -F 'packages/OS400/'))
