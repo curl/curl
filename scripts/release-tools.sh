2fa0d76cc9412ad7f24857cec7f1324ffdcf63bf
Daniel Stenberg <daniel@haxx.se>
2024-04-08 17:14:35 +0200

    release-tools.sh: store the timestamp and release tag too
    When maketgz invokes this script to generate the docs/RELEASE-TOOLS.md
    file that gets bundled in the release, it now also passes on the exact
    timestamp and version number so that those details also get mentioned in
    the document. They will help users reproduce an identical tarball.

--- scripts/release-tools.sh
+++ scripts/release-tools.sh
@@ -25,8 +25,13 @@
 
 set -eu
 
+# this should ideally be passed in
+timestamp=${1:-unknown}
+version=${2:-unknown}
+tag=$(echo "curl-$version" | tr '.' '_')
+
 cat <<MOO
-# Release tools
+# Release tools used for curl $version
 
 The following tools and their Debian package version numbers were used to
 produce this release tarball.
@@ -53,7 +58,7 @@ cat <<MOO
 
 # Reproduce the tarball
 
-- Clone the repo and checkout the release tag
+- Clone the repo and checkout the tag: $tag
 - Install the same set of tools + versions as listed above
 
 ## Do a standard build
@@ -62,8 +67,9 @@ cat <<MOO
 - ./configure [...]
 - make
 
-## Generate the tarball
+## Generate the tarball with the same timestamp
 
+- export SOURCE_DATE_EPOCH=$timestamp
 - ./maketgz [version]
 
 MOO
