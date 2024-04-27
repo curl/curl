2ec2f48a14fd2c56237ae3e8dbc6df20e2b6ba4a
Viktor Szakats <commit@vsz.me>
2024-04-27 03:25:02 +0200

    lib multi

--- lib/multi.c
+++ lib/multi.c
@@ -343,7 +343,7 @@ static size_t hash_fd(void *key, size_t key_length, size_t slots_num)
   curl_socket_t fd = *((curl_socket_t *) key);
   (void) key_length;
 
-  return (fd % (curl_socket_t)slots_num);
+  return (size_t)(fd % (curl_socket_t)slots_num);
 }
 
 /*
