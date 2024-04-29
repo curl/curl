8ed9044cfbe062cfe12303f5e34749b591c301c0
Daniel Stenberg <daniel@haxx.se>
2024-04-29 12:00:23 +0200

    fixup better error out on memory error

--- lib/mime.c
+++ lib/mime.c
@@ -1419,30 +1419,28 @@ CURLcode curl_mime_filedata(curl_mimepart *part, const char *filename)
       part->data = strdup(filename);
       if(!part->data)
         result = CURLE_OUT_OF_MEMORY;
-
-      part->datasize = -1;
-      if(S_ISREG(sbuf.st_mode)) {
-        part->datasize = filesize(filename, sbuf);
-        part->seekfunc = mime_file_seek;
-      }
-
-      part->readfunc = mime_file_read;
-      part->freefunc = mime_file_free;
-      part->kind = MIMEKIND_FILE;
-
-      /* As a side effect, set the filename to the current file's base name.
-         It is possible to withdraw this by explicitly calling
-         curl_mime_filename() with a NULL filename argument after the current
-         call. */
-      base = strippath(filename);
-      if(!base)
-        result = CURLE_OUT_OF_MEMORY;
       else {
-        CURLcode res = curl_mime_filename(part, base);
+        part->datasize = -1;
+        if(S_ISREG(sbuf.st_mode)) {
+          part->datasize = filesize(filename, sbuf);
+          part->seekfunc = mime_file_seek;
+        }
 
-        if(res)
-          result = res;
-        free(base);
+        part->readfunc = mime_file_read;
+        part->freefunc = mime_file_free;
+        part->kind = MIMEKIND_FILE;
+
+        /* As a side effect, set the filename to the current file's base name.
+           It is possible to withdraw this by explicitly calling
+           curl_mime_filename() with a NULL filename argument after the current
+           call. */
+        base = strippath(filename);
+        if(!base)
+          result = CURLE_OUT_OF_MEMORY;
+        else {
+          result = curl_mime_filename(part, base);
+          free(base);
+        }
       }
     }
   }
