848c104639f6463131bf3f03dc23e3e772196018
Daniel Stenberg <daniel@haxx.se>
2024-05-07 16:25:37 +0200

    lib: remove two instances of "only only" messages
    Fixes #13551
    Reported-by: Lucas Nussbaum
    Closes #13552

--- lib/mime.c
+++ lib/mime.c
@@ -1970,7 +1970,7 @@ static CURLcode cr_mime_read(struct Curl_easy *data,
   switch(nread) {
   case 0:
     if((ctx->total_len >= 0) && (ctx->read_len < ctx->total_len)) {
-      failf(data, "client mime read EOF fail, only "
+      failf(data, "client mime read EOF fail, "
             "only %"CURL_FORMAT_CURL_OFF_T"/%"CURL_FORMAT_CURL_OFF_T
             " of needed bytes read", ctx->read_len, ctx->total_len);
       return CURLE_READ_ERROR;
