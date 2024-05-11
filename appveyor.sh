c5029c52ac68b8ba6427e74cb2f97a97137df8df
Viktor Szakats <commit@vsz.me>
2024-05-11 20:57:42 +0200

    try pacman differently

--- appveyor.sh
+++ appveyor.sh
@@ -102,7 +102,7 @@ EOF
   curl="builds/libcurl-vc14.10-x64-${PATHPART}-dll-ssl-dll-ipv6-sspi/bin/curl.exe"
 elif [ "${BUILD_SYSTEM}" = 'autotools' ]; then
   if command -v pacman 2>/dev/null; then
-    pacman --noconfirm --ask 20 --noprogressbar --sync --needed openssl
+    pacman --noconfirm --ask 20 --noprogressbar --sync --needed openssl-devel zlib-devel
     ls -l /usr/lib
     ls -l /usr/include
   fi
