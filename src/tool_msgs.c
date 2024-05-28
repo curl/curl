6946b3a799657c73b4f398b0a4d6d9d20efc1cd0
Daniel Stenberg <daniel@haxx.se>
2024-05-28 09:44:27 +0200

    curl: make warnings and other messages aware of terminal width
    This removes unnecessary line wraps when the terminal is wider than 79
    columns and it also makes messages look better in narrower terminals.
    
    The get_terminal_columns() function is not split out into its own source
    file.
    
    Suggested-by: Elliott Balsley
    Fixes #13804
    Closes #13808

--- src/tool_msgs.c
+++ src/tool_msgs.c
@@ -29,6 +29,8 @@
 
 #include "tool_cfgable.h"
 #include "tool_msgs.h"
+#include "tool_cb_prg.h"
+#include "terminal.h"
 
 #include "memdebug.h" /* keep this as LAST include */
 
@@ -46,7 +48,7 @@ static void voutf(struct GlobalConfig *config,
                   const char *fmt,
                   va_list ap)
 {
-  size_t width = (79 - strlen(prefix));
+  size_t width = (get_terminal_columns() - strlen(prefix));
   DEBUGASSERT(!strchr(fmt, '\n'));
   if(!config->silent) {
     size_t len;
