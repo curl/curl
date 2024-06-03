b76a5e75e943e593de02d10995d5edf4b84ba386
Viktor Szakats <commit@vsz.me>
2024-06-04 01:34:25 +0200

    rand: fix for UWP
    ```
    curl/lib/rand.c: In function 'Curl_win32_random':
    curl/lib/rand.c:75:6: error: implicit declaration of function 'BCryptGenRandom'; did you mean 'CryptGenRandom'? [-Wimplicit-function-declaration]
       75 |   if(BCryptGenRandom(NULL, entropy, (ULONG)length,
          |      ^~~~~~~~~~~~~~~
          |      CryptGenRandom
    curl/lib/rand.c:75:6: error: nested extern declaration of 'BCryptGenRandom' [-Werror=nested-externs]
    ```
    Ref: https://github.com/curl/curl/actions/runs/9358657250/job/25760914494#step:12:79

--- lib/rand.c
+++ lib/rand.c
@@ -48,7 +48,8 @@
 
 #ifdef _WIN32
 
-#if defined(_WIN32_WINNT) && _WIN32_WINNT >= 0x600
+#if defined(_WIN32_WINNT) && _WIN32_WINNT >= 0x600 && \
+  !defined(CURL_WINDOWS_APP)
 #  define HAVE_WIN_BCRYPTGENRANDOM
 #  include <bcrypt.h>
 #  ifdef _MSC_VER
