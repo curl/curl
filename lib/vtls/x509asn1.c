36e9222c42ff74a71ea4e52150285e2264ad4dd0
z2_ <88509734+z2-2z@users.noreply.github.com>
2024-06-20 21:57:07 +0200

    x509asn1: prevent NULL dereference
    Closes #13978

--- lib/vtls/x509asn1.c
+++ lib/vtls/x509asn1.c
@@ -695,6 +695,11 @@ static CURLcode encodeDN(struct dynbuf *store, struct Curl_asn1Element *dn)
 
       str = Curl_dyn_ptr(&temp);
 
+      if(!str) {
+        result = CURLE_BAD_FUNCTION_ARGUMENT;
+        goto error;
+      }
+
       /* Encode delimiter.
          If attribute has a short uppercase name, delimiter is ", ". */
       for(p3 = str; ISUPPER(*p3); p3++)
