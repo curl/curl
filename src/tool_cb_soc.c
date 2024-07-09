07560d2c5ded55b3751088965da2eb7dfc73d786
Mohammad Sadegh <77042998+mohammadshg84@users.noreply.github.com>
2024-07-09 15:56:36 +0330

    Improve code appearance format.

--- src/tool_cb_soc.c
+++ src/tool_cb_soc.c
@@ -35,24 +35,21 @@
 ** Notice that only Linux is supported for the moment.
 */
 
-curl_socket_t tool_socket_open_mptcp_cb(void *clientp,
-                                        curlsocktype purpose,
-                                        struct curl_sockaddr *addr)
-{
+curl_socket_t tool_socket_open_mptcp_cb(void *clientp, curlsocktype purpose, struct curl_sockaddr *addr) {
   int protocol = addr->protocol;
-
+  
   (void)clientp;
   (void)purpose;
 
   if(protocol == IPPROTO_TCP)
-#if defined(__linux__)
-#  ifndef IPPROTO_MPTCP
-#  define IPPROTO_MPTCP 262
-#  endif
-    protocol = IPPROTO_MPTCP;
-#else
-    return CURL_SOCKET_BAD;
-#endif
-
+  #  if defined(__linux__)
+  #  ifndef IPPROTO_MPTCP
+  #  define IPPROTO_MPTCP 262
+  #  endif
+  protocol = IPPROTO_MPTCP;
+  # else
+  return CURL_SOCKET_BAD;
+  # endif
+  
   return socket(addr->family, addr->socktype, protocol);
 }
