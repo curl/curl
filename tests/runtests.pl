58772b0e082eda333e0a5fc8fb0bc7f17a3cd99c
Viktor Szakats <commit@vsz.me>
2024-07-15 02:48:05 +0200

    runtests: set `SOURCE_DATE_EPOCH` to fix failing around midnight
    To make sure that `managen` called by test 1706 uses the same date as
    the test expects in the `%DATE` macro.
    
    Before this patch when tests started running before UTC midnight and
    reached test 1706 after, these dates were different and the test failed.
    
    Follow-up to 0e73b69b3dd6d174226c60406d3c4266754d70f8
    Fixes #14173
    Closes #14187

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -500,7 +500,9 @@ sub checksystemfeatures {
     $versretval = runclient($versioncmd);
     $versnoexec = $!;
 
-    $DATE = strftime "%Y-%m-%d", localtime;
+    my $current_time = int(time());
+    $ENV{'SOURCE_DATE_EPOCH'} = $current_time;
+    $DATE = strftime "%Y-%m-%d", gmtime($current_time);
 
     open(my $versout, "<", "$curlverout");
     @version = <$versout>;
