65de6839d7cdeec3fd2a3e0d279d00c6ea717ba7
Stefan Eissing <stefan@eissing.org>
2024-07-18 12:53:10 +0200

    quiche, use new eos flag
    - remove upload byte counting
    - send initial data with stream open when possible

--- lib/request.c
+++ lib/request.c
@@ -206,7 +206,7 @@ static CURLcode xfer_send(struct Curl_easy *data,
   if(data->req.eos_read &&
     (Curl_bufq_is_empty(&data->req.sendbuf) ||
      Curl_bufq_len(&data->req.sendbuf) == blen)) {
-    infof(data, "sending last upload chunk of %zu bytes", blen);
+    DEBUGF(infof(data, "sending last upload chunk of %zu bytes", blen));
     eos = TRUE;
   }
   result = Curl_xfer_send(data, buf, blen, eos, pnwritten);
