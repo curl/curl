1e1013d01e350cca4b28eac19e1cf3689d3d36d6
Daniel Stenberg <daniel@haxx.se>
2024-07-23 11:06:56 +0200

    lib: survive some NULL input args
    The input string pointer to:
    
    curl_escape
    curl_easy_escape
    curl_unescape
    curl_easy_unescape
    
    The running_handles pointer to:
    
    curl_multi_perform
    curl_multi_socket_action
    curl_multi_socket_all
    curl_multi_socket
    
    Fixes #14247

--- lib/escape.c
+++ lib/escape.c
@@ -60,7 +60,7 @@ char *curl_easy_escape(struct Curl_easy *data, const char *string,
   struct dynbuf d;
   (void)data;
 
-  if(inlength < 0)
+  if(!string || (inlength < 0))
     return NULL;
 
   Curl_dyn_init(&d, CURL_MAX_INPUT_LENGTH * 3);
@@ -181,7 +181,7 @@ char *curl_easy_unescape(struct Curl_easy *data, const char *string,
 {
   char *str = NULL;
   (void)data;
-  if(length >= 0) {
+  if(string && (length >= 0)) {
     size_t inputlen = (size_t)length;
     size_t outputlen;
     CURLcode res = Curl_urldecode(string, inputlen, &str, &outputlen,
