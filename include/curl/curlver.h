a9f63b8e0e44b25081f1d12480f6cdc6a1cac097
Daniel Stenberg <daniel@haxx.se>
2024-07-31 10:48:48 +0200

    RELEASE-NOTES: synced
    Bump to 8.10.0

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -32,13 +32,13 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "8.9.1-DEV"
+#define LIBCURL_VERSION "8.10.0-DEV"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
 #define LIBCURL_VERSION_MAJOR 8
-#define LIBCURL_VERSION_MINOR 9
-#define LIBCURL_VERSION_PATCH 1
+#define LIBCURL_VERSION_MINOR 10
+#define LIBCURL_VERSION_PATCH 0
 
 /* This is the numeric version of the libcurl version number, meant for easier
    parsing and comparisons by programs. The LIBCURL_VERSION_NUM define will
@@ -59,7 +59,7 @@
    CURL_VERSION_BITS() macro since curl's own configure script greps for it
    and needs it to contain the full number.
 */
-#define LIBCURL_VERSION_NUM 0x080901
+#define LIBCURL_VERSION_NUM 0x080a00
 
 /*
  * This is the date and time when the full source package was created. The
