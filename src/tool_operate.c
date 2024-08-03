e26eefd9ce19b1b954a0dc00ca9cf3db487b9d4a
Daniel Stenberg <daniel@haxx.se>
2024-08-03 20:08:27 +0200

    tool_operate: for -O, use "default" as filename when the URL has none
    ... or pick the last directory part from the path if available.
    
    Instead of returning error.
    
    Add test 690 and 691 to verify. Test 76 and 2036 no longer apply.
    
    Closes #13988

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -1147,9 +1147,13 @@ static CURLcode single_transfer(struct GlobalConfig *global,
               break;
             }
             if(!*per->outfile && !config->content_disposition) {
-              errorf(global, "Remote filename has no length");
-              result = CURLE_WRITE_ERROR;
-              break;
+              free(per->outfile);
+              per->outfile = strdup("curl_response");
+              if(!per->outfile) {
+                result = CURLE_OUT_OF_MEMORY;
+                break;
+              }
+              warnf(global, "No remote file name, uses \"%s\"", per->outfile);
             }
           }
           else if(state->urls) {
