36be007a31f661f3a41293d1fb92cdaae97f9ac4
Viktor Szakats <commit@vsz.me>
2024-08-04 00:58:34 +0200

    POSIX

--- src/tool_vms.c
+++ src/tool_vms.c
@@ -94,7 +94,7 @@ void vms_special_exit(int code, int vms_show)
 {
   int vms_code;
 
-  /* The Posix exit mode is only available after VMS 7.0 */
+  /* The POSIX exit mode is only available after VMS 7.0 */
 #if __CRTL_VER >= 70000000
   if(is_vms_shell() == 0) {
     decc$__posix_exit(code);
