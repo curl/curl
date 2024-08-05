7855ac8f2d21c9648b44066bdce9e4b8028f8ec4
Viktor Szakats <commit@vsz.me>
2024-08-05 11:37:56 +0200

    drop unnecessary stdint.h breaking with old MSVC

--- tests/http/clients/h2-upgrade-extreme.c
+++ tests/http/clients/h2-upgrade-extreme.c
@@ -30,7 +30,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 /* #include <error.h> */
 #include <errno.h>
 
--- tests/http/clients/tls-session-reuse.c
+++ tests/http/clients/tls-session-reuse.c
@@ -30,7 +30,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 /* #include <error.h> */
 #include <errno.h>
