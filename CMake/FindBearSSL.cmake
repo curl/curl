589dd568d8b3d50b94ee08a96cd39776d0ab75fc
Viktor Szakats <commit@vsz.me>
2024-08-13 11:35:45 +0200

    bearssl: standardize find_package_handle_standard_args()

--- CMake/FindBearSSL.cmake
+++ CMake/FindBearSSL.cmake
@@ -34,9 +34,10 @@ find_path(BEARSSL_INCLUDE_DIRS "bearssl.h")
 find_library(BEARSSL_LIBRARY "bearssl")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BEARSSL DEFAULT_MSG
-  BEARSSL_INCLUDE_DIRS
-  BEARSSL_LIBRARY
+find_package_handle_standard_args(BEARSSL
+  REQUIRED_VARS
+    BEARSSL_INCLUDE_DIR
+    BEARSSL_LIBRARY
 )
 
 mark_as_advanced(BEARSSL_INCLUDE_DIRS BEARSSL_LIBRARY)
