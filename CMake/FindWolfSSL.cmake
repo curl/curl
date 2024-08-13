cfd102350ebc836fa018b1b5cd829d8b0cdea7af
Viktor Szakats <commit@vsz.me>
2024-08-13 23:19:13 +0200

    formatting

--- CMake/FindWolfSSL.cmake
+++ CMake/FindWolfSSL.cmake
@@ -47,10 +47,8 @@ find_library(WolfSSL_LIBRARY NAMES "wolfssl"
 
 if(WolfSSL_INCLUDE_DIR)
   set(_version_regex "^#define[ \t]+LIBWOLFSSL_VERSION_STRING[ \t]+\"([^\"]+)\".*")
-  file(STRINGS "${WolfSSL_INCLUDE_DIR}/wolfssl/version.h"
-    WolfSSL_VERSION REGEX "${_version_regex}")
-  string(REGEX REPLACE "${_version_regex}" "\\1"
-    WolfSSL_VERSION "${WolfSSL_VERSION}")
+  file(STRINGS "${WolfSSL_INCLUDE_DIR}/wolfssl/version.h" WolfSSL_VERSION REGEX "${_version_regex}")
+  string(REGEX REPLACE "${_version_regex}" "\\1" WolfSSL_VERSION "${WolfSSL_VERSION}")
   unset(_version_regex)
 endif()
 
