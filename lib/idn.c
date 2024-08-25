985a89d5b3bc06b981dd67cc60c02d51bc40c662
Viktor Szakats <commit@vsz.me>
2024-08-25 12:15:56 +0200

    try another fix
    Ref: #12684
    Ref: https://github.com/curl/curl/pull/12606#issuecomment-1885381038
    Ref: https://github.com/curl/curl/commit/37f1c21cb9c809ec870803fc40e1ed2afd9534ac

--- lib/idn.c
+++ lib/idn.c
@@ -150,7 +150,8 @@ static CURLcode mac_ascii_to_idn(const char *in, char **out)
 #ifdef USE_WIN32_IDN
 /* using Windows kernel32 and normaliz libraries. */
 
-#if !defined(_WIN32_WINNT) || _WIN32_WINNT < 0x600
+#if (!defined(_WIN32_WINNT) || _WIN32_WINNT < 0x600) && \
+  (!defined(WINVER) || WINVER < 0x600)
 WINBASEAPI int WINAPI IdnToAscii(DWORD dwFlags,
                                  const WCHAR *lpUnicodeCharStr,
                                  int cchUnicodeChar,
