4abf2b9699840627e942846c51e564a58aacb52a
Stefan Eissing <stefan@eissing.org>
2024-08-29 13:06:47 +0200

    openssl quic: fix memory leak
    When a OpenSSL quic connection filter is aborted early, as the
    server was not responding, the ssl instances where not closed
    as they should.
    
    Fixes #14720
    Reported-by: ralfjunker on github
    Closes #14724

--- lib/vquic/curl_osslq.c
+++ lib/vquic/curl_osslq.c
@@ -434,6 +434,8 @@ static void cf_osslq_destroy(struct Curl_cfilter *cf, struct Curl_easy *data)
   CURL_TRC_CF(data, cf, "destroy");
   if(ctx) {
     CURL_TRC_CF(data, cf, "cf_osslq_destroy()");
+    if(ctx->tls.ossl.ssl)
+      cf_osslq_ctx_close(ctx);
     cf_osslq_ctx_free(ctx);
   }
   cf->ctx = NULL;
