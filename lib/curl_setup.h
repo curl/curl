37b1af8b96ada70f40b4fa361979533c25242fcc
Viktor Szakats <commit@vsz.me>
2024-09-01 05:15:56 +0200

    comment

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -348,7 +348,7 @@
 #define CURL_PRINTF(fmt, arg)
 #endif
 
-/* Override printf mask check rules in "curl/mprintf.h" */
+/* Override default printf mask check rules in "curl/mprintf.h" */
 #define CURL_TEMP_PRINTF CURL_PRINTF
 
 /* Workaround for mainline llvm v16 and earlier missing a built-in macro
