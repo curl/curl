c767a6b70cac1952452681c7fe676976fa8d5d96
Viktor Szakats <commit@vsz.me>
2024-09-04 01:13:03 +0200

    avoid more clashes 3

--- tests/mk-bundle.pl
+++ tests/mk-bundle.pl
@@ -72,7 +72,7 @@ while(my $line = <$fh>) {
                 "testname", "testdata", "testfd", "teststring", "testeh", "test_failure",
                 "xferinfo", "cyclic_add", "geterr", "test_once", "fire",
                 "websocket_close", "websocket", "run_thread", "num_open", "msgbuff",
-                "unit_setup", "unit_stop", "mydtor", "testcase", "tests", "test_parse", "password", "easy") {  # unit
+                "unit_setup", "unit_stop", "mydtor", "testcase", "tests", "test_parse", "password", "easy", "hash_static") {  # unit
             print "#undef $symb\n";
             print "#define $symb ${symb}_$nam\n";
         }
