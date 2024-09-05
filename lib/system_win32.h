eb8ad66f6c3eae22d470f0b92e7b2f69447fb496
Jay Satiro <raysatiro@yahoo.com>
2024-09-05 02:18:25 -0400

    asyn-thread: stop using GetAddrInfoExW on Windows
    - For the threaded resolver backend on Windows, revert back to
      exclusively use the threaded resolver with libcurl-owned threading
      instead of GetAddrInfoExW with Windows-owned threading.
    
    Winsock (the Windows sockets library) has a bug where it does not wait
    for all of the name resolver threads it is managing to terminate before
    returning from WSACleanup. The threads continue to run and may cause a
    crash.
    
    This commit is effectively a revert of several commits that encompass
    all GetAddrInfoExW code in libcurl. A manual review of merge conflicts
    was used to resolve minor changes that had modified the code for
    aesthetic or build reasons in other commits.
    
    Prior to this change if libcurl was built with the threaded resolver
    backend for Windows, and Windows 8 or later was the operating system at
    runtime, and the caller was not impersonating another user, then libcurl
    would use GetAddrInfoExW to handle asynchronous name lookups.
    
    GetAddrInfoExW support was added in a6bbc87f, which preceded 8.6.0, and
    prior to that the threaded resolver backend used libcurl-owned threading
    exclusively on Windows.
    
    Reported-by: Ionuț-Francisc Oancea
    Reported-by: Razvan Pricope
    
    Ref: https://developercommunity.visualstudio.com/t/ASAN:-heap-use-after-free-in-NdrFullPoin/10654169
    
    Fixes https://github.com/curl/curl/issues/13509#issuecomment-2225338110
    Closes https://github.com/curl/curl/pull/14794
    
    ---
    
    Revert "asyn-thread: avoid using GetAddrInfoExW with impersonation"
    
    This reverts commit 0caadc1f24d20514eed2bf6e5ef0adc140f122c3.
    
    Conflicts:
    	lib/system_win32.c
    
    --
    
    Revert "asyn-thread: fix curl_global_cleanup crash in Windows"
    
    This reverts commit 428579f5d136fd473e97fe089c42ffee55b72a8f.
    
    --
    
    Revert "system_win32: fix a function pointer assignment warning"
    
    This reverts commit 26f002e02ef1142a432c8dc087bd27de71ce38bf.
    
    --
    
    Revert "asyn-thread: use GetAddrInfoExW on >= Windows 8"
    
    This reverts commit a6bbc87f9e9ffb46a1801dfb983e7534825ed56b.
    
    Conflicts:
    	lib/asyn-thread.c
    	lib/system_win32.c
    
    --

--- lib/system_win32.h
+++ lib/system_win32.h
@@ -26,13 +26,12 @@
 
 #include "curl_setup.h"
 
-#ifdef _WIN32
+#if defined(_WIN32)
 
 #include <curl/curl.h>
 
 extern LARGE_INTEGER Curl_freq;
 extern bool Curl_isVistaOrGreater;
-extern bool Curl_isWindows8OrGreater;
 
 CURLcode Curl_win32_init(long flags);
 void Curl_win32_cleanup(long init_flags);
@@ -42,35 +41,6 @@ typedef unsigned int(WINAPI *IF_NAMETOINDEX_FN)(const char *);
 
 /* This is used instead of if_nametoindex if available on Windows */
 extern IF_NAMETOINDEX_FN Curl_if_nametoindex;
-
-/* Identical copy of addrinfoexW/ADDRINFOEXW */
-typedef struct addrinfoexW_
-{
-  int                  ai_flags;
-  int                  ai_family;
-  int                  ai_socktype;
-  int                  ai_protocol;
-  size_t               ai_addrlen;
-  PWSTR                ai_canonname;
-  struct sockaddr     *ai_addr;
-  void                *ai_blob;
-  size_t               ai_bloblen;
-  LPGUID               ai_provider;
-  struct addrinfoexW_ *ai_next;
-} ADDRINFOEXW_;
-
-typedef void (CALLBACK *LOOKUP_COMPLETION_FN)(DWORD, DWORD, LPWSAOVERLAPPED);
-typedef void (WSAAPI *FREEADDRINFOEXW_FN)(ADDRINFOEXW_*);
-typedef int (WSAAPI *GETADDRINFOEXCANCEL_FN)(LPHANDLE);
-typedef int (WSAAPI *GETADDRINFOEXW_FN)(PCWSTR, PCWSTR, DWORD, LPGUID,
-  const ADDRINFOEXW_*, ADDRINFOEXW_**, struct timeval*, LPOVERLAPPED,
-  LOOKUP_COMPLETION_FN, LPHANDLE);
-
-extern FREEADDRINFOEXW_FN Curl_FreeAddrInfoExW;
-extern GETADDRINFOEXCANCEL_FN Curl_GetAddrInfoExCancel;
-extern GETADDRINFOEXW_FN Curl_GetAddrInfoExW;
-
-bool Curl_win32_impersonating(void);
 
 /* This is used to dynamically load DLLs */
 HMODULE Curl_load_library(LPCTSTR filename);
