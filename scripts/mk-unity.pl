60c3d0446546332e1645541f2dc2c072cd019fe9
Viktor Szakats <commit@vsz.me>
2024-09-06 10:26:06 +0200

    autotools: add support for 'unity' builds, enable in CI
    Implement the "unity" builds as known from CMake, but for autotools.
    It's limited to `lib` and `src` (CMake also supports it in `tests`).
    
    Enable with: `--enable-unity` (disabled by default)
    
    Unity builds speed up builds significantly. Cygwin and Windows builds in
    particular, but the effect is noticeable on most systems. It also allows
    discovering unity issues with autotools, benefitting also CMake when
    building the same combination. In CI it makes turnaround times quicker.
    
    This closes build performance with CMake. autotools still lags behind
    because it builds shared and static libcurl in two, separate passes.
    CMake does it in one. Manpage compilation isn't batched, it is in CMake.
    After unity and test bundle support the slowest parts of the build are
    the configuration phase (which is effectively a tedious, non-parallel,
    compilation and/or linking of 300+ tiny programs. The next bottleneck
    is compiling individual examples and finally test servers (only slow
    with autotools).
    
    The autotools implementation is slightly less efficient than CMake,
    because 3 sources are permanently excluded while in CMake this isn't
    necessary and solved more efficiently while building libtests. There is
    also no 'unity' support for tests, making them a less efficient also.
    
    Enable it in CI for most `configure` jobs. Except in GHA/dist (though
    it works fine there too), to use the default config there. Also skip for
    the Linux AWC-LC job where it made builds time a few seconds longer
    (reason undiscovered.)
    
    Autotools test suite builds compared between master -> `--enable-unity`:
    - GHA/Linux: 32s -> 12s
      https://github.com/curl/curl/actions/runs/10705668823/job/29681617374
      https://github.com/curl/curl/actions/runs/10742978889/job/29796766297
    - GHA/macOS: 37s -> 10s
      https://github.com/curl/curl/actions/runs/10705668813/job/29681632885
      https://github.com/curl/curl/actions/runs/10742978699/job/29796768875
    - GHA/FreeBSD: 15m25 -> 10m58 (full workflow time, ~qemu)
      https://github.com/curl/curl/actions/runs/10705668811/job/29681607915
      https://github.com/curl/curl/actions/runs/10742978937/job/29796766115
    - GHA/Cygwin: 3m32 -> 1m21
      https://github.com/curl/curl/actions/runs/10705668809/job/29681609965
      https://github.com/curl/curl/actions/runs/10742978645/job/29796756933
    - GHA/MSYS2: 2m42 -> 50s
      https://github.com/curl/curl/actions/runs/10705668808/job/29681621166
      https://github.com/curl/curl/actions/runs/10742978662/job/29799739289
    - GHA/mingw-w64: 5m32 -> 1m23
      https://github.com/curl/curl/actions/runs/10705668808/job/29681628787
      https://github.com/curl/curl/actions/runs/10742978662/job/29799741568
    
    Closes #14815

---
+++ scripts/mk-unity.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+#***************************************************************************
+#                                  _   _ ____  _
+#  Project                     ___| | | |  _ \| |
+#                             / __| | | | |_) | |
+#                            | (__| |_| |  _ <| |___
+#                             \___|\___/|_| \_\_____|
+#
+# Copyright (C) Viktor Szakats
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at https://curl.se/docs/copyright.html.
+#
+# You may opt to use, copy, modify, merge, publish, distribute and/or sell
+# copies of the Software, and permit persons to whom the Software is
+# furnished to do so, under the terms of the COPYING file.
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+# KIND, either express or implied.
+#
+# SPDX-License-Identifier: curl
+#
+###########################################################################
+
+# Helper script for "unity"-like support in autotools, to generate the umbrella
+# C source that includes the individual source files. Reads Makefile.inc and
+# accepts the variable name containing all the source files to include. Also
+# allow a list of exceptions that are to be excluded from the generated file.
+
+use strict;
+use warnings;
+
+if(!@ARGV) {
+    die "Usage: $0 [<c-sources>] [--exclude <exclude-c-sources>]\n";
+}
+
+# Specific sources to exclude or add as an extra source file
+my @src;
+my %exclude;
+my $in_exclude = 0;
+foreach my $src (@ARGV) {
+    if($in_exclude) {
+        $exclude{$src} = 1;
+    }
+    elsif($src eq "--exclude") {
+        $in_exclude = 1;
+    }
+    else {
+        push @src, $src;
+    }
+}
+
+print <<HEADER
+/* !checksrc! disable COPYRIGHT all */
+HEADER
+    ;
+
+foreach my $src (@src) {
+    if($src =~ /\.c$/g && !exists $exclude{$src}) {
+        print "#include \"$src\"\n";
+    }
+}
