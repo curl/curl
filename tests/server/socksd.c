31003ac0b4f2acbc4e7caccf03a0c0b157b84426
Viktor Szakats <commit@vsz.me>
2024-09-09 00:41:21 +0200

    fixup serverlogfile

--- tests/server/socksd.c
+++ tests/server/socksd.c
@@ -957,6 +957,8 @@ int main(int argc, char *argv[])
   bool unlink_socket = false;
 #endif
 
+  serverlogfile = DEFAULT_LOGFILE;
+
   while(argc > arg) {
     if(!strcmp("--version", argv[arg])) {
       printf("socksd IPv4%s\n",
