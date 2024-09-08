2b89b660436f019008921082c7dc1f32387af2c3
Viktor Szakats <commit@vsz.me>
2024-09-09 01:57:28 +0200

    Revert "portname/pidname, make them all static to avoid shadowing in tftpd.c"
    This reverts commit 4b05b4c328160986b1fdf0f600a917385f196f2a.

--- tests/server/sws.c
+++ tests/server/sws.c
@@ -2026,9 +2026,6 @@ static int service_connection(curl_socket_t msgsock, struct httprequest *req,
   return -1;
 }
 
-static const char *pidname = ".http.pid";
-static const char *portname = ".http.port";
-
 int main(int argc, char *argv[])
 {
   srvr_sockaddr_union_t me;
@@ -2041,6 +2038,8 @@ int main(int argc, char *argv[])
   const char *unix_socket = NULL;
   bool unlink_socket = false;
 #endif
+  const char *pidname = ".http.pid";
+  const char *portname = ".http.port";
   struct httprequest *req = NULL;
   int rc = 0;
   int error;
