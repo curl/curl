3c210143ac01a7e1f8e7f0d0f2dfe1df18334854
Daniel Stenberg <daniel@haxx.se>
2024-09-09 09:09:46 +0200

    lib: remove the final strncpy() calls
    wolfssl: use strcpy() as the target buffer is > 40 bytes
    
    gethostname: return failure if the buffer is too small instead

--- lib/curl_gethostname.c
+++ lib/curl_gethostname.c
@@ -59,7 +59,10 @@ int Curl_gethostname(char * const name, GETHOSTNAME_TYPE_ARG2 namelen)
   /* Override hostname when environment variable CURL_GETHOSTNAME is set */
   const char *force_hostname = getenv("CURL_GETHOSTNAME");
   if(force_hostname) {
-    strncpy(name, force_hostname, namelen - 1);
+    if(strlen(force_hostname) < (size_t)namelen)
+      strcpy(name, force_hostname);
+    else
+      return 1; /* can't do it */
     err = 0;
   }
   else {
