effd237b796b2114d316582a269ec8abbe802662
Viktor Szakats <commit@vsz.me>
2024-09-22 17:52:20 +0200

    server: my_port -> server_port

--- tests/server/mqttd.c
+++ tests/server/mqttd.c
@@ -121,7 +121,7 @@ static char loglockfile[256];
 static bool use_ipv6 = FALSE;
 #endif
 static const char *ipv_inuse = "IPv4";
-static unsigned short my_port = DEFAULT_PORT;
+static unsigned short server_port = DEFAULT_PORT;
 
 static void resetdefaults(void)
 {
@@ -1006,7 +1006,7 @@ int main(int argc, char *argv[])
                   argv[arg]);
           return 0;
         }
-        my_port = curlx_ultous(ulnum);
+        server_port = curlx_ultous(ulnum);
         arg++;
       }
     }
@@ -1056,7 +1056,7 @@ int main(int argc, char *argv[])
 
   {
     /* passive daemon style */
-    sock = sockdaemon(sock, &my_port);
+    sock = sockdaemon(sock, &server_port);
     if(CURL_SOCKET_BAD == sock) {
       goto mqttd_cleanup;
     }
@@ -1064,14 +1064,14 @@ int main(int argc, char *argv[])
   }
 
   logmsg("Running %s version", ipv_inuse);
-  logmsg("Listening on port %hu", my_port);
+  logmsg("Listening on port %hu", server_port);
 
   wrotepidfile = write_pidfile(pidname);
   if(!wrotepidfile) {
     goto mqttd_cleanup;
   }
 
-  wroteportfile = write_portfile(portname, my_port);
+  wroteportfile = write_portfile(portname, server_port);
   if(!wroteportfile) {
     goto mqttd_cleanup;
   }
