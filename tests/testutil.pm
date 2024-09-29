0f68715ff770cebbe6b1933e0a5b77f03a35ffd6
Viktor Szakats <commit@vsz.me>
2024-09-29 17:13:55 +0200

    more logging

--- tests/testutil.pm
+++ tests/testutil.pm
@@ -197,6 +197,7 @@ sub runclient {
 #
 sub runclientoutput {
     my ($cmd)=@_;
+    print "runclientoutput: $^O: Executing: '$cmd'\n";
     return `$cmd 2>$dev_null`;
 
 # This is one way to test curl on a remote machine
