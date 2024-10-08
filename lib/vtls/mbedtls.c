e377c917664241d8cccf67316b96d59a280ad8e4
Viktor Szakats <commit@vsz.me>
2024-10-08 22:41:18 +0200

    GHA: add Linux and macOS mbedTLS jobs, fix issue
    - update mbedTLS repo URL.
    - switch local mbedTLS build to use CMake, and Ninja.
      CMake build is required to create and install mbedTLS `pkg-config`
      files. (as of v3.6.1)
      `-DCMAKE_POSITION_INDEPENDENT_CODE=ON` required to avoid this error
      when linking mbedtls to `libcurl.so`:
      ```
      /usr/bin/ld: /home/runner/mbedtls/lib/libmbedcrypto.a(cipher.c.o): warning: relocation against `mbedtls_cipher_base_lookup_table' in read-only section `.text'
      /usr/bin/ld: /home/runner/mbedtls/lib/libmbedtls.a(ssl_tls.c.o): relocation R_X86_64_PC32 against symbol `mbedtls_x509_crt_profile_suiteb' can not be used when making a shared object; recompile with -fPIC
      /usr/bin/ld: final link failed: bad value
      ```
      Ref: https://github.com/curl/curl/actions/runs/11245069259/job/31264386723#step:40:43
    - make local mbedTLS build 10x smaller by omitting programs and tests.
    - GHA/linux: fix cmake warning by adding `-B .` option.
    - GHA/linux: add build-only cmake job for packaged mbedTLS (2.x).
    - fix compiler warning when building with mbedTLS 2.x:
      ```
      /home/runner/work/curl/curl/lib/vtls/mbedtls.c:344:1: error: ‘mbed_cipher_suite_get_str’ defined but not used [-Werror=unused-function]
        344 | mbed_cipher_suite_get_str(uint16_t id, char *buf, size_t buf_size,
            | ^~~~~~~~~~~~~~~~~~~~~~~~~
      ```
      Ref: https://github.com/curl/curl/actions/runs/11244999065/job/31264168295#step:40:50
    
    Also in preparation for #15193
    Closes #15208

--- lib/vtls/mbedtls.c
+++ lib/vtls/mbedtls.c
@@ -340,6 +340,7 @@ mbed_set_ssl_version_min_max(struct Curl_easy *data,
    cipher suite present in other SSL implementations. Provide
    provisional support for specifying the cipher suite here. */
 #ifdef MBEDTLS_TLS_ECJPAKE_WITH_AES_128_CCM_8
+#if MBEDTLS_VERSION_NUMBER >= 0x03020000
 static int
 mbed_cipher_suite_get_str(uint16_t id, char *buf, size_t buf_size,
                           bool prefer_rfc)
@@ -350,6 +351,7 @@ mbed_cipher_suite_get_str(uint16_t id, char *buf, size_t buf_size,
     return Curl_cipher_suite_get_str(id, buf, buf_size, prefer_rfc);
   return 0;
 }
+#endif
 
 static uint16_t
 mbed_cipher_suite_walk_str(const char **str, const char **end)
@@ -1048,7 +1050,7 @@ mbed_connect_step2(struct Curl_cfilter *cf, struct Curl_easy *data)
 
     /* Make a copy of our const peercert because mbedtls_pk_write_pubkey_der
        needs a non-const key, for now.
-       https://github.com/ARMmbed/mbedtls/issues/396 */
+       https://github.com/Mbed-TLS/mbedtls/issues/396 */
 #if MBEDTLS_VERSION_NUMBER == 0x03000000
     if(mbedtls_x509_crt_parse_der(p,
                         peercert->MBEDTLS_PRIVATE(raw).MBEDTLS_PRIVATE(p),
