d43b30eccbe7b9f217b70bc5b57f9431bc8c4395
Viktor Szakats <commit@vsz.me>
2024-10-12 00:18:01 +0200

    move cast to `Curl_getpid()` macro

--- lib/smb.c
+++ lib/smb.c
@@ -28,9 +28,9 @@
 #if !defined(CURL_DISABLE_SMB) && defined(USE_CURL_NTLM_CORE)
 
 #ifdef _WIN32
-#define Curl_getpid() GetCurrentProcessId()
+#define Curl_getpid() ((unsigned int)GetCurrentProcessId())
 #else
-#define Curl_getpid() getpid()
+#define Curl_getpid() ((unsigned int)getpid())
 #endif
 
 #include "smb.h"
@@ -548,7 +548,7 @@ static void smb_format_message(struct Curl_easy *data, struct smb_header *h,
   h->flags2 = smb_swap16(SMB_FLAGS2_IS_LONG_NAME | SMB_FLAGS2_KNOWS_LONG_NAME);
   h->uid = smb_swap16(smbc->uid);
   h->tid = smb_swap16(req->tid);
-  pid = (unsigned int)Curl_getpid();
+  pid = Curl_getpid();
   h->pid_high = smb_swap16((unsigned short)(pid >> 16));
   h->pid = smb_swap16((unsigned short) pid);
 }
