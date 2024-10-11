05d9c213f962396b530c4bd695b3bd2768bbfc7b
Viktor Szakats <commit@vsz.me>
2024-10-11 10:53:32 +0200

    smb: do not redefine `getpid` on Windows
    Make an `#if` branch instead for the single place it was used.
    
    Redefining symbols can backfire if that symbol is used in system
    headers, especially in unity build. We haven't seen a fallout in
    CI or supported envs, but do it anyway for good measure.
    
    Bug report: https://datagirl.xyz/posts/wolfssl_curl_w2k.html

--- lib/smb.c
+++ lib/smb.c
@@ -27,10 +27,6 @@
 
 #if !defined(CURL_DISABLE_SMB) && defined(USE_CURL_NTLM_CORE)
 
-#ifdef _WIN32
-#define getpid GetCurrentProcessId
-#endif
-
 #include "smb.h"
 #include "urldata.h"
 #include "sendf.h"
@@ -546,7 +542,11 @@ static void smb_format_message(struct Curl_easy *data, struct smb_header *h,
   h->flags2 = smb_swap16(SMB_FLAGS2_IS_LONG_NAME | SMB_FLAGS2_KNOWS_LONG_NAME);
   h->uid = smb_swap16(smbc->uid);
   h->tid = smb_swap16(req->tid);
+#ifdef _WIN32
+  pid = (unsigned int)GetCurrentProcessId();
+#else
   pid = (unsigned int)getpid();
+#endif
   h->pid_high = smb_swap16((unsigned short)(pid >> 16));
   h->pid = smb_swap16((unsigned short) pid);
 }
