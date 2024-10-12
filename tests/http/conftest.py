75dfb7b6499bf51d3fd8aba617fdd5bbf2a3f7c9
Viktor Szakats <commit@vsz.me>
2024-10-12 23:56:38 +0200

    pytest: include `buildinfo.txt` in the output
    If present.
    
    It aims to provide TextClutch the same build information that
    `runtests.pl` already is providing.
    
    Ref: https://testclutch.curl.se/static/reports/feature-matrix.html
    Ref: #15256
    Follow-up to 1fdea1684602a1ae2870c67b5f3e8fd34f63da95 #14802
    Closes #15279

--- tests/http/conftest.py
+++ tests/http/conftest.py
@@ -53,6 +53,13 @@ def pytest_report_header(config):
         report.extend([
             f'  VsFTPD: {env.vsftpd_version()}, ftp:{env.ftp_port}, ftps:{env.ftps_port}'
         ])
+    buildinfo_fn = os.path.join(env.build_dir, 'buildinfo.txt')
+    if os.path.exists(buildinfo_fn):
+        with open(buildinfo_fn, 'r') as file_in:
+            for line in file_in:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    report.extend([line])
     return '\n'.join(report)
 
 # TODO: remove this and repeat argument everywhere, pytest-repeat can be used to repeat tests
