2b36ab555f18f7d57a05e000306d82590e490adf
Viktor Szakats <commit@vsz.me>
2024-10-31 10:17:47 +0100

    gen_resp_file.bat: tidy up

--- winbuild/gen_resp_file.bat
+++ winbuild/gen_resp_file.bat
@@ -23,12 +23,6 @@ rem * SPDX-License-Identifier: curl
 rem *
 rem ***************************************************************************
 
-if exist %OUTFILE% (
-  del %OUTFILE%
-)
-
 echo %MACRO_NAME% = \> %OUTFILE%
-for %%i in (%*) do echo		%DIROBJ%/%%i \>> %OUTFILE%
+for %%i in (%*) do echo %DIROBJ%/%%i \>> %OUTFILE%
 echo. >> %OUTFILE%
-
-:END
