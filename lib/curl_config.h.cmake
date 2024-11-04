218ac90b987cb39bfb08d936b65c7e4840e75926
Stefan Eissing <stefan@eissing.org>
2024-11-04 13:42:18 +0100

    cmake: add new HAVE_WOLFSSL_BIO to curl_config.h.cmake

--- lib/curl_config.h.cmake
+++ lib/curl_config.h.cmake
@@ -721,6 +721,9 @@ ${SIZEOF_TIME_T_CODE}
 /* if wolfSSL has the wolfSSL_DES_ecb_encrypt function. */
 #cmakedefine HAVE_WOLFSSL_DES_ECB_ENCRYPT 1
 
+/* if wolfSSL has the wolfSSL_BIO_new function. */
+#cmakedefine HAVE_WOLFSSL_BIO 1
+
 /* if wolfSSL has the wolfSSL_BIO_set_shutdown function. */
 #cmakedefine HAVE_WOLFSSL_FULL_BIO 1
 
