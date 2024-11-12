22c45844afd20c217b2c94911020118ae565c2d3
Viktor Szakats <commit@vsz.me>
2024-11-12 13:37:33 +0100

    strtok: use namespaced `strtok_r` macro instead of redefining it
    krb5 defines `strtok_r` for Windows unconditionally in its public
    header:
    https://github.com/krb5/krb5/blob/dc5554394e5a4363b3e109623edbeb9ad6c18a62/src/include/win-mac.h#L214-L215
    resulting in this warning:
    ```
    lib\strtok.h(31,9): warning C4005: 'strtok_r': macro redefinition
          C:\vcpkg\installed\x64-windows\include\win-mac.h(215,9):
          see previous definition of 'strtok_r'
    ```
    
    The krb5 macro collides with curl's internal definition, in case
    the `strtok_r` function is undetected and falling back to a local
    replacement.
    
    Reported-by: Tal Regev
    Bug: https://github.com/curl/curl/pull/15549#issuecomment-2468251761
    Closes #15564

--- lib/strtok.h
+++ lib/strtok.h
@@ -26,11 +26,11 @@
 #include "curl_setup.h"
 #include <stddef.h>
 
-#ifndef HAVE_STRTOK_R
-char *Curl_strtok_r(char *s, const char *delim, char **last);
-#define strtok_r Curl_strtok_r
-#else
+#ifdef HAVE_STRTOK_R
 #include <string.h>
+#define Curl_strtok_r strtok_r
+#else
+char *Curl_strtok_r(char *s, const char *delim, char **last);
 #endif
 
 #endif /* HEADER_CURL_STRTOK_H */
