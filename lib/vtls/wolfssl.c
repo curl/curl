bbebe46d353f5c58ca19cfe3c9e15069cee136da
Viktor Szakats <commit@vsz.me>
2024-11-17 01:12:31 +0100

    wolfssl: add workaround for symbol collisions
    ```
    In file included from curl/lib/vtls/wolfssl.c:72:
    In file included from wolfssl.git/_pkg/include/wolfssl/openssl/ssl.h:37:
    wolfssl.git/_pkg/include/wolfssl/ssl.h:224:39: error: typedef redefinition with different types ('struct WOLFSSL_ASN1_OTHERNAME' vs 'struct otherName_st')
    typedef struct WOLFSSL_ASN1_OTHERNAME OTHERNAME;
                                          ^
    /usr/local/opt/openssl@3/include/openssl/x509v3.h:160:3: note: previous definition is here
    } OTHERNAME;
      ^
    In file included from curl/lib/vtls/wolfssl.c:72:
    In file included from wolfssl.git/_pkg/include/wolfssl/openssl/ssl.h:42:
    In file included from wolfssl.git/_pkg/include/wolfssl/openssl/evp.h:43:
    wolfssl.git/_pkg/include/wolfssl/openssl/md5.h:61:25: error: typedef redefinition with different types ('WOLFSSL_MD5_CTX' (aka 'struct WOLFSSL_MD5_CTX') vs 'struct MD5state_st')
    typedef WOLFSSL_MD5_CTX MD5_CTX;
                            ^
    /usr/local/opt/openssl@3/include/openssl/md5.h:46:3: note: previous definition is here
    } MD5_CTX;
      ^
    2 errors generated.
    ```

--- lib/vtls/wolfssl.c
+++ lib/vtls/wolfssl.c
@@ -32,6 +32,11 @@
 
 #ifdef USE_WOLFSSL
 
+/* Workaround for collisions of compatibility symbols
+   as of wolfSSL ff680994ba1295dbf8354ef3863928e7422b94f7 */
+#define OTHERNAME CURL_WOLFSSL_OTHERNAME
+#define MD5_CTX   CURL_WOLFSSL_MD5_CTX
+
 #define WOLFSSL_OPTIONS_IGNORE_SYS
 #include <wolfssl/options.h>
 #include <wolfssl/version.h>
