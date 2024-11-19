02194fc708efd06112be62fb8f95f9883589fc37
Viktor Szakats <commit@vsz.me>
2024-11-19 12:08:38 +0100

    inet_pton: fix for AmigaOS cmake build
    Same story as inet_ntop.
    
    ```
    lib/cf-socket.c: In function 'bindlocal':
    lib/cf-socket.c:750:11: warning: pointer targets in initialization differ in signedness [-Wpointer-sign]
              (Curl_inet_pton(AF_INET, myhost, &si4->sin_addr) > 0)) {
               ^~~~~~~~~~~~~~
    lib/curl_addrinfo.c: In function 'Curl_str2addr':
    lib/curl_addrinfo.c:433:6: warning: pointer targets in initialization differ in signedness [-Wpointer-sign]
       if(Curl_inet_pton(AF_INET, address, &in) > 0)
          ^~~~~~~~~~~~~~
    lib/hostip.c: In function 'get_localhost':
    lib/hostip.c:582:6: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
       if(Curl_inet_pton(AF_INET, "127.0.0.1", (char *)&ipv4) < 1)
          ^~~~~~~~~~~~~~
    lib/hostip.c: In function 'Curl_host_is_ipnum':
    lib/hostip.c:650:6: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
       if(Curl_inet_pton(AF_INET, hostname, &in) > 0
          ^~~~~~~~~~~~~~
    lib/hostip.c: In function 'Curl_resolv':
    lib/hostip.c:760:8: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
         if(Curl_inet_pton(AF_INET, hostname, &in) > 0) {
            ^~~~~~~~~~~~~~
    lib/noproxy.c: In function 'Curl_cidr4_match':
    lib/noproxy.c:56:11: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
       if(1 != Curl_inet_pton(AF_INET, ipv4, &address))
               ^~~~~~~~~~~~~~
    lib/noproxy.c:58:11: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
       if(1 != Curl_inet_pton(AF_INET, network, &check))
               ^~~~~~~~~~~~~~
    lib/noproxy.c: In function 'Curl_check_noproxy':
    lib/noproxy.c:165:15: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
           if(1 == Curl_inet_pton(AF_INET, name, &address))
                   ^~~~~~~~~~~~~~
    lib/socks.c: In function 'do_SOCKS5':
    lib/socks.c:923:15: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
           if(1 == Curl_inet_pton(AF_INET, sx->hostname, ip4)) {
                   ^~~~~~~~~~~~~~
    lib/urlapi.c: In function 'ipv6_parse':
    lib/urlapi.c:617:13: warning: pointer targets in initialization differ in signedness [-Wpointer-sign]
         if(1 != Curl_inet_pton(AF_INET6, hostname, dest))
                 ^~~~~~~~~~~~~~
    lib/vtls/openssl.c: In function 'ossl_verifyhost':
    lib/vtls/openssl.c:2124:9: warning: pointer targets in initialization differ in signedness [-Wpointer-sign]
         if(!Curl_inet_pton(AF_INET, peer->hostname, &addr))
             ^~~~~~~~~~~~~~
    lib/vtls/vtls.c: In function 'get_peer_type':
    lib/vtls/vtls.c:1595:8: warning: initialization discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
         if(Curl_inet_pton(AF_INET, hostname, &addr))
            ^~~~~~~~~~~~~~
    ```

--- lib/inet_pton.h
+++ lib/inet_pton.h
@@ -32,7 +32,11 @@ int Curl_inet_pton(int, const char *, void *);
 #ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
 #endif
+#if defined(__AMIGA__)
+#define Curl_inet_pton(x,y,z) inet_pton(x,(unsigned char *)y,z)
+#else
 #define Curl_inet_pton(x,y,z) inet_pton(x,y,z)
+#endif
 #endif
 
 #endif /* HEADER_CURL_INET_PTON_H */
