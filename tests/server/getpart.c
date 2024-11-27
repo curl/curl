0eefbaf5752f1d62fd444338773ea9f80a96aae2
Viktor Szakats <commit@vsz.me>
2024-11-27 12:50:24 +0100

    fixes 2

--- tests/server/getpart.c
+++ tests/server/getpart.c
@@ -51,7 +51,11 @@
 curl_malloc_callback Curl_cmalloc = (curl_malloc_callback)malloc;
 curl_free_callback Curl_cfree = (curl_free_callback)free;
 curl_realloc_callback Curl_crealloc = (curl_realloc_callback)realloc;
+#ifdef _WIN32
+curl_strdup_callback Curl_cstrdup = (curl_strdup_callback)_strdup;
+#else
 curl_strdup_callback Curl_cstrdup = (curl_strdup_callback)strdup;
+#endif
 curl_calloc_callback Curl_ccalloc = (curl_calloc_callback)calloc;
 #if defined(_WIN32) && defined(UNICODE)
 curl_wcsdup_callback Curl_cwcsdup = (curl_wcsdup_callback)_wcsdup;
