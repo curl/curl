6bb76d92e16f5d41d5615df7d55113cf61e2ca08
Daniel Stenberg <daniel@haxx.se>
2024-12-14 10:41:05 +0100

    tool_getparam: fix the ECH parser
    The pn: and ecl: prefixes were not handled correctly.
    
    CodeSonar helped me realize as it poined out an "Unreachable Data Flow"
    
    Closes #15741

--- src/tool_getparam.c
+++ src/tool_getparam.c
@@ -1135,11 +1135,13 @@ static ParameterError parse_ech(struct GlobalConfig *global,
     err = PARAM_LIBCURL_DOESNT_SUPPORT;
   else if(strlen(nextarg) > 4 && strncasecompare("pn:", nextarg, 3)) {
     /* a public_name */
+    nextarg += 3;
     err = getstr(&config->ech_public, nextarg, DENY_BLANK);
   }
   else if(strlen(nextarg) > 5 && strncasecompare("ecl:", nextarg, 4)) {
     /* an ECHConfigList */
-    if('@' != *(nextarg + 4)) {
+    nextarg += 4;
+    if('@' != *nextarg) {
       err = getstr(&config->ech_config, nextarg, DENY_BLANK);
     }
     else {
