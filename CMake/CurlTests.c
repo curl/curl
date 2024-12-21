60f6b8900f0287eed90f6c93372fa5d520eeb785
Viktor Szakats <commit@vsz.me>
2024-12-21 19:20:57 +0100

    more whitespace

--- CMake/CurlTests.c
+++ CMake/CurlTests.c
@@ -153,7 +153,7 @@ int main(void) { return 0; }
 /* Check that off_t can represent 2**63 - 1 correctly.
    We cannot simply define LARGE_OFF_T to be 9223372036854775807,
    since some C++ compilers masquerading as C compilers
-   incorrectly reject 9223372036854775807.  */
+   incorrectly reject 9223372036854775807. */
 #define LARGE_OFF_T (((off_t) 1 << 62) - 1 + ((off_t) 1 << 62))
 int off_t_is_large[(LARGE_OFF_T % 2147483629 == 721
                      && LARGE_OFF_T % 2147483647 == 1)
