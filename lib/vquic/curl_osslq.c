6ef82be830f37f543b5eec9cb8aafec8ba4b9e47
Neil Horman <nhorman@openssl.org>
2025-01-03 10:17:56 -0500

    Use SSL_poll to determine writeability of QUIC streams
    This discussion:
    https://github.com/openssl/openssl/discussions/23339#discussion-6094341
    
    Specifically item number 2 (Send Blocking) was raised by the curl team,
    noting that SSL_want_write returning false was not a good indicator of
    when a stream is writeable.  The suggestion in that discussion was to
    use SSL_poll with an SSL_POLL_EVENT_W flag instead, as that is a proper
    indication of when an SSL_object will allow writing without blocking.
    
    While ssl_want_write updates its state based on the last error
    encountered (implying a need to retry an operation to update the last_error
    state again), SSL_poll checks stream buffer status during the call,
    giving it more up to date information on request.  This is the method
    used by our guide demos (quic-hq-interop specifically), and it works
    well.
    
    This change has been run through the curl test suite, and shown to pass
    all tests.  However, given  the initial problem description I'm not sure
    if there is a test case that explicitly checks for blocking and
    unblocking of streams.  As such some additional testing may be
    warranted.

--- lib/vquic/curl_osslq.c
+++ lib/vquic/curl_osslq.c
@@ -1461,23 +1461,65 @@ static CURLcode cf_osslq_check_and_unblock(struct Curl_cfilter *cf,
 {
   struct cf_osslq_ctx *ctx = cf->ctx;
   struct h3_stream_ctx *stream;
+  SSL_POLL_ITEM *poll_items = NULL;
+  struct Curl_easy **curl_items = NULL;
+  size_t poll_count = 0;
+  size_t result_count = 0;
+  size_t idx_count = 0;
+  CURLcode res = CURLE_OK;
+  struct timeval timeout;
 
   if(ctx->h3.conn) {
     struct Curl_llist_node *e;
+
+    res = CURLE_OUT_OF_MEMORY;
+
+    poll_items = calloc(Curl_llist_count(&data->multi->process),
+                        sizeof(SSL_POLL_ITEM));
+    if (!poll_items)
+        goto out;
+
+    curl_items = calloc(Curl_llist_count(&data->multi->process),
+                        sizeof(struct Curl_easy *));
+    if (!curl_items)
+        goto out;
+
     for(e = Curl_llist_head(&data->multi->process); e; e = Curl_node_next(e)) {
       struct Curl_easy *sdata = Curl_node_elem(e);
-      if(sdata->conn == data->conn) {
+      if (sdata->conn == data->conn) {
         stream = H3_STREAM_CTX(ctx, sdata);
-        if(stream && stream->s.ssl && stream->s.send_blocked &&
-           !SSL_want_write(stream->s.ssl)) {
-          nghttp3_conn_unblock_stream(ctx->h3.conn, stream->s.id);
-          stream->s.send_blocked = FALSE;
-          h3_drain_stream(cf, sdata);
-          CURL_TRC_CF(sdata, cf, "unblocked");
+        if (stream && stream->s.ssl && stream->s.send_blocked) {
+          poll_items[poll_count].desc = SSL_as_poll_descriptor(stream->s.ssl);
+          poll_items[poll_count].events = SSL_POLL_EVENT_W;
+          curl_items[poll_count] = sdata;
+          poll_count++;
         }
       }
     }
+
+    memset(&timeout, 0, sizeof(struct timeval));
+    res = CURLE_UNRECOVERABLE_POLL;
+    if (!SSL_poll(poll_items, poll_count, sizeof(SSL_POLL_ITEM), &timeout,
+                  0, &result_count))
+        goto out;
+
+    res = CURLE_OK;
+
+    for (idx_count = 0; idx_count < poll_count && result_count > 0; idx_count++) {
+      if (poll_items[idx_count].revents && SSL_POLL_EVENT_W) {
+          stream = H3_STREAM_CTX(ctx, curl_items[idx_count]);
+          nghttp3_conn_unblock_stream(ctx->h3.conn, stream->s.id);
+          stream->s.send_blocked = FALSE;
+          h3_drain_stream(cf, curl_items[idx_count]);
+          CURL_TRC_CF(curl_items[idx_count], cf, "unblocked");
+          result_count--;
+      }
+    }
   }
+
+out:
+  free(poll_items);
+  free(curl_items);
   return CURLE_OK;
 }
 
