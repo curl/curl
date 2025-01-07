6472c4a234fbef5c03b4428653457400cef55437
Viktor Szakats <commit@vsz.me>
2025-01-07 23:44:32 +0100

    ci: include `stdint.h` in all builds

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -159,6 +159,8 @@
 
 #endif /* HAVE_CONFIG_H */
 
+#include <stdint.h>
+
 /* ================================================================ */
 /* Definition of preprocessor macros/symbols which modify compiler  */
 /* behavior or generated code characteristics must be done here,   */
