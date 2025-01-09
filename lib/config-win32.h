c5147d895cd13b3f82f985723795fd2f9a89b3a6
Viktor Szakats <commit@vsz.me>
2025-01-09 17:43:14 +0100

    msvc: move `_CRT_*` macros to `curl_setup.h`

--- lib/config-win32.h
+++ lib/config-win32.h
@@ -293,12 +293,6 @@
 #define HAVE_LONGLONG 1
 #endif
 
-/* Define to avoid VS2005 complaining about portable C functions. */
-#if defined(_MSC_VER) && (_MSC_VER >= 1400)
-#define _CRT_SECURE_NO_DEPRECATE 1
-#define _CRT_NONSTDC_NO_DEPRECATE 1
-#endif
-
 /* mingw-w64 and visual studio >= 2005 (MSVCR80)
    all default to 64-bit time_t unless _USE_32BIT_TIME_T is defined */
 #if (defined(_MSC_VER) && (_MSC_VER >= 1400)) || defined(__MINGW32__)
