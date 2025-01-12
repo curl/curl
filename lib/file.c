e519cd99777a3013576e64b99f23b12021054d8d
Viktor Szakats <commit@vsz.me>
2025-01-12 17:16:03 +0100

    file: silence mingw32ce warning
    ```
    lib/doh.c: In function 'doh_strerror':
    lib/doh.c:72: warning: comparison of unsigned expression >= 0 is always true
    ```

--- lib/file.c
+++ lib/file.c
@@ -547,7 +547,7 @@ static CURLcode file_do(struct Curl_easy *data, bool *done)
 
   if(data->state.resume_from) {
     if(!S_ISDIR(statbuf.st_mode)) {
-#ifdef __AMIGA__
+#if defined(__AMIGA__) || defined(__MINGW32CE__)
       if(data->state.resume_from !=
           lseek(fd, (off_t)data->state.resume_from, SEEK_SET))
 #else
