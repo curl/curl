e3b0c969ac1787569fb677485bee86c7d527cdef
Viktor Szakats <commit@vsz.me>
2025-01-13 20:56:03 +0100

    config-win32.h: assume 64-bit time_t (VS2005+)

--- lib/config-win32.h
+++ lib/config-win32.h
@@ -287,9 +287,8 @@
 #define HAVE_LONGLONG 1
 #endif
 
-/* mingw-w64 and visual studio >= 2005 (MSVCR80)
-   all default to 64-bit time_t unless _USE_32BIT_TIME_T is defined */
-#if (defined(_MSC_VER) && (_MSC_VER >= 1400)) || defined(__MINGW32__)
+/* Default to 64-bit time_t unless _USE_32BIT_TIME_T is defined */
+#if defined(_MSC_VER) || defined(__MINGW32__)
 #  ifndef _USE_32BIT_TIME_T
 #    define SIZEOF_TIME_T 8
 #  else
