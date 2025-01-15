8d3c4316d0ea07b1e09dd4b2343f68265c08d059
Viktor Szakats <commit@vsz.me>
2025-01-15 02:56:40 +0100

    strtoofft: drop support for VS2002 and older

--- lib/strtoofft.c
+++ lib/strtoofft.c
@@ -38,20 +38,18 @@
 #if (SIZEOF_CURL_OFF_T > SIZEOF_LONG)
 #  ifdef HAVE_STRTOLL
 #    define strtooff strtoll
-#  else
-#    if defined(_MSC_VER) && (_MSC_VER >= 1300)
-#      if defined(_SAL_VERSION)
-         _Check_return_ _CRTIMP __int64 __cdecl _strtoi64(
-             _In_z_ const char *_String,
-             _Out_opt_ _Deref_post_z_ char **_EndPtr, _In_ int _Radix);
-#      else
-         _CRTIMP __int64 __cdecl _strtoi64(const char *_String,
-                                           char **_EndPtr, int _Radix);
-#      endif
-#      define strtooff _strtoi64
+#  elif defined(_MSC_VER)
+#    ifdef _SAL_VERSION
+       _Check_return_ _CRTIMP __int64 __cdecl _strtoi64(
+           _In_z_ const char *_String,
+           _Out_opt_ _Deref_post_z_ char **_EndPtr, _In_ int _Radix);
 #    else
-#      define PRIVATE_STRTOOFF 1
+       _CRTIMP __int64 __cdecl _strtoi64(const char *_String,
+                                         char **_EndPtr, int _Radix);
 #    endif
+#    define strtooff _strtoi64
+#  else
+#    define PRIVATE_STRTOOFF 1
 #  endif
 #else
 #  define strtooff strtol
