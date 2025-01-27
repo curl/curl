9c339d1ae152586fe5efea0e73201bdc1f790b5c
Stefan Eissing <stefan@eissing.org>
2025-01-27 16:08:08 +0100

    more ifdef for non-HTTP builds

--- lib/url.c
+++ lib/url.c
@@ -997,6 +997,7 @@ static bool url_match_conn(struct connectdata *conn, void *userdata)
   }
 #endif
 
+#ifndef CURL_DISABLE_HTTP
   if(match->may_multiplex &&
      (data->state.http_neg.allowed & (CURL_HTTP_V2x|CURL_HTTP_V3x)) &&
      (needle->handler->protocol & CURLPROTO_HTTP) &&
@@ -1010,6 +1011,7 @@ static bool url_match_conn(struct connectdata *conn, void *userdata)
     infof(data, "Server upgrade cannot be used");
     return FALSE;
   }
+#endif
 
   if(!(needle->handler->flags & PROTOPT_CREDSPERREQUEST)) {
     /* This protocol requires credentials per connection,
