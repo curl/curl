80f46197a3d3ddb6f10963725878988e0721ed56
Viktor Szakats <commit@vsz.me>
2025-02-01 03:18:16 +0100

    strip less const

--- lib/http.c
+++ lib/http.c
@@ -4182,7 +4182,7 @@ CURLcode Curl_http_write_resp(struct Curl_easy *data,
     flags = CLIENTWRITE_BODY;
     if(is_eos)
       flags |= CLIENTWRITE_EOS;
-    result = Curl_client_write(data, flags, (char *)buf, blen);
+    result = Curl_client_write(data, flags, buf, blen);
   }
 out:
   return result;
