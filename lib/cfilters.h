99b541a0f8e20764250686cdd3f37ca1697d53ea
Viktor Szakats <commit@vsz.me>
2025-02-02 04:11:01 +0100

    strip less const (CF_DATA)

--- lib/cfilters.h
+++ lib/cfilters.h
@@ -636,7 +636,7 @@ void Curl_pollset_check(struct Curl_easy *data,
  * invocation.
  */
 struct cf_call_data {
-  struct Curl_easy *data;
+  const struct Curl_easy *data;
 #ifdef DEBUGBUILD
   int depth;
 #endif
@@ -656,7 +656,7 @@ struct cf_call_data {
     (save) = CF_CTX_CALL_DATA(cf); \
     DEBUGASSERT((save).data == NULL || (save).depth > 0); \
     CF_CTX_CALL_DATA(cf).depth++;  \
-    CF_CTX_CALL_DATA(cf).data = (struct Curl_easy *)data; \
+    CF_CTX_CALL_DATA(cf).data = (const struct Curl_easy *)data; \
   } while(0)
 
 #define CF_DATA_RESTORE(cf, save) \
@@ -671,7 +671,7 @@ struct cf_call_data {
 #define CF_DATA_SAVE(save, cf, data) \
   do { \
     (save) = CF_CTX_CALL_DATA(cf); \
-    CF_CTX_CALL_DATA(cf).data = (struct Curl_easy *)data; \
+    CF_CTX_CALL_DATA(cf).data = (const struct Curl_easy *)data; \
   } while(0)
 
 #define CF_DATA_RESTORE(cf, save) \
