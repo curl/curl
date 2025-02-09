bf1a719dd1306efe109b0d1c7c60b69d216cbf65
Viktor Szakats <commit@vsz.me>
2025-02-10 00:46:42 +0100

    comment unused macros from set in x509asn1.c
    ```
    lib/vtls/x509asn1.c:79:0: warning: macro "CURL_ASN1_OBJECT_DESCRIPTOR" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:80:0: warning: macro "CURL_ASN1_INSTANCE_OF" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:81:0: warning: macro "CURL_ASN1_REAL" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:83:0: warning: macro "CURL_ASN1_EMBEDDED" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:85:0: warning: macro "CURL_ASN1_RELATIVE_OID" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:86:0: warning: macro "CURL_ASN1_SEQUENCE" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:87:0: warning: macro "CURL_ASN1_SET" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:91:0: warning: macro "CURL_ASN1_VIDEOTEX_STRING" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:95:0: warning: macro "CURL_ASN1_GRAPHIC_STRING" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:97:0: warning: macro "CURL_ASN1_GENERAL_STRING" is not used [-Wunused-macros]
    lib/vtls/x509asn1.c:99:0: warning: macro "CURL_ASN1_CHARACTER_STRING" is not used [-Wunused-macros]
    ```

--- lib/vtls/x509asn1.c
+++ lib/vtls/x509asn1.c
@@ -78,27 +78,27 @@
 #define CURL_ASN1_OCTET_STRING          4
 #define CURL_ASN1_NULL                  5
 #define CURL_ASN1_OBJECT_IDENTIFIER     6
-#define CURL_ASN1_OBJECT_DESCRIPTOR     7
-#define CURL_ASN1_INSTANCE_OF           8
-#define CURL_ASN1_REAL                  9
+/* #define CURL_ASN1_OBJECT_DESCRIPTOR     7 */
+/* #define CURL_ASN1_INSTANCE_OF           8 */
+/* #define CURL_ASN1_REAL                  9 */
 #define CURL_ASN1_ENUMERATED            10
-#define CURL_ASN1_EMBEDDED              11
+/* #define CURL_ASN1_EMBEDDED              11 */
 #define CURL_ASN1_UTF8_STRING           12
-#define CURL_ASN1_RELATIVE_OID          13
-#define CURL_ASN1_SEQUENCE              16
-#define CURL_ASN1_SET                   17
+/* #define CURL_ASN1_RELATIVE_OID          13 */
+/* #define CURL_ASN1_SEQUENCE              16 */
+/* #define CURL_ASN1_SET                   17 */
 #define CURL_ASN1_NUMERIC_STRING        18
 #define CURL_ASN1_PRINTABLE_STRING      19
 #define CURL_ASN1_TELETEX_STRING        20
-#define CURL_ASN1_VIDEOTEX_STRING       21
+/* #define CURL_ASN1_VIDEOTEX_STRING       21 */
 #define CURL_ASN1_IA5_STRING            22
 #define CURL_ASN1_UTC_TIME              23
 #define CURL_ASN1_GENERALIZED_TIME      24
-#define CURL_ASN1_GRAPHIC_STRING        25
+/* #define CURL_ASN1_GRAPHIC_STRING        25 */
 #define CURL_ASN1_VISIBLE_STRING        26
-#define CURL_ASN1_GENERAL_STRING        27
+/* #define CURL_ASN1_GENERAL_STRING        27 */
 #define CURL_ASN1_UNIVERSAL_STRING      28
-#define CURL_ASN1_CHARACTER_STRING      29
+/* #define CURL_ASN1_CHARACTER_STRING      29 */
 #define CURL_ASN1_BMP_STRING            30
 
 
