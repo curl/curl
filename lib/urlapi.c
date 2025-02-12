2f2ca12e2159c7ae3a4b2e1951a6df1df8a123dc
Daniel Stenberg <daniel@haxx.se>
2025-02-12 15:59:16 +0100

    urlapi: simplify junkscan
    Makes it smaller and possibly somewhat faster

--- lib/urlapi.c
+++ lib/urlapi.c
@@ -334,25 +334,17 @@ static CURLUcode redirect_url(char *base, const char *relurl,
 /* scan for byte values <= 31, 127 and sometimes space */
 static CURLUcode junkscan(const char *url, size_t *urllen, unsigned int flags)
 {
-  static const char badbytes[]={
-    /* */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-    0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
-    0x7f, 0x00 /* null-terminate */
-  };
   size_t n = strlen(url);
-  size_t nfine;
-
+  size_t i;
+  const unsigned char *p = (const unsigned char *)url;
   if(n > CURL_MAX_INPUT_LENGTH)
-    /* excessive input length */
     return CURLUE_MALFORMED_INPUT;
 
-  nfine = strcspn(url, badbytes);
-  if((nfine != n) ||
-     (!(flags & CURLU_ALLOW_SPACE) && strchr(url, ' ')))
-    return CURLUE_MALFORMED_INPUT;
-
+  for(i = 0; i < n; i++) {
+    if(p[i] <= 31 || p[i] == 127 ||
+       (!(flags & CURLU_ALLOW_SPACE) && (p[i] == ' ')))
+      return CURLUE_MALFORMED_INPUT;
+  }
   *urllen = n;
   return CURLUE_OK;
 }
