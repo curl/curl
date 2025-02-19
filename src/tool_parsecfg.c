52ad0c09ad6325c2475e3b6dff5198c15b8a3862
Daniel Stenberg <daniel@haxx.se>
2025-02-19 23:55:31 +0100

    tool_getparam: clear some sensitive arguments better
    curl attempts to clear some flags to hide them from snooping neighbors
    (on platforms where it works). For example the credentials provided with
    -u. Previously it would only do that if there was a space between the
    option and the credentials as in "-u joe:s3cr3t" but not when done
    without a separating space as in "-ujoe:s3cr3t".
    
    This addresses that previous shortcoming.
    
    Reported-by: kayrus on github
    Fixes #16396

--- src/tool_parsecfg.c
+++ src/tool_parsecfg.c
@@ -190,7 +190,8 @@ int parseconfig(const char *filename, struct GlobalConfig *global)
 #ifdef DEBUG_CONFIG
       fprintf(tool_stderr, "PARAM: \"%s\"\n",(param ? param : "(null)"));
 #endif
-      res = getparameter(option, param, NULL, &usedarg, global, operation);
+      res = getparameter(option, param, NULL, NULL,
+                         &usedarg, global, operation);
       operation = global->last;
 
       if(!res && param && *param && !usedarg)
