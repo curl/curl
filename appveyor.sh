7693b544bfdd8f2e62aa3dc088fda5e0210afefd
Viktor Szakats <commit@vsz.me>
2025-02-20 19:23:56 +0100

    VS2008 Debug test with !CURLDEBUG 2

--- appveyor.sh
+++ appveyor.sh
@@ -116,7 +116,7 @@ fi
 
 find . \( -name '*.exe' -o -name '*.dll' -o -name '*.lib' \) -exec file '{}' \;
 if [ -z "${SKIP_RUN:-}" ]; then
-  "${curl}" --disable --version
+  "${curl}" --disable --version || true
 else
   echo "Skip running curl.exe. Reason: ${SKIP_RUN}"
 fi
