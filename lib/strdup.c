499d4cddecf531193633eaaefccbf3c2ca1b9340
Daniel Stenberg <daniel@haxx.se>
2025-02-22 11:49:04 +0100

    fixup make Curl_memdup0() be okay with a zero length input

--- lib/strdup.c
+++ lib/strdup.c
@@ -114,7 +114,10 @@ void *Curl_memdup0(const char *src, size_t length)
   char *buf = malloc(length + 1);
   if(!buf)
     return NULL;
-  memcpy(buf, src, length);
+  if(length) {
+    DEBUGASSERT(src); /* must never be NULL */
+    memcpy(buf, src, length);
+  }
   buf[length] = 0;
   return buf;
 }
