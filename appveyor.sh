4c50fb425dfb641b46d449ddd8bb83f81cfcfd84
Viktor Szakats <commit@vsz.me>
2025-02-24 04:15:58 +0100

    switch to envvar, no luck with Perl globals
    I don't know how to pass around global configuration between Perl
    processes... Perhaps it needs to be added to each server runner
    as an additional option.

--- appveyor.sh
+++ appveyor.sh
@@ -134,8 +134,8 @@ fi
 
 if [ "${TFLAGS}" != 'skipall' ] && \
    [ "${TFLAGS}" != 'skiprun' ]; then
+  export CURL_DIRSUFFIX="/${PRJ_CFG}"
   TFLAGS+=" -c ../src/${PRJ_CFG}/curl.exe"
-  TFLAGS+=" -ds /${PRJ_CFG}"
   if [ -x "$(cygpath "${SYSTEMROOT}/System32/curl.exe")" ]; then
     TFLAGS+=" -ac $(cygpath "${SYSTEMROOT}/System32/curl.exe")"
   elif [ -x "$(cygpath 'C:/msys64/usr/bin/curl.exe')" ]; then
--- tests/runtests.pl
+++ tests/runtests.pl
@@ -2238,14 +2238,6 @@ while(@ARGV) {
         $DBGCURL=$CURL=$ARGV[1];
         shift @ARGV;
     }
-    elsif ($ARGV[0] eq "-ds") {
-        # append this suffix to base paths
-        print "Using test dir suffix: " . $ARGV[1] . "\n";
-        $LIBDIR .= $ARGV[1];
-        $UNITDIR .= $ARGV[1];
-        $SRVDIR .= $ARGV[1];
-        shift @ARGV;
-    }
     elsif ($ARGV[0] eq "-vc") {
         # use this path to a curl used to verify servers
 
