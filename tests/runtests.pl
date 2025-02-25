6a9a5c5e15f12428be51106c6538d2bd567c1292
Viktor Szakats <commit@vsz.me>
2025-02-25 18:33:33 +0100

    Revert "runtests: drop upper-level calls to handle64"
    This reverts commit e5e5c090cf91c34ef7c7a79d8e5e3160b91f610d.
    
    Some of the logic don't seem to be Windows-specific or handle64-related,
    as most of the CI test fail to complete in time (hang?) when removing it:
    https://github.com/curl/curl/actions/runs/13527054998/job/37800227765?pr=16484 (macOS)
    https://github.com/curl/curl/actions/runs/13527054994/job/37800178091?pr=16484 (BSDs)
    https://github.com/curl/curl/actions/runs/13527054982/job/37800212082?pr=16484 (Windows)

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -1852,6 +1852,13 @@ sub singletest {
         # first, remove all lingering log & lock files
         if((!cleardir($logdir) || !cleardir("$logdir/$LOCKDIR"))
             && $clearlocks) {
+            # On Windows, lock files can't be deleted when the process still
+            # has them open, so kill those processes first
+            if(runnerac_clearlocks($runnerid, "$logdir/$LOCKDIR")) {
+                logmsg "ERROR: runner $runnerid seems to have died\n";
+                $singletest_state{$runnerid} = ST_INIT;
+                return (-1, 0);
+            }
             $singletest_state{$runnerid} = ST_CLEARLOCKS;
         } else {
             $singletest_state{$runnerid} = ST_INITED;
