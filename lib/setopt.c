d5bc6da3cbc9b72b05a1819e6d85abcab26ec30e
Daniel Stenberg <daniel@haxx.se>
2025-02-25 10:20:09 +0100

    lib: add CURLFOLLOW_OBEYCODE and CURLFOLLOW_FIRSTONLY
    With this change, the argument passed to the CURLOPT_FOLLOWLOCATION
    option is now instead a "mode" instead of just a boolean. Documentation
    is extended to describe the two new modes.
    
    Test 1571 to 1580 verify.
    
    Closes #16473

--- lib/setopt.c
+++ lib/setopt.c
@@ -525,7 +525,9 @@ static CURLcode setopt_long(struct Curl_easy *data, CURLoption option,
     /*
      * Follow Location: header hints on an HTTP-server.
      */
-    data->set.http_follow_location = enabled;
+    if(uarg > 3)
+      return CURLE_BAD_FUNCTION_ARGUMENT;
+    data->set.http_follow_mode = (unsigned char)uarg;
     break;
 
   case CURLOPT_UNRESTRICTED_AUTH:
