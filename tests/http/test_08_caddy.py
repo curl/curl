a4e9eefa48fc84e47634c891c86d5682552ef8ee
Stefan Eissing <stefan@eissing.org>
2025-02-25 15:07:19 +0100

    OpenSSL/quictls: add support for TLSv1.3 early data
    based on 16450
    
    Adds support for TLSv1.3 early data for TCP and QUIC via ngtcp2.

--- tests/http/test_08_caddy.py
+++ tests/http/test_08_caddy.py
@@ -207,8 +207,11 @@ class TestCaddy:
 
     @pytest.mark.parametrize("proto", ['http/1.1', 'h2', 'h3'])
     def test_08_08_earlydata(self, env: Env, httpd, caddy, proto):
-        if not env.curl_uses_lib('gnutls') and not env.curl_uses_lib('wolfssl'):
-            pytest.skip('TLS earlydata only implemented in GnuTLS/wolfSSL')
+        if not env.curl_uses_lib('gnutls') and \
+           not env.curl_uses_lib('wolfssl') and \
+           not env.curl_uses_lib('quictls') and \
+           not env.curl_uses_lib('openssl'):
+            pytest.skip('TLS earlydata not implemented')
         if proto == 'h3' and not env.have_h3():
             pytest.skip("h3 not supported")
         count = 2
