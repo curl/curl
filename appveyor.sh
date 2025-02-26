344c3d5fa8d62845063f0e9ab6657e7f96c0bb81
Viktor Szakats <commit@vsz.me>
2025-02-26 22:35:09 +0100

    appveyor: switch VS2019 to openssl 1.1.0 (from 1.1.1)

--- appveyor.sh
+++ appveyor.sh
@@ -36,7 +36,7 @@ esac
 if [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2022' ]; then
   openssl_root_win="C:/OpenSSL-v34${openssl_suffix}"
 elif [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2019' ]; then
-  openssl_root_win="C:/OpenSSL${openssl_suffix}"
+  openssl_root_win="C:/OpenSSL-v11${openssl_suffix}"
 else
   openssl_root_win="C:/OpenSSL-v111${openssl_suffix}"
 fi
