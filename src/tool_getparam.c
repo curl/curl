3a9465de6f93673d194883961f28930e1a265cd2
Viktor Szakats <commit@vsz.me>
2025-03-02 19:40:37 +0100

    src/tool_getparam: use CURL_UNCONST() some maybe avoidable 3

--- src/tool_getparam.c
+++ src/tool_getparam.c
@@ -1789,7 +1789,8 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
     if(ARGTYPE(a->desc) >= ARG_STRG) {
       /* this option requires an extra parameter */
       if(!longopt && parse[1]) {
-        nextarg = (char *)&parse[1]; /* this is the actual extra parameter */
+        nextarg = (char *)CURL_UNCONST(&parse[1]); /* this is the actual
+                                                      extra parameter */
         singleopt = TRUE;   /* do not loop anymore after this */
 #ifdef HAVE_WRITABLE_ARGV
         clearthis = &cleararg1[parse + 2 - flag];
@@ -1832,7 +1833,7 @@ ParameterError getparameter(const char *flag, /* f or -long-flag */
          that use nextarg should be marked as such and they will check that
          nextarg is set before continuing, but code analyzers are not always
          that aware of that state */
-      nextarg = (char *)"";
+      nextarg = (char *)CURL_UNCONST("");
 
     switch(cmd) {
     case C_RANDOM_FILE: /* --random-file */
