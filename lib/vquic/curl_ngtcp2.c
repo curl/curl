871af66c6e188a143cf44829e3bf8377d289cfc8
Viktor Szakats <commit@vsz.me>
2025-03-03 01:44:31 +0100

    lib/vquic/curl_ngtcp2: do not drop const

--- lib/vquic/curl_ngtcp2.c
+++ lib/vquic/curl_ngtcp2.c
@@ -977,7 +977,7 @@ static int cb_h3_recv_data(nghttp3_conn *conn, int64_t stream3_id,
   if(!stream)
     return NGHTTP3_ERR_CALLBACK_FAILURE;
 
-  h3_xfer_write_resp(cf, data, stream, (char *)buf, blen, FALSE);
+  h3_xfer_write_resp(cf, data, stream, (const char *)buf, blen, FALSE);
   if(blen) {
     CURL_TRC_CF(data, cf, "[%" FMT_PRId64 "] ACK %zu bytes of DATA",
                 stream->id, blen);
