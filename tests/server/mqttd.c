c51b6f177c10eb68bd215c1ce59f011ee8ddebb5
Viktor Szakats <commit@vsz.me>
2025-03-04 21:58:46 +0100

    sync sockdaemon 1

--- tests/server/mqttd.c
+++ tests/server/mqttd.c
@@ -761,7 +761,9 @@ static curl_socket_t mqttd_sockdaemon(curl_socket_t sock,
         rc = wait_ms(delay);
         if(rc) {
           /* should not happen */
-          logmsg("wait_ms() failed with error (%d)", rc);
+          error = errno;
+          logmsg("wait_ms() failed with error (%d) %s",
+                 error, strerror(error));
           sclose(sock);
           return CURL_SOCKET_BAD;
         }
