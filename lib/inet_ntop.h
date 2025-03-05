8537a5b0bcf4565551774c2b2375c49767e405a7
Viktor Szakats <commit@vsz.me>
2025-03-05 18:06:37 +0100

    windows: do not use winsock2 `inet_ntop()`/`inet_pton()`
    Disable these winsock2 functions on Windows to use the curl wrappers
    and preserve `WSAGetLastError()` aka `SOCKERRNO` error codes.
    
    curl sources uses `inet_pton()` and `inet_ntop()` via its own `Curl_`
    prefixed wrappers. These wrappers promise to not overwrite
    `WSAGetLastError()` aka `SOCKERRNO` error codes when calling them.
    But, for Windows builds with these built-in winsock2 functions detected
    (meaning all supported Windows versions, except Windows CE),
    the wrappers were 1-to-1 mapped to the winsock2 functions, which broke
    this promise.
    
    https://github.com/curl/curl/blob/b06c12b7248592cf001e621d7cd8dc78a827212b/lib/inet_ntop.c#L188-L190
    https://github.com/curl/curl/blob/b06c12b7248592cf001e621d7cd8dc78a827212b/lib/inet_pton.c#L66-L70
    
    These promises are old (a1d598399146984c99baa46db148e87c75261033) and
    may not be valid anymore. In this case, the callers would have to be
    updated to use `SOCKERRNO` to retrieve any error, instead of using
    `errno` as they do now.
    
    https://learn.microsoft.com/windows/win32/api/ws2tcpip/nf-ws2tcpip-inet_ntop
    https://learn.microsoft.com/windows/win32/api/ws2tcpip/nf-ws2tcpip-inet_pton
    
    Closes #16577

--- lib/inet_ntop.h
+++ lib/inet_ntop.h
@@ -38,13 +38,7 @@ char *Curl_inet_ntop(int af, const void *addr, char *buf, size_t size);
 #ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
 #endif
-#ifdef _WIN32
-#if defined(_MSC_VER) && (_MSC_VER <= 1900)
-#define Curl_inet_ntop(af,addr,buf,size) inet_ntop(af, (void *)addr, buf, size)
-#else
-#define Curl_inet_ntop(af,addr,buf,size) inet_ntop(af, addr, buf, size)
-#endif
-#elif defined(__AMIGA__)
+#ifdef __AMIGA__
 #define Curl_inet_ntop(af,addr,buf,size) \
         (char *)inet_ntop(af, (void *)addr, (unsigned char *)buf, \
                           (curl_socklen_t)(size))
