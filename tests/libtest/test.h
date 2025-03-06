7f7da3bbe68616be56070bb83c425bba4811bf4b
Viktor Szakats <commit@vsz.me>
2025-03-06 20:29:35 +0100

    Revert "move CURL_GNUC_DIAG to curl_setup.h"
    This reverts commit 2b24b5cb27b301c8a4e6e1ca51950b9766cc6251.

--- tests/libtest/test.h
+++ tests/libtest/test.h
@@ -44,6 +44,13 @@
 
 #include "curl_printf.h"
 
+/* GCC <4.6 does not support '#pragma GCC diagnostic push' and
+   does not support 'pragma GCC diagnostic' inside functions. */
+#if (defined(__GNUC__) && \
+  ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 6))))
+#define CURL_GNUC_DIAG
+#endif
+
 #ifdef _WIN32
 #define sleep(sec) Sleep((sec)*1000)
 #endif
