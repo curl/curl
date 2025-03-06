e51c494ed1a9eb66390dcad19373a5f75413fb77
Zhang Wen <zhw2101024@gmail.com>
2025-03-06 16:13:12 +0800

    tool_operate: change "1 retries" to "1 retry"
    Closes #16586

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -647,8 +647,9 @@ static CURLcode post_per_transfer(struct GlobalConfig *global,
       }
       warnf(config->global, "Problem %s. "
             "Will retry in %ld seconds. "
-            "%ld retries left.",
-            m[retry], sleeptime/1000L, per->retry_remaining);
+            "%ld %s left.",
+            m[retry], sleeptime/1000L, per->retry_remaining,
+            (per->retry_remaining > 1 ? "retries" : "retry"));
 
       per->retry_remaining--;
       if(!config->retry_delay) {
