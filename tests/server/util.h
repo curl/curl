f9da2b9c51384934eacd4d55aa3b60e42887526b
Viktor Szakats <commit@vsz.me>
2025-03-13 03:49:56 +0100

    resolve: fix building without unix sockets and `CURLDEBUG` fixup

--- tests/server/util.h
+++ tests/server/util.h
@@ -94,11 +94,13 @@ void restore_signal_handlers(bool keep_sigalrm);
 
 #include <curl/curl.h> /* for curl_socket_t */
 
+#ifdef USE_UNIX_SOCKETS
 #ifdef HAVE_SYS_UN_H
 #include <sys/un.h> /* for sockaddr_un */
 #endif
 int bind_unix_socket(curl_socket_t sock, const char *unix_socket,
                      struct sockaddr_un *sau);
+#endif /* USE_UNIX_SOCKETS */
 
 unsigned short util_ultous(unsigned long ulnum);
 
