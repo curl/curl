5a01a4b5cb6959544ea0bb85de1abf72db0fd823
Viktor Szakats <commit@vsz.me>
2025-03-17 18:47:31 +0100

    runtests: auto-detect test bundle env
    If libtests, units and servers binaries are all present, auto-enable
    bundle mode.
    
    Drop manual runtests option.
    
    Also fix to append executable extension to `libtests` and `units`
    executables when launching them.
    
    Follow-up to f4f25505df1d2ede727071eb0c518368a206f64b #15000
    Follow-up to 71cf0d1fca9e1f53524e1545ef0c08d174458d80 #14772

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -88,6 +88,7 @@ use serverhelp qw(
     server_exe
     );
 use pathhelp qw(
+    exe_ext
     sys_native_current_path
     );
 use processhelp qw(
@@ -2265,11 +2266,6 @@ while(@ARGV) {
         $ACURL=shell_quote($ARGV[1]);
         shift @ARGV;
     }
-    elsif ($ARGV[0] eq "-bundle") {
-        # use test bundles
-        $bundle=1;
-        $ENV{'CURL_TEST_BUNDLES'} = 1;
-    }
     elsif ($ARGV[0] eq "-d") {
         # have the servers display protocol output
         $debugprotocol=1;
@@ -2451,7 +2447,6 @@ Usage: runtests.pl [options] [test selection(s)]
   -a       continue even if a test fails
   -ac path use this curl only to talk to APIs (currently only CI test APIs)
   -am      automake style output PASS/FAIL: [number] [name]
-  -bundle  use test bundles
   -c path  use this curl executable
   -d       display server debug info
   -e, --test-event  event-based execution
@@ -2529,6 +2524,15 @@ EOHELP
         exit;
     }
     shift @ARGV;
+}
+
+# Detect a test bundle environment
+if(-e $LIBDIR . "libtests" . exe_ext('TOOL') &&
+   -e $UNITDIR . "units" . exe_ext('TOOL') &&
+   -e $SRVDIR . "servers" . exe_ext('SRV')) {
+    # use test bundles
+    $bundle=1;
+    $ENV{'CURL_TEST_BUNDLES'} = 1;
 }
 
 delete $ENV{'DEBUGINFOD_URLS'} if($ENV{'DEBUGINFOD_URLS'} && $no_debuginfod);
