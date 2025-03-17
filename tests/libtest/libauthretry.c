f65a4253d4c16aea231a86927a7728e856ddae39
Viktor Szakats <commit@vsz.me>
2025-03-18 00:39:57 +0100

    libauthretry: silence clang-tidy warnings
    Seen when running the macOS clang-tidy job with test bundles disabled:
    ```
    tests/libtest/libauthretry.c:96:12: error: The value '126' provided to the cast
      expression is not in the valid range of values for the enum
      [clang-analyzer-optin.core.EnumCastOutOfRange,-warnings-as-errors]
       96 |     return TEST_ERR_MAJOR_BAD;
          |            ^
    tests/libtest/test.h:99:32: note: expanded from macro 'TEST_ERR_MAJOR_BAD'
       99 | #define TEST_ERR_MAJOR_BAD     (CURLcode) 126
          |                                ^~~~~~~~~~~~~~
    include/curl/curl.h:519:9: note: enum declared here
    [...]
    ```
    Ref: https://github.com/curl/curl/actions/runs/13909934306/job/38921798619?pr=16750#step:14:384
    
    Follow-up to fabfa8e4024473035b3e5c3c30c330be726d9bb4 #15825

--- tests/libtest/libauthretry.c
+++ tests/libtest/libauthretry.c
@@ -93,12 +93,12 @@ CURLcode test(char *url)
   if(main_auth_scheme == CURLAUTH_NONE ||
       fallback_auth_scheme == CURLAUTH_NONE) {
     fprintf(stderr, "auth schemes not found on commandline\n");
-    return TEST_ERR_MAJOR_BAD;
+    return TEST_ERR_MAJOR_BAD;  /* NOLINT */
   }
 
   if(curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
     fprintf(stderr, "curl_global_init() failed\n");
-    return TEST_ERR_MAJOR_BAD;
+    return TEST_ERR_MAJOR_BAD;  /* NOLINT */
   }
 
   /* Send wrong password, then right password */
@@ -107,7 +107,7 @@ CURLcode test(char *url)
   if(!curl) {
     fprintf(stderr, "curl_easy_init() failed\n");
     curl_global_cleanup();
-    return TEST_ERR_MAJOR_BAD;
+    return TEST_ERR_MAJOR_BAD;  /* NOLINT */
   }
 
   res = send_wrong_password(curl, url, 100, main_auth_scheme);
@@ -125,7 +125,7 @@ CURLcode test(char *url)
   if(!curl) {
     fprintf(stderr, "curl_easy_init() failed\n");
     curl_global_cleanup();
-    return TEST_ERR_MAJOR_BAD;
+    return TEST_ERR_MAJOR_BAD;  /* NOLINT */
   }
 
   res = send_wrong_password(curl, url, 300, main_auth_scheme);
