8f3de23a1d658c947caf6740c6d9862d00194ba5
Viktor Szakats <commit@vsz.me>
2025-03-18 18:32:55 +0100

    lib579.c: fopen error check

--- tests/libtest/lib579.c
+++ tests/libtest/lib579.c
@@ -45,9 +45,13 @@ static size_t last_ul_total = 0;
 static void progress_final_report(void)
 {
   FILE *moo = fopen(libtest_arg2, "ab");
-  fprintf(moo, "Progress: end UL %zu/%zu\n", last_ul, last_ul_total);
+  fprintf(moo ? moo : stderr, "Progress: end UL %zu/%zu\n",
+                              last_ul, last_ul_total);
+  if(moo)
+    fclose(moo);
+  else
+    fprintf(stderr, "Progress: can't open %s\n", libtest_arg2);
   started = FALSE;
-  fclose(moo);
 }
 
 static int progress_callback(void *clientp, double dltotal, double dlnow,
