c9a2d8afbc18632c01a6d1dea4360b7eb852ec8d
Viktor Szakats <commit@vsz.me>
2025-03-24 16:13:23 +0100

    try backtrack

--- tests/libtest/test613.pl
+++ tests/libtest/test613.pl
@@ -67,8 +67,6 @@ if ($ARGV[0] eq "prepare")
     utime time, timegm(0,0,12,31,11,100), "rofile.txt";
     chmod 0444, "rofile.txt";
     if($^O eq 'cygwin') {
-      system "/bin/setfacl --remove-all rofile.txt";
-      chmod 0444, "rofile.txt";
       system "chattr +r rofile.txt";
     }
 
