39f003fdf78b655a371d4f2f6c8dd4f861ecf222
Viktor Szakats <commit@vsz.me>
2025-03-24 09:50:52 +0100

    verify

--- tests/sshserver.pl
+++ tests/sshserver.pl
@@ -441,6 +441,7 @@ if((! -e pp($hstprvkeyf)) || (! -s pp($hstprvkeyf)) ||
     if(($^O eq 'cygwin' || $^O eq 'msys') && -e "/bin/setfacl") {
         # https://cygwin.com/cygwin-ug-net/setfacl.html
         system "/bin/setfacl --remove-all " . pp($hstprvkeyf);
+        logmsg "TRACE: running setfacl\n";
     }
     elsif(pathhelp::os_is_win()) {
         # https://ss64.com/nt/icacls.html
