647f6f8207478fb9bdb44e4e8ee0804e0dbef7f9
Viktor Szakats <commit@vsz.me>
2025-03-26 23:54:03 +0100

    binarycheck: drop support for checksummed exceptions

--- .github/scripts/binarycheck.pl
+++ .github/scripts/binarycheck.pl
@@ -24,46 +24,17 @@
 ###########################################################################
 
 # This scripts scans the entire git repository for binary files.
-#
-# All files in the git repo that contain signs of being binary are then
-# collected and a sha256sum is generated for all of them. That summary is then
-# compared to the list of pre-vetted files so that only the exact copies of
-# already scrutinized files are deemed okay to "appear binary".
-#
 
 use strict;
 use warnings;
 
 my $root = ".";
-my $sumsfile = ".github/scripts/binarycheck.sums";
 if($ARGV[0]) {
     $root = $ARGV[0];
 }
 
 my @bin;
-my %known;
 my $error = 0;
-
-sub knownbins {
-    open(my $mh, "<", "$sumsfile") ||
-        die "can't read known binaries";
-    while(<$mh>) {
-        my $l = $_;
-        chomp $l;
-        if($l =~ /^([a-f0-9]+)  (.*)/) {
-            my ($sum, $file) = ($1, $2);
-            $known{$file} = 1;
-        }
-        elsif($l =~ /^#/) {
-            # skip comments
-        }
-        else {
-            print STDERR "suspicious line in $sumsfile\n";
-            $error++;
-        }
-    }
-    close($mh);
-}
 
 sub checkfile {
     my ($file) = @_;
@@ -75,10 +46,8 @@ sub checkfile {
         if($l =~ /([\x00-\x08\x0b\x0c\x0e-\x1f\x7f])/) {
             push @bin, $file;
 
-            if(!$known{$file}) {
-                printf STDERR "$file:$line has unknown binary contents\n";
-                $error++;
-            }
+            printf STDERR "$file:$line has binary contents\n";
+            $error++;
             last;
         }
     }
@@ -94,17 +63,9 @@ if(scalar(@files) < 3000) {
     exit 1;
 }
 
-knownbins();
-
 for my $f (@files) {
     chomp $f;
     checkfile("$root/$f");
-}
-
-my $check=system("sha256sum -c $sumsfile");
-if($check) {
-    print STDERR "sha256sum detected a problem\n";
-    $error++;
 }
 
 exit $error;
