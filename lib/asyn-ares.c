78e2a58aa02e57ddc3d77d8121119989e0b8eb2b
Stefan Eissing <stefan@eissing.org>
2025-03-27 14:15:34 +0100

    fix c-ares version of Curl_resolver_set_result()

--- lib/asyn-ares.c
+++ lib/asyn-ares.c
@@ -979,7 +979,7 @@ CURLcode Curl_set_dns_local_ip6(struct Curl_easy *data,
 void Curl_resolver_set_result(struct Curl_easy *data,
                               struct Curl_dns_entry *dnsentry)
 {
-  destroy_async_data(data);
+  Curl_resolver_cancel(data);
   data->state.async.dns = dnsentry;
   data->state.async.done = TRUE;
 }
