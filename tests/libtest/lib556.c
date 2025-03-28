c5352d7aaa4a15556053bff94b536509fe9907f3
Viktor Szakats <commit@vsz.me>
2025-03-28 02:22:53 +0100

    Revert "lib556: `*_FILENO` for which Windows?"
    This reverts commit 6784601f5a9649cbb268f10f3bbb24f4d14f8b52.
    
    All MSVC.

--- tests/libtest/lib556.c
+++ tests/libtest/lib556.c
@@ -26,6 +26,17 @@
 #include "warnless.h"
 #include "memdebug.h"
 
+/* For Windows, mainly (may be moved in a config file?) */
+#ifndef STDIN_FILENO
+#define STDIN_FILENO 0
+#endif
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
+#ifndef STDERR_FILENO
+#define STDERR_FILENO 2
+#endif
+
 CURLcode test(char *URL)
 {
   CURLcode res;
