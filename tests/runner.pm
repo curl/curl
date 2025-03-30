beea21b930420c1b1985cd4331c4e84c602aaf28
Viktor Szakats <commit@vsz.me>
2025-03-30 23:33:10 +0200

    log more runner_test_run cleanup

--- tests/runner.pm
+++ tests/runner.pm
@@ -1233,13 +1233,13 @@ sub runner_test_preprocess {
 sub runner_test_run {
     my ($testnum)=@_;
 
-    print "runner_test_run: trace-1\n";
+    print "runner_test_run: trace-1:", $testnum,"\n";
 
     if(clearlogs()) {
         logmsg "Warning: log messages were lost\n";
     }
 
-    print "runner_test_run: trace-2\n";
+    print "runner_test_run: trace-2:", $testnum,"\n";
 
     #######################################################################
     # Prepare the test environment to run this test case
@@ -1248,7 +1248,7 @@ sub runner_test_run {
         return (-2, clearlogs());
     }
 
-    print "runner_test_run: trace-3\n";
+    print "runner_test_run: trace-3:", $testnum,"\n";
 
     #######################################################################
     # Run the test command
@@ -1263,7 +1263,7 @@ sub runner_test_run {
         return (-2, clearlogs(), \%testtimings);
     }
 
-    print "runner_test_run: trace-4\n";
+    print "runner_test_run: trace-4:", $testnum,"\n";
 
     #######################################################################
     # Clean up after test command
@@ -1272,7 +1272,7 @@ sub runner_test_run {
         return ($error, clearlogs(), \%testtimings);
     }
 
-    print "runner_test_run: trace-5\n";
+    print "runner_test_run: trace-5:", $testnum,"\n";
 
     #######################################################################
     # Verify that the postcheck succeeded
@@ -1281,15 +1281,19 @@ sub runner_test_run {
         return ($error, clearlogs(), \%testtimings);
     }
 
-    print "runner_test_run: trace-6\n";
+    print "runner_test_run: trace-6:", $testnum,"\n";
 
     #######################################################################
     # restore environment variables that were modified
     restore_test_env(0);
 
-    print "runner_test_run: trace-7\n";
+    print "runner_test_run: trace-7:", $testnum,"\n";
 
-    return (0, clearlogs(), \%testtimings, $cmdres, $CURLOUT, $tool, $usedvalgrind);
+    my $clres := clearlogs()
+
+    print "runner_test_run: trace-8:", $testnum,"\n";
+
+    return (0, $clres, \%testtimings, $cmdres, $CURLOUT, $tool, $usedvalgrind);
 }
 
 # Async call runner_shutdown
