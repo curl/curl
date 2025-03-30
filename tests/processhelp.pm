da8a98bdda7d0d6c82dfc94b8eb238b07f9893a3
Viktor Szakats <commit@vsz.me>
2025-03-30 16:10:50 +0200

    log more killpid

--- tests/processhelp.pm
+++ tests/processhelp.pm
@@ -283,6 +283,8 @@ sub killpid {
     # The 'pidlist' argument is a string of whitespace separated pids.
     return if(not defined($pidlist));
 
+    print "killpid: trace-1\n";
+
     # Make 'requested' hold the non-duplicate pids from 'pidlist'.
     @requested = split(' ', $pidlist);
     return if(not @requested);
@@ -296,6 +298,7 @@ sub killpid {
     }
 
     # Send a SIGTERM to processes which are alive to gracefully stop them.
+    print "killpid: trace-2\n";
     foreach my $tmp (@requested) {
         chomp $tmp;
         if($tmp =~ /^(\d+)$/) {
@@ -318,8 +321,10 @@ sub killpid {
         }
     }
 
+    print "killpid: trace-3\n";
     # Allow all signalled processes five seconds to gracefully die.
     if(@signalled) {
+        print "killpid: trace-3a\n";
         my $twentieths = 5 * 20;
         while($twentieths--) {
             for(my $i = scalar(@signalled) - 1; $i >= 0; $i--) {
@@ -340,8 +345,10 @@ sub killpid {
         }
     }
 
+    print "killpid: trace-4\n";
     # Mercilessly SIGKILL processes still alive.
     if(@signalled) {
+        print "killpid: trace-4a\n";
         foreach my $pid (@signalled) {
             if($pid > 0) {
                 print("RUN: Process with pid $pid forced to die with SIGKILL\n")
@@ -354,14 +361,17 @@ sub killpid {
         }
     }
 
+    print "killpid: trace-5\n";
     # Reap processes dead children for sure.
     if(@reapchild) {
+        print "killpid: trace-5a\n";
         foreach my $pid (@reapchild) {
             if($pid > 0) {
                 pidwait($pid, 0);
             }
         }
     }
+    print "killpid: trace-6\n";
 }
 
 #######################################################################
