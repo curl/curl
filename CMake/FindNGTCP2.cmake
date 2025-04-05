e76b77381076a1ca97c426806f3c345a729b7b5b
Viktor Szakats <commit@vsz.me>
2025-04-05 03:12:35 +0200

    find step 4 pkg-config readd uppercase FOUND

--- CMake/FindNGTCP2.cmake
+++ CMake/FindNGTCP2.cmake
@@ -73,6 +73,7 @@ if(CURL_USE_PKGCONFIG AND
 endif()
 
 if(_ngtcp2_FOUND)
+  set(NGTCP2_FOUND TRUE)
   string(REPLACE ";" " " _ngtcp2_CFLAGS "${_ngtcp2_CFLAGS}")
   message(STATUS "Found NGTCP2 (via pkg-config): ${_ngtcp2_INCLUDE_DIRS} (found version \"${NGTCP2_VERSION}\")")
 else()
