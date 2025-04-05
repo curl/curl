9f0983916d27213c85878ab93c4ef9a210cdddb9
Viktor Szakats <commit@vsz.me>
2025-04-05 03:23:53 +0200

    find step 5 iface 2

--- CMake/FindNettle.cmake
+++ CMake/FindNettle.cmake
@@ -84,7 +84,7 @@ else()
   mark_as_advanced(NETTLE_INCLUDE_DIR NETTLE_LIBRARY)
 endif()
 
-if(BEARSSL_FOUND)
+if(NETTLE_FOUND)
   if(CMAKE_VERSION VERSION_LESS 3.13)
     link_directories(${_bearssl_LIBRARY_DIRS})
   endif()
@@ -92,7 +92,7 @@ if(BEARSSL_FOUND)
   if(NOT TARGET CURL::bearssl)
     add_library(CURL::bearssl INTERFACE IMPORTED)
     set_target_properties(CURL::bearssl PROPERTIES
-      VERSION "${BEARSSL_VERSION}"
+      VERSION "${NETTLE_VERSION}"
       CURL_PC_MODULES "${_bearssl_pc_requires}"
       INTERFACE_COMPILE_OPTIONS "${_bearssl_CFLAGS}"
       INTERFACE_INCLUDE_DIRECTORIES "${_bearssl_INCLUDE_DIRS}"
