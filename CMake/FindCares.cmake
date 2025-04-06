18ac359a46839b8014830cc71a0230e4dea2559b
Viktor Szakats <commit@vsz.me>
2025-04-06 02:20:11 +0200

    rename property to the INTERFACE_ namespace for cmake <3.19 compatibility
    Seems to silence:
    ```
    CMake Error at CMake/FindGSS.cmake:360 (set_target_properties):
      INTERFACE_LIBRARY targets may only have whitelisted properties.  The
      property "CURL_PC_MODULES" is not allowed.
    Call Stack (most recent call first):
      CMakeLists.txt:1306 (find_package)
    ```

--- CMake/FindCares.cmake
+++ CMake/FindCares.cmake
@@ -102,7 +102,7 @@ if(CARES_FOUND)
   if(NOT TARGET CURL::cares)
     add_library(CURL::cares INTERFACE IMPORTED)
     set_target_properties(CURL::cares PROPERTIES
-      CURL_PC_MODULES "${_cares_pc_requires}"
+      INTERFACE_CURL_PC_MODULES "${_cares_pc_requires}"
       INTERFACE_COMPILE_OPTIONS "${_cares_CFLAGS}"
       INTERFACE_INCLUDE_DIRECTORIES "${_cares_INCLUDE_DIRS}"
       INTERFACE_LINK_DIRECTORIES "${_cares_LIBRARY_DIRS}"
