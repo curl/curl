a8b363513a4da3d61a7dca0d58c3a1802ad2f196
Viktor Szakats <commit@vsz.me>
2025-04-06 17:42:58 +0200

    FindGSS: formatting

--- CMake/FindGSS.cmake
+++ CMake/FindGSS.cmake
@@ -267,8 +267,8 @@ if(GSS_FOUND)
   if(NOT TARGET CURL::gss)
     add_library(CURL::gss INTERFACE IMPORTED)
     set_target_properties(CURL::gss PROPERTIES
-      INTERFACE_CURL_PC_MODULES "${_gss_pc_requires}"
       INTERFACE_CURL_GSS_FLAVOUR "${_gss_flavour}"
+      INTERFACE_CURL_PC_MODULES "${_gss_pc_requires}"
       INTERFACE_COMPILE_OPTIONS "${_gss_CFLAGS}"
       INTERFACE_INCLUDE_DIRECTORIES "${_gss_INCLUDE_DIRS}"
       INTERFACE_LINK_DIRECTORIES "${_gss_LIBRARY_DIRS}"
