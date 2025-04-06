0b2ec9608ac3b6bfce5cf3aa911745d11f85b881
Viktor Szakats <commit@vsz.me>
2025-04-06 02:15:52 +0200

    drop VERSION property for cmake <3.19 compatibility
    ```
    -- Found Libpsl: /usr/local/include (found version "0.21.5")
    CMake Error at CMake/FindLibpsl.cmake:90 (set_target_properties):
      INTERFACE_LIBRARY targets may only have whitelisted properties.  The
      property "VERSION" is not allowed.
    Call Stack (most recent call first):
      CMakeLists.txt:1252 (find_package)
    ```

--- CMake/FindWolfSSH.cmake
+++ CMake/FindWolfSSH.cmake
@@ -71,7 +71,6 @@ if(WOLFSSH_FOUND)
   if(NOT TARGET CURL::wolfssh)
     add_library(CURL::wolfssh INTERFACE IMPORTED)
     set_target_properties(CURL::wolfssh PROPERTIES
-      VERSION "${WOLFSSH_VERSION}"
       CURL_PC_MODULES "${_wolfssh_pc_requires}"
       INTERFACE_COMPILE_OPTIONS "${_wolfssh_CFLAGS}"
       INTERFACE_INCLUDE_DIRECTORIES "${_wolfssh_INCLUDE_DIRS}"
