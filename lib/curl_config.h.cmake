795e8c271202733f8048a257c51322f9fb95f164
Viktor Szakats <commit@vsz.me>
2025-04-11 02:02:52 +0200

    cmake: add openssl 3.5.0 quicapi + ngtcp2 support

--- lib/curl_config.h.cmake
+++ lib/curl_config.h.cmake
@@ -764,6 +764,9 @@ ${SIZEOF_TIME_T_CODE}
 /* to enable openssl + nghttp3 */
 #cmakedefine USE_OPENSSL_QUIC 1
 
+/* to enable openssl + ngtcp2 + nghttp3 */
+#cmakedefine OPENSSL_QUIC_API2 1
+
 /* Define to 1 if you have the quiche_conn_set_qlog_fd function. */
 #cmakedefine HAVE_QUICHE_CONN_SET_QLOG_FD 1
 
