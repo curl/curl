35c6e3438dfecea56e223712a2a261d93974078b
Viktor Szakats <commit@vsz.me>
2025-04-21 14:26:00 +0200

    test.sh: allow passing custom cmake options

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -10,13 +10,14 @@ cd "$(dirname "$0")"
 command -v dpkg >/dev/null && export CMAKE_GENERATOR=Ninja
 
 mode="${1:-all}"
+shift
 
 cmake_provider="${CMAKE_PROVIDER:-cmake}"
 cmake_consumer="${CMAKE_CONSUMER:-${cmake_provider}}"
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'FetchContent' ]; then
   rm -rf bld-fetchcontent
-  "${cmake_consumer}" -B bld-fetchcontent \
+  "${cmake_consumer}" -B bld-fetchcontent "$@" \
     -DTEST_INTEGRATION_MODE=FetchContent \
     -DFROM_GIT_REPO="${PWD}/../.." \
     -DFROM_GIT_TAG="$(git rev-parse HEAD)"
@@ -26,7 +27,7 @@ fi
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'add_subdirectory' ]; then
   rm -rf curl; ln -s ../.. curl
   rm -rf bld-add_subdirectory
-  "${cmake_consumer}" -B bld-add_subdirectory \
+  "${cmake_consumer}" -B bld-add_subdirectory "$@" \
     -DTEST_INTEGRATION_MODE=add_subdirectory
   "${cmake_consumer}" --build bld-add_subdirectory
 fi
@@ -34,13 +35,13 @@ fi
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
   bld='bld-curl'
   rm -rf "${bld}"
-  "${cmake_provider}" ../.. -B "${bld}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bld}/_pkg" \
+  "${cmake_provider}" ../.. -B "${bld}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bld}/_pkg" "$@" \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_STATIC_LIBS=ON
   "${cmake_provider}" --build "${bld}"
   "${cmake_provider}" --install "${bld}"
   rm -rf bld-find_package
-  "${cmake_consumer}" -B bld-find_package \
+  "${cmake_consumer}" -B bld-find_package "$@" \
     -DTEST_INTEGRATION_MODE=find_package \
     -DCMAKE_PREFIX_PATH="${PWD}/${bld}/_pkg/lib/cmake/CURL"
   "${cmake_consumer}" --build bld-find_package --verbose
