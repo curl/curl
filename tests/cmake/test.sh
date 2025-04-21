ca3736036057318a152d6589930d7328f9d1a259
Viktor Szakats <commit@vsz.me>
2025-04-21 18:36:28 +0200

    test.sh: variabilize

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -16,33 +16,36 @@ cmake_provider="${CMAKE_PROVIDER:-cmake}"
 cmake_consumer="${CMAKE_CONSUMER:-${cmake_provider}}"
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'FetchContent' ]; then
-  rm -rf bld-fetchcontent
-  "${cmake_consumer}" -B bld-fetchcontent "$@" \
+  bldc='bld-fetchcontent'
+  rm -rf "${bldc}"
+  "${cmake_consumer}" -B "${bldc}" "$@" \
     -DTEST_INTEGRATION_MODE=FetchContent \
     -DFROM_GIT_REPO="${PWD}/../.." \
     -DFROM_GIT_TAG="$(git rev-parse HEAD)"
-  "${cmake_consumer}" --build bld-fetchcontent
+  "${cmake_consumer}" --build "${bldc}"
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'add_subdirectory' ]; then
   rm -rf curl; ln -s ../.. curl
-  rm -rf bld-add_subdirectory
-  "${cmake_consumer}" -B bld-add_subdirectory "$@" \
+  bldc='bld-add_subdirectory'
+  rm -rf "${bldc}"
+  "${cmake_consumer}" -B "${bldc}" "$@" \
     -DTEST_INTEGRATION_MODE=add_subdirectory
-  "${cmake_consumer}" --build bld-add_subdirectory
+  "${cmake_consumer}" --build "${bldc}"
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
-  bld='bld-curl'
-  rm -rf "${bld}"
-  "${cmake_provider}" ../.. -B "${bld}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bld}/_pkg" "$@" \
+  bldp='bld-curl'
+  rm -rf "${bldp}"
+  "${cmake_provider}" ../.. -B "${bldp}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bldp}/_pkg" "$@" \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_STATIC_LIBS=ON
-  "${cmake_provider}" --build "${bld}"
-  "${cmake_provider}" --install "${bld}"
-  rm -rf bld-find_package
-  "${cmake_consumer}" -B bld-find_package "$@" \
+  "${cmake_provider}" --build "${bldp}"
+  "${cmake_provider}" --install "${bldp}"
+  bldc='bld-find_package'
+  rm -rf "${bldc}"
+  "${cmake_consumer}" -B "${bldc}" "$@" \
     -DTEST_INTEGRATION_MODE=find_package \
-    -DCMAKE_PREFIX_PATH="${PWD}/${bld}/_pkg/lib/cmake/CURL"
-  "${cmake_consumer}" --build bld-find_package --verbose
+    -DCMAKE_PREFIX_PATH="${PWD}/${bldp}/_pkg/lib/cmake/CURL"
+  "${cmake_consumer}" --build "${bldc}" --verbose
 fi
