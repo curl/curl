aa759e71849d56f99b515900b2613a6f325db3f2
Viktor Szakats <commit@vsz.me>
2025-04-21 13:36:15 +0200

    test.sh: make cmakes customizable

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -9,34 +9,37 @@ cd "$(dirname "$0")"
 
 mode="${1:-all}"
 
+cmake_provider="${CMAKE_PROVIDER:-cmake}"
+cmake_consumer="${CMAKE_CONSUMER:-${cmake_provider}}"
+
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'FetchContent' ]; then
   rm -rf bld-fetchcontent
-  cmake -B bld-fetchcontent \
+  "${cmake_consumer}" -B bld-fetchcontent \
     -DTEST_INTEGRATION_MODE=FetchContent \
     -DFROM_GIT_REPO="${PWD}/../.." \
     -DFROM_GIT_TAG="$(git rev-parse HEAD)"
-  cmake --build bld-fetchcontent
+  "${cmake_consumer}" --build bld-fetchcontent
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'add_subdirectory' ]; then
   rm -rf curl; ln -s ../.. curl
   rm -rf bld-add_subdirectory
-  cmake -B bld-add_subdirectory \
+  "${cmake_consumer}" -B bld-add_subdirectory \
     -DTEST_INTEGRATION_MODE=add_subdirectory
-  cmake --build bld-add_subdirectory
+  "${cmake_consumer}" --build bld-add_subdirectory
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
   bld='bld-curl'
   rm -rf "${bld}"
-  cmake ../.. -B "${bld}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bld}/_pkg" \
+  "${cmake_provider}" ../.. -B "${bld}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bld}/_pkg" \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_STATIC_LIBS=ON
-  cmake --build "${bld}"
-  cmake --install "${bld}"
+  "${cmake_provider}" --build "${bld}"
+  "${cmake_provider}" --install "${bld}"
   rm -rf bld-find_package
-  cmake -B bld-find_package \
+  "${cmake_consumer}" -B bld-find_package \
     -DTEST_INTEGRATION_MODE=find_package \
     -DCMAKE_PREFIX_PATH="${PWD}/${bld}/_pkg/lib/cmake/CURL"
-  cmake --build bld-find_package --verbose
+  "${cmake_consumer}" --build bld-find_package --verbose
 fi
