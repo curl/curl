c3aa22272df5389c76cf4ed42d22a71d637ce924
Viktor Szakats <commit@vsz.me>
2025-04-25 10:52:39 +0200

    cmake: use `CMAKE_COMPILE_WARNING_AS_ERROR` if available
    It's available in CMake >= 3.24.
    
    Ref: https://cmake.org/cmake/help/latest/variable/CMAKE_COMPILE_WARNING_AS_ERROR.html
    
    Closes #17183

--- CMake/PickyWarnings.cmake
+++ CMake/PickyWarnings.cmake
@@ -27,10 +27,14 @@ set(_picky "")
 set(_picky_nocheck "")  # not to pass to feature checks
 
 if(CURL_WERROR)
-  if(MSVC)
-    list(APPEND _picky_nocheck "-WX")
-  else()  # llvm/clang and gcc style options
-    list(APPEND _picky_nocheck "-Werror")
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
+    set(CMAKE_COMPILE_WARNING_AS_ERROR ON)
+  else()
+    if(MSVC)
+      list(APPEND _picky_nocheck "-WX")
+    else()  # llvm/clang and gcc style options
+      list(APPEND _picky_nocheck "-Werror")
+    endif()
   endif()
 
   if((CMAKE_C_COMPILER_ID STREQUAL "GNU" AND
