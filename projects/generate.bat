5b420f63298fcdb80e1f386d0c5e5bb4d0f78b9e
Daniel Stenberg <daniel@haxx.se>
2025-05-08 11:15:09 +0200

    src: drop strcase.[ch] from tool builds
    They're not used there anymore.
    
    Closes #17289

--- projects/generate.bat
+++ projects/generate.bat
@@ -153,15 +153,12 @@ rem
     ) else if "!var!" == "CURL_SRC_RC_FILES" (
       for /f "delims=" %%r in ('dir /b ..\src\*.rc') do call :element %1 src "%%r" %3
     ) else if "!var!" == "CURL_SRC_X_C_FILES" (
-      call :element %1 lib "strcase.c" %3
       call :element %1 lib "nonblock.c" %3
       call :element %1 lib "version_win32.c" %3
     ) else if "!var!" == "CURL_SRC_X_H_FILES" (
       call :element %1 lib "config-win32.h" %3
       call :element %1 lib "curl_setup.h" %3
-      call :element %1 lib "strcase.h" %3
       call :element %1 lib "nonblock.h" %3
-      call :element %1 lib "curl_ctype.h" %3
       call :element %1 lib "version_win32.h" %3
     ) else if "!var!" == "CURL_LIB_C_FILES" (
       for /f "delims=" %%c in ('dir /b ..\lib\*.c') do call :element %1 lib "%%c" %3
