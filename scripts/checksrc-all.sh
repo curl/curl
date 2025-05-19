2fa73874bfdef25484b474c2b3bb7f671ba4557d
Viktor Szakats <commit@vsz.me>
2025-05-19 09:17:16 +0200

    drop `g`

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -9,6 +9,6 @@
     find "$1" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
     shift
   done
-} | sed -E 's|/[^/]+$||g' | sort -u | while read -r dir; do
+} | sed -E 's|/[^/]+$||' | sort -u | while read -r dir; do
   ./scripts/checksrc.pl "${dir}"/*.[ch]
 done
