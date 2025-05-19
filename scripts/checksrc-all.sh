e0717a29c1c10ba0503565eeb3497bab4b3cc25f
Viktor Szakats <commit@vsz.me>
2025-05-19 11:19:35 +0200

    checksrc-all.sh: fix to return error if any of the commands failed, set -eu again

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Copyright (C) Viktor Szakats
 #
 # SPDX-License-Identifier: curl
 
+set -eu
+
+anyfailed=0
+
+while read -r dir; do
+  if ! ./scripts/checksrc.pl "${dir}"/*.[ch]; then
+    anyfailed=1
+  fi
+done <<< "$(
 {
   git ls-files '*.[ch]'
   [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
-} | sed -E 's|/[^/]+$||' | sort -u | while read -r dir; do
-  ./scripts/checksrc.pl "${dir}"/*.[ch]
-done
+} | sed -E 's|/[^/]+$||' | sort -u)"
+
+exit "${anyfailed}"
