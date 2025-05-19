87a1a64cde1dcbf49b7857f388b215be0ec4d92e
Viktor Szakats <commit@vsz.me>
2025-05-19 09:53:43 +0200

    Revert "checksrc-all.sh: set -eu"
    This reverts commit da92b8ed0537a33b61b22d5c5c777fd50472f991.

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -3,11 +3,9 @@
 #
 # SPDX-License-Identifier: curl
 
-set -eu
-
 {
   git ls-files '*.[ch]'
-  [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
+  [ -n "$1" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
 } | sed -E 's|/[^/]+$||' | sort -u | while read -r dir; do
   ./scripts/checksrc.pl "${dir}"/*.[ch]
 done
