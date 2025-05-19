26415b65ab09301d5b2529a67e6f74ac7191c07a
Viktor Szakats <commit@vsz.me>
2025-05-19 11:21:57 +0200

    checksrc-all.sh: back to POSIX

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -1,20 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Copyright (C) Viktor Szakats
 #
 # SPDX-License-Identifier: curl
 
 set -eu
 
+dirs="$({
+    git ls-files '*.[ch]'
+    [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
+  } | sed -E 's|/[^/]+$||' | sort -u)"
+
 anyfailed=0
 
-while read -r dir; do
-  if ! ./scripts/checksrc.pl "${dir}"/*.[ch]; then
+for dir in ${dirs}; do
+  if ! ./scripts/checksrc.pl -v "${dir}"/*.[ch]; then
     anyfailed=1
   fi
-done <<< "$(
-{
-  git ls-files '*.[ch]'
-  [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
-} | sed -E 's|/[^/]+$||' | sort -u)"
+done
 
 exit "${anyfailed}"
