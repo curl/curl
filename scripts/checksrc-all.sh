22b73ea1a1e5d85250f67227b153f61b474c077d
Viktor Szakats <commit@vsz.me>
2025-05-19 10:02:06 +0200

    checksrc-all.sh: avoid error

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -5,7 +5,7 @@
 
 {
   git ls-files '*.[ch]'
-  [ -n "$1" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
+  [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
 } | sed -E 's|/[^/]+$||' | sort -u | while read -r dir; do
   ./scripts/checksrc.pl "${dir}"/*.[ch]
 done
