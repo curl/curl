da92b8ed0537a33b61b22d5c5c777fd50472f991
Viktor Szakats <commit@vsz.me>
2025-05-19 09:26:42 +0200

    checksrc-all.sh: set -eu

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -3,9 +3,11 @@
 #
 # SPDX-License-Identifier: curl
 
+set -eu
+
 {
   git ls-files '*.[ch]'
-  [ -n "$1" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
+  [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
 } | sed -E 's|/[^/]+$||' | sort -u | while read -r dir; do
   ./scripts/checksrc.pl "${dir}"/*.[ch]
 done
