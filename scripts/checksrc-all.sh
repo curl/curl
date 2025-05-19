81e7e3c988b59528a54c4cc424ead4bd3133b3d4
Viktor Szakats <commit@vsz.me>
2025-05-19 11:45:51 +0200

    checksrc-all.sh: do not fail on non-git sandbox

--- scripts/checksrc-all.sh
+++ scripts/checksrc-all.sh
@@ -8,7 +8,7 @@ set -eu
 anyfailed=0
 
 for dir in $({
-    git ls-files '*.[ch]'
+    git ls-files '*.[ch]' || true
     [ -n "${1:-}" ] && find "$@" -name '*.[ch]' | grep -v -F '/CMakeFiles/'
   } | sed -E 's|/[^/]+$||' | sort -u); do
   if ! ./scripts/checksrc.pl -v "${dir}"/*.[ch]; then
