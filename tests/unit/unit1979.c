613308a0f1b7b4cdda194125d4462f633fd37dab
Viktor Szakats <commit@vsz.me>
2025-05-21 09:44:19 +0200

    unit1979, unit1980: fix indent, formatting, scope
    Closes #17403

--- tests/unit/unit1979.c
+++ tests/unit/unit1979.c
@@ -23,7 +23,6 @@
  ***************************************************************************/
 #include "curlcheck.h"
 
-
 #include "http_aws_sigv4.h"
 #include "dynbuf.h"
 
@@ -37,109 +36,109 @@ static void unit_stop(void)
 }
 
 UNITTEST_START
-
-#if !defined(CURL_DISABLE_HTTP) && !defined(CURL_DISABLE_AWS)
-
-struct testcase
 {
-  const char *testname;
-  const bool normalize;
-  const char *url_part;
-  const char *canonical_url;
-};
+#if !defined(CURL_DISABLE_HTTP) && !defined(CURL_DISABLE_AWS)
+  struct testcase {
+    const char *testname;
+    const bool normalize;
+    const char *url_part;
+    const char *canonical_url;
+  };
 
-static const struct testcase testcases[] = {
-  {
-    "test-equals-encode",
-    true,
-    "/a=b",
-    "/a%3Db",
-  },
-  {
-    "test-equals-noencode",
-    false,
-    "/a=b",
-    "/a=b",
-  },
-  {
-    "test-s3-tables",
-    true,
-    "/tables/arn%3Aaws%3As3tables%3Aus-east-1%3A022954301426%3Abucket%2Fja"
-    "soehartablebucket/jasoeharnamespace/jasoehartable/encryption",
-    "/tables/arn%253Aaws%253As3tables%253Aus-east-1%253A022954301426%253Ab"
-    "ucket%252Fjasoehartablebucket/jasoeharnamespace/jasoehartable/encrypt"
-    "ion",
-  },
-  {
-    "get-vanilla",
-    true,
-    "/",
-    "/",
-  },
-  {
-    "get-unreserved",
-    true,
-    "/-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-    "/-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-  },
-  {
-    "get-slashes-unnormalized",
-    false,
-    "//example//",
-    "//example//",
-  },
-  {
-    "get-space-normalized",
-    true,
-    "/example space/",
-    "/example%20space/",
-  },
-  {
-    "get-slash-dot-slash-unnormalized",
-    false,
-    "/./",
-    "/./",
-  },
-  {
-    "get-slash-unnormalized",
-    false,
-    "//",
-    "//",
-  },
-  {
-    "get-relative-relative-unnormalized",
-    false,
-    "/example1/example2/../..",
-    "/example1/example2/../..",
-  }
-};
+  static const struct testcase testcases[] = {
+    {
+      "test-equals-encode",
+      true,
+      "/a=b",
+      "/a%3Db"
+    },
+    {
+      "test-equals-noencode",
+      false,
+      "/a=b",
+      "/a=b"
+    },
+    {
+      "test-s3-tables",
+      true,
+      "/tables/arn%3Aaws%3As3tables%3Aus-east-1%3A022954301426%3Abucket%2Fja"
+      "soehartablebucket/jasoeharnamespace/jasoehartable/encryption",
+      "/tables/arn%253Aaws%253As3tables%253Aus-east-1%253A022954301426%253Ab"
+      "ucket%252Fjasoehartablebucket/jasoeharnamespace/jasoehartable/encrypt"
+      "ion"
+    },
+    {
+      "get-vanilla",
+      true,
+      "/",
+      "/"
+    },
+    {
+      "get-unreserved",
+      true,
+      "/-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+      "/-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      "get-slashes-unnormalized",
+      false,
+      "//example//",
+      "//example//"
+    },
+    {
+      "get-space-normalized",
+      true,
+      "/example space/",
+      "/example%20space/"
+    },
+    {
+      "get-slash-dot-slash-unnormalized",
+      false,
+      "/./",
+      "/./"
+    },
+    {
+      "get-slash-unnormalized",
+      false,
+      "//",
+      "//"
+    },
+    {
+      "get-relative-relative-unnormalized",
+      false,
+      "/example1/example2/../..",
+      "/example1/example2/../.."
+    }
+  };
 
-  struct dynbuf canonical_path;
-
-  char buffer[1024];
-  char *canonical_path_string;
   size_t i;
-  int result;
-  int msnprintf_result;
 
   for(i = 0; i < CURL_ARRAYSIZE(testcases); i++) {
+    struct dynbuf canonical_path;
+
+    char buffer[1024];
+    char *canonical_path_string;
+    int result;
+    int msnprintf_result;
+
     curlx_dyn_init(&canonical_path, CURL_MAX_HTTP_HEADER);
 
     result = canon_path(testcases[i].url_part, strlen(testcases[i].url_part),
-      &canonical_path,
-      testcases[i].normalize);
+                        &canonical_path, testcases[i].normalize);
     canonical_path_string = curlx_dyn_ptr(&canonical_path);
     msnprintf_result = curl_msnprintf(buffer, sizeof(buffer),
-    "%s: Received \"%s\" and should be \"%s\", normalize (%d)",
-      testcases[i].testname, curlx_dyn_ptr(&canonical_path),
-      testcases[i].canonical_url, testcases[i].normalize);
+                                      "%s: Received \"%s\" "
+                                      "and should be \"%s\", normalize (%d)",
+                                      testcases[i].testname,
+                                      curlx_dyn_ptr(&canonical_path),
+                                      testcases[i].canonical_url,
+                                      testcases[i].normalize);
     fail_unless(msnprintf_result >= 0, "curl_msnprintf fails");
     fail_unless(!result && canonical_path_string &&
-      !strcmp(canonical_path_string,
-      testcases[i].canonical_url), buffer);
+                !strcmp(canonical_path_string, testcases[i].canonical_url),
+                buffer);
     curlx_dyn_free(&canonical_path);
   }
-
 #endif /* !defined(CURL_DISABLE_HTTP) && !defined(CURL_DISABLE_AWS) */
-
+}
 UNITTEST_STOP
