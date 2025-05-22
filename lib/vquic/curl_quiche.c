70779199f3cb163c3199d109a544187b6507d839
Stefan Eissing <stefan@eissing.org>
2025-05-22 15:09:58 +0200

    cf: replace the method get_host with query
    Connection filters had a method `get_host()` which had not really been
    documented. Since then, the cf had the `query()` method added. Replace
    the separate get_host with query.
    
    Add `CF_QUERY_HOST_PORT` as query to connection filters to retrieve
    which remote hostname and port the filter (or its sub-filter) is talking
    to. The query is implemented by HTTP and SOCKS filters, all others pass
    it through.
    
    Add `Curl_conn_get_current_host()` to retrieve the remote host and port
    for a connection. During connect, this will return the host the
    connection is talking to right now. Before/After connect, this will
    return `conn->host.name`.
    
    This is used by SASL authentication.
    
    Closes #17419

--- lib/vquic/curl_quiche.c
+++ lib/vquic/curl_quiche.c
@@ -1626,7 +1626,6 @@ struct Curl_cftype Curl_cft_http3 = {
   cf_quiche_connect,
   cf_quiche_close,
   cf_quiche_shutdown,
-  Curl_cf_def_get_host,
   cf_quiche_adjust_pollset,
   cf_quiche_data_pending,
   cf_quiche_send,
