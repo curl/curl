d16ccbd55de80c271fe822f4ba8b6271fd9166ff
Stefan Eissing <stefan@eissing.org>
2025-05-28 14:04:31 +0200

    multi: fix add_handle resizing
    Due to someone being stupid, the resizing of the multi's transfer
    table was actually shrinking it. Oh my.
    
    Add test751 to reproduce, add code assertion.
    
    Fixes #17473
    Reported-by: Jeroen Ooms
    Closes #17475

---
+++ tests/libtest/lib751.c
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ * SPDX-License-Identifier: curl
+ *
+ ***************************************************************************/
+#include "test.h"
+
+#include "testutil.h"
+#include "warnless.h"
+#include "memdebug.h"
+
+#define TEST_HANG_TIMEOUT 60 * 1000
+
+/*
+ * Get a single URL without select().
+ */
+
+CURLcode test(char *URL)
+{
+  CURL *easies[1000];
+  CURLM *m;
+  CURLcode res = CURLE_FAILED_INIT;
+  CURLMcode mres;
+  int i;
+
+  (void)URL;
+  memset(easies, 0, sizeof(easies));
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+  m = curl_multi_init();
+  if(!m) {
+    res = CURLE_OUT_OF_MEMORY;
+    goto test_cleanup;
+  }
+
+  for(i = 0; i < 1000; i++) {
+    CURL *e = curl_easy_init();
+    if(!e) {
+      res = CURLE_OUT_OF_MEMORY;
+      goto test_cleanup;
+    }
+    easies[i] = e;
+
+    res = curl_easy_setopt(e, CURLOPT_URL, "https://www.example.com/");
+    if(!res)
+      res = curl_easy_setopt(e, CURLOPT_VERBOSE, 1L);
+    if(res)
+      goto test_cleanup;
+
+    mres = curl_multi_add_handle(m, e);
+    if(mres != CURLM_OK) {
+      printf("MULTI ERROR: %s\n", curl_multi_strerror(mres));
+      res = CURLE_FAILED_INIT;
+      goto test_cleanup;
+    }
+  }
+
+test_cleanup:
+
+  if(res)
+    printf("ERROR: %s\n", curl_easy_strerror(res));
+
+  for(i = 0; i < 1000; i++) {
+    if(easies[i]) {
+      curl_multi_add_handle(m, easies[i]);
+      curl_easy_cleanup(easies[i]);
+      easies[i] = NULL;
+    }
+  }
+  curl_multi_cleanup(m);
+  curl_global_cleanup();
+
+  return res;
+}
