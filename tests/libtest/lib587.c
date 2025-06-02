b390153876caec82ed8c0fcbb67f224dda607ba4
Viktor Szakats <commit@vsz.me>
2025-06-02 22:05:11 +0200

    lib587: cleanup

--- tests/libtest/lib587.c
+++ tests/libtest/lib587.c
@@ -36,28 +36,11 @@ struct t587_WriteThis {
 static size_t t587_read_callback(char *ptr, size_t size,
                                  size_t nmemb, void *userp)
 {
-#ifdef LIB587
   (void)ptr;
   (void)size;
   (void)nmemb;
   (void)userp;
   return CURL_READFUNC_ABORT;
-#else
-
-  struct t587_WriteThis *pooh = (struct t587_WriteThis *)userp;
-
-  if(size*nmemb < 1)
-    return 0;
-
-  if(pooh->sizeleft) {
-    *ptr = pooh->readptr[0]; /* copy one single byte */
-    pooh->readptr++;                 /* advance pointer */
-    pooh->sizeleft--;                /* less data left */
-    return 1;                        /* we return 1 byte at a time! */
-  }
-
-  return 0;                         /* no more data left to deliver */
-#endif
 }
 
 static CURLcode t587_test_once(char *URL, bool oldstyle)
