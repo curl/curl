1c1653ea877d6a11aba0a92b0df0855835058670
Viktor Szakats <commit@vsz.me>
2025-06-10 18:37:11 +0200

    config-riscos.h: drop a mostly likely redundant undef _LARGE_FILES
    This macro is OS/400-specific.

--- lib/config-riscos.h
+++ lib/config-riscos.h
@@ -186,9 +186,6 @@
 /* Number of bits in a file offset, on hosts where this is settable. */
 #undef _FILE_OFFSET_BITS
 
-/* Define for large files, on AIX-style hosts. */
-#undef _LARGE_FILES
-
 /* Define to empty if `const' does not conform to ANSI C. */
 #undef const
 
