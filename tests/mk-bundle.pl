ff8f8739a148a6df3182e9a78b430cc9ca90c3c6
Viktor Szakats <commit@vsz.me>
2025-06-12 16:38:34 +0200

    mk-bundle.pl syncup

--- tests/mk-bundle.pl
+++ tests/mk-bundle.pl
@@ -30,7 +30,7 @@ use strict;
 use warnings;
 
 if(!@ARGV) {
-    die "Usage: $0 [<inputs>] [--util <util-c-sources>] [--exclude <exclude-c-sources>]\n";
+    die "Usage: $0 [--input] [<inputs>] [--util <util-c-sources>] [--exclude <exclude-c-sources>]\n";
 }
 
 # Specific sources to exclude or add as an extra source file
@@ -40,12 +40,9 @@ my %util;
 my $in_exclude = 0;
 my $in_util = 0;
 foreach my $src (@ARGV) {
-    if($in_exclude) {
-        $exclude{$src} = 1;
-    }
-    elsif($in_util) {
-        $util{$src} = 1;
-        push @src, $src;
+    if($src eq "--input") {
+        $in_exclude = 0;
+        $in_util = 0;
     }
     elsif($src eq "--exclude") {
         $in_exclude = 1;
@@ -54,6 +51,13 @@ foreach my $src (@ARGV) {
     elsif($src eq "--util") {
         $in_exclude = 0;
         $in_util = 1;
+    }
+    elsif($in_exclude) {
+        $exclude{$src} = 1;
+    }
+    elsif($in_util) {
+        $util{$src} = 1;
+        push @src, $src;
     }
     else {
         push @src, $src;
