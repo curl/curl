9a15ba8ad7581fe4450217eeba7ad93d10d5e719
Viktor Szakats <commit@vsz.me>
2025-06-20 13:12:12 +0200

    drop NOLINTs to verify clang-tidy

--- lib/cf-socket.c
+++ lib/cf-socket.c
@@ -1820,8 +1820,6 @@ static CURLcode cf_udp_setup_quic(struct Curl_cfilter *cf,
   /* QUIC needs a connected socket, nonblocking */
   DEBUGASSERT(ctx->sock != CURL_SOCKET_BAD);
 
-  /* error: The 1st argument to 'connect' is -1 but should be >= 0
-     NOLINTNEXTLINE(clang-analyzer-unix.StdCLibraryFunctions) */
   rc = connect(ctx->sock, &ctx->addr.curl_sa_addr,
                (curl_socklen_t)ctx->addr.addrlen);
   if(-1 == rc) {
