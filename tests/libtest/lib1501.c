6811a6eaacce7df5e374b23b9614f787f1f840af
Viktor Szakats <commit@vsz.me>
2025-06-22 11:22:34 +0200

    lib1501: use abort_on_test_timeout_custom fixup

--- tests/libtest/lib1501.c
+++ tests/libtest/lib1501.c
@@ -34,7 +34,7 @@ static CURLcode test_lib1501(char *URL)
      conservative to allow old and slow machines to run this test too */
   static const int MAX_BLOCKED_TIME_MS = 500;
 
-  static const int HANG_TIMEOUT = 30 * 1000;
+  static const long HANG_TIMEOUT = 30 * 1000;
 
   CURL *handle = NULL;
   CURLM *mhandle = NULL;
