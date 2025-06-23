61167bfb3db2943599580cce5b94bd1e1d4f23cf
Viktor Szakats <commit@vsz.me>
2025-06-23 12:32:51 +0200

    libtest drop rest of limits.h because curl/curl.h already includes it 2

--- tests/libtest/testutil.h
+++ tests/libtest/testutil.h
@@ -32,8 +32,6 @@ char *tutil_suburl(const char *base, int i);
 #include <sys/resource.h>
 #endif
 
-#include <limits.h>
-
 #if defined(HAVE_GETRLIMIT) && defined(HAVE_SETRLIMIT)
 void tutil_rlim2str(char *buf, size_t len, rlim_t val);
 #endif
