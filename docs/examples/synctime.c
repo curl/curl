8d48c52d20cbcd94459cf88a560ed4bd1221ae45
Viktor Szakats <commit@vsz.me>
2025-06-27 14:41:25 +0200

    synctime: whitespace

--- docs/examples/synctime.c
+++ docs/examples/synctime.c
@@ -232,10 +232,10 @@ static int conf_init(struct conf *conf)
 {
   int i;
 
-  *conf->http_proxy       = 0;
+  *conf->http_proxy = 0;
   for(i = 0; i < MAX_STRING1; i++)
-    conf->proxy_user[i]     = 0;    /* Clean up password from memory */
-  *conf->timeserver       = 0;
+    conf->proxy_user[i] = 0;    /* Clean up password from memory */
+  *conf->timeserver = 0;
   return 1;
 }
 
