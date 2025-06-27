48277b51c5fe84ca350267f9edc7bc9947b80573
Viktor Szakats <commit@vsz.me>
2025-06-27 11:05:04 +0200

    stub reduce 3

--- tests/libtest/stub_gssapi.c
+++ tests/libtest/stub_gssapi.c
@@ -122,13 +122,6 @@ OM_uint32 gss_delete_sec_context(OM_uint32 *minor_status,
                                  gss_ctx_id_t *context_handle,
                                  gss_buffer_t output_token);
 
-OM_uint32 gss_display_status(OM_uint32 *minor_status,
-                             OM_uint32 status_value,
-                             int status_type,
-                             const gss_OID mech_type,
-                             OM_uint32 *message_context,
-                             gss_buffer_t status_string);
-
 /* upstream prototypes */
 OM_uint32 gss_release_buffer(OM_uint32 * /* minor_status */,
                              gss_buffer_t /* buffer */);
@@ -369,48 +362,6 @@ OM_uint32 gss_delete_sec_context(OM_uint32 *min,
   free(*context_handle);
   *context_handle = NULL;
   *min = 0;
-
-  return GSS_S_COMPLETE;
-}
-
-OM_uint32 gss_display_status(OM_uint32 *min,
-                             OM_uint32 status_value,
-                             int status_type,
-                             const gss_OID mech_type,
-                             OM_uint32 *message_context,
-                             gss_buffer_t status_string)
-{
-  static const char maj_str[] = "Stub GSS error";
-  (void)mech_type;
-  if(min)
-    *min = 0;
-
-  if(message_context)
-    *message_context = 0;
-
-  if(status_string) {
-    status_string->value = NULL;
-    status_string->length = 0;
-
-    if(status_value >= GSS_LAST)
-      return GSS_S_FAILURE;
-
-    switch(status_type) {
-      case GSS_C_GSS_CODE:
-        status_string->value = strdup(maj_str);
-        break;
-      case GSS_C_MECH_CODE:
-        status_string->value = strdup(min_err_table[status_value]);
-        break;
-      default:
-        return GSS_S_FAILURE;
-    }
-
-    if(status_string->value)
-      status_string->length = strlen(status_string->value);
-    else
-      return GSS_S_FAILURE;
-  }
 
   return GSS_S_COMPLETE;
 }
