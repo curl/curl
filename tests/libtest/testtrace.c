86127d3746ab116e5815f0aa26169327d4e754fb
Viktor Szakats <commit@vsz.me>
2025-06-27 14:23:28 +0200

    move remaining exception to testtrace.c

--- tests/libtest/testtrace.c
+++ tests/libtest/testtrace.c
@@ -108,6 +108,7 @@ int libtest_debug_cb(CURL *handle, curl_infotype type,
       known_offset = 1;
     }
     secs = epoch_offset + tv.tv_sec;
+    /* !checksrc! disable BANNEDFUNC 1 */
     now = localtime(&secs);  /* not thread safe but we don't care */
     curl_msnprintf(timebuf, sizeof(timebuf), "%02d:%02d:%02d.%06ld ",
                    now->tm_hour, now->tm_min, now->tm_sec, (long)tv.tv_usec);
