85a4a67cc65fcd7d5022691ebe2650bc6174d3d3
Daniel Stenberg <daniel@haxx.se>
2025-06-29 14:15:37 +0200

    ngtcp2: fix "unused value"
    Coverity complained: "Assigning value from r2 to result here, but that
    stored value is overwritten before it can be used."

--- lib/vquic/curl_ngtcp2.c
+++ lib/vquic/curl_ngtcp2.c
@@ -1582,7 +1582,7 @@ static CURLcode cf_ngtcp2_send(struct Curl_cfilter *cf, struct Curl_easy *data,
   struct h3_stream_ctx *stream = H3_STREAM_CTX(ctx, data);
   struct cf_call_data save;
   struct pkt_io_ctx pktx;
-  CURLcode result = CURLE_OK, r2;
+  CURLcode result = CURLE_OK;
 
   CF_DATA_SAVE(save, cf, data);
   DEBUGASSERT(cf->connected);
@@ -1596,18 +1596,17 @@ static CURLcode cf_ngtcp2_send(struct Curl_cfilter *cf, struct Curl_easy *data,
     return ctx->tls_vrfy_result;
 
   (void)eos; /* use for stream EOF and block handling */
-  r2 = cf_progress_ingress(cf, data, &pktx);
-  if(r2) {
-    result = r2;
-  }
+  result = cf_progress_ingress(cf, data, &pktx);
+  if(result)
+    goto out;
 
   if(!stream || stream->id < 0) {
     if(ctx->shutdown_started) {
       CURL_TRC_CF(data, cf, "cannot open stream on closed connection");
       result = CURLE_SEND_ERROR;
-      goto out;
     }
-    result = h3_stream_open(cf, data, buf, len, pnwritten);
+    else
+      result = h3_stream_open(cf, data, buf, len, pnwritten);
     if(result) {
       CURL_TRC_CF(data, cf, "failed to open stream -> %d", result);
       goto out;
@@ -1656,14 +1655,11 @@ static CURLcode cf_ngtcp2_send(struct Curl_cfilter *cf, struct Curl_easy *data,
   if(*pnwritten > 0 && !ctx->tls_handshake_complete && ctx->use_earlydata)
     ctx->earlydata_skip += *pnwritten;
 
-  r2 = cf_progress_egress(cf, data, &pktx);
-  if(r2)
-    result = r2;
+  result = cf_progress_egress(cf, data, &pktx);
 
 out:
-  r2 = check_and_set_expiry(cf, data, &pktx);
-  if(r2)
-    result = r2;
+  if(!result)
+    result = check_and_set_expiry(cf, data, &pktx);
 
   CURL_TRC_CF(data, cf, "[%" FMT_PRId64 "] cf_send(len=%zu) -> %d, %zu",
               stream ? stream->id : -1, len, result, *pnwritten);
