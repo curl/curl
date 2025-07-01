779acf04a00a496d20fee705e5e7fcf24ad3c6c4
Viktor Szakats <commit@vsz.me>
2025-07-01 16:02:56 +0200

    drop cast for CURLSSLOPT_REVOKE_BEST_EFFORT

--- tests/libtest/lib678.c
+++ tests/libtest/lib678.c
@@ -80,8 +80,7 @@ static CURLcode test_cert_blob(const char *url, const char *cafile)
     curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "CURLOPT_CAINFO_BLOB");
-    curl_easy_setopt(curl, CURLOPT_SSL_OPTIONS,
-                     (long)CURLSSLOPT_REVOKE_BEST_EFFORT);
+    curl_easy_setopt(curl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_REVOKE_BEST_EFFORT);
 
     blob.data = certdata;
     blob.len = certsize;
