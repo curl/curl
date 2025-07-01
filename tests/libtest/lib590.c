964e01a3a760cf502a9ea7d5a2a8f6f999b1695a
Viktor Szakats <commit@vsz.me>
2025-07-01 16:02:20 +0200

    drop casts for CURLAUTH_* in tests

--- tests/libtest/lib590.c
+++ tests/libtest/lib590.c
@@ -59,7 +59,7 @@ static CURLcode test_lib590(char *URL)
   test_setopt(curl, CURLOPT_URL, URL);
   test_setopt(curl, CURLOPT_HEADER, 1L);
   test_setopt(curl, CURLOPT_PROXYAUTH,
-              (long) (CURLAUTH_BASIC | CURLAUTH_DIGEST | CURLAUTH_NTLM));
+              (CURLAUTH_BASIC | CURLAUTH_DIGEST | CURLAUTH_NTLM));
   test_setopt(curl, CURLOPT_PROXY, libtest_arg2); /* set in first.c */
 
   /* set the name + password twice to test that the API is fine with it */
