d2a801d82f5042cc80d6f651edfbfffb9eb16ae9
Viktor Szakats <commit@vsz.me>
2025-07-12 09:24:59 +0200

    move typos to a script

---
+++ .github/scripts/typos.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copyright (C) Viktor Szakats
+#
+# SPDX-License-Identifier: curl
+
+set -eu
+
+cd "$(dirname "${0}")"/../..
+
+typos --version
+
+git ls-files \
+| grep -v -E '(codespell-ignore\.txt|spellcheck\.words|/wcurl|CIPHERS-TLS12\.md|/THANKS)' \
+| typos \
+    --config '.github/scripts/typos.toml' \
+    --file-list -
