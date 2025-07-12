b611a70a0b36da72255b9958c3e2c96db08d5c38
Viktor Szakats <commit@vsz.me>
2025-07-12 09:22:04 +0200

    move codespell to a script

---
+++ .github/scripts/codespell.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright (C) Viktor Szakats
+#
+# SPDX-License-Identifier: curl
+
+set -eu
+
+cd "$(dirname "${0}")"/../..
+
+codespell --version
+# shellcheck disable=SC2046
+codespell \
+  --skip '*/spellcheck.words' \
+  --skip '*/typos.toml' \
+  --skip '*/THANKS' \
+  --skip '*/mk-ca-bundle.pl' \
+  --skip '*/wcurl' \
+  --skip '*/tool_hugehelp.c' \
+  --skip 'packages/*' \
+  --skip '*/test*' \
+  --ignore-words '.github/scripts/codespell-ignore.txt' \
+  $(git ls-files)
