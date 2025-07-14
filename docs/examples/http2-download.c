df1189b974363f8b1f3a0b699b142b6fa47b8e8e
Viktor Szakats <commit@vsz.me>
2025-07-15 01:13:53 +0200

    drop UNDER_CE code examples

--- docs/examples/http2-download.c
+++ docs/examples/http2-download.c
@@ -28,11 +28,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef UNDER_CE
-#define strerror(e) "?"
-#else
 #include <errno.h>
-#endif
 
 /* curl stuff */
 #include <curl/curl.h>
