91367b0534347108148884600e37f72814334a98
Viktor Szakats <commit@vsz.me>
2025-07-15 02:24:05 +0200

    drop CURL_SETERRNO() again

--- lib/strerror.c
+++ lib/strerror.c
@@ -846,7 +846,7 @@ const char *Curl_strerror(int err, char *buf, size_t buflen)
     *p = '\0';
 
   if(errno != old_errno)
-    CURL_SETERRNO(old_errno);
+    errno = old_errno;
 
 #ifdef _WIN32
   if(old_win_err != GetLastError())
@@ -994,7 +994,7 @@ const char *Curl_sspi_strerror(int err, char *buf, size_t buflen)
 #endif
 
   if(errno != old_errno)
-    CURL_SETERRNO(old_errno);
+    errno = old_errno;
 
 #ifdef _WIN32
   if(old_win_err != GetLastError())
