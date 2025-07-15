f844eb4217a0a37eea7f1659b87b25b80b5d3d64
Viktor Szakats <commit@vsz.me>
2025-07-15 02:39:27 +0200

    winapi: update comment

--- lib/curlx/winapi.c
+++ lib/curlx/winapi.c
@@ -61,8 +61,7 @@ const char *curlx_get_winapi_error(DWORD err, char *buf, size_t buflen)
 
   /* We return the local codepage version of the error string because if it is
      output to the user's terminal it will likely be with functions which
-     expect the local codepage (eg fprintf, failf, infof).
-     FormatMessageW -> wcstombs is used for Windows CE compatibility. */
+     expect the local codepage (eg fprintf, failf, infof). */
   if(FormatMessageW((FORMAT_MESSAGE_FROM_SYSTEM |
                      FORMAT_MESSAGE_IGNORE_INSERTS), NULL, err,
                     LANG_NEUTRAL, wbuf, CURL_ARRAYSIZE(wbuf), NULL)) {
