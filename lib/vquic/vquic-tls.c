3ad4b440738ea9a81043dc59d945b0e751c27072
Stefan Eissing <stefan@eissing.org>
2025-07-16 11:10:13 +0200

    openssl: some small cleanups
    - rename Curl_oss_check_peer_cert() to Curl_ossl_check_peer_cert()
    - leave altname match loop after the first success when the match
      was an ip address
    - remove static subj_alt_hostcheck() since it did not really do much
    - use length based infof() output of altname, even though it does
      seem always to be nul terminated
    
    Closes #17940

--- lib/vquic/vquic-tls.c
+++ lib/vquic/vquic-tls.c
@@ -167,7 +167,7 @@ CURLcode Curl_vquic_tls_verify_peer(struct curl_tls_ctx *ctx,
 
 #ifdef USE_OPENSSL
   (void)conn_config;
-  result = Curl_oss_check_peer_cert(cf, data, &ctx->ossl, peer);
+  result = Curl_ossl_check_peer_cert(cf, data, &ctx->ossl, peer);
 #elif defined(USE_GNUTLS)
   if(conn_config->verifyhost) {
     result = Curl_gtls_verifyserver(data, ctx->gtls.session,
