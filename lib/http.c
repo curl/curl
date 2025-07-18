a22103537bef61c2d8e024e5ede524fac792a1c4
Stefan Eissing <stefan@eissing.org>
2025-07-18 14:00:56 +0200

    fix strange if test ending on other line

--- lib/http.c
+++ lib/http.c
@@ -2686,8 +2686,7 @@ CURLcode Curl_http(struct Curl_easy *data, bool *done)
   case CURL_HTTP_VERSION_2:
 #ifndef CURL_DISABLE_PROXY
     if((Curl_conn_http_version(data, conn) != 20) &&
-       conn->bits.proxy && !conn->bits.tunnel_proxy
-      ) {
+       conn->bits.proxy && !conn->bits.tunnel_proxy) {
       result = Curl_http2_switch(data);
       if(result)
         goto fail;
