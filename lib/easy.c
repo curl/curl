ad5e7e2e0e9ba4dae88d6adbd86df31927f50c5c
Viktor Szakats <commit@vsz.me>
2025-07-18 20:05:31 +0200

    catch !ssl + ssls_export combination in source, convert error to warning in builds
    To make it build-system agnostic.
    
    -DCURL_ENABLE_SSL=OFF -DUSE_SSLS_EXPORT=ON

--- lib/easy.c
+++ lib/easy.c
@@ -1328,7 +1328,7 @@ CURLcode curl_easy_ssls_import(CURL *d, const char *session_key,
                                const unsigned char *shmac, size_t shmac_len,
                                const unsigned char *sdata, size_t sdata_len)
 {
-#ifdef USE_SSLS_EXPORT
+#if defined(USE_SSL) && defined(USE_SSLS_EXPORT)
   struct Curl_easy *data = d;
   if(!GOOD_EASY_HANDLE(data))
     return CURLE_BAD_FUNCTION_ARGUMENT;
@@ -1349,7 +1349,7 @@ CURLcode curl_easy_ssls_export(CURL *d,
                                curl_ssls_export_cb *export_fn,
                                void *userptr)
 {
-#ifdef USE_SSLS_EXPORT
+#if defined(USE_SSL) && defined(USE_SSLS_EXPORT)
   struct Curl_easy *data = d;
   if(!GOOD_EASY_HANDLE(data))
     return CURLE_BAD_FUNCTION_ARGUMENT;
