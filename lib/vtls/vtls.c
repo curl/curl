1c489663151f516349a3a7ee7b1757a4af1aced4
Viktor Szakats <commit@vsz.me>
2025-07-18 18:17:06 +0200

    fixed potential unused variable
    lib/vtls/vtls.c:1634:51: error: unused parameter 'conn' [-Werror,-Wunused-parameter]
     1634 |                               struct connectdata *conn)
          |                                                   ^

--- lib/vtls/vtls.c
+++ lib/vtls/vtls.c
@@ -1648,6 +1648,7 @@ static CURLcode cf_ssl_create(struct Curl_cfilter **pcf,
   DEBUGASSERT(data->conn);
 
 #ifdef CURL_DISABLE_HTTP
+  (void)conn;
   /* We only support ALPN for HTTP so far. */
   DEBUGASSERT(!conn->bits.tls_enable_alpn);
   ctx = cf_ctx_new(data, NULL);
