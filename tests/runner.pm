29c5e00f2be60b4ad1578e6740777045bd9560de
Viktor Szakats <commit@vsz.me>
2025-07-19 18:45:25 +0200

    set CURL_ENTROPY per test, not globally

--- tests/runner.pm
+++ tests/runner.pm
@@ -163,7 +163,6 @@ sub runner_init {
 
     # enable memory debugging if curl is compiled with it
     $ENV{'CURL_MEMDEBUG'} = "$logdir/$MEMDUMP";
-    $ENV{'CURL_ENTROPY'}="12345678";
     $ENV{'CURL_FORCETIME'}=1; # for debug NTLM magic
     $ENV{'CURL_GLOBAL_INIT'}=1; # debug curl_global_init/cleanup use
     $ENV{'HOME'}=$pwd;
