24f8442e6a9d76bc51ea6e6026b5cc3479050c53
Viktor Szakats <commit@vsz.me>
2025-07-20 23:24:26 +0200

    windows: target version macro tidy-ups
    - autotools: stop checking for `WINVER` to detect thread-safety.
      To sync with implementation in `easy_lock.h` and with cmake.
    
    - replace numeric version with `_WIN32_WINNT_VISTA`.
    
    - `_WIN32_WINNT_VISTA` is always defined via `setup-win32.h`,
      don't check for it.
    
    Closes #17981

--- lib/curl_threads.c
+++ lib/curl_threads.c
@@ -142,8 +142,7 @@ void Curl_thread_destroy(curl_thread_t *hnd)
 
 int Curl_thread_join(curl_thread_t *hnd)
 {
-#if !defined(_WIN32_WINNT) || !defined(_WIN32_WINNT_VISTA) || \
-    (_WIN32_WINNT < _WIN32_WINNT_VISTA)
+#if !defined(_WIN32_WINNT) || (_WIN32_WINNT < _WIN32_WINNT_VISTA)
   int ret = (WaitForSingleObject(*hnd, INFINITE) == WAIT_OBJECT_0);
 #else
   int ret = (WaitForSingleObjectEx(*hnd, INFINITE, FALSE) == WAIT_OBJECT_0);
