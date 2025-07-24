c435516e1d9d3336c7b509f1ea6a065ffa5f02e3
Viktor Szakats <commit@vsz.me>
2025-07-24 03:06:39 +0200

    simplify min/def versions

--- lib/config-win32.h
+++ lib/config-win32.h
@@ -35,15 +35,9 @@
    /* VS2012 default target settings and minimum build target check. */
 #  if _MSC_VER >= 1700
      /* The minimum and default build targets for VS2012 are Vista and 8,
-        respectively, unless Update 1 is installed and the v110_xp toolset
-        is chosen. */
-#    ifdef _USING_V110_SDK71_
-#      define VS2012_MIN_TARGET 0x0501  /* XP */
-#      define VS2012_DEF_TARGET 0x0501  /* XP */
-#    else
-#      define VS2012_MIN_TARGET 0x0600  /* Vista */
-#      define VS2012_DEF_TARGET 0x0602  /* 8 */
-#    endif
+        respectively */
+#    define VS2012_MIN_TARGET 0x0600  /* Vista */
+#    define VS2012_DEF_TARGET 0x0600  /* Vista */
 
 #    ifndef _WIN32_WINNT
 #    define _WIN32_WINNT VS2012_DEF_TARGET
@@ -52,19 +46,13 @@
 #    define WINVER VS2012_DEF_TARGET
 #    endif
 #    if (_WIN32_WINNT < VS2012_MIN_TARGET) || (WINVER < VS2012_MIN_TARGET)
-#      ifdef _USING_V110_SDK71_
-#        error VS2012 does not support build targets prior to Windows XP
-#      else
-#        error VS2012 does not support build targets prior to Windows Vista
-#      endif
+#      error VS2012 does not support build targets prior to Windows Vista
 #    endif
    /* Default target settings and minimum build target check for
       VS2008 and VS2010 */
 #  else
-#    define VS2008_MIN_TARGET 0x0501  /* XP */
-     /* VS2008 default build target is Windows Vista (0x0600).
-        We override default target to be Windows XP. */
-#    define VS2008_DEF_TARGET 0x0501  /* XP */
+#    define VS2008_MIN_TARGET 0x0600  /* Vista */
+#    define VS2008_DEF_TARGET 0x0600  /* Vista */
 
 #    ifndef _WIN32_WINNT
 #    define _WIN32_WINNT VS2008_DEF_TARGET
@@ -73,7 +61,7 @@
 #    define WINVER VS2008_DEF_TARGET
 #    endif
 #    if (_WIN32_WINNT < VS2008_MIN_TARGET) || (WINVER < VS2008_MIN_TARGET)
-#      error VS2008 does not support build targets prior to Windows XP
+#      error VS2008 does not support build targets prior to Windows Vista
 #    endif
 #  endif
 #endif /* _MSC_VER */
