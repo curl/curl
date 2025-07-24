7d1158af84bc2826f86ded1bb4f180f12c1c266b
Viktor Szakats <commit@vsz.me>
2025-07-24 03:18:24 +0200

    drop runtime pre-vista check and code

--- lib/vtls/schannel.c
+++ lib/vtls/schannel.c
@@ -613,12 +613,8 @@ static CURLcode schannel_acquire_credential_handle(struct Curl_cfilter *cf,
         else
           pszPassword[0] = 0;
 
-        if(Curl_isVistaOrGreater)
-          cert_store = PFXImportCertStore(&datablob, pszPassword,
-                                          PKCS12_NO_PERSIST_KEY);
-        else
-          cert_store = PFXImportCertStore(&datablob, pszPassword, 0);
-
+        cert_store = PFXImportCertStore(&datablob, pszPassword,
+                                        PKCS12_NO_PERSIST_KEY);
         curlx_free(pszPassword);
       }
       if(!blob)
