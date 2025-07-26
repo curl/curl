434b2c23d3e58c0c4e16709c98bc2bf393eb293c
Viktor Szakats <commit@vsz.me>
2025-07-26 17:20:22 +0200

    old-mingw-w64

--- tests/libtest/lib517.c
+++ tests/libtest/lib517.c
@@ -144,7 +144,9 @@ static CURLcode test_lib517(char *URL)
     {"Thu, 31-Dec-1969 23:59:58 GMT", -2 },
     {"Thu, 31-Dec-1969 23:59:59 GMT", 0 }, /* avoids -1 ! */
 #if SIZEOF_TIME_T > 4
-    {"Sun, 06 Nov 2044 08:49:37 GMT", (time_t)(long)2362034977 },
+#if !defined(__GNUC__) || !defined(__MINGW32__) || (__GNUC__ > 4)
+    {"Sun, 06 Nov 2044 08:49:37 GMT", (time_t)2362034977 },
+#endif
     {"Sun, 06 Nov 3144 08:49:37 GMT", 37074617377 },
 #ifndef HAVE_TIME_T_UNSIGNED
 #ifdef _MSC_VER
@@ -152,7 +154,9 @@ static CURLcode test_lib517(char *URL)
 /* unary minus operator applied to unsigned type, result still unsigned */
 #pragma warning(disable:4146)
 #endif
+#if !defined(__GNUC__) || !defined(__MINGW32__) || (__GNUC__ > 4)
     {"Sun, 06 Nov 1900 08:49:37 GMT", -2182259423 },
+#endif
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif
