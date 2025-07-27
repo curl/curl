713969b56ca6ab0d376faeaf00822c20c9821021
Viktor Szakats <commit@vsz.me>
2025-07-27 19:11:56 +0200

    WinCE

--- lib/easy.c
+++ lib/easy.c
@@ -107,7 +107,7 @@ static curl_simple_lock s_lock = CURL_SIMPLE_LOCK_INIT;
  * ways, but at this point it must be defined as the system-supplied strdup
  * so the callback pointer is initialized correctly.
  */
-#if defined(UNDER_CE)
+#ifdef UNDER_CE
 #define system_strdup _strdup
 #elif !defined(HAVE_STRDUP)
 #define system_strdup Curl_strdup
