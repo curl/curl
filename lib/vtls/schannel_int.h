037d48a0b41c31272c56e28f0aa51759ed42ad74
Viktor Szakats <commit@vsz.me>
2025-07-29 19:36:20 +0200

    document SCH_CREDENTIALS_VERSION mingw-w64, approx VS cutoff versions

--- lib/vtls/schannel_int.h
+++ lib/vtls/schannel_int.h
@@ -51,6 +51,7 @@
 #define CERT_STORE_PROV_SYSTEM_W  ((LPCSTR)(size_t)10)
 #endif
 
+/* Offered by mingw-w64 v8+, ~VS2022+ */
 #ifndef SCH_CREDENTIALS_VERSION
 #define SCH_CREDENTIALS_VERSION  0x00000005
 
