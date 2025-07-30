672405ea5b359f1e21691f4e74205aa86fc79d46
Viktor Szakats <commit@vsz.me>
2025-07-30 22:33:26 +0200

    schannel: drop check for `CERT_CHAIN_REVOCATION_CHECK_CHAIN`
    Always available in supported mingw-w64 and MSVC compiler, except
    in UWP mode. For mingw32ce this macro is defined later in the code.
    
    Also available in OpenWatcom 2.

--- lib/vtls/schannel_int.h
+++ lib/vtls/schannel_int.h
@@ -30,8 +30,7 @@
 
 #include "vtls.h"
 
-#if (defined(__MINGW32CE__) || defined(CERT_CHAIN_REVOCATION_CHECK_CHAIN)) && \
-  !defined(CURL_WINDOWS_UWP)
+#ifndef CURL_WINDOWS_UWP
 #define HAS_MANUAL_VERIFY_API
 #endif
 
