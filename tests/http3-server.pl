1140c8bc047ff847f8f5f0105802f2c265c3426f
Viktor Szakats <commit@vsz.me>
2025-07-30 14:11:05 +0200

    tests: fix perl warnings in http2-server, http3-server
    AM libressl heimdal:
    ```
    Global symbol "$verbose" requires explicit package name (did you forget to declare "my $verbose"?) at tests/http2-server.pl line 52.
    Global symbol "$certfile" requires explicit package name (did you forget to declare "my $certfile"?) at tests/http2-server.pl line 109.
    Global symbol "$keyfile" requires explicit package name (did you forget to declare "my $keyfile"?) at tests/http2-server.pl line 110.
    Execution of tests/http2-server.pl aborted due to compilation errors.
    [...]
    ```
    Ref: https://github.com/curl/curl/actions/runs/16622030370/job/47028537336?pr=18099#step:39:3148
    
    Follow-up to 2ec54556d4e3f3ab551b5298adab0c703d85a463 #17877
    
    Closes #18100

--- tests/http3-server.pl
+++ tests/http3-server.pl
@@ -34,6 +34,7 @@ use Cwd 'abs_path';
 use File::Basename;
 use File::Spec;
 
+my $verbose = 0;     # set to 1 for debugging
 my $logdir = "log";
 my $pidfile = "$logdir/nghttpx.pid";
 my $logfile = "$logdir/http3.log";
@@ -106,8 +107,8 @@ while(@ARGV) {
     shift @ARGV;
 }
 
-$certfile = abs_path("certs/$cert.pem");
-$keyfile = abs_path("certs/$cert.key");
+my $certfile = abs_path("certs/$cert.pem");
+my $keyfile = abs_path("certs/$cert.key");
 
 my $cmdline="$nghttpx --http2-proxy --backend=$connect ".
     "--backend-keep-alive-timeout=500ms ".
