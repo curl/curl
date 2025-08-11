08cf30dc548796e77da4ce916af7a98767fd074b
Stefan Eissing <stefan@eissing.org>
2025-08-11 18:03:10 +0200

    trace if synch resolving is using signals or not

--- lib/hostip.c
+++ lib/hostip.c
@@ -1062,6 +1062,16 @@ CURLcode Curl_resolv_timeout(struct Curl_easy *data,
 
   *entry = NULL;
 
+#ifdef USE_ALARM_TIMEOUT
+  CURL_TRC_DNS(data, "starting resolve of %s:%d, timeout of %"
+               FMT_TIMEDIFF_T "ms",
+               hostname, port, timeoutms);
+#else
+  CURL_TRC_DNS(data, "starting resolve of %s:%d, no ALARM available,
+               "timeout of %" FMT_TIMEDIFF_T "ms will not apply",
+               hostname, port, timeoutms);
+#endif
+
   if(timeoutms < 0)
     /* got an already expired timeout */
     return CURLE_OPERATION_TIMEDOUT;
