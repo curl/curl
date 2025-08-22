d47dc0eadb3b744bb9fba4cb0e0070ec9fbcebb0
Viktor Szakats <commit@vsz.me>
2025-08-22 02:13:15 +0200

    more wolfssl CI

--- lib/vtls/wolfssl.c
+++ lib/vtls/wolfssl.c
@@ -336,7 +336,7 @@ static int wssl_bio_cf_out_write(WOLFSSL_BIO *bio,
   }
   result = Curl_conn_cf_send(cf->next, data, buf, blen, FALSE, &nwritten);
   wssl->io_result = result;
-  CURL_TRC_CF(data, cf, "bio_write(len=%d) -> %d, %zu",
+  CURL_TRC_CF(data, cf, "bio_write(len=%d) -> %u, %zu",
               blen, result, nwritten);
 #ifdef USE_FULL_BIO
   wolfSSL_BIO_clear_retry_flags(bio);
@@ -374,7 +374,7 @@ static int wssl_bio_cf_in_read(WOLFSSL_BIO *bio, char *buf, int blen)
      * server response. This allows sending of ClientHello without delay. */
     result = Curl_wssl_setup_x509_store(cf, data, wssl);
     if(result) {
-      CURL_TRC_CF(data, cf, "Curl_wssl_setup_x509_store() -> %d", result);
+      CURL_TRC_CF(data, cf, "Curl_wssl_setup_x509_store() -> %u", result);
       wssl->io_result = result;
       return -1;
     }
@@ -382,7 +382,7 @@ static int wssl_bio_cf_in_read(WOLFSSL_BIO *bio, char *buf, int blen)
 
   result = Curl_conn_cf_recv(cf->next, data, buf, blen, &nread);
   wssl->io_result = result;
-  CURL_TRC_CF(data, cf, "bio_read(len=%d) -> %d, %zu", blen, result, nread);
+  CURL_TRC_CF(data, cf, "bio_read(len=%d) -> %u, %zu", blen, result, nread);
 #ifdef USE_FULL_BIO
   wolfSSL_BIO_clear_retry_flags(bio);
 #endif
@@ -1688,7 +1688,7 @@ static CURLcode wssl_handshake(struct Curl_cfilter *cf,
      * store to verify the coming certificate from the server */
     result = Curl_wssl_setup_x509_store(cf, data, wssl);
     if(result) {
-      CURL_TRC_CF(data, cf, "Curl_wssl_setup_x509_store() -> %d", result);
+      CURL_TRC_CF(data, cf, "Curl_wssl_setup_x509_store() -> %u", result);
       return result;
     }
   }
@@ -1862,7 +1862,7 @@ static CURLcode wssl_send(struct Curl_cfilter *cf,
   }
 
 out:
-  CURL_TRC_CF(data, cf, "wssl_send(len=%zu) -> %d, %zu",
+  CURL_TRC_CF(data, cf, "wssl_send(len=%zu) -> %u, %zu",
               blen, result, *pnwritten);
   return result;
 }
