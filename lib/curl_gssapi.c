738821c988bbb1552d88b9fc4cf0311003084cbd
Viktor Szakats <commit@vsz.me>
2025-08-22 02:09:42 +0200

    more lib CI

--- lib/curl_gssapi.c
+++ lib/curl_gssapi.c
@@ -244,7 +244,7 @@ stub_gss_init_sec_context(OM_uint32 *min,
     }
 
     /* Token format: creds:target:type:padding */
-    used = msnprintf(token, length, "%s:%.*s:%d:", creds,
+    used = msnprintf(token, length, "%s:%.*s:%u:", creds,
                      (int)target_desc.length, (const char *)target_desc.value,
                      ctx->sent);
 
