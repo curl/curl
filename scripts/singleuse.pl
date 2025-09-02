db4e89d46ea53f2e11cb892a7c9b21b2cddf2f84
Stefan Eissing <stefan@eissing.org>
2025-09-02 15:19:34 +0200

    add Curl_multi_clear_dirty to singleuse as internal API

--- scripts/singleuse.pl
+++ scripts/singleuse.pl
@@ -56,6 +56,7 @@ my %wl = (
     'curlx_base64_decode' => 'internal api',
     'curlx_base64_encode' => 'internal api',
     'curlx_base64url_encode' => 'internal api',
+    'Curl_multi_clear_dirty' => 'internal api',
 );
 
 my %api = (
