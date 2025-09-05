4efc5ae96c1f4479bcf45bd5cef5fe7f95090be9
Viktor Szakats <commit@vsz.me>
2025-09-05 10:54:53 +0200

    http2-upload: avoid reserved namespace for local macro

--- docs/examples/http2-upload.c
+++ docs/examples/http2-upload.c
@@ -71,14 +71,14 @@ int my_gettimeofday(struct timeval *tp, void *tzp)
   (void)tzp;
   if(tp) {
     /* Offset between 1601-01-01 and 1970-01-01 in 100 nanosec units */
-    #define _WIN32_FT_OFFSET (116444736000000000)
+    #define WIN32_FT_OFFSET (116444736000000000)
     union {
       CURL_TYPEOF_CURL_OFF_T ns100; /* time since 1 Jan 1601 in 100ns units */
       FILETIME ft;
     } _now;
     GetSystemTimeAsFileTime(&_now.ft);
     tp->tv_usec = (long)((_now.ns100 / 10) % 1000000);
-    tp->tv_sec = (long)((_now.ns100 - _WIN32_FT_OFFSET) / 10000000);
+    tp->tv_sec = (long)((_now.ns100 - WIN32_FT_OFFSET) / 10000000);
   }
   return 0;
 }
