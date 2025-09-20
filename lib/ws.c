34467fdb52fa28084245b6ac26cfda3b9d4be70d
Daniel Stenberg <daniel@haxx.se>
2025-09-21 00:09:09 +0200

    ws: clarify an error message
    Instead of:
    
     "[WS] frame length longer than 64 signed not supported"
    
    Use:
    
     "[WS] frame length longer than 63 bit not supported"

--- lib/ws.c
+++ lib/ws.c
@@ -453,7 +453,7 @@ static CURLcode ws_dec_read_head(struct ws_decoder *dec,
       break;
     case 10:
       if(dec->head[2] > 127) {
-        failf(data, "[WS] frame length longer than 64 signed not supported");
+        failf(data, "[WS] frame length longer than 63 bit not supported");
         return CURLE_RECV_ERROR;
       }
       dec->payload_len = ((curl_off_t)dec->head[2] << 56) |
