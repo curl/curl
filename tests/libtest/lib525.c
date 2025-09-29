ea91445840a2703ac860341e77ed111059fa1824
Viktor Szakats <commit@vsz.me>
2025-09-30 01:32:54 +0200

    checksrc

--- tests/libtest/lib525.c
+++ tests/libtest/lib525.c
@@ -52,6 +52,7 @@ static CURLcode test_lib525(const char *URL)
 
   /* get the file size of the local file */
 #ifdef UNDER_CE
+  /* !checksrc! disable BANNEDFUNC 1 */
   hd = stat(libtest_arg2, &file_info);
 #else
   hd = fstat(fileno(hd_src), &file_info);
