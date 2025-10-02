8eb06f30072a3303d5db98461a25e4ba13abedd4
Viktor Szakats <commit@vsz.me>
2025-10-03 01:48:53 +0200

    chkspeed replace banned function

--- docs/examples/chkspeed.c
+++ docs/examples/chkspeed.c
@@ -100,8 +100,7 @@ int main(int argc, char *argv[])
         case 'm':
         case 'M':
           if(argv[0][2] == '=') {
-            /* !checksrc! disable BANNEDFUNC 1 */
-            long m = strtol((*argv) + 3, NULL, 10);
+            int m = atoi((*argv) + 3);
             switch(m) {
             case 1:
               url = URL_1M;
