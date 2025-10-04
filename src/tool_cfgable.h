bfd27ac0c83be29c763b4925dd43400394d18d29
Viktor Szakats <commit@vsz.me>
2025-10-04 12:24:57 +0200

    drop redefs

--- src/tool_cfgable.h
+++ src/tool_cfgable.h
@@ -39,24 +39,6 @@
 #endif
 #endif
 
-/* make the tool use the libcurl *printf family */
-# undef printf
-# undef fprintf
-# undef msnprintf
-# undef vprintf
-# undef vfprintf
-# undef mvsnprintf
-# undef aprintf
-# undef vaprintf
-# define printf curl_mprintf
-# define fprintf curl_mfprintf
-# define msnprintf curl_msnprintf
-# define vprintf curl_mvprintf
-# define vfprintf curl_mvfprintf
-# define mvsnprintf curl_mvsnprintf
-# define aprintf curl_maprintf
-# define vaprintf curl_mvaprintf
-
 #define checkprefix(a,b)    curl_strnequal(b, STRCONST(a))
 
 #define tool_safefree(ptr)                      \
