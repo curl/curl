e214b1450186538e1336aa1f9c7b90e197bed092
Daniel Stenberg <daniel@haxx.se>
2025-10-06 15:01:48 +0200

    telnet: send failure logged but not returned
    Return error correctly when sending fails.
    
    Reported-by: Joshua Rogers
    Closes #18887

--- lib/telnet.c
+++ lib/telnet.c
@@ -984,6 +984,7 @@ static CURLcode suboption(struct Curl_easy *data, struct TELNET *tn)
       if(bytes_written < 0) {
         err = SOCKERRNO;
         failf(data,"Sending data failed (%d)",err);
+        return CURLE_SEND_ERROR;
       }
       printsub(data, '>', &temp[2], len-2);
       break;
