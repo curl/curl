3049c8e0a0c31174a482dbfa46586aaed6ed9c3f
Viktor Szakats <commit@vsz.me>
2025-10-13 16:18:40 +0200

    examples: return `curl_easy_perform()` results
    Where missing. Or explicitly `(void)` it where we ignore it on purpose.
    
    Reported-by: Joshua Rogers (for `sepheaders.c`)
    
    Closes #19052

--- docs/examples/multithread.c
+++ docs/examples/multithread.c
@@ -57,7 +57,7 @@ static void *pull_one_url(void *pindex)
 
   curl = curl_easy_init();
   curl_easy_setopt(curl, CURLOPT_URL, urls[i]);
-  curl_easy_perform(curl); /* ignores error */
+  (void)curl_easy_perform(curl); /* ignores error */
   curl_easy_cleanup(curl);
 
   return NULL;
