ba58cfd6d4bd78c0456b39e2a145fc80c2ea8784
Stefan Eissing <stefan@eissing.org>
2025-10-13 11:32:17 +0200

    socks_sspi: use the correct free function
    When freeing buffers allocated by SSPI, use its own function, not free().
    
    reported-by: Joshua Rogers

--- lib/socks_sspi.c
+++ lib/socks_sspi.c
@@ -590,9 +590,13 @@ error:
     Curl_pSecFn->FreeContextBuffer(sspi_send_token.pvBuffer);
   if(names.sUserName)
     Curl_pSecFn->FreeContextBuffer(names.sUserName);
-  free(sspi_w_token[0].pvBuffer);
-  free(sspi_w_token[1].pvBuffer);
-  free(sspi_w_token[2].pvBuffer);
+#error xxx
+  if(sspi_w_token[0].pvBuffer)
+    Curl_pSecFn->FreeContextBuffer(sspi_w_token[0].pvBuffer);
+  if(sspi_w_token[1].pvBuffer)
+    Curl_pSecFn->FreeContextBuffer(sspi_w_token[1].pvBuffer);
+  if(sspi_w_token[2].pvBuffer)
+    Curl_pSecFn->FreeContextBuffer(sspi_w_token[2].pvBuffer);
   free(etbuf);
   return result;
 }
