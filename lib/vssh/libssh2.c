88e3fb0d81dc740445e6c6089cf44e935b97d248
Daniel Stenberg <daniel@haxx.se>
2025-10-13 09:36:08 +0200

    libssh2: fix EAGAIN return in ssh_state_auth_agent
    Reported-by: Joshua Rogers

--- lib/vssh/libssh2.c
+++ lib/vssh/libssh2.c
@@ -1850,8 +1850,9 @@ static CURLcode ssh_state_auth_agent(struct Curl_easy *data,
       if(rc != LIBSSH2_ERROR_EAGAIN) {
         /* tried and failed? go to next identity */
         sshc->sshagent_prev_identity = sshc->sshagent_identity;
+        return CURLE_OK;
       }
-      return CURLE_OK;
+      return CURLE_AGAIN;
     }
   }
 
