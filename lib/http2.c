d780b4bfc4f24b47c1d6fd8a4f7c716c43ce4836
Viktor Szakats <commit@vsz.me>
2025-10-15 04:02:13 +0200

    Revert "http2: check push header names by length first"
    This reverts commit 93e91e965e80b94b559293b93d4fb3360b8281a3.

--- lib/http2.c
+++ lib/http2.c
@@ -1590,8 +1590,7 @@ static int on_header(nghttp2_session *session, const nghttp2_frame *frame,
   if(frame->hd.type == NGHTTP2_PUSH_PROMISE) {
     char *h;
 
-    if((namelen == (sizeof(HTTP_PSEUDO_AUTHORITY)-1)) &&
-       !strncmp(HTTP_PSEUDO_AUTHORITY, (const char *)name, namelen)) {
+    if(!strcmp(HTTP_PSEUDO_AUTHORITY, (const char *)name)) {
       /* pseudo headers are lower case */
       int rc = 0;
       char *check = curl_maprintf("%s:%d", cf->conn->host.name,
