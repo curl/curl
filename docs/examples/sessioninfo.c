84349fe494a41f5d800795397b239141b652d8cc
Viktor Szakats <commit@vsz.me>
2025-10-15 04:02:12 +0200

    Revert "examples/sessioninfo: do not disable security"
    This reverts commit 56c892af1f46c7b1c803304ee62e524340007ab2.

--- docs/examples/sessioninfo.c
+++ docs/examples/sessioninfo.c
@@ -96,22 +96,25 @@ static size_t wrfu(void *ptr, size_t size, size_t nmemb, void *stream)
 
 int main(void)
 {
-  CURLcode res = CURLE_OK;
-
   curl_global_init(CURL_GLOBAL_DEFAULT);
 
   curl = curl_easy_init();
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "https://www.example.com/");
+
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, wrfu);
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
     curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
 
-    res = curl_easy_perform(curl);
+    (void)curl_easy_perform(curl);
 
     curl_easy_cleanup(curl);
   }
 
   curl_global_cleanup();
 
-  return (int)res;
+  return 0;
 }
