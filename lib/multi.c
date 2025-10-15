e4840041712ca93dc622e817959fd0de77770cb7
Emre Çalışkan <bilgi@emrecaliskan.com.tr>
2025-10-15 09:28:43 +0300

    refactored code state

--- lib/multi.c
+++ lib/multi.c
@@ -2373,14 +2373,6 @@ static CURLMcode multi_runsingle(struct Curl_multi *multi,
    * again during processing, triggering a re-run later. */
   Curl_uint_bset_remove(&multi->dirty, data->mid);
 
-  /* Reset the retry count at the start of each request.
-   * If the retry count is not reset, when the connection drops,
-   * it will not enter the retry mechanism on CONN_MAX_RETRIES + 1 attempts
-   * and will immediately throw
-   * "Connection died, tried CONN_MAX_RETRIES times before giving up".
-   * By resetting it here, we ensure each new request starts fresh. */
-  data->state.retrycount = 0;
-
   do {
     /* A "stream" here is a logical stream if the protocol can handle that
        (HTTP/2), or the full connection for older protocols */
