f247862b9ae7fc67970b1d53cd05299c5d6a8e7a
Daniel Stenberg <daniel@haxx.se>
2025-10-19 13:09:42 +0200

    curl_get_line: enhance the API
    To make sure callers can properly differentiate between errors and know
    cleanly when EOF happens. Updated all users and unit test 3200.
    
    Triggered by a remark by ZeroPath

--- lib/netrc.c
+++ lib/netrc.c
@@ -81,20 +81,25 @@ static NETRCcode file2memory(const char *filename, struct dynbuf *filebuf)
   curlx_dyn_init(&linebuf, MAX_NETRC_LINE);
 
   if(file) {
-    ret = NETRC_OK;
-    while(Curl_get_line(&linebuf, file)) {
-      CURLcode result;
-      const char *line = curlx_dyn_ptr(&linebuf);
-      /* skip comments on load */
-      curlx_str_passblanks(&line);
-      if(*line == '#')
-        continue;
-      result = curlx_dyn_add(filebuf, line);
+    CURLcode result = CURLE_OK;
+    bool eof;
+    do {
+      const char *line;
+      result = Curl_get_line(&linebuf, file, &eof);
+      if(!result) {
+        line = curlx_dyn_ptr(&linebuf);
+        /* skip comments on load */
+        curlx_str_passblanks(&line);
+        if(*line == '#')
+          continue;
+        result = curlx_dyn_add(filebuf, line);
+      }
       if(result) {
         ret = curl2netrc(result);
         goto done;
       }
-    }
+    } while(!eof);
+    ret = NETRC_OK;
   }
 done:
   curlx_dyn_free(&linebuf);
