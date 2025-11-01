fc7f0864c96c036ae4f536ad9ebf86fb2a7d3057
Viktor Szakats <commit@vsz.me>
2025-11-01 12:52:08 +0100

    tftp.c sp

--- lib/tftp.c
+++ lib/tftp.c
@@ -381,7 +381,7 @@ static CURLcode tftp_parse_option_ack(struct tftp_conn *state,
 static CURLcode tftp_option_add(struct tftp_conn *state, size_t *csize,
                                 char *buf, const char *option)
 {
-  if(( strlen(option) + *csize + 1) > (size_t)state->blksize)
+  if((strlen(option) + *csize + 1) > (size_t)state->blksize)
     return CURLE_TFTP_ILLEGAL;
   strcpy(buf, option);
   *csize += strlen(option) + 1;
