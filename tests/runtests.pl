3b64b75ecf05edf9b5cdc243974465495a170642
Viktor Szakats <commit@vsz.me>
2025-11-02 03:28:54 +0100

    stop parallelism (TEMP)

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -2454,7 +2454,7 @@ while(@ARGV) {
         $jobs=1;
         my $xtra = $1;
         if($xtra =~ s/(\d+)$//) {
-            $jobs = $1;
+            my $jobs_ignore = $1;
         }
     }
     elsif($ARGV[0] eq "-k") {
