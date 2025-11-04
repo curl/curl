d866f2145a7261653f7cb1cb3694a285236f290e
x2018 <xkernel.wang@foxmail.com>
2025-11-04 19:04:08 +0800

    tool_ipfs: minor change for code style

--- src/tool_ipfs.c
+++ src/tool_ipfs.c
@@ -176,6 +176,7 @@ CURLcode ipfs_url_rewrite(CURLU *uh, const char *protocol, char **url,
   }
 
   curl_url_get(gatewayurl, CURLUPART_PORT, &gwport, CURLU_URLDECODE);
+
   if(curl_url_get(gatewayurl, CURLUPART_PATH, &gwpath, CURLU_URLDECODE))
     goto clean;
 
@@ -192,7 +193,6 @@ CURLcode ipfs_url_rewrite(CURLU *uh, const char *protocol, char **url,
   /* if the input path is just a slash, clear it */
   if(inputpath && (inputpath[0] == '/') && !inputpath[1])
     *inputpath = '\0';
-
 
   pathbuffer = curl_maprintf("%s%s%s/%s%s", gwpath,
                              has_trailing_slash(gwpath) ? "" : "/",
