ee3a49b4334c1301179eed56e3effb81aec93dcd
Viktor Szakats <commit@vsz.me>
2025-11-10 00:32:02 +0100

    scripts: fix shellcheck SC2046 warnings
    Also:
    - shellcheck.sh: also add `set -eu`.
    - shellcheck.sh, yamlcheck.sh: run from repo root always.
    
    The yamllint.sh issue pointed out by Aisle,
    ref: 20251109163515_6eb31da3-deb2-4f4d-8327-935904f27da5

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -7,7 +7,7 @@ set -eu
 
 cd "$(dirname "${0}")"/../..
 
-# shellcheck disable=SC2046
+git ls-files -z | xargs -0 -r \
 codespell \
   --skip '.github/scripts/pyspelling.words' \
   --skip '.github/scripts/typos.toml' \
@@ -16,4 +16,4 @@ codespell \
   --skip 'scripts/wcurl' \
   --ignore-regex '.*spellchecker:disable-line' \
   --ignore-words '.github/scripts/codespell-ignore.words' \
-  $(git ls-files)
+  --
--- .github/scripts/shellcheck.sh
+++ .github/scripts/shellcheck.sh
@@ -3,7 +3,11 @@
 #
 # SPDX-License-Identifier: curl
 
-# shellcheck disable=SC2046
-shellcheck --exclude=1091 \
+set -eu
+
+cd "$(dirname "${0}")"/../..
+
+git grep -z -l -E '^#!(/usr/bin/env bash|/bin/sh|/bin/bash)' | xargs -0 -r \
+shellcheck --exclude=1091,2248 \
   --enable=avoid-nullary-conditions,deprecate-which \
-  $(grep -l -E '^#!(/usr/bin/env bash|/bin/sh|/bin/bash)' $(git ls-files))
+  --
