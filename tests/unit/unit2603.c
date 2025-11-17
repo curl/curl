bc79712d8f837bca0d0dc61482d796fd613a8d36
Stefan Eissing <stefan@eissing.org>
2025-11-17 10:03:11 +0100

    fix unit2603 for new signature

--- tests/unit/unit2603.c
+++ tests/unit/unit2603.c
@@ -74,7 +74,7 @@ static void parse_success(const struct tcase *t)
     buflen = strlen(buf);
     in_len += buflen;
     nread = Curl_h1_req_parse_read(&p, buf, buflen, t->default_scheme,
-                                   0, &err);
+                                   NULL, 0, &err);
     if(nread < 0) {
       curl_mfprintf(stderr, "got err %d parsing: '%s'\n", err, buf);
       fail("error consuming");
