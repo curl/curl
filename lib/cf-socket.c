01e39e35648406258a73b38182b4a5e6354d62e1
Viktor Szakats <commit@vsz.me>
2025-11-17 01:21:50 +0100

    split CURL_WINSOCK_KEEP_SSO from KEEPALIVE_FACTOR logic

--- lib/cf-socket.c
+++ lib/cf-socket.c
@@ -154,8 +154,9 @@ static void nosigpipe(struct Curl_cfilter *cf,
 /*  Win 10, v 1709 (10.0.16299) and later can use SetSockOpt TCP_KEEP____
  *  so should use seconds */
 #define CURL_WINSOCK_KEEP_SSO
-#define KEEPALIVE_FACTOR(x)
-#elif defined(USE_WINSOCK) || \
+#endif
+
+#if defined(USE_WINSOCK) || \
    (defined(__sun) && !defined(TCP_KEEPIDLE)) || \
    (defined(__DragonFly__) && __DragonFly_version < 500702) || \
    (defined(_WIN32) && !defined(TCP_KEEPIDLE))
