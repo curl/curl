8bb8984e9d5882a9b3f92717441a9ab92f33ace5
Viktor Szakats <commit@vsz.me>
2025-11-19 01:36:28 +0100

    curl_setup.h: document more funcs flagged by `_CRT_SECURE_NO_WARNINGS`
    Based on these logs (non-Unicode, Unicode Schannel):
    https://github.com/curl/curl/actions/runs/19446115443/job/55640968722?pr=19175
    https://github.com/curl/curl/actions/runs/19446115443/job/55640968764?pr=19175
    
    Follow-up to 5fa2d8320c4196435c1d554b06dfdcca73824dec #19175
    
    Closes #19597

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -95,9 +95,10 @@
                                       unlink(), etc. */
 #endif
 #ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS  /* for __sys_errlist, __sys_nerr, _wfopen(),
-                                    _wopen(), freopen(), getenv(), gmtime(),
-                                    sprintf(), strcpy(), wcscpy(), wcsncpy()
+#define _CRT_SECURE_NO_WARNINGS  /* for __sys_errlist, __sys_nerr, _open(),
+                                    _wfopen(), _wopen(), fopen(), freopen(),
+                                    getenv(), gmtime(), mbstowcs(), sprintf(),
+                                    strcpy(), wcscpy(), wcsncpy(), wcstombs(),
                                     in tests: localtime(), open(), sscanf() */
 #endif
 #endif /* _MSC_VER */
