8e6ba780460bf4526b46551efc14eaa663e1f5fb
Viktor Szakats <commit@vsz.me>
2025-11-27 13:30:17 +0100

    dbg 3

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -322,10 +322,13 @@ else {
     logmsg "TRACE-1: " . $ENV{"NGHTTPX"} . "\n";
 }
 if($ENV{"NGHTTPX"}) {
+    my $cmdd = "\"$ENV{'NGHTTPX'}\" -v 2>&1";
     my $cmd = "\"$ENV{'NGHTTPX'}\" -v 2>$dev_null";
     logmsg "TRACE-3: '$cmd'\n";
     my $nghttpx_version=join(' ', `$cmd`);
     logmsg "TRACE-4: '$nghttpx_version'\n";
+    my $nghttpx_versiond=join(' ', `$cmd`);
+    logmsg "TRACE-5: '$nghttpx_versiond'\n";
     $nghttpx_h3 = $nghttpx_version =~ /nghttp3\//;
     chomp $nghttpx_h3;
 }
