6010d734dcbdad4d0f3746b40fee31edf787706b
Viktor Szakats <commit@vsz.me>
2025-11-27 23:48:42 +0100

    try sth 4

--- appveyor.sh
+++ appveyor.sh
@@ -46,17 +46,13 @@ if [ "${BUILD_SYSTEM}" = 'CMake' ]; then
      [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2015' ] || \
      [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2017' ]; then
     (
-      pwd
-      echo "|$PWD|"
-      curl --version || true
       curl --disable --fail --verbose --show-error --connect-timeout 15 --max-time 60 --retry 3 --retry-connrefused \
         --location 'https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-win64-x64.zip' --output bin.zip
       ls -l
-      unzip bin.zip
+      unzip -v bin.zip
       rm -f bin.zip
-      pwd
     )
-    PATH="/c/projects/curl/cmake-3.18.4-win64-x64/bin:$PATH"
+    PATH="$PWD/cmake-3.18.4-win64-x64/bin:$PATH"
   fi
 fi
 
