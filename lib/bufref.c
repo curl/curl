d5c3b35c0dbceaebb0d90b240556f8550f637df0
Juan Belon|EpicGames <juan.belon@xa.epicgames.com>
2025-11-28 11:24:40 +0000

    Add optional external assertion handler for debug builds

--- lib/bufref.c
+++ lib/bufref.c
@@ -29,6 +29,7 @@
 
 #ifdef DEBUGBUILD
 #define SIGNATURE 0x5c48e9b2    /* Random pattern. */
+curl_assert_callback curl_global_assert_handler = NULL; /* External assertion handler for custom integrations */
 #endif
 
 /*
