8a55c7823d3970f2e1280325822613394b5aeab6
Viktor Szakats <commit@vsz.me>
2025-12-04 00:36:21 +0100

    verify

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -1799,11 +1799,9 @@ sub singletest_check {
                     $lim_max = $1;
                 }
             }
-            logmsg "did $allocs allocations, $lim_allocs allowed\n"
-                if($verbose);
+            logmsg "did $allocs allocations, $lim_allocs allowed\n";
 
-            logmsg "allocated $max maximum, $lim_max allowed\n"
-                if($verbose);
+            logmsg "allocated $max maximum, $lim_max allowed\n";
 
             if($allocs > $lim_allocs) {
                 logmsg "\n** TOO MANY ALLOCS\n";
