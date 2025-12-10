0295c9401d5bfe8bd490c3f14fecda54c6032fce
Viktor Szakats <commit@vsz.me>
2025-12-10 15:37:44 +0100

    ldap: drop PP logic for old, unsupported, Windows SDKs
    `LDAP_VENDOR_NAME` and `winber.h` are available in all supported
    MS SDK and mingw-w64 versions. Stop checking for them.
    
    Also drop redundant parenthesis in PP expression.
    
    Closes #19918

--- lib/ldap.c
+++ lib/ldap.c
@@ -64,19 +64,14 @@
 #  pragma warning(pop)
 # endif
 # include <winldap.h>
-# ifndef LDAP_VENDOR_NAME
-#  error Your Platform SDK is NOT sufficient for LDAP support! \
-         Update your Platform SDK, or disable LDAP support!
-# else
-#  include <winber.h>
-# endif
+# include <winber.h>
 #else
 # define LDAP_DEPRECATED 1      /* Be sure ldap_init() is defined. */
 # ifdef HAVE_LBER_H
 #  include <lber.h>
 # endif
 # include <ldap.h>
-# if (defined(HAVE_LDAP_SSL) && defined(HAVE_LDAP_SSL_H))
+# if defined(HAVE_LDAP_SSL) && defined(HAVE_LDAP_SSL_H)
 #  include <ldap_ssl.h>
 # endif /* HAVE_LDAP_SSL && HAVE_LDAP_SSL_H */
 #endif
