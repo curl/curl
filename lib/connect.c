4dea3914840254120df49ebdb35aab9036eef872
Viktor Szakats <commit@vsz.me>
2025-12-16 03:34:06 +0100

    Revert "test if enabled"
    This reverts commit f6bccc68f59a9b9f7fb38045117c0d12783391a6.
    
    Works. Disabled in CI jobs:
    
    - curl-for-win / Linux gcc glibc minimal (pull_request)
    - macOS / AM clang !ssl HTTP-only (pull_request)
    - Windows / dl-mingw, CM 15.1.0-x86_64 schannel (pull_request)

--- lib/connect.c
+++ lib/connect.c
@@ -244,7 +244,6 @@ bool Curl_addr2string(struct sockaddr *sa, curl_socklen_t salen,
   struct sockaddr_un *su = NULL;
 #else
   (void)salen;
-#warning "connect.c: !USE_UNIX_SOCKETS"
 #endif
 
   switch(sa->sa_family) {
