89cc0dad9ff869e644f5892effb2949101bab170
Viktor Szakats <commit@vsz.me>
2025-12-19 02:09:15 +0100

    curl_range.c drop sendf.h

--- lib/curl_range.c
+++ lib/curl_range.c
@@ -25,7 +25,6 @@
 #include "curl_setup.h"
 #include <curl/curl.h>
 #include "curl_range.h"
-#include "sendf.h"
 #include "curlx/strparse.h"
 
 /* Only include this function if one or more of FTP, FILE are enabled. */
