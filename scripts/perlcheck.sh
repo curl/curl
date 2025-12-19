9b40e919239bbe0858c6bd5a9e022a7663c0828b
Viktor Szakats <commit@vsz.me>
2025-12-19 15:50:01 +0100

    perlcheck.sh sort always

--- scripts/perlcheck.sh
+++ scripts/perlcheck.sh
@@ -40,11 +40,9 @@ echo "parallel: ${procs}"
   if [ -n "${1:-}" ]; then
     for A in "$@"; do printf '%s\n' "$A"; done
   elif git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    {
-      git ls-files | grep -E '\.(pl|pm)$'
-      git grep -l '^#!/usr/bin/env perl'
-    } | sort -u
+    git ls-files '*.pl' '*.pm'
+    git grep -l '^#!/usr/bin/env perl'
   else
     find . -type f \( -name '*.pl' -o -name '*.pm' \)
   fi
-} | xargs -n 1 -P "${procs}" perl -c -Itests --
+} | sort -u | xargs -n 1 -P "${procs}" perl -c -Itests --
