c0d8fedb49a31b265adc7c904873a2300ded9260
Viktor Szakats <commit@vsz.me>
2025-12-20 04:41:02 +0100

    tool_urlglob: constify an argument
    Also: add argument names to prototypes.
    
    Closes #20045

--- src/tool_urlglob.h
+++ src/tool_urlglob.h
@@ -71,9 +71,11 @@ struct URLGlob {
   size_t pos;        /* column position of error or 0 */
 };
 
-CURLcode glob_url(struct URLGlob *, char *, curl_off_t *, FILE *);
-CURLcode glob_next_url(char **, struct URLGlob *);
-CURLcode glob_match_url(char **, const char *, struct URLGlob *);
+CURLcode glob_url(struct URLGlob *glob, const char *url, curl_off_t *urlnum,
+                  FILE *error);
+CURLcode glob_next_url(char **globbed, struct URLGlob *glob);
+CURLcode glob_match_url(char **output, const char *filename,
+                        struct URLGlob *glob);
 void glob_cleanup(struct URLGlob *glob);
 bool glob_inuse(struct URLGlob *glob);
 
