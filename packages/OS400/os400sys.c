9266c3c0221a25a848307693cb3372a2dbc72a18
Viktor Szakats <commit@vsz.me>
2025-12-24 01:18:22 +0100

    null terminator no longer necessary in the source buffer

--- packages/OS400/os400sys.c
+++ packages/OS400/os400sys.c
@@ -705,7 +705,6 @@ char *Curl_ldap_get_dn_a(void *ld, LDAPMessage *entry)
     return cp2;
 
   QadrtConvertE2A(cp2, cp, i, i);
-  cp2[i] = '\0';
 
   /* No way to allocate a buffer here, because it will be released by
      ldap_memfree() and ldap_memalloc() does not exist. The solution is to
@@ -735,7 +734,6 @@ char *Curl_ldap_first_attribute_a(void *ld, LDAPMessage *entry,
     return cp2;
 
   QadrtConvertE2A(cp2, cp, i, i);
-  cp2[i] = '\0';
 
   /* No way to allocate a buffer here, because it will be released by
      ldap_memfree() and ldap_memalloc() does not exist. The solution is to
@@ -765,7 +763,6 @@ char *Curl_ldap_next_attribute_a(void *ld, LDAPMessage *entry,
     return cp2;
 
   QadrtConvertE2A(cp2, cp, i, i);
-  cp2[i] = '\0';
 
   /* No way to allocate a buffer here, because it will be released by
      ldap_memfree() and ldap_memalloc() does not exist. The solution is to
