deaef0a71e3a771e0e867d76ecfb178ff5cdb7e5
Viktor Szakats <commit@vsz.me>
2025-12-31 14:35:20 +0100

    openssl: fix building against no-ocsp openssl with Apple SecTrust
    Fixing:
    ```
    lib/vtls/openssl.c:4750:8: error: variable 'sectrust_verified' set but not used [-Werror,-Wunused-but-set-variable]
     4750 |   bool sectrust_verified = FALSE;
          |        ^
    1 error generated.
    ```

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -4744,7 +4744,8 @@ CURLcode Curl_ossl_check_peer_cert(struct Curl_cfilter *cf,
   long ossl_verify;
   X509 *server_cert;
   bool verified = FALSE;
-#ifdef USE_APPLE_SECTRUST
+#if !defined(OPENSSL_NO_TLSEXT) && !defined(OPENSSL_NO_OCSP) && \
+  defined(USE_APPLE_SECTRUST)
   bool sectrust_verified = FALSE;
 #endif
 
@@ -4799,7 +4800,9 @@ CURLcode Curl_ossl_check_peer_cert(struct Curl_cfilter *cf,
     if(verified) {
       infof(data, "SSL certificate verified via Apple SecTrust.");
       ssl_config->certverifyresult = X509_V_OK;
+#if !defined(OPENSSL_NO_TLSEXT) && !defined(OPENSSL_NO_OCSP)
       sectrust_verified = TRUE;
+#endif
     }
   }
 #endif
