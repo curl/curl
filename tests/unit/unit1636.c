c27adb9f777a30d343ff268e6b4cc24f78d8a5f5
Daniel Stenberg <daniel@haxx.se>
2026-01-03 12:16:06 +0100

    progress: narrower time display, multiple fixes
    - Each time field is now 7 characters wide, so that the total width
      never exceeds 79 columns so that it works correctly also in Windows
      terminals. The title lines are adjusted accordingly.
    
      This is accomplished by using h:mm:ss style up to 10 hours, and for
      longer periods switch to "nnX nnY" style output. For hours, days,
      months and years.
    
      For less than one hour, the hour field is now dropped.
    
      When no time info is provided, the field is now space-only. No more
      `-:--:--`.
    
      Also fixed the output for really long times which previously was
      completely broken. The largest time now shows as ">99999y". (Becase
      I can't figure out a better way).
    
    - For sizes, the widths are now properly fixed to 6 characters. When
      displaying a unit with less than 3 digits, it shows two decimal
      precision like "16777215 => 15.99M" and one decmal otherwise: "262143
      => 255.9k"
    
      Also fixes the decimal math. 131071 is 127.9k, which it previously did
      not show.
    
    - The time and size field outputs are now properly verified in test
      1636.
    
    Fixes #20122
    Closes #20173
    
    fixup use only space when no time exists
    
    Drop the hour from the display when zero

---
+++ tests/unit/unit1636.c
@@ -0,0 +1,74 @@
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ * SPDX-License-Identifier: curl
+ *
+ ***************************************************************************/
+#include "unitcheck.h"
+#include "unitprotos.h"
+
+static CURLcode test_unit1636(const char *arg)
+{
+  UNITTEST_BEGIN_SIMPLE
+  {
+    char buffer[9];
+    curl_off_t secs;
+    int i;
+    static const curl_off_t check[] = {
+      /* bytes to check */
+      131072,
+      12645826,
+      1073741824,
+      12938588979,
+      1099445657078333,
+      0 /* end of list */
+    };
+
+    puts("time2str");
+    for(i = 0, secs = 0; i < 63; i++) {
+      time2str(buffer, sizeof(buffer), secs);
+      curl_mprintf("%20" FMT_OFF_T " - %s\n", secs, buffer);
+      if(strlen(buffer) != 7) {
+        curl_mprintf("^^ was too long!\n");
+      }
+      secs *= 2;
+      secs++;
+    }
+    puts("max6out");
+    for(i = 0, secs = 0; i < 63; i++) {
+      max6out(secs, buffer, sizeof(buffer));
+      curl_mprintf("%20" FMT_OFF_T " - %s\n", secs, buffer);
+      if(strlen(buffer) != 6) {
+        curl_mprintf("^^ was too long!\n");
+      }
+      secs *= 2;
+      secs++;
+    }
+    for(i = 0; check[i]; i++) {
+      secs = check[i];
+      max6out(secs, buffer, sizeof(buffer));
+      curl_mprintf("%20" FMT_OFF_T " - %s\n", secs, buffer);
+      if(strlen(buffer) != 6) {
+        curl_mprintf("^^ was too long!\n");
+      }
+    }
+  }
+  UNITTEST_END(curl_global_cleanup())
+}
