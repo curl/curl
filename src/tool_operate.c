0b78df6dc7e784ad8f4ebef9a8ec7381aa3d78a0
Viktor Szakats <commit@vsz.me>
2026-01-12 12:20:30 +0100

    try sth else 4 (src)
    src\tool_cb_wrt.c(247,23): error C2220: the following warning is treated as an error
    src\tool_cb_wrt.c(247,23): warning C4242: 'initializing': conversion from 'bit' to 'bool', possible loss of data
    src\tool_operate.c(1141,32): warning C4242: 'initializing': conversion from 'bit' to 'bool', possible loss of data
    src\tool_operate.c(1142,28): warning C4242: 'initializing': conversion from 'bit' to 'bool', possible loss of data
    src\tool_operate.c(2148,32): warning C4242: 'initializing': conversion from 'bit' to 'bool', possible loss of data
    src\tool_operate.c(2149,28): warning C4242: 'initializing': conversion from 'bit' to 'bool', possible loss of data
    
    https://ci.appveyor.com/project/curlorg/curl/builds/53311787/job/yfpcwxuuyl2qq14v

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -1166,8 +1166,8 @@ static CURLcode create_single(struct OperationConfig *config,
                               CURLSH *share, struct State *state,
                               bool *added, bool *skipped)
 {
-  const bool orig_isatty = global->isatty;
-  const bool orig_noprogress = global->noprogress;
+  const bool orig_isatty = (bool)global->isatty;
+  const bool orig_noprogress = (bool)global->noprogress;
   CURLcode result = CURLE_OK;
   while(state->urlnode) {
     struct per_transfer *per = NULL;
@@ -2174,8 +2174,8 @@ static CURLcode run_all_transfers(CURLSH *share,
                                   CURLcode result)
 {
   /* Save the values of noprogress and isatty to restore them later on */
-  bool orig_noprogress = global->noprogress;
-  bool orig_isatty = global->isatty;
+  bool orig_noprogress = (bool)global->noprogress;
+  bool orig_isatty = (bool)global->isatty;
   struct per_transfer *per;
 
   /* Time to actually do the transfers */
