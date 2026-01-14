a4dc70ac2725f8ad9f4ac02bed280f129717b318
Viktor Szakats <commit@vsz.me>
2026-01-14 17:18:57 +0100

    cw-pause.c avoid fwd decls

--- lib/cw-pause.c
+++ lib/cw-pause.c
@@ -70,23 +70,6 @@ struct cw_pause_ctx {
   size_t buf_total;
 };
 
-static CURLcode cw_pause_write(struct Curl_easy *data,
-                               struct Curl_cwriter *writer, int type,
-                               const char *buf, size_t nbytes);
-static void cw_pause_close(struct Curl_easy *data,
-                           struct Curl_cwriter *writer);
-static CURLcode cw_pause_init(struct Curl_easy *data,
-                              struct Curl_cwriter *writer);
-
-const struct Curl_cwtype Curl_cwt_pause = {
-  "cw-pause",
-  NULL,
-  cw_pause_init,
-  cw_pause_write,
-  cw_pause_close,
-  sizeof(struct cw_pause_ctx)
-};
-
 static CURLcode cw_pause_init(struct Curl_easy *data,
                               struct Curl_cwriter *writer)
 {
@@ -219,6 +202,15 @@ static CURLcode cw_pause_write(struct Curl_easy *data,
 
   return result;
 }
+
+const struct Curl_cwtype Curl_cwt_pause = {
+  "cw-pause",
+  NULL,
+  cw_pause_init,
+  cw_pause_write,
+  cw_pause_close,
+  sizeof(struct cw_pause_ctx)
+};
 
 CURLcode Curl_cw_pause_flush(struct Curl_easy *data)
 {
