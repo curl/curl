606fa31d54ac1572add9da346ac4d49ba9888f0c
Viktor Szakats <commit@vsz.me>
2026-01-14 10:13:56 +0100

    tool_doswin.c drop unnecessary fwd decl

--- src/tool_doswin.c
+++ src/tool_doswin.c
@@ -58,9 +58,6 @@
 #  endif
 #endif
 
-/* only used by msdosify() */
-static SANITIZEcode truncate_dryrun(const char *path,
-                                    const size_t truncate_pos);
 static SANITIZEcode msdosify(char ** const sanitized, const char *file_name,
                              int flags);
 #endif /* MSDOS */
