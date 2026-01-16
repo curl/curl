9ad3c233a1b613ccbd3d578e3998219acdf52c57
Viktor Szakats <commit@vsz.me>
2026-01-16 18:13:44 +0100

    md4/md5 sync a comment block

--- lib/md4.c
+++ lib/md4.c
@@ -179,8 +179,8 @@ static void MD4_Final(unsigned char *result, MD4_CTX *ctx)
  * claimed, and the software is hereby placed in the public domain. In case
  * this attempt to disclaim copyright and place the software in the public
  * domain is deemed null and void, then the software is Copyright (c) 2001
- * Alexander Peslyak and it is hereby released to the general public under the
- * following terms:
+ * Alexander Peslyak and it is hereby released to the general public under
+ * the following terms:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted.
