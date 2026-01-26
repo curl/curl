5f7438082fdf58b4dadae237415cca7ea64e2f7c
Viktor Szakats <commit@vsz.me>
2026-01-26 02:48:39 +0100

    tool_setopt.c drop parents from return expr

--- src/tool_setopt.c
+++ src/tool_setopt.c
@@ -707,6 +707,6 @@ CURLcode tool_setopt_str(CURL *curl, struct OperationConfig *config,
 /* return TRUE if the error code is "lethal" */
 bool setopt_bad(CURLcode result)
 {
-  return (result && (result != CURLE_NOT_BUILT_IN) &&
-          (result != CURLE_UNKNOWN_OPTION));
+  return result && (result != CURLE_NOT_BUILT_IN) &&
+         (result != CURLE_UNKNOWN_OPTION);
 }
