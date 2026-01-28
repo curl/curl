55f2640ef3aade3df84d9aad205ba4d78867edda
Viktor Szakats <commit@vsz.me>
2026-01-28 05:11:49 +0100

    getreferrer.c fix formatting

--- docs/examples/ghiper.c
+++ docs/examples/ghiper.c
@@ -158,8 +158,8 @@ static gboolean timer_cb(gpointer data)
   struct GlobalInfo *g = (struct GlobalInfo *)data;
   CURLMcode mresult;
 
-  mresult = curl_multi_socket_action(g->multi,
-                                CURL_SOCKET_TIMEOUT, 0, &g->still_running);
+  mresult = curl_multi_socket_action(g->multi, CURL_SOCKET_TIMEOUT, 0,
+                                     &g->still_running);
   mcode_or_die("timer_cb: curl_multi_socket_action", mresult);
   check_multi_info(g);
   return FALSE;
