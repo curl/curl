a79df8e8ca3b65578b4e6d7b44d829ef7938dac7
Viktor Szakats <commit@vsz.me>
2026-02-01 01:39:35 +0100

    simplify mappings

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -490,22 +490,22 @@
 #  include <sys/types.h>
 #  include <sys/stat.h>
    /* Large file (>2Gb) support using Win32 functions. */
-#  define curl_lseek(fdes, offset, whence)  _lseeki64(fdes, offset, whence)
+#  define curl_lseek                      _lseeki64
 #  undef  fstat
-#  define fstat(fdes, stp)                  _fstati64(fdes, stp)
-#  define struct_stat                       struct _stati64
-#  define LSEEK_ERROR                       (__int64)-1
+#  define fstat(fdes, stp)                _fstati64(fdes, stp)
+#  define struct_stat                     struct _stati64
+#  define LSEEK_ERROR                     (__int64)-1
 #elif defined(__DJGPP__)
    /* Requires DJGPP 2.04 */
 #  include <unistd.h>
-#  define curl_lseek(fdes, offset, whence)  llseek(fdes, offset, whence)
-#  define LSEEK_ERROR                       (offset_t)-1
+#  define curl_lseek                      llseek
+#  define LSEEK_ERROR                     (offset_t)-1
 #elif defined(__AMIGA__)
-#  define curl_lseek(fdes, offset, whence)  lseek(fdes, (off_t)(offset), whence)
-#  define LSEEK_ERROR                       (off_t)-1
+#  define curl_lseek(fd, offset, whence)  lseek(fd, (off_t)(offset), whence)
+#  define LSEEK_ERROR                     (off_t)-1
 #else
-#  define curl_lseek(fdes, offset, whence)  lseek(fdes, offset, whence)
-#  define LSEEK_ERROR                       (off_t)-1
+#  define curl_lseek                      lseek
+#  define LSEEK_ERROR                     (off_t)-1
 #endif
 
 #ifndef struct_stat
