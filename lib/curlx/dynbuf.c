5bdbad87c5fbfa2bdf65ff41eda409cfb37fe4f0
Viktor Szakats <commit@vsz.me>
2026-02-03 14:10:02 +0100

    curlx: drop unused `curlx_saferealloc()`
    Unused since 67ae101666f1023263c475b88b266bc9b5e658c0 #19949
    
    Closes #20504

--- lib/curlx/dynbuf.c
+++ lib/curlx/dynbuf.c
@@ -102,8 +102,6 @@ static CURLcode dyn_nappend(struct dynbuf *s,
   }
 
   if(a != s->allc) {
-    /* this logic is not using curlx_saferealloc() to make the tool not have to
-       include that as well when it uses this code */
     void *p = curlx_realloc(s->bufr, a);
     if(!p) {
       curlx_dyn_free(s);
--- lib/curlx/strdup.h
+++ lib/curlx/strdup.h
@@ -32,5 +32,4 @@ char *curlx_strdup_low(const char *str);
 #endif
 void *curlx_memdup(const void *src, size_t buffer_length);
 void *curlx_memdup0(const char *src, size_t length);
-void *curlx_saferealloc(void *ptr, size_t size);
 #endif /* HEADER_CURLX_STRDUP_H */
