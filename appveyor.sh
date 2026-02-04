7482a8bc8ab76caa2ff19a99e0adafc2802ede7f
Viktor Szakats <commit@vsz.me>
2026-02-04 15:50:25 +0100

    verify

--- appveyor.sh
+++ appveyor.sh
@@ -59,7 +59,7 @@ if [ -n "${CMAKE_GENERATOR:-}" ]; then
     fi
     curl --disable --fail --silent --show-error --connect-timeout 15 --max-time 60 --retry 3 --retry-connrefused \
       --location "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${fn}.zip" --output bin.zip
-    openssl dgst -sha256 bin.zip
+    openssl dgst -sha256 bin.zip | tee /dev/stderr | grep -q -a -F -w -- "${CMAKE_SHA256}"
     7z x -y bin.zip >/dev/null
     rm -f bin.zip
     PATH="$PWD/${fn}/bin:$PATH"
