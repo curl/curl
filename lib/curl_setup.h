8f07bbd1bd6b3d94ea864aa1d67002b46db701d9
Stefan Eissing <stefan@eissing.org>
2026-02-06 12:52:30 +0100

    thread pools
    Add curl_thrdpool and associated method for managing a pool
    of threads that can resize between min/max number of threads
    waiting for "work" to "process".
    
    Threads above the min number will exit after an idle timeout.
    A pool can be waited for to become idle, e.g. having
    processed everything.
    A pool can be destroyed either by joining all threads or
    detaching them. In the later case the pool's memory will be
    released when the last thread exits.
    
    Add unit3217 test to verify thread pools operation.
    
    Other adjustments:
    * curl_setup.h: define `USE_THREADS` when either POSIX or
      WINDOWS threads are configured for simpler ifdefs
    * memdebug.c: in atexit handle, inspect global var under lock.
      Otherwise thread sanitizer will report unsynchronized data
      access

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -676,6 +676,10 @@
 #endif
 
 #if defined(USE_THREADS_POSIX) || defined(USE_THREADS_WIN32)
+#define USE_THREADS
+#endif
+
+#ifdef USE_THREADS
 #  define CURLRES_ASYNCH
 #  define CURLRES_THREADED
 #elif defined(USE_ARES)
