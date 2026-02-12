70faa9952bc4dc10b4ba90afa77206fbbde13cda
Viktor Szakats <commit@vsz.me>
2026-02-12 02:35:19 +0100

    mention libressl

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -39,6 +39,7 @@
  *  typedef struct X509_name_st X509_NAME;
  *  etc.
  * OpenSSL has the same, but only in <openssl/ssl.h> and <openssl/x509*.h>.
+ * LibreSSL automatically undefines these symbols.
  *
  * this C-preprocessed symbols break these OpenSSL headers.
  * The workaround is to undefine those defines here (and only here).
