bb123b407bf3c1f30dc3f7d4130a437537ac52e8
Viktor Szakats <commit@vsz.me>
2026-02-24 20:47:29 +0100

    try avoiding codespell hits

--- scripts/config-since.sh
+++ scripts/config-since.sh
@@ -13,25 +13,25 @@ cd "$(dirname "$0")"/..
 
 prevtag="${1:-curl-8_18_0}"
 
-fn='lib/curl_config-cmake.h.in'
-fo="_config-at-${prevtag}"
+fnew='lib/curl_config-cmake.h.in'
+fold="_config-at-${prevtag}"
 
-if ! git show "${prevtag}:${fn}" > "${fo}" 2>/dev/null; then
+if ! git show "${prevtag}:${fnew}" > "${fold}" 2>/dev/null; then
   # for compatibility with 8.17.0 and earlier
-  if ! git show "${prevtag}:lib/curl_config.h.cmake" > "${fo}"; then
-    rm -f "${fo:?}"
+  if ! git show "${prevtag}:lib/curl_config.h.cmake" > "${fold}"; then
+    rm -f "${fold:?}"
     exit 1
   fi
 fi
 
-fot="$(mktemp)"
-fnt="$(mktemp)"
+foldd="$(mktemp)"
+fnewd="$(mktemp)"
 
-grep -E '(cmakedefine|{SIZEOF)' "${fo}" | sed -E -e 's/cmake//g' -e 's/ +1//g' | sort > "${fot}"
-grep -E '(cmakedefine|{SIZEOF)' "${fn}" | sed -E -e 's/cmake//g' -e 's/ +1//g' | sort > "${fnt}"
+grep -E '(cmakedefine|{SIZEOF)' "${fold}" | sed -E -e 's/cmake//g' -e 's/ +1//g' | sort > "${foldd}"
+grep -E '(cmakedefine|{SIZEOF)' "${fnew}" | sed -E -e 's/cmake//g' -e 's/ +1//g' | sort > "${fnewd}"
 
 echo "New settings at current Git commit since ${prevtag}:"
-diff -u "${fot}" "${fnt}" | tail -n +3 | grep '^+' || true
+diff -u "${foldd}" "${fnewd}" | tail -n +3 | grep '^+' || true
 echo '---'
 
-rm -rf "${fot:?}" "${fnt:?}" "${fo:?}"
+rm -rf "${foldd:?}" "${fnewd:?}" "${fold:?}"
