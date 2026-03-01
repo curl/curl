4facb753c94d68b751d3c85e0bc3273c149cf943
Viktor Szakats <commit@vsz.me>
2026-03-01 02:07:36 +0100

    src update a comment

--- src/tool_setup.h
+++ src/tool_setup.h
@@ -50,7 +50,7 @@ extern FILE *tool_stderr;
 #include "curlx/fopen.h" /* for curlx_f*() */
 #include "curlx/multibyte.h" /* for curlx_convert_*() */
 #include "curlx/strcopy.h" /* for curlx_strcopy() */
-#include "curlx/strdup.h" /* for curlx_memdup*() and curlx_tcsdup() */
+#include "curlx/strdup.h" /* for curlx_memdup*() */
 #include "curlx/strerr.h" /* for curlx_strerror() */
 #include "curlx/strparse.h" /* for curlx_str_* parsing functions */
 #include "curlx/timediff.h" /* for timediff_t type and related functions */
