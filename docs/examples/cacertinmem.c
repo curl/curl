38ee35353c1db18343817aa62ffb98aa966fd12a
Viktor Szakats <commit@vsz.me>
2026-03-03 18:44:29 +0100

    examples: improve OpenSSL certificate examples
    - add/fix/synchronize error messages and comments.
    - consistently return error from the callback on failure.
    - fix potential leaks on OpenSSL API failures.
    - fix to not pass the nul-terminator to BIO read.
    - scope a variable.
    - sync code/formatting between the two examples.
    
    Closes #20807

--- docs/examples/cacertinmem.c
+++ docs/examples/cacertinmem.c
@@ -79,23 +79,31 @@ static CURLcode sslctx_function(CURL *curl, void *sslctx, void *pointer)
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"
     "-----END CERTIFICATE-----\n";
 
-  BIO *cbio = BIO_new_mem_buf(mypem, sizeof(mypem));
-  X509_STORE *cts = SSL_CTX_get_cert_store((SSL_CTX *)sslctx);
+  CURLcode result = CURLE_ABORTED_BY_CALLBACK;
+  BIO *cbio = NULL;
+  X509_STORE *cts;
   ossl_valsize_t i;
   STACK_OF(X509_INFO) * inf;
 
   (void)curl;
   (void)pointer;
 
-  if(!cts || !cbio) {
-    return CURLE_ABORTED_BY_CALLBACK;
+  cts = SSL_CTX_get_cert_store((SSL_CTX *)sslctx);
+  if(!cts) {
+    printf("SSL_CTX_get_cert_store() failed\n");
+    goto out;
+  }
+
+  cbio = BIO_new_mem_buf(mypem, sizeof(mypem) - 1);
+  if(!cbio) {
+    printf("BIO_new_mem_buf() failed\n");
+    goto out;
   }
 
   inf = PEM_X509_INFO_read_bio(cbio, NULL, NULL, NULL);
-
   if(!inf) {
-    BIO_free(cbio);
-    return CURLE_ABORTED_BY_CALLBACK;
+    printf("PEM_X509_INFO_read_bio() failed\n");
+    goto out;
   }
 
   for(i = 0; i < sk_X509_INFO_num(inf); i++) {
@@ -109,9 +117,15 @@ static CURLcode sslctx_function(CURL *curl, void *sslctx, void *pointer)
   }
 
   sk_X509_INFO_pop_free(inf, X509_INFO_free);
-  BIO_free(cbio);
 
-  return CURLE_OK;
+  result = CURLE_OK;
+
+out:
+
+  if(cbio)
+    BIO_free(cbio);
+
+  return result;
 }
 
 int main(void)
