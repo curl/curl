bd12777715fb5be2c2365a59abcf43af593b7a7e
Viktor Szakats <commit@vsz.me>
2026-03-09 01:08:28 +0100

    fixes

--- lib/imap.c
+++ lib/imap.c
@@ -1452,7 +1452,7 @@ static CURLcode imap_state_fetch_resp(struct Curl_easy *data,
       if(result)
         return result;
 
-      infof(data, "Written %zu bytes, %" FMT_OFF_TU
+      infof(data, "Written %zu bytes, %" FMT_OFF_T
             " bytes are left for transfer", chunk, (curl_off_t)(size - chunk));
 
       /* Have we used the entire overflow or part of it?*/
