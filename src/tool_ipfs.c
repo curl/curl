04b42bb4b3acc1c5862f280f90141d79bb385358
Daniel Stenberg <daniel@haxx.se>
2026-03-17 14:58:56 +0100

    fixup spaces

--- src/tool_ipfs.c
+++ src/tool_ipfs.c
@@ -167,7 +167,7 @@ CURLcode ipfs_url_rewrite(CURLU *uh, const char *protocol, char **url,
   if(curl_url_get(gatewayurl, CURLUPART_HOST, &gwhost, CURLU_URLDECODE) ||
      curl_url_get(gatewayurl, CURLUPART_SCHEME, &gwscheme, CURLU_URLDECODE) ||
      curl_url_get(gatewayurl, CURLUPART_PORT, &gwport,
-                  CURLU_URLDECODE|CURLU_DEFAULT_PORT) ||
+                  CURLU_URLDECODE | CURLU_DEFAULT_PORT) ||
      curl_url_get(gatewayurl, CURLUPART_PATH, &gwpath, CURLU_URLDECODE))
     goto clean;
 
