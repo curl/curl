14782b36fea2a5f69029b2b898302b6ba5aef838
Daniel Stenberg <daniel@haxx.se>
2026-03-19 16:51:07 +0100

    x509asn1: fixed and adapted for ASN1tostr unit testing
    - move defines to header file
    - make bit2str require < 8 unused bits
    - make bool strings stricter
    - make UTime2str show + or - for custom time zones
    - removed unused 'type' argument to ASN1tostr() function
    - fix int2str for negative values. All values below 10000 are now shown
      in decimal properly, also possibly negative values.
    
    Add unit test 1667 to verify ASN1tostr
    
    Closes #21013

--- lib/vtls/x509asn1.h
+++ lib/vtls/x509asn1.h
@@ -26,6 +26,7 @@
  ***************************************************************************/
 #include "curl_setup.h"
 
+/* forward-declare to make it visible in all builds */
 struct Curl_asn1Element;
 
 #if defined(USE_GNUTLS) || defined(USE_WOLFSSL) || defined(USE_SCHANNEL) || \
@@ -33,6 +34,42 @@ struct Curl_asn1Element;
 
 #include "cfilters.h"
 #include "urldata.h"
+
+/* ASN.1 classes. */
+/* #define CURL_ASN1_UNIVERSAL             0 */
+/* #define CURL_ASN1_APPLICATION           1 */
+/* #define CURL_ASN1_CONTEXT_SPECIFIC      2 */
+/* #define CURL_ASN1_PRIVATE               3 */
+
+/* ASN.1 types. */
+#define CURL_ASN1_BOOLEAN               1
+#define CURL_ASN1_INTEGER               2
+#define CURL_ASN1_BIT_STRING            3
+#define CURL_ASN1_OCTET_STRING          4
+#define CURL_ASN1_NULL                  5
+#define CURL_ASN1_OBJECT_IDENTIFIER     6
+/* #define CURL_ASN1_OBJECT_DESCRIPTOR     7 */
+/* #define CURL_ASN1_INSTANCE_OF           8 */
+/* #define CURL_ASN1_REAL                  9 */
+#define CURL_ASN1_ENUMERATED            10
+/* #define CURL_ASN1_EMBEDDED              11 */
+#define CURL_ASN1_UTF8_STRING           12
+/* #define CURL_ASN1_RELATIVE_OID          13 */
+/* #define CURL_ASN1_SEQUENCE              16 */
+/* #define CURL_ASN1_SET                   17 */
+#define CURL_ASN1_NUMERIC_STRING        18
+#define CURL_ASN1_PRINTABLE_STRING      19
+#define CURL_ASN1_TELETEX_STRING        20
+/* #define CURL_ASN1_VIDEOTEX_STRING       21 */
+#define CURL_ASN1_IA5_STRING            22
+#define CURL_ASN1_UTC_TIME              23
+#define CURL_ASN1_GENERALIZED_TIME      24
+/* #define CURL_ASN1_GRAPHIC_STRING        25 */
+#define CURL_ASN1_VISIBLE_STRING        26
+/* #define CURL_ASN1_GENERAL_STRING        27 */
+#define CURL_ASN1_UNIVERSAL_STRING      28
+/* #define CURL_ASN1_CHARACTER_STRING      29 */
+#define CURL_ASN1_BMP_STRING            30
 
 /*
  * Types.
