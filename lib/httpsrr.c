a2e77ecf9794ade5f99ecfae172411f1ae3c6a6c
Viktor Szakats <commit@vsz.me>
2026-03-20 13:41:25 +0100

    httpsrr: fix `-Wimplicit-int-enum-cast`
    Fixing (seen in HTTPS-RR builds with clang 21+ in curl-for-win dev):
    ```
    In file included from _a64-linux-gnu-bld/lib/CMakeFiles/libcurl_object.dir/Unity/unity_0_c.c:34:
    lib/cf-https-connect.c:681:28: error: implicit conversion from 'unsigned char' to enumeration type 'enum alpnid' is invalid in C++ [-Werror,-Wimplicit-int-enum-cast]
      681 |         enum alpnid alpn = rr->alpns[i];
          |                     ~~~~   ^~~~~~~~~~~~
    ```
    
    Follow-up to 59405ffb7d8be6b26e8823dee8ccd604be4ee10d #20990
    Follow-up to 0d4fdbf15d8eec908b3e63b606f112b18a63015e #16054
    
    Cherry-picked from #21032

--- lib/httpsrr.c
+++ lib/httpsrr.c
@@ -32,7 +32,7 @@
 #include "curlx/strdup.h"
 
 static CURLcode httpsrr_decode_alpn(const uint8_t *cp, size_t len,
-                                    unsigned char *alpns)
+                                    enum alpnid *alpns)
 {
   /*
    * The wire-format value for "alpn" consists of at least one alpn-id
@@ -58,7 +58,7 @@ static CURLcode httpsrr_decode_alpn(const uint8_t *cp, size_t len,
         /* this ALPN id is already stored */
         ;
       else
-        alpns[idnum++] = (unsigned char)id;
+        alpns[idnum++] = id;
     }
     cp += tlen;
     len -= tlen;
