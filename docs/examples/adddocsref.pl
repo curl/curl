03026685dfffbfbc7a508d9f914408faa5703ddb
Viktor Szakats <commit@vsz.me>
2026-03-22 02:42:17 +0100

    adddocsref.pl: replace shell commands with built-in call

--- docs/examples/adddocsref.pl
+++ docs/examples/adddocsref.pl
@@ -28,6 +28,8 @@
 use strict;
 use warnings;
 
+use File::Copy;
+
 my $docroot="https://curl.se/libcurl/c";
 
 for my $f (@ARGV) {
@@ -56,6 +58,6 @@ for my $f (@ARGV) {
     close(F);
     close(NEW);
 
-    system("mv $f $f.org");
-    system("mv $f.new $f");
+    move($f, "$f.org");
+    move("$f.new", $f);
 }
