b1c6b036ba7c9eb94c7943ea76e564eea20d9ebc
Daniel Stenberg <daniel@haxx.se>
2026-03-24 08:35:08 +0100

    CVE-2026-4873: connection reuse ignores TLS requirement
    Cherry-picked from 507e7be573b0a76fca5
    
    url: do not reuse a non-tls starttls connection if new requires TLS
    
    Reported-by: Arkadi Vainbrand
    
    Closes #21082

--- lib/url.c
+++ lib/url.c
@@ -800,7 +800,7 @@ struct url_conn_match {
   BIT(want_proxy_ntlm_http);
   BIT(want_nego_http);
   BIT(want_proxy_nego_http);
-
+  BIT(req_tls); /* require TLS use from a clear-text start */
   BIT(wait_pipe);
   BIT(force_reuse);
   BIT(seen_pending_conn);
@@ -951,6 +951,9 @@ static bool url_match_ssl_use(struct connectdata *conn,
     if(get_protocol_family(conn->handler) != m->needle->handler->protocol)
       return FALSE;
   }
+  else if(m->req_tls)
+    /* a clear-text STARTTLS protocol with required TLS */
+    return FALSE;
   return TRUE;
 }
 
@@ -1413,6 +1416,7 @@ ConnectionExists(struct Curl_easy *data,
     (needle->handler->protocol & PROTO_FAMILY_HTTP);
 #endif
 #endif
+  match.req_tls = data->set.use_ssl >= CURLUSESSL_CONTROL;
 
   /* Find a connection in the pool that matches what "data + needle"
    * requires. If a suitable candidate is found, it is attached to "data". */
