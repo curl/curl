70c55c3c8a36d2cf4402e8f1f89b941963ba243e
Stefan Eissing <stefan@eissing.org>
2026-04-17 15:21:12 +0200

    pytest, check 429 handling
    Add a "limit" handler to httpd that responds 429 after 5 requests and
    then 429s all requests for 2 seconds. After which another 4 requests
    are served before 429 strikes again.

--- tests/http/test_05_errors.py
+++ tests/http/test_05_errors.py
@@ -217,3 +217,15 @@ class TestErrors:
         # - CURLE_RECV_ERROR (56) - some TLS backends fail with that
         assert r.exit_code in [35, 56], \
             f'unexpected error {r.exit_code}\n{r.dump_logs()}'
+
+    # Get a resource many times with limited requests
+    def test_05_10_limits(self, env: Env, httpd):
+        proto = 'h2'
+        count = 11
+        curl = CurlClient(env=env)
+        url = f'https://{env.authority_for(env.domain1, proto)}/curltest/limit?id=[0-{count-1}]'
+        r = curl.http_download(urls=[url], alpn_proto=proto, extra_args=[
+            '--parallel', '--retry', '2'
+        ])
+        r.check_stats(count=count, http_status=200, exitcode=0)
+
