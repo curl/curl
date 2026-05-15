f8d5d302eb765375a9ec3b2c623e478935047bf3
Daniel Stenberg <daniel@haxx.se>
2026-05-15 22:50:36 +0200

    fixup extend tests

--- tests/libtest/lib1560.c
+++ tests/libtest/lib1560.c
@@ -626,8 +626,14 @@ static const struct testcase get_parts_list[] = {
 
 static const struct urltestcase get_url_list[] = {
   {"https://127.1.", "https://127.0.0.1/", 0, 0, CURLUE_OK},
+  {"https://127.1.:443", "https://127.0.0.1:443/", 0, 0, CURLUE_OK},
+  {"https://127.1.?moo", "https://127.0.0.1/?moo", 0, 0, CURLUE_OK},
+  {"https://127.1.#moo", "https://127.0.0.1/#moo", 0, 0, CURLUE_OK},
   {"https://127.1.a", "https://127.1.a/", 0, 0, CURLUE_OK},
   {"https://127.1..", "", 0, 0, CURLUE_BAD_HOSTNAME},
+  {"https://127.1..:443", "", 0, 0, CURLUE_BAD_HOSTNAME},
+  {"https://127.1..?moo", "", 0, 0, CURLUE_BAD_HOSTNAME},
+  {"https://127.1..#moo", "", 0, 0, CURLUE_BAD_HOSTNAME},
   {"https://127.1.1.", "https://127.1.0.1/", 0, 0, CURLUE_OK},
   {"https://127.1.1./foo", "https://127.1.0.1/foo", 0, 0, CURLUE_OK},
   {"https://127.1.1.1.", "https://127.1.1.1/", 0, 0, CURLUE_OK},
