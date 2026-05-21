3c1472fa3daad218abb0645cab41be553e42535d
Viktor Szakats <commit@vsz.me>
2026-05-21 11:58:43 +0200

    one more in older gcc compilers after prev
    ```
    In function 'Curl_multi_xfers_running',
        inlined from 'multi_socket.isra' at ../../lib/multi.c:3292:28:
    ../../lib/multi.c:4132:15: error: null pointer dereference [-Werror=null-dereference]
     4132 |   return multi->xfers_alive;
          |          ~~~~~^~~~~~~~~~~~~
    ```
    https://github.com/curl/curl/actions/runs/26218822231/job/77148186045

--- lib/multi.c
+++ lib/multi.c
@@ -1708,6 +1708,7 @@ CURLMcode curl_multi_wakeup(CURLM *m)
 static bool multi_ischanged(struct Curl_multi *multi, bool clear)
 {
   bool retval = FALSE;
+  DEBUGASSERT(multi);
   if(multi) {
     retval = (bool)multi->recheckstate;
     if(clear)
@@ -4129,6 +4130,9 @@ struct Curl_easy *Curl_multi_get_easy(struct Curl_multi *multi,
 
 unsigned int Curl_multi_xfers_running(struct Curl_multi *multi)
 {
+  DEBUGASSERT(multi);
+  if(!multi)
+    return FALSE;
   return multi->xfers_alive;
 }
 
