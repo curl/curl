9af920bcd8aa36b1bb8ce14abc10dc8382b2d158
Viktor Szakats <commit@vsz.me>
2026-05-26 21:50:34 +0200

    libssh.c simplify wording, version -> v

--- lib/vssh/libssh.c
+++ lib/vssh/libssh.c
@@ -2990,8 +2990,8 @@ static CURLcode sftp_send(struct Curl_easy *data, int sockindex,
    * https://datatracker.ietf.org/doc/html/draft-ietf-secsh-filexfer-02
    *
    * libssh started applying appropriate read/write length limits
-   * internally since version 0.11.0, hence such an operation is
-   * not needed for versions after (and including) 0.11.0.
+   * internally since v0.11.0, hence such an operation is not needed
+   * for v0.11.0 and greater.
    */
   if(len > 32768)
     len = 32768;
