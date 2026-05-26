c682d1626722b380abc8da9bbb6a95e212d9298b
Stefan Eissing <stefan@eissing.org>
2026-05-26 11:38:25 +0200

    silence clang-tidy nonsense
    About a "unnecessary define" - my ass.

--- lib/multi_ev.c
+++ lib/multi_ev.c
@@ -40,7 +40,9 @@ static void mev_in_callback(struct Curl_multi *multi, bool value)
   multi->in_callback = value;
 }
 
+#ifdef DEBUGBUILD
 #define SH_ENTRY_MAGIC 0x570091d
+#endif
 
 /* Information about a socket for which we inform the libcurl application
  * what to supervise (CURL_POLL_IN/CURL_POLL_OUT/CURL_POLL_REMOVE)
