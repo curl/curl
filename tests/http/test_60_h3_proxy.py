183df727e29e14eed79680e2c777fe4592ae5101
Viktor Szakats <commit@vsz.me>
2026-05-31 14:44:07 +0200

    drop redundant preconditions from test_60_04_guard_proxy

--- tests/http/test_60_h3_proxy.py
+++ tests/http/test_60_h3_proxy.py
@@ -305,8 +305,6 @@ class TestH3Proxy:
 
     # Guard checks for unsupported HTTP/3 proxy options.
 
-    @MARK_NEEDS_HTTPS_PROXY
-    @MARK_NEEDS_HTTP3
     @pytest.mark.skipif(
         condition=Env.curl_uses_lib("ngtcp2"),
         reason="guard only applies to non-ngtcp2 builds",
