29af32d755eb3dd023a821442b16f165732b0567
Viktor Szakats <commit@vsz.me>
2026-06-09 11:58:26 +0200

    certs.py narrow down exceptions
    Except block handles 'BaseException'

--- tests/http/testenv/certs.py
+++ tests/http/testenv/certs.py
@@ -484,8 +484,7 @@ class TestCA:
             else:
                 try:
                     names.append(x509.IPAddress(ipaddress.ip_address(name)))
-                # TODO: specify specific exceptions here
-                except:  # noqa: E722
+                except ValueError:
                     names.append(x509.DNSName(name))
 
         return csr.add_extension(
