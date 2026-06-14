61b7f2ca050d6a66c965fa19f2a28a28fe17d896
Viktor Szakats <commit@vsz.me>
2026-06-14 13:01:45 +0200

    util.c 2

--- tests/server/util.c
+++ tests/server/util.c
@@ -716,8 +716,7 @@ int bind_unix_socket(curl_socket_t sock, const char *unix_socket,
     /* dead socket, cleanup and retry bind */
     rc = unlink(unix_socket);
     if(rc) {
-      logmsg("Error binding socket, failed to unlink(%s) = %d %d (%s)",
-             unix_socket, rc,
+      logmsg("Error binding socket, failed to unlink %s: %d (%s)", unix_socket,
              errno, curlx_strerror(errno, errbuf, sizeof(errbuf)));
       return rc;
     }
