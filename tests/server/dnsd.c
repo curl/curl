9002d3350cc2086f95c8e850dee3e3ef9d8b11ac
Viktor Szakats <commit@vsz.me>
2026-06-14 14:36:24 +0200

    servers: minor socket error handling fixes
    - sws: fix socket error code in `select()` failure message.
      Spotted by Copilot
      Bug: https://github.com/curl/curl/pull/21998#discussion_r3409469444
    - sws: do not call `SOCKERRNO` twice on error.
    - dnsd: do not call `SOCKERRNO` twice on error.
    - dnsd: replace `goto` with `while()` to sync with rest of code.
    - dnsd: `sendto()` fail message fixes:
      - replace `int` cast with `%zu` mask.
      - drop redundant newline.
      - show socket error string like rest of code.
      - report not-fully-sent error separately from socket errors.
    
    Closes #22007

--- tests/server/dnsd.c
+++ tests/server/dnsd.c
@@ -346,15 +346,20 @@ static struct resp *resp_queue;
 static CURLcode send_resp(curl_socket_t sock, struct resp *resp)
 {
   ssize_t rc;
+  int sockerr = 0;
 
-sending:
-  rc = sendto(sock, (const void *)resp->body.data, (SENDTO3)resp->body.dlen, 0,
-              &resp->addr, resp->addrlen);
-  if((rc < 0) && (SOCKERRNO == SOCKEINTR))
-    goto sending;
-  if(rc != (ssize_t)resp->body.dlen) {
-    logmsg("failed sending %d bytes, errno=%d\n",
-           (int)resp->body.dlen, SOCKERRNO);
+  do {
+    rc = sendto(sock, (const void *)resp->body.data, (SENDTO3)resp->body.dlen,
+                0, &resp->addr, resp->addrlen);
+  } while((rc < 0) && ((sockerr = SOCKERRNO) == SOCKEINTR));
+  if(rc < 0) {
+    char errbuf[STRERROR_LEN];
+    logmsg("failed sending %zu bytes, error: (%d) %s", resp->body.dlen,
+           sockerr, curlx_strerror(sockerr, errbuf, sizeof(errbuf)));
+    return CURLE_SEND_ERROR;
+  }
+  else if(rc != (ssize_t)resp->body.dlen) {
+    logmsg("failed sending %zu bytes, sent: %zd", resp->body.dlen, rc);
     return CURLE_SEND_ERROR;
   }
   logmsg("[%d] sent response", resp->qid);
