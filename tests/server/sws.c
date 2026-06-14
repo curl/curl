ce99444c456f688aececd43b3bff3d4d59e83b12
Viktor Szakats <commit@vsz.me>
2026-06-14 12:54:59 +0200

    sws.c fixup

--- tests/server/sws.c
+++ tests/server/sws.c
@@ -2443,7 +2443,7 @@ sws_cleanup:
 
 #ifdef USE_UNIX_SOCKETS
   if(unlink_socket && socket_domain == AF_UNIX && unix_socket) {
-    int rc = unlink(unix_socket);
+    rc = unlink(unix_socket);
     logmsg("unlink(%s) = %d %d (%s)", unix_socket, rc,
            errno, curlx_strerror(errno, errbuf, sizeof(errbuf)));
   }
