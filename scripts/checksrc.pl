9f25dcea55713aa37451c52b809e16ae1509bfe4
Daniel Stenberg <daniel@haxx.se>
2026-06-15 13:51:52 +0200

    checksrc: detect and warn for (void)!
    It's plain weird. Don't do it.
    
    Closes #22023

--- scripts/checksrc.pl
+++ scripts/checksrc.pl
@@ -202,6 +202,7 @@ my %warnings = (
     'TYPEDEFSTRUCT'         => 'typedefed struct',
     'UNUSEDIGNORE'          => 'a warning ignore was not used',
     'USESAFEFREE'           => 'replace curlx_free() + NULL assignment with curlx_safefree()',
+    'VOIDEXCL'              => '(void)! is not something we like',
     );
 
 sub readskiplist {
@@ -1163,6 +1164,12 @@ sub scanfile {
             checkwarn("EXCLAMATIONSPACE",
                       $line, length($1)+1, $file, $ol,
                       "space after exclamation mark");
+        }
+
+        if($nostr =~ /(.*)\(void\)\!/) {
+            checkwarn("VOIDEXCL",
+                      $line, length($1)+1, $file, $ol,
+                      "exclamation after (void) is weird");
         }
 
         if($nostr =~ /(.*)\b(EACCES|EADDRINUSE|EADDRNOTAVAIL|EAFNOSUPPORT|EBADF|ECONNREFUSED|ECONNRESET|EINPROGRESS|EINTR|EINVAL|EISCONN|EMSGSIZE|ENOMEM|ETIMEDOUT|EWOULDBLOCK)\b/) {
