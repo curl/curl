e9f101dbf2d77481a715be9b9d1d6d75cda62a6b
Viktor Szakats <commit@vsz.me>
2026-06-16 16:00:13 +0200

    keep some folds

--- tests/test1222.pl
+++ tests/test1222.pl
@@ -238,7 +238,8 @@ sub scan_man_page {
 }
 
 # Read symbols-in-versions.
-open(my $fh, "<", "$root/docs/libcurl/symbols-in-versions") or die "$root/docs/libcurl/symbols-in-versions";
+open(my $fh, "<", "$root/docs/libcurl/symbols-in-versions") or
+    die "$root/docs/libcurl/symbols-in-versions";
 while(<$fh>) {
     if($_ =~ /^((?:CURL|LIBCURL)\S+)\s+\S+\s*(\S*)\s*(\S*)$/) {
         if($3 eq "") {
