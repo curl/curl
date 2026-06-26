db729683ea6392cec798bfcf531cbdc530e895be
Viktor Szakats <commit@vsz.me>
2026-06-26 18:16:08 +0200

    cmake_uninstall.in.cmake -f

--- CMake/cmake_uninstall.in.cmake
+++ CMake/cmake_uninstall.in.cmake
@@ -35,14 +35,7 @@ string(REGEX REPLACE "\n" ";" _files "${_files}")
 foreach(_file ${_files})
   message(STATUS "Uninstalling $ENV{DESTDIR}${_file}")
   if(IS_SYMLINK "$ENV{DESTDIR}${_file}" OR EXISTS "$ENV{DESTDIR}${_file}")
-    execute_process(
-      COMMAND "@CMAKE_COMMAND@" -E rm -- "$ENV{DESTDIR}${_file}"
-      RESULT_VARIABLE rm_retval
-      ERROR_QUIET
-    )
-    if(NOT "${rm_retval}" STREQUAL 0)
-      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${_file}")
-    endif()
+    execute_process(COMMAND "@CMAKE_COMMAND@" -E rm -f -- "$ENV{DESTDIR}${_file}")
   else()
     message(STATUS "File $ENV{DESTDIR}${_file} does not exist.")
   endif()
