2f443b933ef7ef3b9a4865a7d4d974a73b4d1f04
Viktor Szakats <commit@vsz.me>
2026-07-04 18:00:40 +0200

    comment nits 7

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -632,7 +632,7 @@ typedef enum {
                                      match */
   CURLE_SSL_INVALIDCERTSTATUS,   /* 91 - invalid certificate status */
   CURLE_HTTP2_STREAM,            /* 92 - stream error in HTTP/2 framing layer
-                                    */
+                                  */
   CURLE_RECURSIVE_API_CALL,      /* 93 - an api function was called from
                                     inside a callback */
   CURLE_AUTH_ERROR,              /* 94 - an authentication function returned an
