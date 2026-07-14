2162805d3aef6342bb08a6b100ef16ad9252d167
Viktor Szakats <commit@vsz.me>
2026-07-14 19:24:26 +0200

    schannel.c unfold more

--- lib/vtls/schannel.c
+++ lib/vtls/schannel.c
@@ -474,8 +474,7 @@ static CURLcode get_client_cert(struct Curl_cfilter *cf,
         int str_w_len = 0;
         if(pwd_len > 0)
           str_w_len = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS,
-                                          sslc->key_passwd,
-                                          (int)pwd_len,
+                                          sslc->key_passwd, (int)pwd_len,
                                           pszPassword, (int)(pwd_len + 1));
 
         if((str_w_len >= 0) && (str_w_len <= (int)pwd_len))
