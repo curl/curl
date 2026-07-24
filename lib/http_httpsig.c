0732d644212129723051cdc10d7107b894bb63c2
Daniel Stenberg <daniel@haxx.se>
2026-07-24 17:55:35 +0200

    nah shorter error

--- lib/http_httpsig.c
+++ lib/http_httpsig.c
@@ -432,9 +432,7 @@ static CURLcode parse_components(struct Curl_easy *data,
         else if(!strcmp(start, "query"))
           components[ncomp++] = SIG_QUERY;
         else {
-          failf(data, "httpsig: unknown component '%s'; use method/authority/"
-                "path/query for derived components, or append ':' to sign a "
-                "request header field", start);
+          failf(data, "httpsig: unknown component '%s'", start);
           return CURLE_BAD_FUNCTION_ARGUMENT;
         }
       }
