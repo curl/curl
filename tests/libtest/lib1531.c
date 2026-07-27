f9d46af2b00f124d4c64f71b1fa6c487dbc28b83
Viktor Szakats <commit@vsz.me>
2026-07-27 12:22:12 +0200

    move const before type where missing

--- tests/libtest/lib1531.c
+++ tests/libtest/lib1531.c
@@ -25,8 +25,8 @@
 
 static CURLcode test_lib1531(const char *URL)
 {
-  static char const testdata[] = ".abc\0xyz";
-  static curl_off_t const testdatalen = sizeof(testdata) - 1;
+  static const char testdata[] = ".abc\0xyz";
+  static const curl_off_t testdatalen = sizeof(testdata) - 1;
 
   CURL *curl;
   CURLM *multi;
