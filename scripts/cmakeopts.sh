3578a1a97000131d6c1b076f28cd44147008ac26
Viktor Szakats <commit@vsz.me>
2026-07-27 03:09:39 +0200

    fixes

--- scripts/cmakeopts.sh
+++ scripts/cmakeopts.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Copyright (C) Viktor Szakats
 #
 # SPDX-License-Identifier: curl
@@ -9,13 +9,25 @@ set -eu
 
 cd -- "$(dirname "$0")"/..
 
-{
-  git grep -o -E 'option\([A-Z][A-Z0-9_]+' ':!tests/cmake' | grep -i cmake | sed -E -e 's/^.+://g' -e 's/option\(//g'
-  git grep -h -o -E 'curl_dependency_option\([A-Z][A-Z0-9_]+' | sed -e 's/curl_dependency_option(//g'
-  # shellcheck disable=SC2016
-  git grep -h -o -E '^# - `[A-Z0-9_]+`: ' ':CMake/Find**' | grep -o -E '[A-Z0-9_]+' | grep -v -E '(_FOUND|_VERSION|NGTCP2_CRYPTO_BACKEND)'
-} | sort -u | while read -r opt; do
-  if ! grep -q -F -- "$opt" docs/INSTALL-CMAKE.md; then
+anyerr=0
+while read -r opt; do
+  if ! grep -q -F -- "- \`$opt\`:" docs/INSTALL-CMAKE.md; then
     echo "CMake option missing from documentation: '$opt'"
+    anyerr=1
   fi
-done
+done < <(
+  {
+    git grep -o -E 'option\([A-Z][A-Z0-9_]+' ':!tests/cmake' |
+      grep -i cmake |
+      sed -E -e 's/^.+://g' -e 's/option\(//g'
+
+    git grep -h -o -E 'curl_dependency_option\([A-Z][A-Z0-9_]+' |
+      sed -e 's/curl_dependency_option(//g'
+
+    git grep -h -o -E "^# - \`[A-Z0-9_]+\`: " ':CMake/Find**' |
+      grep -o -E '[A-Z0-9_]+' |
+      grep -v -E '(_FOUND|_VERSION|NGTCP2_CRYPTO_BACKEND)'
+  } | sort -u
+)
+
+exit "${anyerr}"
