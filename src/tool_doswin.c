c55afb17f7f79ecd5688d9ccb9e857bdabeda821
Emanuel Krollmann <115734183+11soda11@users.noreply.github.com>
2026-07-27 16:59:29 +0200

    switch order

--- src/tool_doswin.c
+++ src/tool_doswin.c
@@ -813,8 +813,7 @@ static int read_auth_val(curl_socket_t sock, uint64_t* buf)
 curl_socket_t win32_stdin_read_thread(void)
 {
   int rc = 0;
-  static struct win_thread_data tdata =
-    (struct win_thread_data){ CURL_SOCKET_BAD, NULL };
+  static struct win_thread_data tdata = { NULL, CURL_SOCKET_BAD };
   static HANDLE stdin_thread = NULL;
   static curl_socket_t socket_r = CURL_SOCKET_BAD;
   curl_socket_t socket_l = CURL_SOCKET_BAD;
