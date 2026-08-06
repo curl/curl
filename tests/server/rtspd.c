80596dc3c1f1d681a616f6ca3b85e25e3a48a3c8
Viktor Szakats <commit@vsz.me>
2026-08-06 09:29:53 +0200

    rtspd

--- tests/server/rtspd.c
+++ tests/server/rtspd.c
@@ -952,7 +952,6 @@ static int test_rtspd(int argc, const char *argv[])
   int wrotepidfile = 0;
   int wroteportfile = 0;
   int flag;
-  unsigned short port = 8999;
   struct rtspd_httprequest req;
   int rc;
   int sockerr;
@@ -964,6 +963,7 @@ static int test_rtspd(int argc, const char *argv[])
   pidname = ".rtsp.pid";
   serverlogfile = "log/rtspd.log";
   serverlogslocked = 0;
+  server_port = 8999;
 
   while(argc > arg) {
     const char *opt;
@@ -1016,7 +1016,7 @@ static int test_rtspd(int argc, const char *argv[])
       if(argc > arg) {
         opt = argv[arg];
         if(!curlx_str_number(&opt, &num, 0xffff))
-          port = (unsigned short)num;
+          server_port = (unsigned short)num;
         arg++;
       }
     }
@@ -1074,7 +1074,7 @@ static int test_rtspd(int argc, const char *argv[])
     memset(&me.sa6, 0, sizeof(me.sa6));
     me.sa6.sin6_family = AF_INET6;
     me.sa6.sin6_addr = in6addr_any;
-    me.sa6.sin6_port = htons(port);
+    me.sa6.sin6_port = htons(server_port);
     rc = bind(sock, &me.sa, sizeof(me.sa6));
   }
   else
@@ -1083,17 +1083,17 @@ static int test_rtspd(int argc, const char *argv[])
     memset(&me.sa4, 0, sizeof(me.sa4));
     me.sa4.sin_family = AF_INET;
     me.sa4.sin_addr.s_addr = INADDR_ANY;
-    me.sa4.sin_port = htons(port);
+    me.sa4.sin_port = htons(server_port);
     rc = bind(sock, &me.sa, sizeof(me.sa4));
   }
   if(rc) {
     sockerr = SOCKERRNO;
-    logmsg("Error binding socket on port %hu (%d) %s", port,
+    logmsg("Error binding socket on port %hu (%d) %s", server_port,
            sockerr, curlx_strerror(sockerr, errbuf, sizeof(errbuf)));
     goto server_cleanup;
   }
 
-  if(!port) {
+  if(!server_port) {
     /* The system was supposed to choose a port number, figure out which
        port we actually got and update the listener port value with it. */
     curl_socklen_t la_size;
@@ -1114,17 +1114,17 @@ static int test_rtspd(int argc, const char *argv[])
     }
     switch(localaddr.sa.sa_family) {
     case AF_INET:
-      port = ntohs(localaddr.sa4.sin_port);
+      server_port = ntohs(localaddr.sa4.sin_port);
       break;
 #ifdef USE_IPV6
     case AF_INET6:
-      port = ntohs(localaddr.sa6.sin6_port);
+      server_port = ntohs(localaddr.sa6.sin6_port);
       break;
 #endif
     default:
       break;
     }
-    if(!port) {
+    if(!server_port) {
       /* Real failure, listener port shall not be zero beyond this point. */
       logmsg("Apparently getsockname() succeeded, with listener port zero.");
       logmsg("A valid reason for this failure is a binary built without");
@@ -1134,7 +1134,7 @@ static int test_rtspd(int argc, const char *argv[])
       goto server_cleanup;
     }
   }
-  logmsg("Running %s version on port %d", socket_type, (int)port);
+  logmsg("Running %s version on port %d", socket_type, (int)server_port);
 
   /* start accepting connections */
   if(listen(sock, 5)) {
@@ -1154,7 +1154,7 @@ static int test_rtspd(int argc, const char *argv[])
     goto server_cleanup;
 
   if(portname) {
-    wroteportfile = write_portfile(portname, port);
+    wroteportfile = write_portfile(portname, server_port);
     if(!wroteportfile)
       goto server_cleanup;
   }
@@ -1280,7 +1280,7 @@ server_cleanup:
 
   if(got_exit_signal) {
     logmsg("========> %s rtspd (port: %d pid: %ld) exits with signal (%d)",
-           socket_type, (int)port, (long)our_getpid(), exit_signal);
+           socket_type, (int)server_port, (long)our_getpid(), exit_signal);
     /*
      * To properly set the return status of the process we
      * must raise the same signal SIGINT or SIGTERM that we
