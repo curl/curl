1db93bdb7c1943132b13543539aaaf4866f31419
Viktor Szakats <commit@vsz.me>
2026-08-15 16:18:48 +0200

    config-win32.h: limit use to MSVC IDE Project builds
    This is the only purpose it's maintained for.
    
    Drop mingw-w64 and other logic falling outside of VS2010-2013 needs.
    
    mingw-w64/MSVC platform differences keep being maintained/documented as
    part of the CMake pre-fill logic in `CMake/win32-cache.cmake`.
    
    Follow-up to ba8752e5566076acc8bdec7ae4ec78901f7050f4 #12224
    
    Closes #22595

--- lib/config-win32.h
+++ lib/config-win32.h
@@ -24,55 +24,33 @@
  *
  ***************************************************************************/
 
-/* ================================================================ */
-/*               Hand crafted config file for Windows               */
-/* ================================================================ */
+/* ======================================================================== */
+/* Handcrafted config file for building via Visual Studio IDE Project Files */
+/* ======================================================================== */
+
+#if !defined(_MSC_VER) || _MSC_VER > 1800
+#error This manual configuration requires MSVC 2010-2013 (IDE Project builds)
+#endif
 
 /* ---------------------------------------------------------------- */
 /*                          HEADER FILES                            */
 /* ---------------------------------------------------------------- */
 
-/* Define if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
-
-/* Define if you have the <io.h> header file. */
 #define HAVE_IO_H 1
-
-/* Define if you have the <locale.h> header file. */
 #define HAVE_LOCALE_H 1
-
-/* Define to 1 if you have the <stdbool.h> header file. */
-#if (defined(_MSC_VER) && (_MSC_VER >= 1800)) || defined(__MINGW32__)
+#if _MSC_VER >= 1800
 #define HAVE_STDBOOL_H 1
 #endif
-
-/* Define if you have the <sys/param.h> header file. */
-#ifdef __MINGW32__
-#define HAVE_SYS_PARAM_H 1
-#endif
-
-/* Define if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
-
-/* Define if you have the <sys/utime.h> header file. */
 #define HAVE_SYS_UTIME_H 1
-
-/* Define if you have the <unistd.h> header file. */
-#ifdef __MINGW32__
-#define HAVE_UNISTD_H 1
-#endif
-
-/* Define to 1 if you have the <libgen.h> header file. */
-#ifdef __MINGW32__
-#define HAVE_LIBGEN_H 1
-#endif
 
 /* ---------------------------------------------------------------- */
 /*                        OTHER HEADER INFO                         */
 /* ---------------------------------------------------------------- */
 
 /* Define to 1 if bool is an available type. */
-#if (defined(_MSC_VER) && (_MSC_VER >= 1800)) || defined(__MINGW32__)
+#if _MSC_VER >= 1800
 #define HAVE_BOOL_T 1
 #endif
 
@@ -80,80 +58,30 @@
 /*                             FUNCTIONS                            */
 /* ---------------------------------------------------------------- */
 
-/* Define if you have the closesocket function. */
 #define HAVE_CLOSESOCKET 1
-
-/* Define to 1 if you have the `getpeername' function. */
-#define HAVE_GETPEERNAME 1
-
-/* Define to 1 if you have the getsockname function. */
-#define HAVE_GETSOCKNAME 1
-
-/* Define if you have the gethostname function. */
+#define HAVE_FREEADDRINFO 1
+#define HAVE_GETADDRINFO 1
+#define HAVE_GETADDRINFO_THREADSAFE 1
 #define HAVE_GETHOSTNAME 1
-
-/* Define if you have the gettimeofday function. */
-#ifdef __MINGW32__
-#define HAVE_GETTIMEOFDAY 1
-#endif
-
-/* Define if you have the ioctlsocket function. */
+#define HAVE_GETPEERNAME 1
+#define HAVE_GETSOCKNAME 1
 #define HAVE_IOCTLSOCKET 1
-
-/* Define if you have a working ioctlsocket FIONBIO function. */
 #define HAVE_IOCTLSOCKET_FIONBIO 1
-
-/* Define if you have the setlocale function. */
 #define HAVE_SETLOCALE 1
-
-/* Define if you have the socket function. */
 #define HAVE_SOCKET 1
-
-/* Define if you have the utime function. */
 #define HAVE_UTIME 1
-
-/* Define if you have the recv function. */
 #define HAVE_RECV 1
-
-/* Define to the type of arg 1 for recv. */
 #define RECV_TYPE_ARG1 SOCKET
-
-/* Define to the type of arg 2 for recv. */
 #define RECV_TYPE_ARG2 char *
-
-/* Define to the type of arg 3 for recv. */
 #define RECV_TYPE_ARG3 int
-
-/* Define to the type of arg 4 for recv. */
 #define RECV_TYPE_ARG4 int
-
-/* Define to the function return type for recv. */
 #define RECV_TYPE_RETV int
-
-/* Define if you have the send function. */
 #define HAVE_SEND 1
-
-/* Define to the type of arg 1 for send. */
 #define SEND_TYPE_ARG1 SOCKET
-
-/* Define to the type of arg 2 for send. */
 #define SEND_TYPE_ARG2 char *
-
-/* Define to the type of arg 3 for send. */
 #define SEND_TYPE_ARG3 int
-
-/* Define to the type of arg 4 for send. */
 #define SEND_TYPE_ARG4 int
-
-/* Define to the function return type for send. */
 #define SEND_TYPE_RETV int
-
-/* Define to 1 if you have the `basename' function. */
-#ifdef __MINGW32__
-#define HAVE_BASENAME 1
-#endif
-
-/* Define to 1 if you have the signal function. */
 #define HAVE_SIGNAL 1
 
 /* ---------------------------------------------------------------- */
@@ -162,53 +90,33 @@
 
 /* Define if ssize_t is not an available 'typedefed' type. */
 #ifndef _SSIZE_T_DEFINED
-#  ifdef __MINGW32__
-#  elif defined(_WIN64)
-#    define _SSIZE_T_DEFINED
-#    define ssize_t __int64
-#  else
-#    define _SSIZE_T_DEFINED
-#    define ssize_t int
-#  endif
+#define _SSIZE_T_DEFINED
+#ifdef _WIN64
+#  define ssize_t __int64
+#else
+#  define ssize_t int
+#endif
 #endif
 
 /* ---------------------------------------------------------------- */
 /*                            TYPE SIZES                            */
 /* ---------------------------------------------------------------- */
 
-/* Define to the size of `int', as computed by sizeof. */
 #define SIZEOF_INT 4
-
-/* Define to the size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 4
-
-/* Define to the size of `size_t', as computed by sizeof. */
 #ifdef _WIN64
 #  define SIZEOF_SIZE_T 8
 #else
 #  define SIZEOF_SIZE_T 4
 #endif
-
-/* Define to the size of `curl_off_t', as computed by sizeof. */
 #define SIZEOF_CURL_OFF_T 8
-
-/* ---------------------------------------------------------------- */
-/*                        COMPILER SPECIFIC                         */
-/* ---------------------------------------------------------------- */
-
 /* Default to 64-bit time_t unless _USE_32BIT_TIME_T is defined */
-#if defined(_MSC_VER) || defined(__MINGW32__)
-#  ifndef _USE_32BIT_TIME_T
-#    define SIZEOF_TIME_T 8
-#  else
-#    define SIZEOF_TIME_T 4
-#  endif
+#ifndef _USE_32BIT_TIME_T
+#  define SIZEOF_TIME_T 8
+#else
+#  define SIZEOF_TIME_T 4
 #endif
-
-/* Windows XP is required for freeaddrinfo, getaddrinfo */
-#define HAVE_FREEADDRINFO           1
-#define HAVE_GETADDRINFO            1
-#define HAVE_GETADDRINFO_THREADSAFE 1
+#define SIZEOF_OFF_T 4
 
 /* ---------------------------------------------------------------- */
 /*                          STRUCT RELATED                          */
@@ -222,23 +130,6 @@
 
 /* Define if struct sockaddr_in6 has the sin6_scope_id member. */
 #define HAVE_SOCKADDR_IN6_SIN6_SCOPE_ID 1
-
-/* ---------------------------------------------------------------- */
-/*                        LARGE FILE SUPPORT                        */
-/* ---------------------------------------------------------------- */
-
-/* Number of bits in a file offset, on hosts where this is settable. */
-#ifdef __MINGW32__
-#  undef _FILE_OFFSET_BITS
-#  define _FILE_OFFSET_BITS 64
-#endif
-
-/* Define to the size of `off_t', as computed by sizeof. */
-#ifdef __MINGW32__
-#  define SIZEOF_OFF_T 8
-#else
-#  define SIZEOF_OFF_T 4
-#endif
 
 /* ---------------------------------------------------------------- */
 /*                       DNS RESOLVER SPECIALTY                     */
@@ -279,16 +170,10 @@
 
 /* Define cpu-machine-OS */
 #ifndef CURL_OS
-#  if defined(_M_IX86) || defined(__i386__) /* x86 (MSVC or gcc) */
+#  ifdef _M_IX86 /* x86 */
 #  define CURL_OS "i386-pc-win32"
-#  elif defined(_M_X64) || defined(__x86_64__) /* x86_64 (VS2005+ or gcc) */
+#  elif defined(_M_X64) /* x86_64 */
 #  define CURL_OS "x86_64-pc-win32"
-#  elif defined(_M_IA64) || defined(__ia64__) /* Itanium */
-#  define CURL_OS "ia64-pc-win32"
-#  elif defined(_M_ARM_NT) || defined(__arm__) /* ARMv7-Thumb2 */
-#  define CURL_OS "thumbv7a-pc-win32"
-#  elif defined(_M_ARM64) || defined(__aarch64__) /* ARM64 (Windows 10) */
-#  define CURL_OS "aarch64-pc-win32"
 #  else
 #  define CURL_OS "unknown-pc-win32"
 #  endif
