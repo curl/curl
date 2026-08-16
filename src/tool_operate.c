91c23d402f3c39513d6738460b413e758f526304
Viktor Szakats <commit@vsz.me>
2026-08-16 12:28:45 +0200

    tool_operate.c CURL* -> varname curl

--- src/tool_operate.c
+++ src/tool_operate.c
@@ -2178,20 +2178,20 @@ static bool win32_using_schannel(CURLcode *resultp)
                                      1 = yes */
   *resultp = CURLE_OK;
   if(using_schannel == -1) {
-    CURL *curltls = curl_easy_init();
+    CURL *curl = curl_easy_init();
     /* The TLS backend remains, so keep the info */
     const struct curl_tlssessioninfo *tls_backend_info = NULL;
 
-    if(!curltls)
+    if(!curl)
       *resultp = CURLE_OUT_OF_MEMORY;
     else {
-      *resultp = curl_easy_getinfo(curltls, CURLINFO_TLS_SSL_PTR,
+      *resultp = curl_easy_getinfo(curl, CURLINFO_TLS_SSL_PTR,
                                    &tls_backend_info);
       if(!*resultp)
         using_schannel =
           (tls_backend_info->backend == CURLSSLBACKEND_SCHANNEL);
     }
-    curl_easy_cleanup(curltls);
+    curl_easy_cleanup(curl);
     if(*resultp)
       return TRUE;
   }
