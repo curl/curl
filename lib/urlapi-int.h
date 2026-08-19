6119ec35cee8dcf4528ac7e5154276cd89052177
Daniel Stenberg <daniel@haxx.se>
2026-08-19 08:25:40 +0200

    CVE-2026-6429: netrc credential leak with reused proxy connection
    Based on b4024bf808bd558026fdc6
    
    Imported Curl_url_same_origin() from 32a513e180ce83d5e9b708 (and edited)

--- lib/urlapi-int.h
+++ lib/urlapi-int.h
@@ -32,6 +32,8 @@ CURLUcode Curl_url_set_authority(CURLU *u, const char *authority);
 
 CURLUcode Curl_junkscan(const char *url, size_t *urllen, bool allowspace);
 
+bool Curl_url_same_origin(CURLU *base, CURLU *href);
+
 #ifdef UNITTESTS
 UNITTEST CURLUcode Curl_parse_port(struct Curl_URL *u, struct dynbuf *host,
                                    bool has_scheme);
