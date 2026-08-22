7aa1bf2acc00380ab1d2e2fb07b269299d9e856b
Viktor Szakats <commit@vsz.me>
2026-08-23 01:40:44 +0200

    try supporting CFLAGS

--- .github/scripts/verify-examples.pl
+++ .github/scripts/verify-examples.pl
@@ -43,7 +43,7 @@ sub testcompile {
     }
     my $rc = system('$cc -c test.c -I include -W -Wall -pedantic -Werror ' .
         '-Wno-unused-parameter -Wno-unused-but-set-variable ' .
-        '-DCURL_ALLOW_OLD_MULTI_SOCKET -DCURL_DISABLE_DEPRECATION') >> 8;
+        '-DCURL_ALLOW_OLD_MULTI_SOCKET -DCURL_DISABLE_DEPRECATION ' . $ENV{'CFLAGS'}) >> 8;
     return $rc;
 }
 
