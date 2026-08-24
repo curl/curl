3591eac92009df09fffa769c03927961e5fe76cb
Viktor Szakats <commit@vsz.me>
2026-08-24 15:33:41 +0200

    cleanup-f

--- .github/scripts/verify-examples.pl
+++ .github/scripts/verify-examples.pl
@@ -42,7 +42,7 @@ sub testcompile {
     if(defined $ENV{'CFLAGS'}) {
         $cflags .= ' ' . $ENV{'CFLAGS'};
     }
-    my $rc = system($cc . ' -c test.c -I include -W -Wall -pedantic -Werror ' . $cflags .
+    my $rc = system($cc . ' -c test.c -I include -W -Wall -pedantic -Werror ' . $cflags . ' ' .
         '-Wno-unused-parameter -Wno-unused-but-set-variable ' .
         '-DCURL_ALLOW_OLD_MULTI_SOCKET -DCURL_DISABLE_DEPRECATION') >> 8;
     return $rc;
