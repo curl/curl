c3c70440e8d9938c0bb732595751e8aebc614f24
Viktor Szakats <commit@vsz.me>
2026-08-26 09:41:03 +0200

    setopt: make NULL `CURLOPT_AWS_SIGV4` disable aws-sigv4 auth
    Disable `CURLAUTH_AWS_SIGV4` when `CURLOPT_AWS_SIGV4` is set to NULL.
    Syncing behavior with `CURLAUTH_HTTPSIG`.
    
    Reported-by: Stanislav Fort
    
    Closes #22681

--- tests/libtest/lib1978.c
+++ tests/libtest/lib1978.c
@@ -90,6 +90,11 @@ static CURLcode test_lib1978(const char *URL)
   easy_setopt(curl, CURLOPT_CONNECT_TO, connect_to);
 
   result = curl_easy_perform(curl);
+  if(result)
+    goto test_cleanup;
+
+  easy_setopt(curl, CURLOPT_AWS_SIGV4, NULL);
+  result = curl_easy_perform(curl);
 
 test_cleanup:
 
