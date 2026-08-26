5fc42e1d8ca3fdae5a112b6d82bf131728c24344
Viktor Szakats <commit@vsz.me>
2026-08-26 11:00:22 +0200

    vquic.c indent

--- lib/vquic/vquic.c
+++ lib/vquic/vquic.c
@@ -565,8 +565,8 @@ static size_t vquic_msghdr_get_udp_gro(struct msghdr *msg)
 #endif /* (HAVE_SENDMMSG || HAVE_SENDMSG) && !HAVE_APPLE_MSG_X */
 
 #if (defined(HAVE_SENDMMSG) || defined(HAVE_SENDMSG) || \
-     defined(HAVE_APPLE_MSG_X)) && \
-     (defined(IP_RECVTOS) || defined(IP_TOS)) && defined(IPTOS_ECN_MASK)
+  defined(HAVE_APPLE_MSG_X)) && \
+  (defined(IP_RECVTOS) || defined(IP_TOS)) && defined(IPTOS_ECN_MASK)
 static uint8_t vquic_msghdr_get_ecn(struct msghdr *msg, int family)
 {
   struct cmsghdr *cmsg;
