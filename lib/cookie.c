b6d5507e20dafb7df76e119d5087ba649a18f350
Daniel Stenberg <daniel@haxx.se>
2026-08-28 17:38:13 +0200

    CVE-2026-82209: domain-scoped PSL domain cookie
    cookie: cookies set for an exact PSL domain is host-only
    
    Added test 2318 to verify.
    
    Reported-by: Stanislav Fort (Aisle Research)

--- lib/cookie.c
+++ lib/cookie.c
@@ -802,7 +802,26 @@ static bool is_public_suffix(struct Curl_easy *data,
         lcase[dlen] = 0;
         Curl_strntolower(lcookie, co->domain, clen);
         lcookie[clen] = 0;
-        acceptable = psl_is_cookie_domain_acceptable(psl, lcase, lcookie);
+        if(domain) {
+          Curl_strntolower(lcase, domain, dlen);
+          lcase[dlen] = 0;
+          acceptable = psl_is_cookie_domain_acceptable(psl, lcase, lcookie);
+
+          /* if the cookie is acceptable, but is set for a PSL domain, then it
+             cannot be tailmatching */
+          if(acceptable && co->tailmatch &&
+             curl_strequal(co->domain, domain) &&
+             psl_is_public_suffix(psl, lcookie))
+            co->tailmatch = FALSE;
+        }
+        else {
+          /* libpsl says localhost is a PSL, we think not */
+          acceptable =
+            curl_strequal(lcookie, "localhost") ||
+            /* note that this PSL function returns the opposite value than
+               psl_is_cookie_domain_acceptable() does */
+            !psl_is_public_suffix(psl, lcookie);
+        }
         Curl_psl_release(data);
       }
       else
@@ -977,6 +996,9 @@ CURLcode Curl_cookie_add(
     /* The __Secure- prefix only requires that the cookie be set secure */
     goto fail;
 
+  if(is_public_suffix(data, co, domain))
+    goto fail;
+
   if(co->prefix_host) {
     /*
      * The __Host- prefix requires the cookie to be secure, have a "/" path
@@ -1005,9 +1027,6 @@ CURLcode Curl_cookie_add(
   /* remove expired cookies */
   if(!noexpire)
     remove_expired(ci);
-
-  if(is_public_suffix(data, co, domain))
-    goto fail;
 
   if(!replace_existing(data, co, ci, secure, &replaces))
     goto fail;
