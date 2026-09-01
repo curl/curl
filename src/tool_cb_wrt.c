30107badd5aa8569cf6005ff6a8adb81c25a4a17
Daniel Stenberg <daniel@haxx.se>
2026-09-01 17:36:28 +0200

    tool_cb_wrt: try up to .9999 when no-clobbering
    Previously it gave up after 100.
    
    Some users use this feature when downloading large number of files in a
    glob range and then 100 is on the small side.
    
    Tweaked test 1683 to verify this even with the new limit.
    
    Closes #22784

--- src/tool_cb_wrt.c
+++ src/tool_cb_wrt.c
@@ -34,6 +34,8 @@
 #define OPENMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
 #endif
 
+#define MAX_CLOBBER_ATTEMPTS 9999
+
 /* create/open a local file for writing, return TRUE on success */
 bool tool_create_output_file(struct OutStruct *outs,
                              struct OperationConfig *config)
@@ -62,12 +64,24 @@ bool tool_create_output_file(struct OutStruct *outs,
       int next_num = 1;
       struct dynbuf fbuffer;
       char *newfile;
+      int max_num = MAX_CLOBBER_ATTEMPTS;
+#ifdef DEBUGBUILD
+      char *p = curl_getenv("CURL_MAX_CLOBBER");
+      if(p) {
+        const char *pp = p;
+        curl_off_t val;
+        if(!curlx_str_number(&pp, &val, 100000))
+          max_num = (int)val;
+        curl_free(p);
+      }
+#endif
       curlx_dyn_init(&fbuffer, 1025);
       /* !checksrc! disable ERRNOVAR 1 */
       while(fd == -1 && /* have not successfully opened a file */
-            (errno == EEXIST || errno == EISDIR) &&
             /* because we keep having files that already exist */
-            next_num < 100 /* and we have not reached the retry limit */) {
+            (errno == EEXIST || errno == EISDIR) &&
+            /* and we have not reached the retry limit */
+            (next_num < max_num)) {
         curlx_dyn_reset(&fbuffer);
         if(curlx_dyn_addf(&fbuffer, "%s.%d", fname, next_num))
           return FALSE;
