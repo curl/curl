5f19ec3dbe835793738afce2d05fd971ecc3183c
Daniel Stenberg <daniel@haxx.se>
2026-09-02 14:09:30 +0200

    CVE-2026-82208: wolfSSL CA-cache hit overrides callback

--- lib/vtls/wolfssl.c
+++ lib/vtls/wolfssl.c
@@ -617,9 +617,6 @@ static CURLcode wssl_populate_x509_store(struct Curl_cfilter *cf,
   bool imported_native_ca = FALSE;
   bool imported_ca_info_blob = FALSE;
 
-  /* We do not want to do this again, no matter the outcome */
-  wssl->x509_store_setup = TRUE;
-
 #ifndef NO_FILESYSTEM
   /* load native CA certificates */
   if(conn_config->native_ca_store) {
@@ -824,6 +821,9 @@ CURLcode Curl_wssl_setup_x509_store(struct Curl_cfilter *cf,
   CURLcode result = CURLE_OK;
   WOLFSSL_X509_STORE *cached_store;
   bool cache_criteria_met;
+
+  /* We do not want to do this again, no matter the outcome */
+  wssl->x509_store_setup = TRUE;
 
   /* Consider the X509 store cacheable if it comes exclusively from a CAfile,
      or no source is provided and we are falling back to wolfSSL's built-in
