b3e1bfcc46fff3f5a207b981ce32bb16770dcc76
Viktor Szakats <commit@vsz.me>
2026-09-08 02:14:55 +0200

    GHA: share apt/brew/pip install logic between workflows, related tidy-ups
    Via a local custom/composite Action. To de-duplicate logic, and allow
    maintaining a single copy. It may also allow for more elaborate
    workarounds for package install issues, if it becomes necessary.
    
    Also:
    - tried using `background: true` to parellize apt/brew/pip installs,
      but it's not supported in custom Actions.
    - shellcheck-ci: run on composite actions.
    - checksrc: run zizmor on composite actions.
      (actionlint does not support them)
    - checksrc: run zizmor on dependabot YAML.
    - tidy up install prereq step names.
    - shellcheck-ci.sh: make it runnable from any directory.
    - drop `ca-certificates` Homebrew workaround.
      Not necessary at the moment.
      Follow-up to c155cbc6380467487fcbca126366638149f04cf0 #22438
    - GHA/label: move egress firewall test from GHA/Linux/aws-lc.
      The firewall runner machine does not support the `$/` local action
      syntax.
      Follow-up to e5e1c356107ea4f8208757deb69776d94496cfae #22864
    
    Refs:
    https://github.blog/changelog/2026-07-30-reference-same-repository-actions-with-self-repository-syntax/
    https://docs.github.com/en/actions/tutorials/create-actions/create-a-composite-action
    https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-syntax#example-using-an-action-in-the-same-repository-as-the-workflow
    
    Ref: #22074 (first attempt)
    
    Closes #22867

--- .github/scripts/shellcheck-ci.sh
+++ .github/scripts/shellcheck-ci.sh
@@ -7,10 +7,12 @@
 
 set -eu
 
+cd -- "$(dirname "${0}")"/../..
+
 export SHELLCHECK_OPTS='--exclude=1090,1091,2086,2153 --enable=avoid-nullary-conditions,deprecate-which'
 
 # GHA
-git ls-files '.github/workflows/*.yml' | while read -r f; do
+git ls-files '.github/actions/**/*.yml' '.github/workflows/*.yml' | while read -r f; do
   echo "Verifying ${f}..."
   {
     echo '#!/usr/bin/env bash'
