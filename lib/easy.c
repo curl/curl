7d7fa4d7dd182b1bbb72ca20232fc15d00f6b335
Daniel Stenberg <daniel@haxx.se>
2026-09-10 08:53:25 +0200

    easy: duplicating a zero byte memory area needs no malloc
    Added test 1791 to reproduce and verify fix.
    
    Reported-by: Max Dymond
    Closes #22893

--- lib/easy.c
+++ lib/easy.c
@@ -957,10 +957,10 @@ static CURLcode dupset(struct Curl_easy *dst, struct Curl_easy *src)
     if(src->set.postfieldsize == -1)
       dst->set.str_copypostfields = curlx_strdup(src->set.str_copypostfields);
     else
-      /* postfieldsize is curl_off_t, curlx_memdup() takes a size_t ... */
+      /* postfieldsize is curl_off_t, curlx_memdup0() takes a size_t ... */
       dst->set.str_copypostfields =
-        curlx_memdup(src->set.str_copypostfields,
-                     curlx_sotouz(src->set.postfieldsize));
+        curlx_memdup0(src->set.str_copypostfields,
+                      curlx_sotouz(src->set.postfieldsize));
     if(!dst->set.str_copypostfields)
       return CURLE_OUT_OF_MEMORY;
     /* point to the new copy */
