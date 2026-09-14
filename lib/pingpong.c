a0d4bd6046fa73fff543b6ed4587f44b30c64f6a
Daniel Stenberg <daniel@haxx.se>
2026-09-14 09:25:03 +0200

    fixup return on maxloop before the read

--- lib/pingpong.c
+++ lib/pingpong.c
@@ -263,9 +263,12 @@ CURLcode Curl_pp_readresp(struct Curl_easy *data,
       pp->nfinal = 0; /* now gone */
     }
     if(!pp->overflow) {
+      if(!--maxloops) /* don't get stuck */
+        return CURLE_OK;
+
       result = pingpong_read(data, sockindex, buffer, sizeof(buffer),
                              &gotbytes);
-      if((result == CURLE_AGAIN) || !--maxloops)
+      if(result == CURLE_AGAIN)
         return CURLE_OK;
 
       if(result)
