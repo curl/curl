a9b935e4143db94df62b9351878e10c4d76f3893
Stefan Eissing <stefan@eissing.org>
2026-09-18 10:45:56 +0200

    more precise destription of set capacity

--- lib/u32_ptrset.h
+++ lib/u32_ptrset.h
@@ -27,7 +27,7 @@
 
 typedef void Curl_u32_ptrset_dtor(uint32_t id, void *ptr);
 
-/* A set that can hold up INT32_MAX pointers identified by an `id'.
+/* A set that can hold up to INT32_MAX+1 pointers identified by an `id'.
  * Setting a pointer for an existing id replaces the previous one.
  * Getting the pointer for an id not in the set returns NULL.
  * Setting an id to NULL unsets the id.
