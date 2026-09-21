ce359773039d9925935ae21f345c312742b1fea9
Filippo Tedeschi <filippotedeschi98@gmail.com>
2026-09-21 15:13:19 +0200

    getinfo: make sure CURLINFO_REDIRECT_URL does not contain creds
    Follow-up to 7a6bd02: make sure data->info.wouldredirect (exposed via
    CURLINFO_REDIRECT_URL and %{redirect_url}) does not contain credentials
    inherited from the original request URL or netrc when resolving relative
    redirect targets in FOLLOW_FAKE mode.
    
    Verified in test 1877
    
    Closes #23020

--- lib/http.c
+++ lib/http.c
@@ -1294,6 +1294,26 @@ CURLcode Curl_http_follow(struct Curl_easy *data, const char *newurl,
   if(type == FOLLOW_FAKE) {
     /* we are only figuring out the new URL if we would have followed locations
        but now we are done so we can get out! */
+    if(!uc) {
+      CURLU *u = curl_url();
+      char *nocred = NULL;
+
+      if(!u) {
+        curlx_free(follow_url);
+        return CURLE_OUT_OF_MEMORY;
+      }
+      if(!curl_url_set(u, CURLUPART_URL, follow_url,
+                       CURLU_NON_SUPPORT_SCHEME |
+                       (data->set.path_as_is ? CURLU_PATH_AS_IS : 0)) &&
+         !curl_url_set(u, CURLUPART_USER, NULL, 0) &&
+         !curl_url_set(u, CURLUPART_PASSWORD, NULL, 0))
+        (void)curl_url_get(u, CURLUPART_URL, &nocred, CURLU_GET_EMPTY);
+      curl_url_cleanup(u);
+      if(nocred) {
+        curlx_free(follow_url);
+        follow_url = nocred;
+      }
+    }
     data->info.wouldredirect = follow_url;
 
     if(reachedmax) {
