ed3311426619196be2c8928e26fb41092191a871
Filippo Tedeschi <filippotedeschi98@gmail.com>
2026-09-21 15:50:38 +0200

    http: preserve raw malformed Location and fail closed on OOM
    When Location URL parsing fails (e.g. test 1518 with invalid space in URL), keep the raw duplicated Location header without credential stripping. When URL parsing succeeded, strip credentials and return CURLE_OUT_OF_MEMORY if curl_url() fails.

--- lib/http.c
+++ lib/http.c
@@ -1287,12 +1287,16 @@ CURLcode Curl_http_follow(struct Curl_easy *data, const char *newurl,
   DEBUGASSERT(follow_url);
 
   if(type == FOLLOW_FAKE) {
-    CURLU *u = curl_url();
-    char *nocred = NULL;
-
     /* we are only figuring out the new URL if we would have followed locations
        but now we are done so we can get out! */
-    if(u) {
+    if(!uc) {
+      CURLU *u = curl_url();
+      char *nocred = NULL;
+
+      if(!u) {
+        curlx_free(follow_url);
+        return CURLE_OUT_OF_MEMORY;
+      }
       if(!curl_url_set(u, CURLUPART_URL, follow_url,
                        CURLU_NON_SUPPORT_SCHEME |
                        (data->set.path_as_is ? CURLU_PATH_AS_IS : 0)) &&
@@ -1300,9 +1304,12 @@ CURLcode Curl_http_follow(struct Curl_easy *data, const char *newurl,
          !curl_url_set(u, CURLUPART_PASSWORD, NULL, 0))
         (void)curl_url_get(u, CURLUPART_URL, &nocred, CURLU_GET_EMPTY);
       curl_url_cleanup(u);
+      if(nocred) {
+        curlx_free(follow_url);
+        follow_url = nocred;
+      }
     }
-    curlx_free(follow_url);
-    data->info.wouldredirect = nocred;
+    data->info.wouldredirect = follow_url;
 
     if(reachedmax) {
       failf(data, "Maximum (%d) redirects followed", data->set.maxredirs);
