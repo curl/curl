c7e59f7101eb1e50082efa66a308ebae6fb1b419
Stefan Eissing <stefan@eissing.org>
2026-09-25 14:23:04 +0200

    apple-sectrust: check ocsp copy alloc
    Check the result of the OCSP data allocation and return
    CURLE_OUT_OF_MEMORY when this failed.
    
    reported-by: Joshua Rogers

--- lib/vtls/apple.c
+++ lib/vtls/apple.c
@@ -204,7 +204,10 @@ CURLcode Curl_vtls_apple_verify(struct Curl_cfilter *cf,
 #if defined(HAVE_BUILTIN_AVAILABLE) && defined(SUPPORTS_SecOCSP)
     if(__builtin_available(macOS 10.9, iOS 7, tvOS 9, watchOS 2, *)) {
       CFDataRef ocspdata = CFDataCreate(NULL, ocsp_buf, (CFIndex)ocsp_len);
-
+      if(!ocspdata) {
+        result = CURLE_OUT_OF_MEMORY;
+        goto out;
+      }
       status = SecTrustSetOCSPResponse(trust, ocspdata);
       CFRelease(ocspdata);
       if(status != noErr) {
